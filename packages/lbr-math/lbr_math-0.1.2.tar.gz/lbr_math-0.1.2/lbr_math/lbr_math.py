f4ca924ba1109db7ef9de819563bac391aebcc9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:02:51 +0000

    Add package lbr-math/0.1.2/lbr_math-0.1.2.tar.gz

---
+++ packages/lbr-math/lbr_math-0.1.2.tar.gz/lbr_math-0.1.2/lbr_math/lbr_math.py
@@ -0,0 +1,8 @@
+#lbr加法操作
+def lbr_add(a,b):
+    return a + b
+
+
+#lbr减法操作
+def lbr_sub(a,b):
+    return a - b
---
+++ packages/lbr-math/lbr_math-0.1.2.tar.gz/lbr_math-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="lbr_math",
+    version="0.1.2",
+    packages=['lbr_math'],
+    install_requires=[
+
+    ],
+)
