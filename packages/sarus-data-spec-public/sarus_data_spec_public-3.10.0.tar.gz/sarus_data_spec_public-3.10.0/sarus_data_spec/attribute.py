d56f65bd55160b0b0323ed85946c2f5d0015244c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:02:41 +0000

    Add package sarus-data-spec-public/3.10.0/sarus_data_spec_public-3.10.0.tar.gz

---
+++ packages/sarus-data-spec-public/sarus_data_spec_public-3.10.0.tar.gz/sarus_data_spec_public-3.10.0/sarus_data_spec/attribute.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import Dict, Type
+
+from sarus_data_spec.base import Referring
+import sarus_data_spec.protobuf as sp
+import sarus_data_spec.typing as st
+
+
+class Attribute(Referring[sp.Attribute]):
+    """A python class to describe attributes"""
+
+    def __init__(self, protobuf: sp.Attribute, store: bool = True) -> None:
+        self._referred = {protobuf.object}
+        # This has to be defined before it is initialized
+        super().__init__(protobuf=protobuf, store=store)
+
+    def prototype(self) -> Type[sp.Attribute]:
+        """Return the type of the underlying protobuf."""
+        return sp.Attribute
+
+    def name(self) -> str:
+        return self.protobuf().name
+
+
+def attach_properties(
+    dataspec: st.DataSpec, name: str = "", properties: Dict[str, str] = {}
+) -> Attribute:
+    return Attribute(
+        sp.Attribute(object=dataspec.uuid(), properties=properties, name=name)
+    )
