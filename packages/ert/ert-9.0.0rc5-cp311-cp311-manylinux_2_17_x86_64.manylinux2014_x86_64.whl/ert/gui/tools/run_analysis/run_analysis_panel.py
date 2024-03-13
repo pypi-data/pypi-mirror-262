21badce38c887c20cfc940ffdc418bb6898abac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:03:08 +0000

    Add package ert/9.0.0rc5/ert-9.0.0rc5-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ert/ert-9.0.0rc5-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert/gui/tools/run_analysis/run_analysis_panel.py
@@ -0,0 +1,30 @@
+from qtpy.QtWidgets import QFormLayout, QLineEdit, QWidget
+
+from ert.config import AnalysisModule
+from ert.gui.ertwidgets.analysismoduleedit import AnalysisModuleEdit
+from ert.gui.ertwidgets.caseselector import CaseSelector
+
+
+class RunAnalysisPanel(QWidget):
+    def __init__(self, analysis_module: AnalysisModule, ensemble_size: int, notifier):
+        QWidget.__init__(self)
+
+        self.setWindowTitle("Run analysis")
+        self.activateWindow()
+        self.analysis_module = AnalysisModuleEdit(analysis_module, ensemble_size)
+        self.target_case_text = QLineEdit()
+        self.source_case_selector = CaseSelector(notifier, update_ert=False)
+
+        self.setMinimumSize(400, 100)
+
+        layout = QFormLayout()
+        layout.addRow("Analysis", self.analysis_module)
+        layout.addRow("Target case", self.target_case_text)
+        layout.addRow("Source case", self.source_case_selector)
+        self.setLayout(layout)
+
+    def target_case(self):
+        return str(self.target_case_text.text())
+
+    def source_case(self):
+        return self.source_case_selector.currentData()
---
+++ packages/ert/ert-9.0.0rc5-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert/scheduler/driver.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+import asyncio
+from abc import ABC, abstractmethod
+from typing import Dict, Optional
+
+from ert.scheduler.event import Event
+
+
+class Driver(ABC):
+    """Adapter for the HPC cluster."""
+
+    def __init__(self, **kwargs: Dict[str, str]) -> None:
+        self._event_queue: Optional[asyncio.Queue[Event]] = None
+
+    @property
+    def event_queue(self) -> asyncio.Queue[Event]:
+        if self._event_queue is None:
+            self._event_queue = asyncio.Queue()
+        return self._event_queue
+
+    @abstractmethod
+    async def submit(
+        self,
+        iens: int,
+        executable: str,
+        /,
+        *args: str,
+        name: str = "dummy",
+        runpath: Optional[str] = None,
+    ) -> None:
+        """Submit a program to execute on the cluster.
+
+        Args:
+          iens: Realization number. (Unique for each job)
+          executable: Program to execute.
+          args: List of arguments to send to the program.
+          cwd: Working directory.
+          name: Name of job as submitted to compute cluster
+        """
+
+    @abstractmethod
+    async def kill(self, iens: int) -> None:
+        """Terminate execution of a job associated with a realization.
+
+        Args:
+          iens: Realization number.
+        """
+
+    @abstractmethod
+    async def poll(self) -> None:
+        """Poll for new job events"""
+
+    @abstractmethod
+    async def finish(self) -> None:
+        """make sure that all the jobs / realizations are complete."""
