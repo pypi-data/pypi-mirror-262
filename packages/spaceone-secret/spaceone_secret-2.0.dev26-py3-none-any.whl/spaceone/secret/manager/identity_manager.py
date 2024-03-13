906c83d0a0b369f2ed502e1ac2ad3be5e2d2ff22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:02:51 +0000

    Add package spaceone-secret/2.0.dev26/spaceone_secret-2.0.dev26-py3-none-any.whl

---
+++ packages/spaceone-secret/spaceone_secret-2.0.dev26-py3-none-any.whl/spaceone/secret/manager/identity_manager.py
@@ -0,0 +1,43 @@
+from spaceone.core import config
+from spaceone.core.manager import BaseManager
+from spaceone.core.connector.space_connector import SpaceConnector
+
+
+class IdentityManager(BaseManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.identity_conn: SpaceConnector = self.locator.get_connector(
+            "SpaceConnector", service="identity"
+        )
+
+    def check_workspace(self, workspace_id, domain_id):
+        system_token = config.get_global("TOKEN")
+        return self.identity_conn.dispatch(
+            "Workspace.check",
+            {"workspace_id": workspace_id, "domain_id": domain_id},
+            token=system_token,
+        )
+
+    def get_trusted_account(self, trusted_account_id):
+        return self.identity_conn.dispatch(
+            "TrustedAccount.get",
+            {"trusted_account_id": trusted_account_id},
+        )
+
+    def list_trusted_accounts(self, query):
+        return self.identity_conn.dispatch("TrustedAccount.list", {"query": query})
+
+    def get_service_account(self, service_account_id):
+        return self.identity_conn.dispatch(
+            "ServiceAccount.get",
+            {"service_account_id": service_account_id},
+        )
+
+    def list_service_accounts(self, query):
+        return self.identity_conn.dispatch("ServiceAccount.list", {"query": query})
+
+    def get_project(self, project_id):
+        return self.identity_conn.dispatch("Project.get", {"project_id": project_id})
+
+    def list_projects(self, query):
+        return self.identity_conn.dispatch("Project.list", {"query": query})
