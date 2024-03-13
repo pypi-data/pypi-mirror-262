d626d954df28f89ec79f349f478c1ced4f20a4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:03:00 +0000

    Add package mtlbs/0.0.4/mtlbs-0.0.4.tar.gz

---
+++ packages/mtlbs/mtlbs-0.0.4.tar.gz/mtlbs-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'A modular login system'
+
+# Setting up
+setup(
+    name="mtlbs",
+    version=VERSION,
+    author="Loftea",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['ntplib', 'uuid', 'importlib'],
+    keywords=['python', 'login', 'GUI', 'simple', 'modular', 'user', 'fast', 'beginner'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
