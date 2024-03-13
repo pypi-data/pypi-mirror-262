fc3860a9176e985d5dc2af17ced5faa11a708dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:03:00 +0000

    Add package XpiumLibraryFlutter/0.0.3/XpiumLibraryFlutter-0.0.3-py3-none-any.whl

---
+++ packages/XpiumLibraryFlutter/XpiumLibraryFlutter-0.0.3-py3-none-any.whl/XpiumLibraryFlutter/submodule1/__init__.py
@@ -0,0 +1,4 @@
+#submodule1/__init__.py
+# -*- coding: utf-8 -*-
+from .module1 import PrintM
+from .module2 import Module12
---
+++ packages/XpiumLibraryFlutter/XpiumLibraryFlutter-0.0.3-py3-none-any.whl/XpiumLibraryFlutter/submodule1/module1.py
@@ -0,0 +1,8 @@
+#submodule1/module1.py
+# -*- coding: utf-8 -*-
+from robot.api.deco import keyword
+
+class PrintM:
+    @keyword("XpiumA")
+    def XpiumAA(self):
+        print("Hello, world! เทสภาษาไทย111")
