d1f67c4948519726546079d7ad15ede9eb8288a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:03:19 +0000

    Add package dndgmod/0.2.0/dndgmod-0.2.0-py3-none-any.whl

---
+++ packages/dndgmod/dndgmod-0.2.0-py3-none-any.whl/dndgmod/subcommands/compile.py
@@ -0,0 +1,28 @@
+from .._util.find import find_dndg
+
+import subprocess
+import shutil
+from typing import Annotated
+
+import typer
+
+def compile_(
+        ctx: typer.Context,
+        debug: Annotated[bool, typer.Option(help="Compile the debug version of Dungeons and Degenerate Gamblers."
+                                            )] = False,
+        backup: Annotated[bool, typer.Option(help="Back up your existing copy of Dungeons and Degenerate Gamblers "
+                                                  "before compiling.")] = True,
+):
+    """Compile Dungeons & Degenerate Gamblers with any installed mods."""
+    pck_path = find_dndg()
+    exe_path = pck_path.parent / "DnDG_64.exe"
+    data_directory = ctx.obj["data_directory"]
+
+    if backup:
+        shutil.move(pck_path, data_directory / "DnDG_Backup.pck")
+        shutil.move(exe_path, data_directory / "DnDG_Backup.exe")
+
+    godot_path = data_directory / "dependencies" / "Godot_v3.5.3-stable_win64.exe"
+    input_directory = data_directory / "decomped_src"
+    subprocess.run([godot_path, "--no-window", "--path", input_directory,
+                    "--export" + ("-debug"*debug), "dndgmod", exe_path])
---
+++ packages/dndgmod/dndgmod-0.2.0-py3-none-any.whl/dndgmod/subcommands/decompile.py
@@ -0,0 +1,16 @@
+from .._util.find import find_dndg
+
+import subprocess
+from typing import Annotated
+
+import typer
+
+def decompile(
+        ctx: typer.Context
+):
+    """Decompile Dungeons & Degenerate Gamblers."""
+    dndg_path = find_dndg()
+    data_directory = ctx.obj["data_directory"]
+    gdre_tools_path = data_directory / "dependencies" / "gdre_tools.exe"
+    output_directory = data_directory / "decomped_src"
+    subprocess.run([gdre_tools_path, "--headless", f"--recover={dndg_path}", f"--output-dir={output_directory}"])
