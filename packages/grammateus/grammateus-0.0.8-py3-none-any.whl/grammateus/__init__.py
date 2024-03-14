41f783262482ce651e40654ef025dc0f79211037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:03:26 +0000

    Add package grammateus/0.0.8/grammateus-0.0.8-py3-none-any.whl

---
+++ packages/grammateus/grammateus-0.0.8-py3-none-any.whl/grammateus/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from .entities import Grammateus
+from .anthropic_record import AnthropicRecord
+
+__all__ = [
+    'Grammateus',
+    'AnthropicRecord'
+]
