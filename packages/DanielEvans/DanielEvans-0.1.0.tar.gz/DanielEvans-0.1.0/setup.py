100969bbf8d08ea40e134d12c92292897df834ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:03:15 +0000

    Add package DanielEvans/0.1.0/DanielEvans-0.1.0.tar.gz

---
+++ packages/DanielEvans/DanielEvans-0.1.0.tar.gz/DanielEvans-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DanielEvans',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A Python package called DanielEvans',
+    author='Daniel Evans',
+    author_email='DanielEvans@outlook.com',
+    install_requires=[
+        # List your package dependencies here, if any
+    ],
+)
