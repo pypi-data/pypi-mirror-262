f575802edda1ca1c5d00b6180542dacf0eab9e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:03:44 +0000

    Add package prl-cli/0.0.26/prl-cli-0.0.26.tar.gz

---
+++ packages/prl-cli/prl-cli-0.0.26.tar.gz/prl-cli-0.0.26/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="prl-cli",
+    version="0.0.26",
+    author="Langston Nashold, Rayan Krishnan",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"": ["jsonschemas/*"]},
+    install_requires=[
+        "Click",
+        "gql",
+        "attrs",
+        "jsonschema",
+        "tqdm",
+        "boto3",
+        "requests",
+        "aiohttp",
+        "pandas",
+    ],
+    entry_points={
+        "console_scripts": [
+            "prl = prl.cli.main:cli",
+        ],
+    },
+    url="http://pypi.python.org/pypi/prl-cli/",
+)
