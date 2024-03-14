3b3752a537d1ed44035f8b3f6d32424fa4b11f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 03:03:11 +0000

    Add package fdk-rdf-parser/2.6.0/fdk_rdf_parser-2.6.0-py3-none-any.whl

---
+++ packages/fdk-rdf-parser/fdk_rdf_parser-2.6.0-py3-none-any.whl/fdk_rdf_parser/classes/harvest_meta_data.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from typing import (
+    List,
+    Optional,
+)
+
+
+@dataclass
+class HarvestMetaData:
+    firstHarvested: Optional[str] = None
+    changed: Optional[List[str]] = None
