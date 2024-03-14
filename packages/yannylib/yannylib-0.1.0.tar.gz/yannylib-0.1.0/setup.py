67a12c7366b76eaef4fb99a178b26f54d7578b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 17:03:35 +0000

    Add package yannylib/0.1.0/yannylib-0.1.0.tar.gz

---
+++ packages/yannylib/yannylib-0.1.0.tar.gz/yannylib-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='yannylib',
+version='0.1.0',
+author='Quinn Colello',
+author_email='qcolello@berkeley.edu',
+description='Some matplotlib functions wrapped',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
