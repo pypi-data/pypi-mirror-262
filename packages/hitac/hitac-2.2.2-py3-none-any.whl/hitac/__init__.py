323f187e2e84ebc23b74cf2dd98e0fcbc79ee2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:03:04 +0000

    Add package hitac/2.2.2/hitac-2.2.2-py3-none-any.whl

---
+++ packages/hitac/hitac-2.2.2-py3-none-any.whl/hitac/__init__.py
@@ -0,0 +1,13 @@
+"""Init module for the library."""
+
+import importlib
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+importlib.import_module("hitac._utils")
+
+if importlib.util.find_spec("q2_types") is not None:
+    importlib.import_module("hitac.qiime")
