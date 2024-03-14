e9b2bd47e3a70f3ab1a3e9a900fc2b61b4e12051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 03:03:11 +0000

    Add package rws-ddlpy/0.3.0/rws-ddlpy-0.3.0.tar.gz

---
+++ packages/rws-ddlpy/rws-ddlpy-0.3.0.tar.gz/rws-ddlpy-0.3.0/ddlpy/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Data Distributie Laag. Service from Rijkswaterstaat for distributing water quantity data.."""
+
+__author__ = """Fedor Baart"""
+__email__ = 'fedor.baart@deltares.nl'
+__version__ = '0.3.0'
+
+from ddlpy.ddlpy import locations
+from ddlpy.ddlpy import (measurements, 
+                         measurements_latest, 
+                         measurements_available, 
+                         measurements_amount,
+                         simplify_dataframe,
+                         )
+
+__all__ = ['locations', 
+           'measurements',
+           'measurements_latest', 
+           'measurements_available', 
+           'measurements_amount',
+           'simplify_dataframe',
+           ]
---
+++ packages/rws-ddlpy/rws-ddlpy-0.3.0.tar.gz/rws-ddlpy-0.3.0/docs/modules.rst
@@ -0,0 +1,19 @@
+Modules
+=========
+
+ddlpy.cli module
+----------------
+
+.. automodule:: ddlpy.cli
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+ddlpy module
+---------------
+
+.. automodule:: ddlpy
+   :members:
+   :undoc-members:
+   :show-inheritance:
