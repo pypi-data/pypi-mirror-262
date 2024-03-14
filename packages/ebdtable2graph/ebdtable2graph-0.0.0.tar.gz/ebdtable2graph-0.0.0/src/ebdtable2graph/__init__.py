67186e3b115f6aa36e1dfda2c852895bd5947710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:03:36 +0000

    Add package ebdtable2graph/0.0.0/ebdtable2graph-0.0.0.tar.gz

---
+++ packages/ebdtable2graph/ebdtable2graph-0.0.0.tar.gz/ebdtable2graph-0.0.0/src/ebdtable2graph/__init__.py
@@ -0,0 +1,7 @@
+"""
+contains the conversion logic
+"""
+
+from ebdtable2graph.graph_conversion import convert_table_to_digraph, convert_table_to_graph
+from ebdtable2graph.graphviz import convert_dot_to_svg_kroki, convert_graph_to_dot
+from ebdtable2graph.plantuml import convert_graph_to_plantuml, convert_plantuml_to_svg_kroki
