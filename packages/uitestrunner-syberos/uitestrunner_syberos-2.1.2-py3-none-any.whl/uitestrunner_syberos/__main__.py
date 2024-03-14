047e602c3a74fb679cb3c7357a2bf2f79945869f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:04:36 +0000

    Add package uitestrunner-syberos/2.1.2/uitestrunner_syberos-2.1.2-py3-none-any.whl

---
+++ packages/uitestrunner-syberos/uitestrunner_syberos-2.1.2-py3-none-any.whl/uitestrunner_syberos/__main__.py
@@ -0,0 +1,37 @@
+# Copyright (C) <2021>  YUANXIN INFORMATION TECHNOLOGY GROUP CO.LTD and Jinzhe Wang
+# This file is part of uitestrunner_syberos
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import os
+import shutil
+from pathlib import Path
+import ocrCraftModel4uts
+import ocrLangModel4uts
+
+
+ocr_mods_path = os.path.dirname(os.path.abspath(__file__)) + "/ocr_models/"
+if not Path(ocr_mods_path).exists():
+    os.mkdir(ocr_mods_path)
+else:
+    if not Path(ocr_mods_path).is_dir():
+        os.remove(os.path.dirname(os.path.abspath(__file__)) + "/ocr_models")
+        os.mkdir(ocr_mods_path)
+for mod in os.listdir(ocrCraftModel4uts.get_path()):
+    if not Path(ocrCraftModel4uts.get_path() + mod).is_dir():
+        if not Path(ocr_mods_path + mod).exists():
+            shutil.copy(ocrCraftModel4uts.get_path() + mod, ocr_mods_path)
+for mod in os.listdir(ocrLangModel4uts.get_path()):
+    if not Path(ocrLangModel4uts.get_path() + mod).is_dir():
+        if not Path(ocr_mods_path + mod).exists():
+            shutil.copy(ocrLangModel4uts.get_path() + mod, ocr_mods_path)
