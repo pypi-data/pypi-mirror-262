b4e0d44b516f6c89c3e40f54804ffb912daef917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:03:29 +0000

    Add package BrazilFiscalReport/0.2.1/BrazilFiscalReport-0.2.1.tar.gz

---
+++ packages/BrazilFiscalReport/BrazilFiscalReport-0.2.1.tar.gz/BrazilFiscalReport-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="BrazilFiscalReport",
+    version="0.2.1",
+    long_description="""
+    Python library for generating Brazilian auxiliary
+    fiscal documents in PDF from XML documents.
+    """,
+    url="https://github.com/Engenere/BrazilFiscalReport",
+    author="Engenere",
+    keywords="brazil fiscal report",
+    packages=["brazilfiscalreport"],
+    license="AGPL-3.0",
+    install_requires=["fpdf2"],
+)
