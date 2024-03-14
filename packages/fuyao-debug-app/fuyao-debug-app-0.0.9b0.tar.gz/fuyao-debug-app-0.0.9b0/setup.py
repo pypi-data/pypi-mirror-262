94d5c9a338d96ff50db1d060bcdbc296f6ae950a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 15:03:31 +0000

    Add package fuyao-debug-app/0.0.9b0/fuyao-debug-app-0.0.9b0.tar.gz

---
+++ packages/fuyao-debug-app/fuyao-debug-app-0.0.9b0.tar.gz/fuyao-debug-app-0.0.9b0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name='fuyao-debug-app',
+    version='0.0.9b',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    py_modules=['app', 'db', 'utils'],
+    packages=find_packages(),
+    include_package_data=True,
+    data_files=[('config', ['config/config.properties']),
+                ('db', ['config/config.properties'])],
+    install_requires=[
+        requirements
+    ],
+    entry_points='''
+        [console_scripts]
+        fuyao-debug-app=app:cli
+    ''',
+)
