c32caf40b7b067b1797c6824d639b3de64685753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:03:52 +0000

    Add package uv/0.1.20/uv-0.1.20.tar.gz

---
+++ packages/uv/uv-0.1.20.tar.gz/uv-0.1.20/crates/distribution-types/src/error.rs
@@ -0,0 +1,22 @@
+use url::Url;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+
+    #[error(transparent)]
+    Utf8(#[from] std::string::FromUtf8Error),
+
+    #[error(transparent)]
+    WheelFilename(#[from] distribution_filename::WheelFilenameError),
+
+    #[error("Unable to extract filename from URL: {0}")]
+    UrlFilename(Url),
+
+    #[error("Distribution not found at: {0}")]
+    NotFound(Url),
+
+    #[error("Unsupported scheme `{0}` on URL: {1} ({2})")]
+    UnsupportedScheme(String, String, String),
+}
