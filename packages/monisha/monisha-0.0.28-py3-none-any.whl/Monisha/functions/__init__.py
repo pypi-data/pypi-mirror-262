19ee3008895f03725903e75d2537a1dd9ce2f335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 06:03:57 +0000

    Add package monisha/0.0.28/monisha-0.0.28-py3-none-any.whl

---
+++ packages/monisha/monisha-0.0.28-py3-none-any.whl/Monisha/functions/__init__.py
@@ -0,0 +1,9 @@
+from .function06 import Okeys, Ukeys, Bkeys, Mkeys, Ykeys, Uname
+from .function01 import timend, uptime, Timemod
+from .function04 import Rawexo, Extions
+from .function02 import Dbytes, Hbytes
+from .function07 import Cmd, Wosd
+from .function03 import HKeys
+from .function10 import Fonts
+from .function05 import Doxo
+from .function08 import Num
---
+++ packages/monisha/monisha-0.0.28-py3-none-any.whl/Monisha/functions/function03.py
@@ -0,0 +1,7 @@
+
+class HKeys(object):
+
+    DATA01 = "0123456789"
+    DATA02 = "abcdefghijklmnopqrstuvwxyz"
+    DATA03 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    DATA04 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
