2ff49e4cba683863576d0951a195f2ac8833222c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:04:00 +0000

    Add package hosted-flasks/0.0.1/hosted-flasks-0.0.1.tar.gz

---
+++ packages/hosted-flasks/hosted-flasks-0.0.1.tar.gz/hosted-flasks-0.0.1/hosted_flasks/log_setup.py
@@ -0,0 +1,15 @@
+import logging
+
+import os
+
+# setup logging to stdout
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL") or "INFO"
+FORMAT    = "[%(name)s] [%(levelname)s] %(message)s"
+DATEFMT   = "%Y-%m-%d %H:%M:%S %z"
+
+logging.basicConfig(level=LOG_LEVEL, format=FORMAT, datefmt=DATEFMT)
+formatter = logging.Formatter(FORMAT, DATEFMT)
+logging.getLogger().handlers[0].setFormatter(formatter)
