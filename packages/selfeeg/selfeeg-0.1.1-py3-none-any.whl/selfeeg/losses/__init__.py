680ea89bea01e0edd4984b47e4feecac0f1d5afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:04:08 +0000

    Add package selfeeg/0.1.1/selfeeg-0.1.1-py3-none-any.whl

---
+++ packages/selfeeg/selfeeg-0.1.1-py3-none-any.whl/selfeeg/losses/__init__.py
@@ -0,0 +1,1 @@
+from .losses import Barlow_loss, BYOL_loss, Moco_loss, SimCLR_loss, SimSiam_loss, VICReg_loss
---
+++ packages/selfeeg/selfeeg-0.1.1-py3-none-any.whl/selfeeg/ssl/__init__.py
@@ -0,0 +1,12 @@
+from .ssl import (
+    BYOL,
+    Barlow_Twins,
+    EarlyStopping,
+    MoCo,
+    SimCLR,
+    SimSiam,
+    SSL_Base,
+    VICReg,
+    evaluateLoss,
+    fine_tune,
+)
