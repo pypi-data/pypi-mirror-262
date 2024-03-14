85500c9cc75cc7e59599238c69d4dcf58b42e3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:03:16 +0000

    Add package paramount/0.2.3/paramount-0.2.3-py3-none-any.whl

---
+++ packages/paramount/paramount-0.2.3-py3-none-any.whl/paramount/cli.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import streamlit.web.cli as stcli
+
+# Set Streamlit configuration options via environment variables
+os.environ["STREAMLIT_BROWSER_GATHER_USAGE_STATS"] = "false"
+os.environ["STREAMLIT_THEME_BASE"] = "light"
+os.environ["STREAMLIT_THEME_PRIMARY_COLOR"] = "#4489bb"
+os.environ["STREAMLIT_SERVER_HEADLESS"] = "true"
+
+
+def main():
+    directory = os.path.dirname(__file__)
+    app_path = os.path.join(directory, 'Evaluate.py')
+    sys.argv = ["streamlit", "run", app_path] + sys.argv[1:]
+    stcli.main()
+
+
+if __name__ == "__main__":
+    main()
