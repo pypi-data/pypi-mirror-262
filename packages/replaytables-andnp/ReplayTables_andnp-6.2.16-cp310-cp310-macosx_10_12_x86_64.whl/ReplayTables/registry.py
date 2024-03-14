8e1656e7e1d59af6aabd42cbbf951a31c303a0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 15:03:11 +0000

    Add package replaytables-andnp/6.2.16/ReplayTables_andnp-6.2.16-cp310-cp310-macosx_10_12_x86_64.whl

---
+++ packages/replaytables-andnp/ReplayTables_andnp-6.2.16-cp310-cp310-macosx_10_12_x86_64.whl/ReplayTables/registry.py
@@ -0,0 +1,30 @@
+import numpy as np
+from typing import Any, Dict
+from ReplayTables.ReplayBuffer import ReplayBuffer
+
+from ReplayTables.BackwardsReplay import BackwardsReplay, BackwardsReplayConfig
+from ReplayTables.PER import PrioritizedReplay, PERConfig
+from ReplayTables.PSER import PrioritizedSequenceReplay, PSERConfig
+
+def build_buffer(buffer_type: str, max_size: int, lag: int, rng: np.random.Generator, config: Dict[str, Any]) -> ReplayBuffer:
+    buffer_type = buffer_type.lower()
+
+    # TODO: remove once mypy fixes typing inference
+    c: Any = None
+
+    if buffer_type == 'uniform' or buffer_type == 'standard':
+        return ReplayBuffer(max_size, lag, rng)
+
+    elif buffer_type == 'backwards':
+        c = BackwardsReplayConfig(**config)
+        return BackwardsReplay(max_size, lag, rng, c)
+
+    elif buffer_type == 'per':
+        c = PERConfig(**config)
+        return PrioritizedReplay(max_size, lag, rng, c)
+
+    elif buffer_type == 'pser':
+        c = PSERConfig(**config)
+        return PrioritizedSequenceReplay(max_size, lag, rng, c)
+
+    raise Exception('Unable to determine type of buffer')
