c0eb54db6e61503118a42b8eb141214c530e75db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:03:19 +0000

    Add package mayatk/0.9.17/mayatk-0.9.17.tar.gz

---
+++ packages/mayatk/mayatk-0.9.17.tar.gz/mayatk-0.9.17/mayatk/core_utils/project.py
@@ -0,0 +1,304 @@
+# !/usr/bin/python
+# coding=utf-8
+import os
+
+try:
+    import pymel.core as pm
+except ImportError as error:
+    print(__file__, error)
+import pythontk as ptk
+
+
+class Project(ptk.HelpMixin):
+    """ """
+
+    @staticmethod
+    def get_recent_files(index=None, format="standard"):
+        """
+        Get a list of recent files.
+
+        Parameters:
+            index (slice or int): Return the recent file directory path at the given index or slice.
+                    Index 0 would be the most recent file.
+                    For example, use index=slice(0, 5) to get the 5 most recent files.
+                    If there are only 3 files, it will return those 3 files without throwing an error.
+            format (str): Defines the format of the returned paths. Possible options are 'standard', 'timestamp',
+                    'standard|timestamp', 'timestamp|standard'. 'standard' returns paths as strings, 'timestamp'
+                    returns timestamped paths, 'standard|timestamp' returns a dictionary with standard paths as
+                    keys and timestamped paths as values, 'timestamp|standard' does the opposite.
+
+        Returns:
+            (list or dict): A list or dictionary of recent files depending on the 'format' parameter.
+
+        Examples:
+            get_recent_files() --> Returns all recent files in standard format
+            get_recent_files(0) --> Returns the most recent file in standard format
+            get_recent_files(slice(0, 5)) --> Returns the 5 most recent files in standard format.
+            get_recent_files(format='timestamp') --> Returns all recent files in timestamp format.
+            get_recent_files(format='standard|timestamp') --> Returns a dictionary with standard paths as keys and timestamped paths as values.
+        """
+        files = pm.optionVar(query="RecentFilesList")
+        if not files:
+            return []
+
+        result = [
+            ptk.format_path(f)
+            for f in reversed(files)
+            if ptk.is_valid(f) and "Autosave" not in f
+        ]
+
+        if index is not None:
+            try:
+                result = result[index]
+            except (IndexError, TypeError):
+                print(f"Incorrect index or slice. Returning empty list.")
+                return []
+
+        format = format.split("|")
+        if len(format) == 2 and "timestamp" in format and "standard" in format:
+            if format[0] == "timestamp":
+                result = {ptk.time_stamp(res): res for res in result}
+            else:
+                result = {res: ptk.time_stamp(res) for res in result}
+        elif "timestamp" in format:
+            result = [ptk.time_stamp(res) for res in result]
+        # else return the standard format
+
+        return result
+
+    @staticmethod
+    def get_recent_projects(index=None, format="standard"):
+        """
+        Get a list of recently set projects.
+
+        Parameters:
+            index (slice or int): Return the recent project directory path at the given index or slice.
+                    Index 0 would be the most recent project.
+                    For example, use index=slice(0, 5) to get the 5 most recent projects.
+                    If there are only 3 projects, it will return those 3 projects without throwing an error.
+            format (str): Defines the format of the returned paths. Possible options are 'standard', 'timestamp',
+                    'standard|timestamp', 'timestamp|standard'. 'standard' returns paths as strings, 'timestamp'
+                    returns timestamped paths, 'standard|timestamp' returns a dictionary with standard paths as
+                    keys and timestamped paths as values, 'timestamp|standard' does the opposite.
+
+        Returns:
+            (list or dict): A list or dictionary of recent projects depending on the 'format' parameter.
+
+        Examples:
+            get_recent_projects() --> Returns all recent projects in standard format
+            get_recent_projects(0) --> Returns the most recent project in standard format
+            get_recent_projects(slice(0, 5)) --> Returns the 5 most recent projects in standard format.
+            get_recent_projects(format='timestamp') --> Returns all recent projects in timestamp format.
+            get_recent_projects(format='standard|timestamp') --> Returns a dictionary with standard paths as keys and timestamped paths as values.
+        """
+        files = pm.optionVar(query="RecentProjectsList")
+        if not files:
+            return []
+
+        result = [ptk.format_path(f) for f in reversed(files) if ptk.is_valid(f)]
+
+        if index is not None:
+            try:
+                result = result[index]
+            except (IndexError, TypeError):
+                print("Incorrect index or slice. Returning empty list.")
+                return []
+
+        format = format.split("|")
+        if len(format) == 2 and "timestamp" in format and "standard" in format:
+            if format[0] == "timestamp":
+                result = {ptk.time_stamp(res): res for res in result}
+            else:
+                result = {res: ptk.time_stamp(res) for res in result}
+        elif "timestamp" in format:
+            result = [ptk.time_stamp(res) for res in result]
+        # else return the standard format
+
+        return result
+
+    @staticmethod
+    def find_autosave_directories():
+        """
+        Search for and compile a list of existing autosave directories based on
+        predefined locations: the current workspace's autosave directory, the autosave
+        directory specified in the MAYA_AUTOSAVE_FOLDER environment variable, and the
+        user's home directory autosave folder.
+
+        Returns:
+            list: A list of strings, each being a path to an existing autosave directory.
+        """
+        import itertools
+
+        # Directories to check for autosave files
+        potential_dirs = [
+            os.path.join(
+                pm.workspace(q=True, rd=True), "autosave"
+            ),  # Workspace autosave
+            os.environ.get("MAYA_AUTOSAVE_FOLDER"),  # Environment variable autosave
+            os.path.expanduser("~/maya/autosave"),  # Home directory autosave
+        ]
+
+        # Split environment autosave paths and filter out non-existing paths
+        autosave_dirs = filter(
+            os.path.exists,
+            itertools.chain.from_iterable(
+                (d.split(";") if d else [] for d in potential_dirs)
+            ),
+        )
+        return list(autosave_dirs)
+
+    @classmethod
+    def get_recent_autosave(cls, index=None, format="standard"):
+        """
+        Retrieves a list or dictionary of recent Maya autosave files, optionally filtered
+        by an index or a slice, and formatted according to the specified output format.
+
+        Parameters:
+            index (slice|int|None): If provided, specifies the subset of autosave files to
+                return. Can be an integer for a specific file, or a slice object for a range.
+                Defaults to None, which returns all autosave files.
+            format (str): Determines the format of the returned paths. Options are 'standard',
+                'timestamp', 'standard|timestamp', and 'timestamp|standard'. 'standard' returns
+                paths as strings, 'timestamp' returns paths with timestamps, and the combined
+                formats return dictionaries with the paths formatted as specified.
+
+        Returns:
+            list|dict: Depending on the 'format' parameter, a list of file paths, a list of
+                timestamped file paths, or a dictionary with file paths as keys and their
+                timestamped counterparts as values, or vice versa.
+
+        Raises:
+            IndexError: If an integer index is out of range.
+            TypeError: If the index is neither an integer nor a slice.
+        """
+        import itertools
+        import glob
+
+        autosave_dirs = cls.find_autosave_directories()
+        result = []
+
+        for autosave_dir in autosave_dirs:
+            files = itertools.chain(
+                glob.iglob(os.path.join(autosave_dir, "*.mb")),
+                glob.iglob(os.path.join(autosave_dir, "*.ma")),
+            )
+
+            for file in files:
+                result.append(ptk.format_path(file))
+
+        if index is not None:
+            try:
+                result = result[index]
+            except (IndexError, TypeError):
+                print("Incorrect index or slice. Returning empty list.")
+                return []
+
+        format_parts = format.split("|")
+        if (
+            len(format_parts) == 2
+            and "timestamp" in format_parts
+            and "standard" in format_parts
+        ):
+            result = (
+                {res: ptk.time_stamp(res) for res in result}
+                if format_parts[0] == "standard"
+                else {ptk.time_stamp(res): res for res in result}
+            )
+        elif "timestamp" in format_parts:
+            result = [ptk.time_stamp(res) for res in result]
+
+        return result
+
+    @staticmethod
+    def get_workspace_scenes(fullPath=True):
+        """Get a list of maya scene files from the current workspace directory.
+
+        Parameters:
+            fullPath (bool): Return the full path instead of just the filename.
+
+        Returns:
+            (list)
+        """
+        workspace_dir = str(pm.workspace(q=True, rd=1))  # get current project path.
+
+        files = ptk.get_dir_contents(
+            workspace_dir, "filepath", inc_files=("*.mb", "*.ma")
+        )
+        # Replace any backslashes with forward slashes.
+        result = [ptk.format_path(f) for f in files]
+
+        if not fullPath:
+            result = [f.split("\\")[-1] for f in result]
+
+        return result
+
+    @staticmethod
+    def reference_scene(file_path):
+        """Reference a Maya scene.
+
+        Parameters:
+            file_path (str): The path to the Maya scene file to reference.
+        """
+        if os.path.exists(file_path):
+            pm.system.createReference(file_path)
+        else:
+            raise FileNotFoundError(f"No such file: '{file_path}'")
+
+    @staticmethod
+    def remove_reference(file_path):
+        """Remove a reference to a Maya scene.
+
+        Parameters:
+            file_path (str): The path to the Maya scene file to remove the reference to.
+        """
+        ref_node = pm.system.FileReference(file_path)
+        if ref_node.isReferenced():
+            ref_node.remove()
+
+    @staticmethod
+    def is_referenced(file_path):
+        """Check if a Maya scene is referenced.
+
+        Parameters:
+            file_path (str): The path to the Maya scene file to check.
+
+        Returns:
+            (bool): True if the scene is referenced, False otherwise.
+        """
+        ref_node = pm.system.FileReference(file_path)
+        return ref_node.isReferenced()
+
+    @staticmethod
+    def get_reference_nodes(file_path):
+        """Get the nodes from a referenced Maya scene.
+
+        Parameters:
+            file_path (str): The path to the Maya scene file to get the nodes from.
+
+        Returns:
+            (list): A list of nodes in the referenced scene.
+        """
+        ref_node = pm.system.FileReference(file_path)
+        if ref_node.isReferenced():
+            return ref_node.nodes()
+        else:
+            return []
+
+    @staticmethod
+    def list_references():
+        """List all references in the current Maya scene.
+
+        Returns:
+            (list): A list of all references in the current Maya scene.
+        """
+        return [ref.filePath() for ref in pm.system.listReferences()]
+
+
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    pass
+
+# -----------------------------------------------------------------------------
+# Notes
+# -----------------------------------------------------------------------------
---
+++ packages/mayatk/mayatk-0.9.17.tar.gz/mayatk-0.9.17/mayatk/mat_utils/_mat_utils.py
@@ -0,0 +1,341 @@
+# !/usr/bin/python
+# coding=utf-8
+try:
+    import pymel.core as pm
+except ImportError as error:
+    print(__file__, error)
+import pythontk as ptk
+
+# from this package:
+from mayatk import node_utils
+
+
+class MatUtils(ptk.HelpMixin):
+    """ """
+
+    @staticmethod
+    def get_mats(objs):
+        """Returns the set of materials assigned to a given list of objects or components.
+
+        Parameters:
+            objs (list): The objects or components to retrieve the material from.
+
+        Returns:
+            set: The set of materials assigned to the objects or components.
+        """
+        # Initialize an empty set to store the materials
+        mats = set()
+
+        for obj in pm.ls(objs, flatten=True):
+            shading_grps = []
+
+            if isinstance(obj, pm.MeshFace):
+                all_shading_grps = pm.ls(type="shadingEngine")
+                shading_grps = [
+                    sg for sg in all_shading_grps if pm.sets(sg, isMember=obj)
+                ]
+
+                if not shading_grps:
+                    pm.hyperShade(obj, shaderNetworksSelectMaterialNodes=True)
+                    mats_ = pm.ls(pm.selected(), materials=True)
+                    mats.update(mats_)
+            else:
+                # Check if the object has a shape node
+                shape = None
+                if hasattr(obj, "getShape"):
+                    shape = obj.getShape()
+
+                if shape:
+                    shading_grps = pm.listConnections(shape, type="shadingEngine")
+
+            for shading_grp in shading_grps:
+                materials = pm.ls(
+                    pm.listConnections(f"{shading_grp}.surfaceShader"), materials=True
+                )
+                mats.update(materials)
+
+        return mats
+
+    @staticmethod
+    def get_scene_mats(inc=[], exc=[]):
+        """Retrieves all materials from the current scene, optionally including or excluding certain objects.
+
+        Parameters:
+            inc (str/int/obj/list, optional): The objects to include in the search. Supports using the '*' operator for pattern matching. Defaults to [].
+            exc (str/int/obj/list, optional): The objects to exclude from the search. Supports using the '*' operator for pattern matching. Defaults to [].
+
+        Returns:
+            list: A list of materials in the scene.
+        """
+        matList = pm.ls(mat=1, flatten=1)
+
+        # convert to dictionary to filter material names and types.
+        d = {m.name(): pm.nodeType(m) for m in matList}
+        filtered = ptk.filter_dict(d, inc, exc, keys=True, values=True)
+
+        # use the filtered results to reconstruct a filtered list of actual materials.
+        return [m for m in matList if m.name() in filtered]
+
+    @staticmethod
+    def get_fav_mats():
+        """Retrieves the list of favorite materials in Maya.
+
+        Returns:
+            list: A list of favorite materials.
+        """
+        import os.path
+        import maya.app.general.tlfavorites as _fav
+
+        version = pm.about(version=True).split(" ")[-1]  # Get the Maya version year
+        path = os.path.expandvars(
+            f"%USERPROFILE%/Documents/maya/{version}/prefs/renderNodeTypeFavorites"
+        )
+        renderNodeTypeFavorites = _fav.readFavorites(path)
+        materials = [i for i in renderNodeTypeFavorites if "/" not in i]
+        del _fav
+
+        return materials
+
+    @staticmethod
+    def create_mat(mat_type, prefix="", name=""):
+        """Creates a material based on the provided type or a random Lambert material if 'mat_type' is 'random'.
+
+        Parameters:
+            mat_type (str): The type of the material, e.g. 'lambert', 'blinn', or 'random' for a random Lambert material.
+            prefix (str, optional): An optional prefix to append to the material name. Defaults to "".
+            name (str, optional): The name of the material. Defaults to "".
+
+        Returns:
+            obj: The created material.
+        """
+        import random
+
+        if mat_type == "random":
+            rgb = [
+                random.randint(0, 255) for _ in range(3)
+            ]  # Generate a list containing 3 values between 0-255
+            name = "{}{}_{}_{}_{}".format(
+                prefix, name, str(rgb[0]), str(rgb[1]), str(rgb[2])
+            )
+            mat = pm.shadingNode("lambert", asShader=True, name=name)
+            convertedRGB = [round(float(v) / 255, 3) for v in rgb]
+            pm.setAttr(name + ".color", convertedRGB)
+        else:
+            name = prefix + name if name else mat_type
+            mat = pm.shadingNode(mat_type, asShader=True, name=name)
+
+        return mat
+
+    @staticmethod
+    def assign_mat(objects, mat):
+        """Assigns a material to a list of objects or components.
+
+        Parameters:
+            objects (str/obj/list): The objects or components to assign the material to.
+            mat (obj): The material to assign.
+        """
+        try:
+            pm.nodeType(mat)
+        except Exception:
+            mat = pm.shadingNode(mat, asShader=True)
+
+        # Check for existing shading group connected to the material
+        shading_groups = mat.listConnections(type="shadingEngine")
+        if shading_groups:
+            shading_group = shading_groups[0]
+        else:
+            shading_group = pm.sets(renderable=True, noSurfaceShader=True, empty=True)
+            pm.connectAttr(
+                f"{mat}.outColor", f"{shading_group}.surfaceShader", force=True
+            )
+
+        for obj in pm.ls(objects):
+            pm.sets(shading_group, forceElement=obj)
+
+    @classmethod
+    def find_by_mat_id(cls, material, objects=None, shell=False):
+        """Find objects or faces by the material ID in a 3D scene.
+
+        This function takes as input a material and a set of objects (or entire scene by default) and
+        returns a list of objects or faces that are associated with the input material.
+
+        If the 'shell' parameter is set to True, this function returns the complete objects that have
+        the input material. If 'shell' is set to False, the function returns the individual faces that
+        have the input material.
+
+        Note: If the material is a multi-material (such as VRayMultiSubTex), an error will be raised.
+
+        Args:
+            cls: The class object. This argument is implicitly passed and doesn't need to be provided by the user.
+            material (str): The material (e.g. 'lambert1') used to find the associated objects/faces.
+            objects (list, optional): List of objects (e.g. ['pCube1', 'pCube2']) to be considered.
+                                      If not provided, all objects in the scene will be considered.
+            shell (bool, optional): Determines whether to return complete objects (True) or individual faces (False).
+                                    Default is False.
+
+        Raises:
+            TypeError: If material is a multimaterial.
+
+        Returns:
+            list: A list of objects or faces (depending on 'shell' parameter) that are associated with the input material.
+
+        """
+        if pm.nodeType(material) == "VRayMultiSubTex":
+            raise TypeError(
+                "Invalid material type. If material is a multimaterial, please select a submaterial."
+            )
+        # If objects are not specified, consider all objects in the scene
+        if not objects:
+            objects = pm.ls(geometry=True)  # Get all mesh objects
+
+        # Convert mesh shapes to transform nodes
+        objects = node_utils.NodeUtils.get_transform_node(objects)
+
+        # Find the shading groups associated with the material
+        shading_groups = pm.listConnections(material, type="shadingEngine")
+
+        objs_with_material = []
+        for sg in shading_groups:
+            connected_objs = pm.sets(sg, query=True, noIntermediate=True)
+            flattened = pm.ls(connected_objs, flatten=True)
+            # Only add objects to the list if they are in the specified objects list
+            for obj in flattened:
+                transform_node = node_utils.NodeUtils.get_transform_node(obj)
+                if transform_node in objects:
+                    # Check if the object is a face. If not, convert to faces
+                    if not isinstance(obj, pm.MeshFace):
+                        shape_node = pm.listRelatives(transform_node, shapes=True)[0]
+                        face_count = pm.polyEvaluate(shape_node, f=True)
+                        faces = [f"{shape_node}.f[{i}]" for i in range(face_count)]
+                        objs_with_material.extend(faces)
+                    else:
+                        objs_with_material.append(obj)
+
+        if shell:
+            objs_with_material = set(pm.ls(objs_with_material, objectsOnly=True))
+
+        return objs_with_material
+
+    @staticmethod
+    def reload_textures(materials=None, inc=None, exc=None):
+        """Reloads textures connected to specified materials with inclusion/exclusion filters.
+
+        Parameters:
+            materials (str/obj/list): Material or list of materials to process. Defaults to all materials in the scene.
+            inc (str/list): Inclusion patterns for filtering textures.
+            exc (str/list): Exclusion patterns for filtering textures.
+        """
+        materials = pm.ls(materials) if materials else pm.ls(mat=True)
+
+        file_nodes = []
+        for material in materials:
+            # Traverse the connections to find file nodes
+            file_nodes.extend(pm.listConnections(material, type="file"))
+
+        # Remove duplicates
+        file_nodes = list(set(file_nodes))
+
+        # Apply inclusion and exclusion filters using ptk.filter_list
+        if inc or exc:
+            file_nodes = ptk.filter_list(
+                file_nodes,
+                inc=inc,
+                exc=exc,
+                map_func=lambda fn: fn.fileTextureName.get(),
+            )
+
+        for fn in file_nodes:
+            # Reload the texture by resetting the file path
+            file_path = fn.fileTextureName.get()
+            fn.fileTextureName.set(file_path)
+
+    @staticmethod
+    def get_mat_swatch_icon(mat, size=[20, 20]):
+        """Get an icon with a color fill matching the given materials RBG value.
+
+        Parameters:
+            mat (obj)(str): The material or the material's name.
+            size (list): Desired icon size.
+
+        Returns:
+            (obj) pixmap icon.
+        """
+        from PySide2.QtGui import QPixmap, QColor, QIcon
+
+        try:
+            # get the string name if a mat object is given.
+            matName = mat.name() if not isinstance(mat, (str)) else mat
+            # convert from 0-1 to 0-255 value and then to an integer
+            r = int(pm.getAttr(matName + ".colorR") * 255)
+            g = int(pm.getAttr(matName + ".colorG") * 255)
+            b = int(pm.getAttr(matName + ".colorB") * 255)
+            pixmap = QPixmap(size[0], size[1])
+            pixmap.fill(QColor.fromRgb(r, g, b))
+
+            return QIcon(pixmap)
+
+        except Exception:
+            pass
+
+        #  The following is a version that attempts to build a more accurate material swatch icon, but is crashing Maya
+        # """Generates a QIcon representing the color swatch of a given Maya material node.
+
+        # Parameters:
+        #     mat (str or PyNode): The name or PyNode object of the Maya material. Must be of type 'shadingDependNode'.
+        #     size (list of int): A list [width, height] specifying the dimensions of the QIcon in pixels. Default is [20, 20].
+
+        # Returns:
+        #     QIcon: A QIcon object filled with the color and transparency attributes of the specified material.
+        #            Returns None if the material is invalid or an exception occurs.
+        # Example:
+        #     icon = get_mat_swatch_icon('lambert1', [20, 20])
+        # """
+        # from PySide2.QtGui import QPixmap, QPainter, QColor, QIcon
+
+        # try:
+        #     # Check if the given 'mat' is a valid Maya material node
+        #     if not pm.objectType(mat, isAType="shadingDependNode"):
+        #         print(
+        #             f"Not a valid material. Expected 'shadingDependNode', got '{mat}' of type '{type(mat).__name__}'."
+        #         )
+        #         return None
+
+        #     # Initialize QPixmap
+        #     pixmap = QPixmap(size[0], size[1])
+        #     pixmap.fill(QColor(0, 0, 0, 0))  # Transparent Background
+
+        #     # Initialize QPainter
+        #     painter = QPainter(pixmap)
+
+        #     # Fetch material attributes
+        #     colorR = pm.getAttr(f"{mat}.colorR") * 255
+        #     colorG = pm.getAttr(f"{mat}.colorG") * 255
+        #     colorB = pm.getAttr(f"{mat}.colorB") * 255
+        #     transparency = pm.getAttr(
+        #         f"{mat}.transparencyR"
+        #     )  # Assuming R, G, B are the same for simplicity
+
+        #     # Set QColor based on material attributes
+        #     brushColor = QColor(colorR, colorG, colorB)
+        #     brushColor.setAlpha(255 * (1 - transparency))
+
+        #     # Draw swatch
+        #     painter.setBrush(brushColor)
+        #     painter.drawRect(0, 0, size[0], size[1])
+        #     painter.end()
+
+        #     return QIcon(pixmap)
+
+        # except Exception as e:
+        #     print(f"Exception: {e}")
+        #     return None
+
+
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    ...
+
+# -----------------------------------------------------------------------------
+# Notes
+# -----------------------------------------------------------------------------
