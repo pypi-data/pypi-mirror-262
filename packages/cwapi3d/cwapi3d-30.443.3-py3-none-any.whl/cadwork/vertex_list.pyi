16d0914d72566f5e71fb2bd8e765f71e0c00da07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:03:21 +0000

    Add package cwapi3d/30.443.3/cwapi3d-30.443.3-py3-none-any.whl

---
+++ packages/cwapi3d/cwapi3d-30.443.3-py3-none-any.whl/cadwork/vertex_list.pyi
@@ -0,0 +1,29 @@
+from cadwork import point_3d
+
+class vertex_list:
+    def count(self) -> int:
+        """count
+        Args:
+
+        Returns:
+            int
+        """
+
+    def at(self, index: int) -> point_3d:
+        """at
+        Args:
+            index ( int): index
+
+        Returns:
+            point_3d
+        """
+
+    def append(self, vertex: point_3d) -> None:
+        """append
+        Args:
+            vertex ( point_3d): vertex
+
+        Returns:
+            None
+        """
+
