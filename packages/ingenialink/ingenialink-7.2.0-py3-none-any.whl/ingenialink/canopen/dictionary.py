8f384cfa6558a2aded610a5d2970f6b5346bdfbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 03:03:09 +0000

    Add package ingenialink/7.2.0/ingenialink-7.2.0-py3-none-any.whl

---
+++ packages/ingenialink/ingenialink-7.2.0-py3-none-any.whl/ingenialink/canopen/dictionary.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+from ingenialink.dictionary import Dictionary
+from ingenialink.canopen.register import CanopenRegister
+
+import ingenialogger
+import xml.etree.ElementTree as ET
+
+logger = ingenialogger.get_logger(__name__)
+
+
+class CanopenDictionary(Dictionary):
+    """Contains all registers and information of a CANopen dictionary.
+
+    Args:
+        dictionary_path: Path to the Ingenia dictionary.
+
+    """
+
+    def __init__(self, dictionary_path: str) -> None:
+        super().__init__(dictionary_path)
+
+    def _read_xdf_register(self, register: ET.Element) -> Optional[CanopenRegister]:
+        current_read_register = super()._read_xdf_register(register)
+        if current_read_register is None:
+            return None
+        try:
+            aux_var = int(register.attrib["address"], 16)
+            idx = aux_var >> 8
+            subidx = aux_var & 0xFF
+
+            canopen_register = CanopenRegister(
+                idx,
+                subidx,
+                current_read_register.dtype,
+                current_read_register.access,
+                identifier=current_read_register.identifier,
+                units=current_read_register.units,
+                cyclic=current_read_register.cyclic,
+                phy=current_read_register.phy,
+                subnode=current_read_register.subnode,
+                storage=current_read_register.storage,
+                reg_range=current_read_register.range,
+                labels=current_read_register.labels,
+                enums=current_read_register.enums,
+                cat_id=current_read_register.cat_id,
+                scat_id=current_read_register.scat_id,
+                internal_use=current_read_register.internal_use,
+                address_type=current_read_register.address_type,
+            )
+
+            return canopen_register
+
+        except KeyError as ke:
+            logger.error(
+                f"Register with ID {current_read_register.identifier} has not attribute {ke}"
+            )
+            return None
---
+++ packages/ingenialink/ingenialink-7.2.0-py3-none-any.whl/ingenialink/ethernet/dictionary.py
@@ -0,0 +1,68 @@
+from typing import Optional, Dict, List
+import xml.etree.ElementTree as ET
+
+import ingenialogger
+
+from ingenialink.dictionary import Dictionary
+from ingenialink.ethernet.register import EthernetRegister
+
+logger = ingenialogger.get_logger(__name__)
+
+
+class EthernetDictionary(Dictionary):
+    """Contains all registers and information of a Ethernet dictionary.
+
+    Args:
+        dictionary_path: Path to the Ingenia dictionary.
+
+    """
+
+    def __init__(self, dictionary_path: str) -> None:
+        self._registers: List[Dict[str, EthernetRegister]] = []  # type: ignore [assignment]
+        super().__init__(dictionary_path)
+
+    def _read_xdf_register(self, register: ET.Element) -> Optional[EthernetRegister]:
+        current_read_register = super()._read_xdf_register(register)
+        if current_read_register is None:
+            return None
+        try:
+            reg_address = int(register.attrib["address"], 16)
+
+            ethernet_register = EthernetRegister(
+                reg_address,
+                current_read_register.dtype,
+                current_read_register.access,
+                identifier=current_read_register.identifier,
+                units=current_read_register.units,
+                cyclic=current_read_register.cyclic,
+                phy=current_read_register.phy,
+                subnode=current_read_register.subnode,
+                storage=current_read_register.storage,
+                reg_range=current_read_register.range,
+                labels=current_read_register.labels,
+                enums=current_read_register.enums,
+                cat_id=current_read_register.cat_id,
+                scat_id=current_read_register.scat_id,
+                internal_use=current_read_register.internal_use,
+                address_type=current_read_register.address_type,
+            )
+
+            return ethernet_register
+
+        except KeyError as ke:
+            logger.error(
+                f"Register with ID {current_read_register.identifier} has not attribute {ke}"
+            )
+            return None
+
+    def registers(self, subnode: int) -> Dict[str, EthernetRegister]:  # type: ignore [override]
+        """Gets the register dictionary to the targeted subnode.
+
+        Args:
+            subnode: Identifier for the subnode.
+
+        Returns:
+            Dictionary of all the registers for a subnode.
+
+        """
+        return self._registers[subnode]
