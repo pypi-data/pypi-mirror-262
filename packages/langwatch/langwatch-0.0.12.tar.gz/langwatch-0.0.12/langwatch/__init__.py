44ac9c3037dc06355415f82f960c58b53d0f42b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 06:04:07 +0000

    Add package langwatch/0.0.12/langwatch-0.0.12.tar.gz

---
+++ packages/langwatch/langwatch-0.0.12.tar.gz/langwatch-0.0.12/langwatch/__init__.py
@@ -0,0 +1,23 @@
+"""
+LangWatch
+
+This is the top level module for [LangWatch](https://github.com/langwatch/langwatch),
+it holds the api_key and the endpoint the tracing is going to be send to.
+
+For LLM and library-specific tracing functions, check out the other files on this module.
+"""
+
+import os
+from warnings import warn
+
+from langwatch.tracer import span, create_span, capture_rag
+
+endpoint = (
+    os.environ.get("LANGWATCH_ENDPOINT") or "https://app.langwatch.ai"
+)
+api_key = os.environ.get("LANGWATCH_API_KEY")
+
+if not api_key:
+    warn("LANGWATCH_API_KEY is not set, LLMs traces will not be sent, go to https://langwatch.ai to set it up")
+
+__all__ = ("endpoint", "api_key", "span", "create_span")
