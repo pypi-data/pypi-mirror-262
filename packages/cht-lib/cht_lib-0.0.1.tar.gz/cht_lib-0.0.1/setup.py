80ee40ec46c074f0459d72a4eb11d2cbdde7aa66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:04:25 +0000

    Add package cht-lib/0.0.1/cht_lib-0.0.1.tar.gz

---
+++ packages/cht-lib/cht_lib-0.0.1.tar.gz/cht_lib-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='cht_lib',
+    version='0.0.1',
+    author='Chitaev',
+    author_email='your@domain.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
