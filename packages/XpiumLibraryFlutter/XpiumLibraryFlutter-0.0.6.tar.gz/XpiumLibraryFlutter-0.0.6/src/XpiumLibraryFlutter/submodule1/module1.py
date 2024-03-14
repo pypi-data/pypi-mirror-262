55386328b2e11779fa80739b9919ed47947c6ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 15:03:30 +0000

    Add package XpiumLibraryFlutter/0.0.6/XpiumLibraryFlutter-0.0.6.tar.gz

---
+++ packages/XpiumLibraryFlutter/XpiumLibraryFlutter-0.0.6.tar.gz/XpiumLibraryFlutter-0.0.6/src/XpiumLibraryFlutter/submodule1/module1.py
@@ -0,0 +1,8 @@
+#submodule1/module1.py
+# -*- coding: utf-8 -*-
+from robot.api.deco import keyword
+
+class XPrint:
+    @keyword("XPrint Log")
+    def XPrint_log(self):
+        print("Hello, world! เทสภาษาไทย111")
