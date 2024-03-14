b5928c36eeb1840817240064c4aa88f47c9120bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 06:03:48 +0000

    Add package inngest/0.3.6a1/inngest-0.3.6a1.tar.gz

---
+++ packages/inngest/inngest-0.3.6a1.tar.gz/inngest-0.3.6a1/tests/test_fast_api.py
@@ -0,0 +1,145 @@
+import json
+import typing
+import unittest
+
+import fastapi
+import fastapi.testclient
+
+import inngest
+import inngest.fast_api
+from inngest._internal import const
+
+from . import base, cases, dev_server, http_proxy, net
+
+_framework = "fast_api"
+_dev_server_origin = f"http://{net.HOST}:{dev_server.PORT}"
+
+_client = inngest.Inngest(
+    api_base_url=_dev_server_origin,
+    app_id=_framework,
+    event_api_base_url=_dev_server_origin,
+    is_production=False,
+)
+
+_cases = cases.create_async_cases(_client, _framework)
+_fns: list[inngest.Function] = []
+for case in _cases:
+    if isinstance(case.fn, list):
+        _fns.extend(case.fn)
+    else:
+        _fns.append(case.fn)
+
+
+class TestFastAPI(unittest.TestCase):
+    app: fastapi.FastAPI
+    client: inngest.Inngest
+    dev_server_port: int
+    fast_api_client: fastapi.testclient.TestClient
+    proxy: http_proxy.Proxy
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.app = fastapi.FastAPI()
+        cls.client = _client
+
+        inngest.fast_api.serve(
+            cls.app,
+            cls.client,
+            _fns,
+        )
+        cls.fast_api_client = fastapi.testclient.TestClient(cls.app)
+        cls.proxy = http_proxy.Proxy(cls.on_proxy_request).start()
+        base.register(cls.proxy.port)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+        cls.proxy.stop()
+
+    @classmethod
+    def on_proxy_request(
+        cls,
+        *,
+        body: typing.Optional[bytes],
+        headers: dict[str, list[str]],
+        method: str,
+        path: str,
+    ) -> http_proxy.Response:
+        if body is None or len(body) == 0:
+            body = json.dumps({}).encode("utf-8")
+
+        new_headers = {key: value[0] for key, value in headers.items()}
+
+        if method == "POST":
+            res = cls.fast_api_client.post(
+                path,
+                content=body,
+                headers=new_headers,
+            )
+        elif method == "PUT":
+            res = cls.fast_api_client.put(
+                path,
+                content=body,
+                headers=new_headers,
+            )
+        else:
+            raise Exception(f"unsupported method: {method}")
+
+        return http_proxy.Response(
+            body=res.content,
+            headers=dict(res.headers),
+            status_code=res.status_code,
+        )
+
+
+for case in _cases:
+    test_name = f"test_{case.name}"
+    setattr(TestFastAPI, test_name, case.run_test)
+
+
+class TestRegistration(unittest.TestCase):
+    def test_dev_server_to_prod(self) -> None:
+        """Ensure that Dev Server cannot initiate a registration request when in
+        production mode.
+        """
+        client = inngest.Inngest(
+            app_id=f"{_framework}_registration",
+            event_key="test",
+            signing_key="signkey-prod-0486c9",
+        )
+
+        @client.create_function(
+            fn_id="foo",
+            retries=0,
+            trigger=inngest.TriggerEvent(event="app/foo"),
+        )
+        async def fn(
+            ctx: inngest.Context,
+            step: inngest.Step,
+        ) -> None:
+            pass
+
+        app = fastapi.FastAPI()
+        inngest.fast_api.serve(
+            app,
+            client,
+            [fn],
+        )
+        fast_api_client = fastapi.testclient.TestClient(app)
+        res = fast_api_client.put(
+            "/api/inngest",
+            headers={
+                const.HeaderKey.SERVER_KIND.value.lower(): const.ServerKind.DEV_SERVER.value,
+            },
+        )
+        assert res.status_code == 400
+        body: object = res.json()
+        assert (
+            isinstance(body, dict)
+            and body["code"] == const.ErrorCode.SERVER_KIND_MISMATCH.value
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
