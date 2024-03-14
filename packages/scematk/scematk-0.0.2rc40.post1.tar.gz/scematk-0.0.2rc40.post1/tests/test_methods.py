13efd1d1cf9de8255dd2aedfe49aa29f4a8acdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:03:33 +0000

    Add package scematk/0.0.2rc40.post1/scematk-0.0.2rc40.post1.tar.gz

---
+++ packages/scematk/scematk-0.0.2rc40.post1.tar.gz/scematk-0.0.2rc40.post1/tests/test_methods.py
@@ -0,0 +1,6 @@
+#   ---------------------------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   ---------------------------------------------------------------------------------
+"""This is a sample python file for testing functions from the source code."""
+from __future__ import annotations
