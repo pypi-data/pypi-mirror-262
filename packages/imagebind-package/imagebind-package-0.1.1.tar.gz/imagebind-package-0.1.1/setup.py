da29629ff4e02b067cbd4d94f5c023874f878b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 14:03:53 +0000

    Add package imagebind-package/0.1.1/imagebind-package-0.1.1.tar.gz

---
+++ packages/imagebind-package/imagebind-package-0.1.1.tar.gz/imagebind-package-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="imagebind-package",
+    version="0.1.1",
+    author='Raghav Dixit',
+    packages=find_packages(),
+    include_package_data=True,
+    description="Updated version of Imagebind package with bug fixes.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/raghavdixit99/ImageBind",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=required,
+    dependency_links=["https://download.pytorch.org/whl/cu113"],
+)
