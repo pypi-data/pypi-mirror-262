94883ec62ee50880eba2c12dd0307ce5ce904828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 14:04:14 +0000

    Add package pre-processing-text-basic-tools-br/0.9.4/pre_processing_text_basic_tools_br-0.9.4.tar.gz

---
+++ packages/pre-processing-text-basic-tools-br/pre_processing_text_basic_tools_br-0.9.4.tar.gz/pre_processing_text_basic_tools_br-0.9.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open(r'README.md','r',encoding='utf-8') as f:
+    descricao_longa = f.read()
+
+setup(
+    name='pre_processing_text_basic_tools_br',
+    version='0.9.4',
+    packages=find_packages(),
+    install_requires = ['regex'],
+    description='Kit de ferramentas para processos básicos de Processamento de Linguagem Natural.',
+    long_description=descricao_longa,
+    long_description_content_type="text/markdown",
+    author='Igor Caetano de Souza',
+    project_urls={
+        "GitHub Repository":"https://github.com/IgorCaetano/pre_processing_text_basic_tools_br"        
+    }
+)
