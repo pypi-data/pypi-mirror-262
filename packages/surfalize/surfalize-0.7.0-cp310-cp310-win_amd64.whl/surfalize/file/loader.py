4f3b045310d0eaf7ceb4fee73b346a919074b772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:04:19 +0000

    Add package surfalize/0.7.0/surfalize-0.7.0-cp310-cp310-win_amd64.whl

---
+++ packages/surfalize/surfalize-0.7.0-cp310-cp310-win_amd64.whl/surfalize/file/loader.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from functools import partial
+from ..exceptions import UnsupportedFileFormatError
+
+from .vk import read_vk4, read_vk6_vk7
+from .plu import read_plu
+from .plux import read_plux
+from .sur import read_sur
+from .zmg import read_zmg
+from .opd import read_opd
+from .xyz import read_xyz
+from .nms import read_nms
+
+dispatcher = {
+    '.sur': read_sur,
+    '.vk4': read_vk4,
+    '.vk6': read_vk6_vk7,
+    '.vk7': read_vk6_vk7,
+    '.plu': read_plu,
+    '.plux': read_plux,
+    '.zmg': read_zmg,
+    '.opd': read_opd,
+    '.xyz': read_xyz,
+    '.nms': read_nms
+}
+
+supported_formats = list(dispatcher.keys())
+
+def load_file(filepath, encoding='utf-8'):
+    filepath = Path(filepath)
+    try:
+        loader = dispatcher[filepath.suffix]
+        if 'encoding' in loader.__code__.co_varnames:
+            loader = partial(loader, encoding=encoding)
+    except KeyError:
+        raise UnsupportedFileFormatError(f"File format {filepath.suffix} is currently not supported.")
+    return loader(filepath)
