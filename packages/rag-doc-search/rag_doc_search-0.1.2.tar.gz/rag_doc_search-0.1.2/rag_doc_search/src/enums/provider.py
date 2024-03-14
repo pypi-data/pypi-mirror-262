e83bbe76eb82290548ad89898b418d66984380b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 03:02:59 +0000

    Add package rag-doc-search/0.1.2/rag_doc_search-0.1.2.tar.gz

---
+++ packages/rag-doc-search/rag_doc_search-0.1.2.tar.gz/rag_doc_search-0.1.2/rag_doc_search/src/enums/provider.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class VectorStoreProvider(Enum):
+    """
+    Enum representing the providers available for a vector stores.
+
+    Attributes:
+    - `PGVector`: Refers to the PGVector provider.
+    - `FAISS`: Refers to the FAISS provider.
+    """
+    PGVector = "PGVector"
+    FAISS = "FAISS"
+
+
+class AIProvider(Enum):
+    """
+    Enum representing providers available for the AI model API.
+
+    Attributes:
+    - `OPENAI`: Refers to the OpenAI provider.
+    - `BEDROCK`: Refers to the Bedrock provider.
+    """
+    OPENAI = "OPENAI"
+    BEDROCK = "BEDROCK"
