0fd74a0bbb0b87d8c1c45a1dde5775c7078f41d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:03:18 +0000

    Add package TineAutomationToolkit/0.0.2/TineAutomationToolkit-0.0.2.tar.gz

---
+++ packages/TineAutomationToolkit/TineAutomationToolkit-0.0.2.tar.gz/TineAutomationToolkit-0.0.2/TineAutomationToolkit/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from TineAutomationToolkit.keywords import *
+
+
+class TineAutomationToolkit(ToolkitsTest):
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        print("This Main นี้คือ เมน")
