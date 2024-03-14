34721e01ce63fd6095ce2e98d6ec5e36a1b5d16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 06:03:45 +0000

    Add package pycountries/0.0.15/pycountries-0.0.15.tar.gz

---
+++ packages/pycountries/pycountries-0.0.15.tar.gz/pycountries-0.0.15/src/pycountries/__init__.py
@@ -0,0 +1,9 @@
+from pycountries.countries import Country
+from pycountries.currencies import Currency
+
+__version__ = "0.0.15"
+__author__ = "Ivan Koldakov"
+__all__ = [
+    "Country",
+    "Currency",
+]
