5600f44d50cd298dd8009c774812dbe6bcc55ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 14:04:03 +0000

    Add package pt-br-verbs-lemmatizer/0.0.6/pt_br_verbs_lemmatizer-0.0.6.tar.gz

---
+++ packages/pt-br-verbs-lemmatizer/pt_br_verbs_lemmatizer-0.0.6.tar.gz/pt_br_verbs_lemmatizer-0.0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+caminho_dataset = os.path.join('dataset','dic_pt_br_verbs_lemma.msgpack')
+
+with open(r'README.md','r',encoding='utf-8') as f:
+    descricao_longa = f.read()
+
+setup(
+    name='pt_br_verbs_lemmatizer',
+    version='0.0.6',
+    packages=find_packages(),
+    # package_dir={"":"pt_br_verbs_lemmatizer"},
+    package_data={'dataset': ["*.msgpack"]},
+    include_package_data=True,
+    install_requires = ['msgpack==1.0.7'],
+    description='Program designed to lemmatize the various verbal inflections present in the Brazilian Portuguese language quickly and efficiently.',
+    long_description=descricao_longa,
+    long_description_content_type="text/markdown",
+    author='Igor Caetano de Souza',
+    project_urls={
+        "GitHub Repository":"https://github.com/IgorCaetano/portuguese_br_verbs_lemmatizer"
+    },    
+)
