2c6767ea7b8538b285095f83dcf53c529dc24d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:04:23 +0000

    Add package peakoscope/1.0.0/peakoscope-1.0.0.tar.gz

---
+++ packages/peakoscope/peakoscope-1.0.0.tar.gz/peakoscope-1.0.0/peakoscope/errors.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# This file is part of Peakoscope.
+# Copyright (C) 2021-2024  Eivind Tøstesen
+# Peakoscope is licensed under GPLv3.
+"""Python module for errors and exceptions.
+
+"""
+
+
+class PeakyBlunder(Exception):
+    pass
