197f5b5945c3060ca0154578545fb3719dede988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:03:47 +0000

    Add package robocorp-actions/0.0.9/robocorp_actions-0.0.9.tar.gz

---
+++ packages/robocorp-actions/robocorp_actions-0.0.9.tar.gz/robocorp_actions-0.0.9/src/robocorp/actions/cli.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def main(args=None, exit: bool = True) -> int:
+    """Entry point for running actions from robocorp-actions."""
+    from robocorp.actions._args_dispatcher import _ActionsArgDispatcher
+
+    # Note: robocorp-actions is almost only robocorp-tasks but with a few
+    # different APIs.
+    #
+    # As both are under our control, we rely on internal APIs to make
+    # it seem to the external world as everything is action-based (and not
+    # task-based).
+
+    if args is None:
+        args = sys.argv[1:]
+
+    from robocorp.tasks import _constants
+    from robocorp.tasks.cli import main
+
+    _constants.DEFAULT_TASK_SEARCH_GLOB = "*action*.py"
+    _constants.MODULE_ENTRY_POINT = "robocorp.actions"
+
+    return main(args, exit, argument_dispatcher=_ActionsArgDispatcher())
