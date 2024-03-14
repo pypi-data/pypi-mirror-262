31f234f94f6c4dc949b4c2c293e383d0d62cee4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:03:38 +0000

    Add package glow2d/3.3.0/glow2d-3.3.0.tar.gz

---
+++ packages/glow2d/glow2d-3.3.0.tar.gz/glow2d-3.3.0/src/glow2d/__init__.py
@@ -0,0 +1,23 @@
+"""
+Evaluate GLOW model on the great circle passing through an origin location along a given bearing.
+
+Classes
+-------
+- glow2d_geo
+- glow2d_polar
+
+Functions
+---------
+- geo_model
+- polar_model
+
+Misc. Variables
+---------------
+- __version__
+"""
+
+from ._glow2d import glow2d_geo, glow2d_polar, geo_model, polar_model
+
+__version__ = '3.3.0'
+
+__all__ = ['glow2d_geo', 'glow2d_polar', 'geo_model', 'polar_model', '__version__']
