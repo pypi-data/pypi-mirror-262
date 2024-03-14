d394b59ca1b83c435cfd5138ff6bd9adc5aebebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 03:02:39 +0000

    Add package staticviking2/0.0.2/staticviking2-0.0.2.tar.gz

---
+++ packages/staticviking2/staticviking2-0.0.2.tar.gz/staticviking2-0.0.2/README.md
@@ -0,0 +1,14 @@
+# Static Viking
+
+Checkout more here ![https://martinbaun.com/blog/posts/staticviking-zeroconf-static-website-for-blogs-wikis/](https://martinbaun.com/blog/posts/staticviking-zeroconf-static-website-for-blogs-wikis/)
+
+```
+Usage:
+  staticviking.py build [--config=<config>] [--future]
+  staticviking.py serve
+  staticviking.py buildserve [--config=<config>] [--future]
+  staticviking.py print [--config=<config>]
+
+Options:
+  -h --help
+```
---
+++ packages/staticviking2/staticviking2-0.0.2.tar.gz/staticviking2-0.0.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="staticviking2",
+    packages=["staticviking"],
+    package_dir={"staticviking": "staticviking"},
+    package_data={
+        'staticviking': ['default_config.json', 'themes/*'],
+        'staticviking.templates.abc': ["**"],
+    },
+    version="0.0.2",
+    author="Martin Baun",
+    author_email="pypi@martinbaun.com",
+    description="Simplest static page engine for Developers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://martinbaun.com/blog/posts/staticviking-zeroconf-static-website-for-blogs-wikis/",
+    install_requires=[],  # Used for dependencies
+    entry_points={
+        'console_scripts': ['staticviking = staticviking.staticviking:cli']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=['blog', 'productivity', 'static site generator'],
+    python_requires='>=3.8',
+    include_package_data=True,
+)
