9163103c5ea9aa712becbaec012b8044dc1d5c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:02:45 +0000

    Add package meg-qc/0.1.2/meg_qc-0.1.2.tar.gz

---
+++ packages/meg-qc/meg_qc-0.1.2.tar.gz/meg_qc-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import versioneer
+
+setup(
+    name='meg_qc',
+    version= versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=['meg_qc'],
+    url='https://github.com/AaronReer/MEGqc',
+    license='MIT',
+    author='ANCP',
+    author_email='aaron.reer@uol.de',
+)
