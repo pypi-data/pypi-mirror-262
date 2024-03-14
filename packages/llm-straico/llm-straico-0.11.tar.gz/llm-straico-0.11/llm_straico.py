d214ef8994c42b1a2c729feb10fadd88792f6254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 15:03:19 +0000

    Add package llm-straico/0.11/llm-straico-0.11.tar.gz

---
+++ packages/llm-straico/llm-straico-0.11.tar.gz/llm-straico-0.11/llm_straico.py
@@ -0,0 +1,18 @@
+import straico
+import llm
+import os
+
+__version__ = 0.11
+client = straico.StraicoClient(os.environ.get("STRAICO_API_KEY"))
+
+@llm.hookimpl
+def register_models(register):
+    register(StraicoGPT35())
+
+class StraicoGPT35(llm.Model):
+    model_id = "straico-gpt-3.5"
+
+    def execute(self, prompt, stream, response, conversation):
+        result = client.make_model_request("openai/gpt-3.5-turbo-0125", prompt)['completion']['choices'][0]['message']['content']
+        return result
+                                                                                                  
