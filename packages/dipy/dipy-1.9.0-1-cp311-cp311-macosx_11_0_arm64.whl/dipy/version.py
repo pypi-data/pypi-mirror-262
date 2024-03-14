32df517af383c4ac1478db3dc156db184c7f5f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:02:58 +0000

    Add package dipy/1.9.0/dipy-1.9.0-1-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/dipy/dipy-1.9.0-1-cp311-cp311-macosx_11_0_arm64.whl/dipy/version.py
@@ -0,0 +1,12 @@
+# THIS FILE IS GENERATED DURING THE DIPY BUILD
+# See tools/version_utils.py for details
+
+short_version = '1.9.0'
+version = '1.9.0'
+full_version = '1.9.0'
+git_revision = '431e72d'
+commit_count = '934'
+release = True
+
+if not release:
+    version = full_version
