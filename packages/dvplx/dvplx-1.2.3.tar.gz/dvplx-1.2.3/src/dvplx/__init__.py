dc1be83b08fbf40973d99395abbafdcc23c573ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 03:02:56 +0000

    Add package dvplx/1.2.3/dvplx-1.2.3.tar.gz

---
+++ packages/dvplx/dvplx-1.2.3.tar.gz/dvplx-1.2.3/src/dvplx/__init__.py
@@ -0,0 +1,19 @@
+from .dvpl import (
+    CompressDVPL,
+    DecompressDVPL,
+    readDVPLFooter,
+    DVPL_FOOTER_SIZE
+)
+
+from .converter import (
+    ConvertDVPLFiles,
+    VerifyDVPLFiles,
+)
+
+from .color import (
+    Color
+)
+
+__all__ = ['CompressDVPL', 'DecompressDVPL','readDVPLFooter', 'DVPL_FOOTER_SIZE','ConvertDVPLFiles', 'VerifyDVPLFiles','Color']
+
+__version__ = '1.2.3'
