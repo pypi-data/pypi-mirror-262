a71ddd9c434a0e2c3cb55ab7d52539948fa49bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:04:11 +0000

    Add package blackboxai/0.6/blackboxai-0.6.tar.gz

---
+++ packages/blackboxai/blackboxai-0.6.tar.gz/blackboxai-0.6/setup.py
@@ -0,0 +1,18 @@
+# from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name="blackboxai",
+    version="0.6",
+    packages=find_packages(),
+    package_dir = {'': '.'},
+    include_package_data=True,
+    setup_requires= ['requests'],
+    entry_points={
+        "console_scripts": [
+            "blackboxai = blackboxai:runChat"
+        ]
+    }
+)
