8903cc07760dac1e00d1bc25f219384e058c5adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:04:06 +0000

    Add package gptel/0.3.0/gptel-0.3.0-py3-none-any.whl

---
+++ packages/gptel/gptel-0.3.0-py3-none-any.whl/gptel/__init__.py
@@ -0,0 +1,21 @@
+from .base import (
+    AbstractApplication,
+    AbstractClient,
+    BotContext,
+    ReplyImage,
+    ReplyTyping,
+    ReplyHelpCommands,
+)
+from .bot import GPTelBot
+
+__all__ = [
+    "GPTelBot",
+    "AbstractApplication",
+    "AbstractClient",
+    "BotContext",
+    "ReplyTyping",
+    "ReplyImage",
+    "ReplyHelpCommands",
+]
+
+__version__ = "0.3.0"
---
+++ packages/gptel/gptel-0.3.0-py3-none-any.whl/gptel/base.py
@@ -0,0 +1,81 @@
+from abc import abstractmethod
+from typing import Callable, Dict, Optional, List
+from dataclasses import dataclass, field
+
+
+@dataclass
+class BotCommand:
+    command: str
+    description: str
+    handler: Callable
+    show_in_menu: Optional[bool] = True
+
+
+@dataclass
+class ApplicationConfig:
+    name: str
+    description: str
+    token: str
+    data_default: Dict = field(default_factory=dict)
+    commands: List[BotCommand] = field(default_factory=list)
+
+
+class AbstractApplication:
+    data_default: Dict = {}
+    config: ApplicationConfig
+
+    def __init__(self, config: ApplicationConfig):
+        self.config = config
+
+    @abstractmethod
+    async def setup(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    @abstractmethod
+    def add_handlers(self):
+        pass
+
+    @abstractmethod
+    def set_chat_handler(self, handler: Callable):
+        pass
+
+    @abstractmethod
+    def set_audio_handler(self, handler: Callable):
+        pass
+
+
+class AbstractClient:
+    @abstractmethod
+    def get_application(self, config: ApplicationConfig) -> AbstractApplication:
+        pass
+
+
+class ReplyBase:
+    pass
+
+
+class ReplyTyping(ReplyBase):
+    pass
+
+
+class ReplyImage(ReplyBase):
+    url: str
+
+    def __init__(self, url: str):
+        self.url = url
+
+
+class ReplyHelpCommands(ReplyBase):
+    pass
+
+
+class BotContext:
+    def __init__(self):
+        self.user_html = ""
+        self.message = ""
+        self.tmp_audio_path = None
+        self.data = {}
