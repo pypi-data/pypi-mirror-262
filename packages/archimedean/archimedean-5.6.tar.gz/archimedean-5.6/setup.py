5a7d3b56f9f761ddb696f08198120dbdcc65bbf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 03:02:58 +0000

    Add package archimedean/5.6/archimedean-5.6.tar.gz

---
+++ packages/archimedean/archimedean-5.6.tar.gz/archimedean-5.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name='archimedean',
+    version='5.6',
+    author='Daniel Li',
+    author_email='daniel.miami2005@gmail.com',
+    description='The Ultimate Archimedean CLI',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'click',
+        'requests',
+        'bs4',
+        'python-dotenv'
+    ],
+    scripts=['./bin/archimedean'],
+    python_requires='>=3.6'
+)
