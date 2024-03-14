b41fb5e1da58e265dd8f119d351eea58245836ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:03:26 +0000

    Add package templify/0.0.16/templify-0.0.16.tar.gz

---
+++ packages/templify/templify-0.0.16.tar.gz/templify-0.0.16/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    description = f.read()
+
+setup(
+    name='templify',
+    version='0.0.16',
+    packages=find_packages(),
+    install_requires=[
+        'django>=4.2',
+        # 'typer~=0.9.0'
+    ],
+    author='Yunusov Abdulmajid',
+    author_email='toepammiddle@email.com',
+    description='This package provides templates for you to start developing your application',
+    entry_points={
+        "console_scripts": [
+            "templify = templify:main",
+        ],
+    },
+    url='https://github.com/toEpam/templify',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    long_description=description,
+    long_description_content_type='text/markdown',
+    # download_url='https://github.com/toEpam/templify/archive/refs/heads/master.zip'
+)
