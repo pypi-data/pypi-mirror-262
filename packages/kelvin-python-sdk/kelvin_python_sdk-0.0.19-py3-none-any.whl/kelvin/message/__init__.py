3ac3ec5312a8b351f0b7d3f3acd67b915ab58a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:02:49 +0000

    Add package kelvin-python-sdk/0.0.19/kelvin_python_sdk-0.0.19-py3-none-any.whl

---
+++ packages/kelvin-python-sdk/kelvin_python_sdk-0.0.19-py3-none-any.whl/kelvin/message/__init__.py
@@ -0,0 +1,54 @@
+"""Kelvin Messages."""
+
+from __future__ import annotations
+
+from .base_messages import (
+    ControlChangeMsg,
+    ControlChangePayload,
+    ControlChangeStatus,
+    ControlChangeStatusPayload,
+    RecommendationActions,
+    RecommendationControlChange,
+    RecommendationMsg,
+)
+from .message import Message
+from .msg_builders import AssetParameter, AssetParameters, ControlChange, Recommendation
+from .msg_type import (
+    KMessageType,
+    KMessageTypeControl,
+    KMessageTypeControlStatus,
+    KMessageTypeData,
+    KMessageTypeParameter,
+    KMessageTypePrimitive,
+    KMessageTypeRecommendation,
+)
+from .primitives import AssetDataMessage, Boolean, BooleanParameter, Number, NumberParameter, String, StringParameter
+
+__all__ = [
+    "Message",
+    "Boolean",
+    "Number",
+    "String",
+    "NumberParameter",
+    "BooleanParameter",
+    "StringParameter",
+    "KMessageType",
+    "KMessageTypeData",
+    "KMessageTypePrimitive",
+    "KMessageTypeParameter",
+    "KMessageTypeControl",
+    "KMessageTypeRecommendation",
+    "KMessageTypeControlStatus",
+    "RecommendationMsg",
+    "RecommendationActions",
+    "RecommendationControlChange",
+    "ControlChangeMsg",
+    "ControlChangePayload",
+    "ControlChangeStatus",
+    "ControlChangeStatusPayload",
+    "Recommendation",
+    "ControlChange",
+    "AssetParameter",
+    "AssetParameters",
+    "AssetDataMessage",
+]
