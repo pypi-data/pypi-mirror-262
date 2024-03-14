233883c0a17705be12c5fdbc34867d7df8f4a531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 15:03:22 +0000

    Add package randovania/7.4.1/randovania-7.4.1-py3-none-any.whl

---
+++ packages/randovania/randovania-7.4.1-py3-none-any.whl/randovania/game_description/db/configurable_node.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import dataclasses
+from typing import TYPE_CHECKING
+
+from randovania.game_description.db.node import Node, NodeContext
+
+if TYPE_CHECKING:
+    from randovania.game_description.requirements.base import Requirement
+
+
+@dataclasses.dataclass(frozen=True, slots=True)
+class ConfigurableNode(Node):
+    def __repr__(self) -> str:
+        return f"ConfigurableNode({self.name!r})"
+
+    def requirement_to_leave(self, context: NodeContext) -> Requirement:
+        return context.patches.configurable_nodes[context.node_provider.identifier_for_node(self)]
