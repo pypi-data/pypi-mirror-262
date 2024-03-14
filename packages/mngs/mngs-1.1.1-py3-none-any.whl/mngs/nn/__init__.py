df36c3e3d68bc9cda51f647d8173c7a8eb2cf9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 14:03:35 +0000

    Add package mngs/1.1.1/mngs-1.1.1-py3-none-any.whl

---
+++ packages/mngs/mngs-1.1.1-py3-none-any.whl/mngs/nn/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: "2023-05-15 16:48:33 (ywatanabe)"
+
+from ._SpatialAttention import SpatialAttention
+# from ._FreqDropout import FreqDropout
+from ._MNet_1000 import MNet_1000, MNet_config
+from ._ChannelGainChanger import ChannelGainChanger
+from ._FreqGainChanger import FreqGainChanger
+from ._BNet import BNet, BNet_config
+from ._SwapChannels import SwapChannels
+from ._DropoutChannels import DropoutChannels
+from ._ResNet1D import ResNet1D, ResNetBasicBlock
