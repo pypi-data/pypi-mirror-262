68a9dfc0f73abc4116035458ac524e9f598ab010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:02:47 +0000

    Add package findly-unified-reporting-sdk/0.1.3/findly_unified_reporting_sdk-0.1.3.tar.gz

---
+++ packages/findly-unified-reporting-sdk/findly_unified_reporting_sdk-0.1.3.tar.gz/findly_unified_reporting_sdk-0.1.3/src/findly/unified_reporting_sdk/__init__.py
@@ -0,0 +1,7 @@
+from findly.unified_reporting_sdk.urs import Urs
+from findly.unified_reporting_sdk.protos.semantic_layer_pb2 import (
+    DataSourceIntegration,
+    QueryArgs
+)
+
+__all__ = ['Urs', 'QueryArgs', 'DataSourceIntegration']
