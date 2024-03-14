c3ae328f3724236cc5e5da6442b4de10b3b28720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 17:03:29 +0000

    Add package bluecellulab/2.4.0/bluecellulab-2.4.0.tar.gz

---
+++ packages/bluecellulab/bluecellulab-2.4.0.tar.gz/bluecellulab-2.4.0/bluecellulab/type_aliases.py
@@ -0,0 +1,10 @@
+"""Type aliases used within the package."""
+
+from typing_extensions import TypeAlias
+from neuron import h as hoc_type
+
+HocObjectType: TypeAlias = hoc_type   # until NEURON is typed, most NEURON types are this
+NeuronRNG: TypeAlias = hoc_type
+NeuronVector: TypeAlias = hoc_type
+NeuronSection: TypeAlias = hoc_type
+TStim: TypeAlias = hoc_type
