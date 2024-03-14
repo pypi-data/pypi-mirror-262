542c24a4a54fb8c8ad3ae7a838f4994f6e2fa76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 17:03:22 +0000

    Add package chaostoolkit-reliably/0.76.0/chaostoolkit_reliably-0.76.0-py3-none-any.whl

---
+++ packages/chaostoolkit-reliably/chaostoolkit_reliably-0.76.0-py3-none-any.whl/chaosreliably/activities/safeguard/probes.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+import httpx
+from chaoslib.types import Configuration, Secrets
+from logzero import logger
+
+__all__ = ["call_endpoint"]
+
+
+def call_endpoint(
+    url: str,
+    auth: Optional[str] = None,
+    configuration: Configuration = None,
+    secrets: Secrets = None,
+) -> bool:
+    headers = {}
+    if auth:
+        headers["Authorization"] = auth
+
+    r = httpx.get(url, headers=headers)
+    if r.status_code != 200:
+        logger.critical("Safeguard endpoint returned a non 200 response")
+        return False
+
+    result = r.json()
+    if not result.get("ok"):
+        logger.critical(
+            f"Safeguard endpoint returned with an error: {result['error']}"
+        )
+        return False
+
+    return True
