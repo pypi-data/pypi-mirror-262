ce5efe14303978876cf47dcffe71ca052cf97cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:02:59 +0000

    Add package log-reg-dre/0.1.7/log-reg-dre-0.1.7.tar.gz

---
+++ packages/log-reg-dre/log-reg-dre-0.1.7.tar.gz/log-reg-dre-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='log-reg-dre',  
+    version='0.1.7', 
+    author='Sean Zhang',
+    author_email='szhang120@gmail.com',
+    description='Combines logistic regression for estimating density ratios with RU regression to mitigate distributional shift',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown', 
+    packages=find_packages(),  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
+    install_requires=[
+        'numpy',
+        'torch',
+        'matplotlib',
+        'scikit-learn',
+    ],
+)
+
