c452745e4a7c015ed73cef18366bc52a67a0ff00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 03:02:39 +0000

    Add package lca-algebraic-dev/1.0.5.1681180.dev0/lca_algebraic_dev-1.0.5.1681180.dev0-py2.py3-none-any.whl

---
+++ packages/lca-algebraic-dev/lca_algebraic_dev-1.0.5.1681180.dev0-py2.py3-none-any.whl/lca_algebraic/log.py
@@ -0,0 +1,14 @@
+import logging
+import os
+
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+# logging.basicConfig(level=LOGLEVEL, format="[%(levelname)s] %(message)s")
+
+logger = logging.getLogger("lca_algebraic")
+logger.setLevel(LOGLEVEL)
+
+ch = logging.StreamHandler()
+ch.setLevel(LOGLEVEL)
+ch.setFormatter(logging.Formatter("[%(levelname)s] %(message)s"))
+
+logger.addHandler(ch)
