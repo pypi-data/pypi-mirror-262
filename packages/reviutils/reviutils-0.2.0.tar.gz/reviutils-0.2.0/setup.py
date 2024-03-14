f07fe05b2eb0f3d19db5de68ac1ab301a43d908a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:03:27 +0000

    Add package reviutils/0.2.0/reviutils-0.2.0.tar.gz

---
+++ packages/reviutils/reviutils-0.2.0.tar.gz/reviutils-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+    
+setup(
+    name='reviutils',
+    version='0.2.0',
+    description='A common library frequently used on python',
+    url='https://github.com/Viyyy/viutils',
+    author='Re.VI',
+    author_email='reviy-top@outlook.com',
+    license='Apache License 2.0',
+    packages=['reviutils.api'],
+    install_requires=['numpy'],
+    zip_safe=False
+)
