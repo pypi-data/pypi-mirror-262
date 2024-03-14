4915fcb1cfb86258df716ee628d8e96af08767e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:03:55 +0000

    Add package rooms-shared-services/0.1.47/rooms_shared_services-0.1.47.tar.gz

---
+++ packages/rooms-shared-services/rooms_shared_services-0.1.47.tar.gz/rooms_shared_services-0.1.47/rooms_shared_services/src/models/texts/languages.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Language(Enum):
+    en = "english"
+    ru = "russian"
+    he = "hebrew"
+    de = "german"
+    pl = "polish"
+    fr = "french"
+    it = "italian"
+    
