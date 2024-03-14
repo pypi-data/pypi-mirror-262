56811ef7c9b072885ae6b1326e13266cf8437da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 17:03:43 +0000

    Add package cookiesparser/1.2/cookiesparser-1.2-py3-none-any.whl

---
+++ packages/cookiesparser/cookiesparser-1.2-py3-none-any.whl/cookiesparser/__init__.py
@@ -0,0 +1,14 @@
+from .cparser import (
+    parse,
+    encode_cookies
+)
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
