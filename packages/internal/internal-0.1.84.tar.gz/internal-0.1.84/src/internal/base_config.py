a8e1bccac2e9c20ecb7ac4690ec383f4c79e5405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 15:03:17 +0000

    Add package internal/0.1.84/internal-0.1.84.tar.gz

---
+++ packages/internal/internal-0.1.84.tar.gz/internal-0.1.84/src/internal/base_config.py
@@ -0,0 +1,46 @@
+from pydantic_settings import BaseSettings
+
+
+class BaseConfig(BaseSettings):
+    DEBUG: bool = False
+    RUN_PORT: int = 5000
+    TIME_ZONE: str = "Asia/Taipei"
+
+    # Request
+    REQUEST_VERIFY_SSL: bool = True
+    REQUEST_PROXY: str = ''
+    REQUEST_RETRY: int = 5
+    REQUEST_CONN_TIMEOUT: float = 5
+    REQUEST_READ_TIMEOUT: float = 5
+    REQUEST_WRITE_TIMEOUT: float = 5
+    REQUEST_POOL_TIMEOUT: float = 5
+
+    # AWS
+    AWS_ACCESS_KEY_ID: str = ""
+    AWS_SECRET_KEY: str = ""
+    AWS_REGION: str = ""
+    AWS_PARAMETER_PATH_PREFIX: str = ""
+    AWS_LOGGROUP_NAME: str = ""
+
+    # MongoDB
+    DATABASE_HOST: str = ''
+    DATABASE_USERNAME: str = ''
+    DATABASE_PASSWORD: str = ''
+    DATABASE_PORT: int = 27017
+    DATABASE_NAME: str = ""
+    DATABASE_SSL: bool = True
+    DATABASE_SSL_CA_CERTS: str = "/rds-combined-ca-bundle.pem"
+
+    # Micro Service
+    AUTH_BASE_URL: str = "http://auth-service-api"
+    ORGANIZATION_BASE_URL: str = "http://organization-service-api"
+    CUSTOMER_BASE_URL: str = "http://customer-service-api"
+    CAR_BASE_URL: str = "http://car-service-api"
+    RELATIONSHIP_MANAGEMENT_BASE_URL: str = "http://relationship-management-service-api"
+    TICKET_BASE_URL: str = "http://ticket-service-api"
+    NOTIFY_BASE_URL: str = "http://notify-service-api"
+    THIRD_PARTY_BASE_URL: str = "http://third-party-service-api"
+    SCHEDULER_BASE_URL: str = "http://scheduler-service-api"
+
+    class Config:
+        case_sensitive = False
