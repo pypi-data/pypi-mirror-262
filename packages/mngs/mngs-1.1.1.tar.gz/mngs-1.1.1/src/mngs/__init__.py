92a9bbab46d0a69ea9a456d4174b0cee066a544a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 14:03:35 +0000

    Add package mngs/1.1.1/mngs-1.1.1.tar.gz

---
+++ packages/mngs/mngs-1.1.1.tar.gz/mngs-1.1.1/src/mngs/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Time-stamp: "2024-03-14 12:00:09 (ywatanabe)"
+
+from . import dsp
+from . import general
+from . import general as gen
+from . import gists, io, linalg, ml, nn, os, plt, resource, stats
+from .general.debug import *
+
+__copyright__ = "Copyright (C) 2024 Yusuke Watanabe"
+__version__ = "1.1.1"
+__license__ = "GPL3.0"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywata1989@gmail.com"
+__url__ = "https://github.com/ywatanabe1989/mngs"
