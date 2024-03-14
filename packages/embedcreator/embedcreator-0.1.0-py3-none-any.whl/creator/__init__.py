0e47845fdda84f5e721f00305c9a4cfb27a59941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 06:03:59 +0000

    Add package embedcreator/0.1.0/embedcreator-0.1.0-py3-none-any.whl

---
+++ packages/embedcreator/embedcreator-0.1.0-py3-none-any.whl/creator/__init__.py
@@ -0,0 +1,6 @@
+from creator.ec_methods import *
+from creator.embed_creator import *
+from creator.modal_input import *
+from creator.minimal_help import *
+from creator.paginator import *
+__version__ = "0.1.0"
