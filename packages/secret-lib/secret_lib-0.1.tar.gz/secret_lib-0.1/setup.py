4915e8e4c668d9719d5e71bbef1dbcc7a91b44ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 16:03:28 +0000

    Add package secret-lib/0.1/secret_lib-0.1.tar.gz

---
+++ packages/secret-lib/secret_lib-0.1.tar.gz/secret_lib-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="secret_lib",
+    version="0.1",
+    author="vladimir gorea",
+    author_email="vladimir@smileservices.dev",
+    description="A wrapper for different configuration/secret sources.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/smileservices/secret_lib",
+    packages=find_packages(),
+    install_requires=[
+        "hvac",
+        "python-dotenv"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
