2cb028a867f50c6a6ddf5b99649ca66b863e9c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:04:00 +0000

    Add package latex-generator-tdv/0.1.1/latex_generator_tdv-0.1.1.tar.gz

---
+++ packages/latex-generator-tdv/latex_generator_tdv-0.1.1.tar.gz/latex_generator_tdv-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='latex_generator_tdv',
+      version='0.1.1',
+      description='A simple latex generator module',
+      license='MIT',
+      author='Daniil Adv Python',
+      packages=['latex_generator_tdv'],
+      zip_safe=False,
+)
+
+# pypi-AgEIcHlwaS5vcmcCJGM2ZjI1Y2MwLTQxYTktNDZmNi04M2U3LWYyZmJhNThmM2M5NQACKlszLCJiMjBjNDQ3YS0wZWU5LTQ5YjMtOWZmMi1iNzIwZTA0YjIyM2QiXQAABiAWPwFJMNb8_lF2_sWZhiXUCmzNRfJqXpom8ZTp9cdPMg
