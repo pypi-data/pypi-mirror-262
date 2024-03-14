1c7105abd66f28bf9d707ce5bc7a49f90a42e2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:03:25 +0000

    Add package sigmaepsilon.math/1.1.3b0/sigmaepsilon_math-1.1.3b0-py3-none-any.whl

---
+++ packages/sigmaepsilon.math/sigmaepsilon_math-1.1.3b0-py3-none-any.whl/sigmaepsilon/math/approx/__init__.py
@@ -0,0 +1,15 @@
+from .ls import moving_least_squares, least_squares, weighted_least_squares
+from .functions import CubicWeightFunction, ConstantWeightFunction, SingularWeightFunction
+from .lagrange import gen_Lagrange_1d, approx_Lagrange_1d
+
+
+__all__ = [
+    "moving_least_squares",
+    "least_squares",
+    "weighted_least_squares",
+    "CubicWeightFunction",
+    "ConstantWeightFunction",
+    "SingularWeightFunction",
+    "gen_Lagrange_1d",
+    "approx_Lagrange_1d",
+]
