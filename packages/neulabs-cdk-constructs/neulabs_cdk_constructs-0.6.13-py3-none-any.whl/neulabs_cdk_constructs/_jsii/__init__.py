f1323c99e804f6a6fddb583f0cd19773c35bc43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 03:02:54 +0000

    Add package neulabs-cdk-constructs/0.6.13/neulabs_cdk_constructs-0.6.13-py3-none-any.whl

---
+++ packages/neulabs-cdk-constructs/neulabs_cdk_constructs-0.6.13-py3-none-any.whl/neulabs_cdk_constructs/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.aws_apigatewayv2_alpha._jsii
+import aws_cdk.aws_apigatewayv2_integrations_alpha._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "neulabs-cdk-constructs",
+    "0.6.13",
+    __name__[0:-6],
+    "neulabs-cdk-constructs@0.6.13.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
