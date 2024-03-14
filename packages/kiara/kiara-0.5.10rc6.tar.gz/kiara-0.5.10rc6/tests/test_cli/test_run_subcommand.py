0acbfa34b3dc58016083cab3043e1c9ba96a437e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 04:02:44 +0000

    Add package kiara/0.5.10rc6/kiara-0.5.10rc6.tar.gz

---
+++ packages/kiara/kiara-0.5.10rc6.tar.gz/kiara-0.5.10rc6/tests/test_cli/test_run_subcommand.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from click.testing import CliRunner
+
+from kiara.interfaces.cli import cli
+
+#  Copyright (c) 2021, University of Luxembourg / DHARPA project
+#  Copyright (c) 2021, Markus Binsteiner
+#
+#  Mozilla Public License, version 2.0 (see LICENSE or https://www.mozilla.org/en-US/MPL/2.0/)
+
+
+def test_run_without_module():
+
+    runner = CliRunner()
+    result = runner.invoke(cli, "run")
+    assert result.exit_code == 2
+    assert "Missing argument" in result.stdout
+
+
+def test_run_without_args():
+
+    runner = CliRunner()
+    result = runner.invoke(cli, "run logic.and")
+    assert result.exit_code == 1
+    assert "invalid or insufficient input" in result.stdout
+    assert "not set" in result.stdout
+
+
+def test_run_with_missing_arg():
+
+    runner = CliRunner()
+    result = runner.invoke(cli, "run logic.and a=true")
+    assert result.exit_code == 1
+    assert "not set" in result.stdout
+    assert "valid" in result.stdout
+    assert "invalid or insufficient input" in result.stdout
+
+
+def test_run_with_valid_inputs():
+
+    runner = CliRunner()
+    result = runner.invoke(cli, "run logic.and a=true b=true")
+    assert result.exit_code == 0
+    assert "True" in result.stdout
+
+
+def test_run_with_save():
+
+    runner = CliRunner(env={"KIARA_CONTEXT": "_unit_tests_run"})
+    runner.invoke(cli, "context delete -f")
+    result = runner.invoke(cli, "run logic.and a=true b=true --save test_save")
+    assert result.exit_code == 0
+    assert "True" in result.stdout
+
+    result_data = runner.invoke(cli, "data list")
+    assert "test_save.y" in result_data.stdout
