a915709590abbd660d97b267f6d4c2dbd9b4d413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-14 05:03:34 +0000

    Add package azureml-dataprep/4.14.1/azureml_dataprep-4.14.1-py3-none-any.whl

---
+++ packages/azureml-dataprep/azureml_dataprep-4.14.1-py3-none-any.whl/azureml/dataprep/api/_dataflow_script_resolver.py
@@ -0,0 +1,10 @@
+from .engineapi.typedefinitions import GetScriptMessageArguments
+
+def resolve_dataflow(dataflow: 'azureml.dataprep.Dataflow', span_context: 'DPrepSpanContext' = None) -> str:
+    from .engineless_dataflow import EnginelessDataflow
+    if isinstance(dataflow, EnginelessDataflow):
+        return dataflow._to_yaml_string()
+    if dataflow._rs_dataflow_yaml is not None:
+        return dataflow._rs_dataflow_yaml
+    activity_data = dataflow._dataflow_to_anonymous_activity_data(dataflow)
+    return dataflow._engine_api.get_script(GetScriptMessageArguments(anonymous_activity=activity_data, span_context=span_context))
