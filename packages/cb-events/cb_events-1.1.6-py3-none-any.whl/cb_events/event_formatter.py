3c2ca6b8e26a42bac6595c77e2ebb6adcb26c0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:04:36 +0000

    Add package cb-events/1.1.6/cb_events-1.1.6-py3-none-any.whl

---
+++ packages/cb-events/cb_events-1.1.6-py3-none-any.whl/cb_events/event_formatter.py
@@ -0,0 +1,41 @@
+"""Format Chaturbate events as messages."""
+from __future__ import annotations
+
+from typing import Any
+
+
+class EventFormatter:
+    """Format Chaturbate events as messages."""
+
+    def __init__(self, event_id: str, method: str, obj: dict[str, Any]) -> None:
+        """Initialize the event formatter."""
+        self.id: str = event_id
+        self.method: str = method
+        self.object: dict[str, Any] = obj
+
+    def format_as_message(self) -> str:
+        """Format the event as a message."""
+        user = self.object.get("user", {}).get("username")
+        message = self.object.get("message", {}).get("message", "")
+        tokens = self.object.get("tip", {}).get("tokens", "")
+        is_anon = self.object.get("tip", {}).get("isAnon", False)
+        tip_message = self.object.get("tip", {}).get("message", "")[3:] if self.object.get("tip", {}).get("message", "").startswith(" | ") else ""  # noqa: E501
+
+        message_mappings: dict[str, str] = {
+            "broadcastStart": "Broadcast started",
+            "broadcastStop": "Broadcast stopped",
+            "userEnter": f"{user} entered the room",
+            "userLeave": f"{user} left the room",
+            "follow": f"{user} has followed",
+            "unfollow": f"{user} has unfollowed",
+            "fanclubJoin": f"{user} joined the fan club",
+            "chatMessage": f"{user} sent chat message: {message}",
+            "privateMessage": (f"{self.object.get('message', {}).get('fromUser', '')}",
+                f" sent private message to {self.object.get('message', {}).get('toUser', '')}:"
+                f" {self.object.get('message', {}).get('message', '')}"),
+            "tip": f"{user} sent {tokens} tokens {'anonymously ' if is_anon else ''}{tip_message}",
+            "roomSubjectChange": f"Room Subject changed to {self.object.get('subject', '')}",
+            "mediaPurchase": (f"{user} purchased {self.object.get('media', {}).get('type', '')}",
+                f" set: {self.object.get('media', {}).get('name', '')}"),
+        }
+        return message_mappings.get(self.method, "Unknown event")
---
+++ packages/cb-events/cb_events-1.1.6-py3-none-any.whl/cb_events/poller.py
@@ -0,0 +1,131 @@
+"""Poller for Chaturbate API.
+
+This module provides the following classes and functions:
+
+- CBAPIPoller: Poller for the Chaturbate API.
+- Event: Represents a Chaturbate event.
+- EventFormatter: Formats Chaturbate events as messages.
+- log_events: Logs events.
+"""
+from __future__ import annotations
+
+import asyncio
+import logging
+from typing import Any, Callable
+
+import aiohttp
+from aiolimiter import AsyncLimiter
+from typing_extensions import Self
+
+from cb_events.models import Event
+from cb_events.event_formatter import EventFormatter
+from cb_events.exceptions import BaseURLError, ChaturbateAPIError
+
+SERVER_ERROR = (501, 502, 521)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s: %(message)s",
+    datefmt="%d %b %Y %H:%M:%S",
+)
+
+class CBAPIPoller:
+    """Poller for Chaturbate API."""
+
+    def __init__(
+        self,
+        url: str | None = None,
+        rate_limit: int = 2000,
+        session: aiohttp.ClientSession | None = None,
+    ) -> None:
+        """Initialize the poller."""
+        self.url: str | None = url
+        self.rate_limit: int = rate_limit
+        self.session: aiohttp.ClientSession = session or aiohttp.ClientSession()
+        self.event_callback: Callable[[Any], None] | None = None
+
+    async def __aenter__(self) -> Self:
+        """Enter the poller context."""
+        return self
+
+    async def __aexit__(self, *args: object) -> None:
+        """Exit the poller context."""
+        await self.close()
+
+    async def close(self) -> None:
+        """Close the session."""
+        if self.session:
+            await self.session.close()
+
+    async def poll_cb_api(self) -> None:
+        """Poll the Chaturbate API."""
+        limiter = AsyncLimiter(self.rate_limit)
+        backoff_delay: int = 1  # Initial backoff delay
+        if not self.url:
+            msg = "Please set the BASE_URL environment variable with the correct URL."
+            raise BaseURLError(msg)
+
+        logging.debug("Polling Chaturbate API at %s", self.url)
+        try:
+            while True:
+                async with limiter, self.session.get(self.url) as response:
+                    await self.handle_response(response, backoff_delay)
+                    backoff_delay = 1  # Reset backoff on success
+        except aiohttp.ClientError as e:
+            logging.exception("An error occurred during API polling")
+            raise ChaturbateAPIError from e
+
+    async def handle_response(
+        self, response: aiohttp.ClientResponse, backoff_delay: int,
+    ) -> None:
+        """Handle the response."""
+        if response.status == aiohttp.http.HTTPStatus.OK:
+            await self.handle_successful_response(response)
+        elif response.status in SERVER_ERROR:
+            await self.handle_server_error(response, backoff_delay)
+        else:
+            response.raise_for_status()
+
+    async def handle_successful_response(
+        self, response: aiohttp.ClientResponse,
+    ) -> None:
+        """Handle successful response."""
+        json_response: dict[str, Any] = await response.json()
+        for message in json_response.get("events", []):
+            event: Any = Event.from_dict(message)
+            await self.process_event(event)
+
+        # Use nextUrl from response for the next request
+        if "nextUrl" in json_response:
+            self.url = json_response["nextUrl"]
+
+    async def handle_server_error(
+        self, response: aiohttp.ClientResponse, backoff_delay: int,
+    ) -> None:
+        """Handle server errors."""
+        backoff_delay *= 2
+        logging.warning(
+            "Server error %s, retrying in %s seconds", response.status, backoff_delay,
+        )
+        await asyncio.sleep(backoff_delay)
+
+    async def process_event(self, event: Event) -> None:
+        """Process the event."""
+        if self.event_callback:
+            await self.event_callback(event)
+        else:
+            await log_events(event)
+
+    async def fetch_events(
+        self, event_callback: Callable[[Any], None] | None = None,
+    ) -> None:
+        """Fetch events from the Chaturbate API."""
+        self.event_callback = event_callback
+        await self.poll_cb_api()
+
+
+async def log_events(event: Event) -> None:
+    """Log events."""
+    formatter: EventFormatter = EventFormatter(event.id, event.method, event.object)
+    formatted_message = formatter.format_as_message()
+    logging.info(formatted_message)
