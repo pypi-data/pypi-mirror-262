8ef5f0cdbd40bf2d01d950b962b1da280544ce8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 15:04:28 +0000

    Add package quickapiclient/0.0.4/quickapiclient-0.0.4.tar.gz

---
+++ packages/quickapiclient/quickapiclient-0.0.4.tar.gz/quickapiclient-0.0.4/quickapi/__init__.py
@@ -0,0 +1,11 @@
+from .client import (  # noqa: F401
+    BaseApi,
+    BaseApiMethod,
+    BaseRequestBody,
+    BaseRequestParams,
+    BaseResponseBody,
+    ClientSetupError,
+    HTTPError,
+    ResponseSerializationError,
+)
+from .exceptions import QuickApiException  # noqa: F401
