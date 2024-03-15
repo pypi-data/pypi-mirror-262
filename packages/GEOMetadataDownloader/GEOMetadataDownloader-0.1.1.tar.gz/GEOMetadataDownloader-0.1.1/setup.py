d199e7c60e8770f8c5ed782fa21f3fe375b9e871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:30 +0000

    Add package GEOMetadataDownloader/0.1.1/GEOMetadataDownloader-0.1.1.tar.gz

---
+++ packages/GEOMetadataDownloader/GEOMetadataDownloader-0.1.1.tar.gz/GEOMetadataDownloader-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GEOMetadataDownloader',
+    version='0.1.1',
+    packages=find_packages(),
+    license='MIT',
+    description='GEOMetadataDownloader is a Python package designed to effortlessly fetch and organize metadata information from GEO datasets. Seamlessly download and categorize metadata based on types such as RNA, SRA, and more into convenient spreadsheet formats.',
+    author='Edris Sharif Rahmani',
+    author_email='rahmani.biotech@gmail.com',
+    url='https://github.com/sharifrahmanie/GEOMetadataDownloader',
+    download_url='https://github.com/sharifrahmanie/GEOMetadataDownloader/archive/refs/tags/0.1.tar.gz',
+    keywords=['Gene Expression Omnibus', 'GEO', 'Metadata', 'GSE series', 'GEO sample information'],
+    install_requires=[
+        'GEOparse',
+        'tqdm',
+        'pandas'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
+
