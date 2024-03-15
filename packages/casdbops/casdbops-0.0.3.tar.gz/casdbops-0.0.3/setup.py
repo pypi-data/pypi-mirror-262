282b6f383f1ebd5df7ba0e18a4dbdb2f22f7d4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 05:05:56 +0000

    Add package casdbops/0.0.3/casdbops-0.0.3.tar.gz

---
+++ packages/casdbops/casdbops-0.0.3.tar.gz/casdbops-0.0.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path):
+    requirements=[]
+    print(os.listdir(os.getcwd()),"\n",os.listdir(os.path.dirname(os.getcwd())))
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+__version__ = "0.0.3"
+REPO_NAME = "cassandraconnectorpkg"
+PKG_NAME= "casdbops"
+AUTHOR_USER_NAME = "pranav-c01"
+AUTHOR_EMAIL = "pranavc430@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={"Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",},
+    packages=find_packages(),
+    install_requires = ["astrapy==0.2.0","pandas","numpy","ensure","pytest==7.1.3"]
+    )
+
+
+
