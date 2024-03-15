b8d7d900f139d6533d6dfe8c4ef4ea7a0add6d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:05:01 +0000

    Add package ScopeFoundry/1.4.0/ScopeFoundry-1.4.0.tar.gz

---
+++ packages/ScopeFoundry/ScopeFoundry-1.4.0.tar.gz/ScopeFoundry-1.4.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+setup(
+    name = 'ScopeFoundry',
+    
+    version = '1.4.0',
+    
+    description = 'a platform for laboratory equipment control and scientific data analysis',
+    long_description =open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    
+    # Author details
+    author='Edward S. Barnard',
+    author_email='esbarnard@lbl.gov',
+
+    # Choose your license
+    license='BSD',
+    
+    url='http://www.scopefoundry.org/',
+
+    package_dir={'ScopeFoundry': '.'},
+    
+    packages=['ScopeFoundry', 'ScopeFoundry.scanning','ScopeFoundry.examples','ScopeFoundry.graphics', 'ScopeFoundry.data_browser' ],
+    
+    #packages=find_packages('.', exclude=['contrib', 'docs', 'tests']),
+    #include_package_data=True,  
+    
+    package_data={
+        '':["*.ui", "*.icns", '*.png', '*.svg'], # include QT ui files and logo and icons
+        },
+        
+    install_requires = [
+    	'numpy', 'h5py', 'qtpy', 'xreload', 'uuid7'],
+    
+    extras_require={
+        'all' : ['qtconsole', 'pyqtdarktheme'],
+    }
+
+    )
