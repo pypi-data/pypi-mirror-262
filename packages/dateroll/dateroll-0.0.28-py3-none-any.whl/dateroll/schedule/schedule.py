fc68a13018e5830badfeec538bc6598dbcac887a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:04:23 +0000

    Add package dateroll/0.0.28/dateroll-0.0.28-py3-none-any.whl

---
+++ packages/dateroll/dateroll-0.0.28-py3-none-any.whl/dateroll/schedule/schedule.py
@@ -0,0 +1,47 @@
+
+import code
+
+class Schedule: 
+    def __init__(self,start,stop,step,debug=False):
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.debug = debug
+        if self.step >=0:
+            self.direction = 'forward'
+        else:
+            self.direction = 'backward'
+
+    @property
+    def dates(self):
+        '''
+            gives the date range
+            direction of date generations is given by the sign of step
+        '''
+        dates = []
+
+        # backward generation
+        if self.direction == 'backward':
+            cursor = self.start
+            while cursor > self.stop:
+                dates.append(cursor)
+                # we use plus sign because step<0
+                cursor += self.step
+            dates.append(self.start)
+        else:
+            # foward generation
+            cursor = self.start
+            while cursor < self.stop:
+                dates.append(cursor)
+                cursor += self.step
+            dates.append(self.stop)
+
+        return sorted(dates)
+
+# default stub is FRONT short
+# old stuff we didn't add back:
+#         stub="short",
+#         ret="l",
+#         monthEndRule="anniv",
+#         ie="[)",
+#         dc=NotImplementedError,
