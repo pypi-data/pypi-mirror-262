d652f3216d386e852c4b7b4452c4ed591f38047a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 03:04:22 +0000

    Add package magic-class/0.7.10/magic_class-0.7.10.tar.gz

---
+++ packages/magic-class/magic_class-0.7.10.tar.gz/magic_class-0.7.10/magicclass/utils/__init__.py
@@ -0,0 +1,29 @@
+from ._functions import (
+    iter_members,
+    Tooltips,
+    get_signature,
+    argcount,
+    get_level,
+    is_instance_method,
+    method_as_getter,
+    eval_attribute,
+    copy_info,
+    show_tree,
+    rst_to_html,
+    is_type_like,
+)
+
+from .qt import (
+    MessageBoxMode,
+    show_messagebox,
+    to_clipboard,
+    open_url,
+    screen_center,
+    move_to_screen_center,
+    screen_scale,
+)
+
+from ._click import click
+from ._recent import call_recent_menu
+from .qtsignal import QtSignal
+from .qthreading import thread_worker, Timer, Callback
