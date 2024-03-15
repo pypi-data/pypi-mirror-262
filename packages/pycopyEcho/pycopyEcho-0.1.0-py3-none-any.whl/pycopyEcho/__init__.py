0fb13e549bb7259dbc33ae689888de229048f021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:03:50 +0000

    Add package pycopyEcho/0.1.0/pycopyEcho-0.1.0-py3-none-any.whl

---
+++ packages/pycopyEcho/pycopyEcho-0.1.0-py3-none-any.whl/pycopyEcho/__init__.py
@@ -0,0 +1,1 @@
+from .copy import copy_dir_file
---
+++ packages/pycopyEcho/pycopyEcho-0.1.0-py3-none-any.whl/pycopyEcho/copy.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+def copy_dir_file(src_dir, dst_dir):
+    """
+    复制src_dir目录下的所有内容到dst_dir目录
+    :param src_dir: 源文件目录
+    :param dst_dir: 目标目录
+    :return:
+    """
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    if os.path.exists(src_dir):
+        for file in os.listdir(src_dir):
+            file_path = os.path.join(src_dir, file)
+            dst_path = os.path.join(dst_dir, file)
+            if os.path.isfile(os.path.join(src_dir, file)):
+                shutil.copy2(file_path, dst_path)
+            else:
+                copy_dir_file(file_path, dst_path)
