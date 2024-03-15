16cb002aefe3f4c8644569f62b745b2282c676cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:03:51 +0000

    Add package local-augment/0.0.1/local-augment-0.0.1.tar.gz

---
+++ packages/local-augment/local-augment-0.0.1.tar.gz/local-augment-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="local-augment",
+    version="0.0.1",
+    license="MIT license",
+    author="TayDream",
+    author_email="19859860010@163.com",
+    description="A package for local data augmentation in deep learning",
+    url="https://github.com/dream-oyh/LocalAugment",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
