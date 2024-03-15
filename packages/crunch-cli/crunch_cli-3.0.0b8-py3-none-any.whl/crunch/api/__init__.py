302ecb824368eca2db301a1c0c8481d557cf4f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:32 +0000

    Add package crunch-cli/3.0.0b8/crunch_cli-3.0.0b8-py3-none-any.whl

---
+++ packages/crunch-cli/crunch_cli-3.0.0b8-py3-none-any.whl/crunch/api/__init__.py
@@ -0,0 +1,18 @@
+from .domain import *
+from .client import Client
+from . import auth
+
+from .errors import (
+    ApiException,
+    InternalServerException,
+    ValidationFailedException,
+    CheckException,
+    CompetitionNameNotFoundException,
+    CrunchNotFoundException,
+    DailySubmissionLimitExceededException,
+    ForbiddenLibraryException,
+    InvalidProjectTokenException,
+    NeverSubmittedException,
+    ProjectNotFoundException,
+    RoundNotFoundException,
+)
