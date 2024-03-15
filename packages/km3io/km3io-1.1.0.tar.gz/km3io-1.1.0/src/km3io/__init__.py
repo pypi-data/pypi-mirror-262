3b7cfbc5d6b91f5b141e200cb3fc5295a9ce91e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:14 +0000

    Add package km3io/1.1.0/km3io-1.1.0.tar.gz

---
+++ packages/km3io/km3io-1.1.0.tar.gz/km3io-1.1.0/src/km3io/__init__.py
@@ -0,0 +1,26 @@
+try:
+    from importlib.metadata import version as get_version
+
+    version = get_version(__name__)
+except ImportError:
+    from pkg_resources import get_distribution
+
+    version = get_distribution(__name__).version
+
+
+import warnings
+import os
+
+# Getting rid of OpenMP warnings, related to Numba
+# See: https://github.com/numba/numba/issues/5275
+# This needs to be done before import numpy
+os.environ["KMP_WARNINGS"] = "off"
+
+with warnings.catch_warnings():
+    for warning_category in (FutureWarning, DeprecationWarning):
+        warnings.simplefilter("ignore", category=warning_category)
+    import uproot3
+
+from .offline import OfflineReader
+from .online import OnlineReader
+from .acoustics import RawAcousticReader
