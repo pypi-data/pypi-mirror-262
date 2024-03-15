8c5bb97fd695b37d1595624bac59d088c1c1aaf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 07:04:12 +0000

    Add package cfn-lint/1.2.5a5/cfn_lint-1.2.5a5-py3-none-any.whl

---
+++ packages/cfn-lint/cfn_lint-1.2.5a5-py3-none-any.whl/cfnlint/transform.py
@@ -0,0 +1,7 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+# ruff: noqa: F401
+from cfnlint.template.transforms._sam import Transform
