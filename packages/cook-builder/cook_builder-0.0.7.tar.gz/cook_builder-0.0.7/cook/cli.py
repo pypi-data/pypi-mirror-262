411a7d445630a8df1aacebe0a2ad9ed3fadcd0b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:04:59 +0000

    Add package cook-builder/0.0.7/cook_builder-0.0.7.tar.gz

---
+++ packages/cook-builder/cook_builder-0.0.7.tar.gz/cook_builder-0.0.7/cook/cli.py
@@ -0,0 +1,31 @@
+import argparse
+import pathlib
+
+from .cook import Cook
+from .receipe import Receipe
+from .configuration import Configuration
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('receipe_path', nargs='?', default='.')
+    parser.add_argument('-p', '--project')
+    parser.add_argument('-s', '--build_server')
+
+    args = parser.parse_args()
+
+    base_path = pathlib.Path.cwd() / args.receipe_path
+
+    receipe = Receipe(base_path)
+    receipe.load()
+
+    configuration = Configuration(receipe)
+    configuration.setup(args.project, args.build_server)
+
+    cook = Cook(receipe, configuration)
+    cook.cook()
+
+
+if __name__ == '__main__':
+    main()
