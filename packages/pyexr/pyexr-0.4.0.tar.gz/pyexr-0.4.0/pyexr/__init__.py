fed80ad40d38c0ff3e9be34eec88b27446874ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:35 +0000

    Add package pyexr/0.4.0/pyexr-0.4.0.tar.gz

---
+++ packages/pyexr/pyexr-0.4.0.tar.gz/pyexr-0.4.0/pyexr/__init__.py
@@ -0,0 +1,38 @@
+# Don't manually change, let poetry-dynamic-versioning handle it
+__version__ = "0.4.0"
+
+__all__ = [
+    "ExrError",
+    "FLOAT",
+    "HALF",
+    "NO_COMPRESSION",
+    "PIZ_COMPRESSION",
+    "PXR24_COMPRESSION",
+    "RLE_COMPRESSION",
+    "UINT",
+    "ZIPS_COMPRESSION",
+    "ZIP_COMPRESSION",
+    "open",
+    "read",
+    "read_all",
+    "tonemap",
+    "write",
+]
+
+from pyexr.exr import (
+    ExrError,
+    FLOAT,
+    HALF,
+    NO_COMPRESSION,
+    PIZ_COMPRESSION,
+    PXR24_COMPRESSION,
+    RLE_COMPRESSION,
+    UINT,
+    ZIPS_COMPRESSION,
+    ZIP_COMPRESSION,
+    open,
+    read,
+    read_all,
+    tonemap,
+    write,
+)
