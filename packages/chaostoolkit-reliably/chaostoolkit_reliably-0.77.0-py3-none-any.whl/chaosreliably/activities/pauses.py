982eec2e43c9552e6fd087bbbd5875f54f18d3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 03:04:16 +0000

    Add package chaostoolkit-reliably/0.77.0/chaostoolkit_reliably-0.77.0-py3-none-any.whl

---
+++ packages/chaostoolkit-reliably/chaostoolkit_reliably-0.77.0-py3-none-any.whl/chaosreliably/activities/pauses.py
@@ -0,0 +1,35 @@
+import threading
+
+from logzero import logger
+
+__all__ = ["pause_execution"]
+
+CURRENT_PAUSE = threading.Event()
+
+
+def pause_execution(
+    duration: int = 0,
+    username: str = "",
+    user_id: str = "",
+) -> None:
+    """
+    Pause the execution of the experiment until the resume state has been
+    received.
+    """
+    m = "Pausing activity"
+    if duration:
+        m = f"{m} for {duration}s or"
+    m = f"{m} until the execution is resumed"
+    logger.info(m)
+
+    if CURRENT_PAUSE.wait(duration or None) is False:
+        logger.info("Resuming execution...")
+
+
+###############################################################################
+# Private functions
+###############################################################################
+def reset() -> None:
+    global CURRENT_PAUSE
+    CURRENT_PAUSE.set()
+    CURRENT_PAUSE.clear()
