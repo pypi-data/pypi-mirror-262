b79d0dfe979ec079e57f95851a564263e340cf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 05:04:21 +0000

    Add package azure-functions-extension-blob/1.0.0a1/azure-functions-extension-blob-1.0.0a1.tar.gz

---
+++ packages/azure-functions-extension-blob/azure-functions-extension-blob-1.0.0a1.tar.gz/azure-functions-extension-blob-1.0.0a1/setup.py
@@ -0,0 +1,26 @@
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='azure-functions-extension-blob',
+    version='1.0.0a1',
+    author='Azure Functions team at Microsoft Corp.',
+    author_email='azurefunctions@microsoft.com',
+    description='Blob Python worker extension for Azure Functions.',
+    packages=find_packages(exclude=[
+        'azure.functions.extension', 'azure.functions',
+        'azure', 'tests', 'samples'
+    ]),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'azure-functions-extension-base',
+        'azure-storage-blob==12.19.0'
+    ]
+)
