a13172fb810634f6774f1971327a387f1e10e04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:03:54 +0000

    Add package dbnd-examples/1.0.14.3/dbnd_examples-1.0.14.3-py2.py3-none-any.whl

---
+++ packages/dbnd-examples/dbnd_examples-1.0.14.3-py2.py3-none-any.whl/dbnd_examples/tests/orchestration/tool_sklearn/test_sklearn_linear_reg_pipeline.py
@@ -0,0 +1,16 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+import logging
+
+from dbnd.testing.helpers_pytest import assert_run_task
+from dbnd_examples.orchestration.tool_sklearn.sklearn_example import linear_reg_pipeline
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def test_sklearn_tracking():
+    task = assert_run_task(linear_reg_pipeline.task())
+    assert task is not None
+    assert task.result is not None
