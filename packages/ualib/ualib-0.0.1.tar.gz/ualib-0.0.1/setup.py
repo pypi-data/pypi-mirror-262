e8071b70b724baff6e55d864ea43572f97e47a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:59 +0000

    Add package ualib/0.0.1/ualib-0.0.1.tar.gz

---
+++ packages/ualib/ualib-0.0.1.tar.gz/ualib-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Place Holder'
+
+# Setting up
+setup(
+    name="ualib",
+    version=VERSION,
+    author="Loftea",
+    description=DESCRIPTION,
+    packages=find_packages(),
+)
