d6972efffbe13003d832bbd2b1fb326057eb7dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 14:03:35 +0000

    Add package sleipnirgroup-jormungandr/0.0.1.dev54/sleipnirgroup_jormungandr-0.0.1.dev54-cp310-cp310-win_amd64.whl

---
+++ packages/sleipnirgroup-jormungandr/sleipnirgroup_jormungandr-0.0.1.dev54-cp310-cp310-win_amd64.whl/jormungandr/cpp/Main.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) Sleipnir contributors
+
+#include <pybind11/pybind11.h>
+
+#include "autodiff/BindExpressionType.hpp"
+#include "autodiff/BindVariable.hpp"
+#include "autodiff/BindVariableMatrices.hpp"
+#include "optimization/BindConstraints.hpp"
+#include "optimization/BindOptimizationProblem.hpp"
+#include "optimization/BindSolverExitCondition.hpp"
+#include "optimization/BindSolverIterationInfo.hpp"
+#include "optimization/BindSolverStatus.hpp"
+
+namespace py = pybind11;
+
+namespace sleipnir {
+
+PYBIND11_MODULE(_jormungandr, m) {
+  m.doc() =
+      "A linearity-exploiting sparse nonlinear constrained optimization "
+      "problem solver that uses the interior-point method.";
+
+  py::module_ autodiff = m.def_submodule("autodiff");
+  py::module_ optimization = m.def_submodule("optimization");
+
+  BindExpressionType(autodiff);
+  BindConstraints(optimization);
+  BindVariable(autodiff);
+  BindVariableMatrices(autodiff);
+
+  BindSolverExitCondition(optimization);
+  BindSolverIterationInfo(optimization);
+  BindSolverStatus(optimization);
+  BindOptimizationProblem(optimization);
+}
+
+}  // namespace sleipnir
