27656e6a53992a23ff9554410090459cc4222628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:31 +0000

    Add package lusid-configuration-sdk/2.0.15/lusid_configuration_sdk-2.0.15-py3-none-any.whl

---
+++ packages/lusid-configuration-sdk/lusid_configuration_sdk-2.0.15-py3-none-any.whl/lusid_configuration/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from lusid_configuration.api.application_metadata_api import ApplicationMetadataApi
+from lusid_configuration.api.configuration_sets_api import ConfigurationSetsApi
+
