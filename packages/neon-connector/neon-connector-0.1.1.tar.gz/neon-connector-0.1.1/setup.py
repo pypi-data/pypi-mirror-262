349a8366011c3e21709050366be5c89583af56f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 15:04:58 +0000

    Add package neon-connector/0.1.1/neon-connector-0.1.1.tar.gz

---
+++ packages/neon-connector/neon-connector-0.1.1.tar.gz/neon-connector-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neon-connector',
+    version='0.1.1',
+    author='Timotius Marselo',
+    author_email='timotiusmarselo@gmail.com',
+    description='Connect to Neon DB and perform operations through Python.',
+    url='https://github.com/supertypeai/neon_connector',
+    packages=find_packages(),
+    install_requires=[
+        # List of dependencies required by your package
+        'python-dotenv==1.0.0',
+        'psycopg2==2.9.9',
+        'pandas==2.2.1',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires = ">=3.6"
+)
