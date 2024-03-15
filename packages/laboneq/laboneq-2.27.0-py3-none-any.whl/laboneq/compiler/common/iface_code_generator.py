f387e72bbceeb664e916d0b2dc8a3a561ed95d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 05:05:26 +0000

    Add package laboneq/2.27.0/laboneq-2.27.0-py3-none-any.whl

---
+++ packages/laboneq/laboneq-2.27.0-py3-none-any.whl/laboneq/compiler/common/iface_code_generator.py
@@ -0,0 +1,30 @@
+# Copyright 2022 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import abc
+from abc import abstractmethod
+
+from laboneq.compiler.common.compiler_settings import CompilerSettings
+from laboneq.compiler.common.iface_compiler_output import RTCompilerOutput
+from laboneq.compiler.common.signal_obj import SignalObj
+
+
+class ICodeGenerator(abc.ABC):
+    @abstractmethod
+    def __init__(
+        self,
+        ir,
+        signals: list[SignalObj],
+        settings: CompilerSettings | dict | None = None,
+    ):
+        ...
+
+    @abstractmethod
+    def generate_code(self):
+        ...
+
+    @abstractmethod
+    def get_output(self) -> RTCompilerOutput:
+        ...
