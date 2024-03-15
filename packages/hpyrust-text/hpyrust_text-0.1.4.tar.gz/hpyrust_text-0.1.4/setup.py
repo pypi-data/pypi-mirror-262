8df964ee3cee662138fe7aaf32addda4e9444987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:04:26 +0000

    Add package hpyrust-text/0.1.4/hpyrust_text-0.1.4.tar.gz

---
+++ packages/hpyrust-text/hpyrust_text-0.1.4.tar.gz/hpyrust_text-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="hpyrust_text",
+    version="0.1.4",
+    rust_extensions=[RustExtension("hpyrust_text.hpyrust_text", binding=Binding.PyO3, path="Cargo.toml")],
+    packages=["hpyrust_text"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    zip_safe=False,
+    python_requires=">=3.7",
+)
