b14756540284518c9834e54209caf3b10133a69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 03:03:48 +0000

    Add package patosyadir/0.0.1/patosyadir-0.0.1.tar.gz

---
+++ packages/patosyadir/patosyadir-0.0.1.tar.gz/patosyadir-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='patosyadir', 
+    version='0.0.1',  # Версия пакета
+    author='Skidanov EA',
+    author_email='your@domain.com',
+    description='patosya package',
+    packages=setuptools.find_packages(),
+)
