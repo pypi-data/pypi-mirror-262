b4ca68c2e79d5268c5f7939ce67c376839f6f041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 15:04:40 +0000

    Add package aimsgb/1.1.1/aimsgb-1.1.1.tar.gz

---
+++ packages/aimsgb/aimsgb-1.1.1.tar.gz/aimsgb-1.1.1/README.rst
@@ -0,0 +1,81 @@
+Introduction
+============
+aimsgb is an efficient and open-source Python library for generating atomic coordinates in periodic grain boundary models. It is designed to
+construct various grain boundary structures from cubic and non-cubic initial
+configurations. A convenient command line tool has also been provided to enable
+easy and fast construction of tilt and twist boundaries by assigining the degree
+of fit (Σ), rotation axis, grain boundary plane and initial crystal structure.
+
+We also provide a web-based GUI to access aimsgb framework: `aimsgb.org
+<https://aimsgb.org/>`_
+
+Install aimsgb
+==============
+Method 1: Use Pip
+-----------------
+The easiest way to install aimsgb is to simply run a one-liner in pip::
+
+   pip install aimsgb
+
+Method 2: Use Git to install
+----------------------------
+1. Clone the latest version from github::
+
+    git clone https://github.com/ksyang2013/aimsgb.git
+
+2. Navigate to aimsgb folder::
+
+    cd aimsgb
+
+3. Type in the root of the repo::
+
+    pip install .
+
+4. or to install the package in development mode::
+
+    pip install -e .
+
+
+Usage
+==================
+Refer to the `documentation
+<https://aimsgb-docs.readthedocs.io/en/latest/>`_ for more details.
+
+How to cite aimsgb
+==================
+
+If you use aimsgb in your research, please consider citing the following work:
+
+    Jianli Cheng, Jian Luo, Kesong Yang. *Aimsgb: An algorithm and open-source python
+    library to generate periodic grain boundary structures.* Computational Materials
+    Science, 2018, 155, 92-103. `doi:10.1016/j.commatsci.2018.08.029
+    <https://doi.org/10.1016/j.commatsci.2018.08.029>`_
+
+
+Copyright
+=========
+Copyright (C) 2018 The Regents of the University of California
+
+All Rights Reserved. Permission to copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice, this paragraph and the following three paragraphs appear in all copies. Permission to make commercial use of this software may be obtained by contacting:
+
+Office of Innovation and Commercialization
+9500 Gilman Drive, Mail Code 0910
+University of California
+La Jolla, CA 92093-0910
+(858) 534-5815
+innovation@ucsd.edu
+
+This software program and documentation are copyrighted by The Regents of the University of California. The software program and documentation are supplied “as is”, without any accompanying services from The Regents. The Regents does not warrant that the operation of the program will be uninterrupted or error-free. The end-user understands that the program was developed for research purposes and is advised not to rely exclusively on the program for any reason.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN “AS IS” BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+Authors
+=======
+Dr. Jianli Cheng (chengjianli90@gmail.com)
+
+Prof. Kesong Yang  (kesong@ucsd.edu)
+
+About the aimsgb Development Team
+=================================
+http://materials.ucsd.edu/
---
+++ packages/aimsgb/aimsgb-1.1.1.tar.gz/aimsgb-1.1.1/aimsgb/agb.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+from __future__ import division
+
+import sys
+import argparse
+import numpy as np
+
+from aimsgb import Grain, GBInformation, GrainBoundary
+
+__author__ = "Jianli Cheng and Kesong YANG"
+__copyright__ = "Copyright (C) 2018 The Regents of the University of California"
+__maintainer__ = "Jianli Cheng"
+__email__ = "jic198@ucsd.edu"
+__status__ = "Production"
+__date__ = "January 26, 2018"
+aimsgb_descript = """**************************************************************************************************
+       aimsgb - Ab-initio Interface Materials Simulation for Grain Boundaries (aimsgb.org)      
+                        VERSION 0.1  2017 - 2018                                    
+Aimsgb is one open-source python library to generate periodic grain boundary structures.    
+A reference for the usage of aimsgb software is:
+Jianli Cheng, Jian Luo, and Kesong Yang, Aimsgb: An Algorithm and Open-Source Python Library  
+to Generate Periodic Grain Boundary Structures, Comput. Mater. Sci. 155, 92-103, (2018). 
+**************************************************************************************************
+
+Aimsgb Command Line Tools 
+"""
+
+
+def gb_list(args):
+    axis = list(map(int, args.axis))
+    sigma = args.sigma
+    print(GBInformation(axis, sigma).__str__())
+
+
+def gb(args):
+    axis = list(map(int, args.axis))
+    plane = args.plane
+    initial_struct = None
+    if args.filename:
+        initial_struct = Grain.from_file(args.filename)
+    elif args.mpid:
+        initial_struct = Grain.from_mp_id(args.mpid)
+    if initial_struct is None:
+        raise ValueError("Please provide either filename or mpid.")
+    
+    gb = GrainBoundary(axis, args.sigma, plane, initial_struct, args.uc_a, args.uc_b)
+    to_primitive = False if args.conventional else True
+    structure = Grain.stack_grains(gb.grain_a, gb.grain_b, direction=gb.direction,
+                                  to_primitive=to_primitive)
+    structure.to(filename=args.out, fmt=args.fmt)
+    print(f"CSL Matrix (det={np.linalg.det(gb.csl)}):\n{gb.csl}")
+    print(f"{args.out} of sigma{gb.sigma}[{args.axis}]/({gb.plane_str}) is created")
+
+
+def main():
+    parser = argparse.ArgumentParser(description=aimsgb_descript,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    subparsers = parser.add_subparsers(help="command", dest="command")
+
+    parser_gb_list = subparsers.add_parser(
+        "list", help="Show the values of sigma, theta, GB plane and CSL matrix "
+        "from a given rotation axis\nEXAMPLE: aimsgb list 001",
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser_gb_list.add_argument("axis", metavar="rotation axis", type=str,
+                                help="The rotation axis of GB\n"
+                                "EXAMPLE: aimsgb list 001")
+    parser_gb_list.add_argument("sigma", default=30, type=int, nargs="?",
+                                help="Set the sigma limit for on screen output "
+                                     "(default: %(default)s)\n"
+                                     "EXAMPLE: aimsgb list 001 100")
+    parser_gb_list.set_defaults(func=gb_list)
+
+    parser_gb = subparsers.add_parser(
+        "gb", help="Build grain boundary based on rotation axis, sigma, GB plane, "
+                   "and input structure file.\nThe user can also specify many "
+                   "optional arguments, such grain size and interface terminations."
+                   "\nEXAMPLE1: aimsgb gb 001 5 1 2 0 -f POSCAR"
+                   "\nEXAMPLE2: aimsgb gb 001 5 1 2 0 -mp mp-13 -ua 2 -ub 3"
+                   "\nEXAMPLE3: aimsgb gb 001 5 1 2 0 -f POSCAR -ua 3 -ub 2 -dl 0b1t1b0t",
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser_gb.add_argument("axis", metavar="rotation axis", type=str,
+                           help="The rotation axis of GB, EXAMPLE: 110")
+    parser_gb.add_argument("sigma", type=int,
+                           help="The sigma value for grain boundary, EXAMPLE: 3")
+    parser_gb.add_argument("plane", type=int, nargs=3,
+                           help="The GB plane for grain boundary, EXAMPLE: 1 1 0")
+    parser_gb.add_argument("-f", "--filename", type=str,
+                           help="The initial structure file for grain boundary.")
+    parser_gb.add_argument("-mp", "--mpid", type=str,
+                           help="The mpid to get initial structure from Materials Project.")
+    parser_gb.add_argument("-o", "--out", type=str, default="POSCAR", 
+                           help="The output filename. (default: %(default)s)")
+    parser_gb.add_argument("-ua", "--uc_a", default=1, type=int,
+                           help="The size (uc) for grain A. (default: %(default)s)"
+                                "\nEXAMPLE: aimsgb gb 001 5 1 2 0 -f POSCAR -ua 2")
+    parser_gb.add_argument("-ub", "--uc_b", default=1, type=int,
+                           help="The size (uc) for grain B. (default: %(default)s)"
+                                "\nEXAMPLE: aimsgb gb 001 5 1 2 0 -f POSCAR -ub 2")
+    parser_gb.add_argument("-dl", "--delete_layer", default="0b0t0b0t", type=str,
+                           help="Delete bottom or top layers for each grain. "
+                                "(default: %(default)s)"
+                                "\nEXAMPLE: aimsgb gb 001 5 1 2 0 -f POSCAR -dl 0b1t1b0t")
+    parser_gb.add_argument("-v", "--vacuum", default=0.0, type=float,
+                           help="Set vacuum thickness for grain boundary. "
+                                "(default: %(default)s angstrom)"
+                                "\nEXAMPLE: aimsgb gb 001 5 1 2 0 -f POSCAR -v 20")
+    parser_gb.add_argument("-t", "--tol", default=0.25, type=float,
+                           help="Tolerance factor to determine if two "
+                                "atoms are at the same plane. "
+                                "(default: %(default)s angstrom)"
+                                "\nEXAMPLE: aimsgb gb 001 5 1 2 0 -f POSCAR -dl 0b1t1b0t -t 0.5")
+    parser_gb.add_argument("-ad", "--add_if_dist", default=0.0, type=float,
+                           help="Add extra distance between two grains. "
+                                "(default: %(default)s angstrom)"
+                                "\nEXAMPLE: aimsgb gb 001 5 1 2 0 -f POSCAR -ad 0.5")
+    parser_gb.add_argument("-c", "--conventional", action="store_true",
+                           help="Get conventional GB, not primitive"
+                                "\nEXAMPLE: aimsgb gb 001 5 1 2 0 -f POSCAR -c")
+    parser_gb.add_argument("-fmt", "--fmt", default="poscar", const="poscar",
+                           nargs="?", choices=["poscar", "cif", "cssr", "json"],
+                           help="Choose the output format. (default: %(default)s)"
+                                "\nEXAMPLE: aimsgb gb 001 5 1 2 0 -f POSCAR -fmt cif")
+    parser_gb.set_defaults(func=gb)
+
+    args = parser.parse_args()
+
+    try:
+        getattr(args, "func")
+    except AttributeError:
+        parser.print_help()
+        sys.exit(0)
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
