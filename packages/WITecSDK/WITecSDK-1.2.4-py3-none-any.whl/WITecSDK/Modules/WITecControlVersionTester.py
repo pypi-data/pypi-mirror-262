ee0a0a8ca2c6f9bda652830b592917c2098b6415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 05:04:41 +0000

    Add package WITecSDK/1.2.4/WITecSDK-1.2.4-py3-none-any.whl

---
+++ packages/WITecSDK/WITecSDK-1.2.4-py3-none-any.whl/WITecSDK/Modules/WITecControlVersionTester.py
@@ -0,0 +1,16 @@
+﻿from WITecSDK.Parameters import COMParameters
+
+class WITecControlVersionTester:
+
+    def __init__(self, aCOMParameters: COMParameters):
+        self._programVersionCOM = aCOMParameters.GetStringParameter("Status|Software|Application|ProgramVersion")
+        self.Version: float = float(self.StringVersion.split(",")[1].strip())
+        # Create IsVersionGreater... attributes
+        versions: list[float,...] = [5.1, 5.2, 5.3, 6.0, 6.1, 6.2, 6.3]
+        isversions: list[bool,...] = [(str(int(ver * 10)), self.Version >= ver) for ver in versions]
+        for verstr, isver in isversions:
+            setattr(self, 'IsVersionGreater' + verstr, isver)
+    
+    @property
+    def StringVersion(self) -> str:
+        return self._programVersionCOM.GetValue()
