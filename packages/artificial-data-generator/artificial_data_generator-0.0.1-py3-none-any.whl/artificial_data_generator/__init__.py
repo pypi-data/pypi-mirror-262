bf7a16dd75521998d475936ad18c73ad10fd9b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:03:59 +0000

    Add package artificial-data-generator/0.0.1/artificial_data_generator-0.0.1-py3-none-any.whl

---
+++ packages/artificial-data-generator/artificial_data_generator-0.0.1-py3-none-any.whl/artificial_data_generator/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2022 Sigrun May,
+# Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+# Copyright (c) 2022 Sigrun May,
+# Ostfalia Hochschule für angewandte Wissenschaften
+#
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+"""Data generator main package."""
+
+__version__ = "0.0.1"
