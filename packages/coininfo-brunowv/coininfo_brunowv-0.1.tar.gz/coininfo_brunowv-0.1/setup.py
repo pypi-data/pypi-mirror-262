654a0731b7eb30804eb335abec8778720fc6f7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:04:10 +0000

    Add package coininfo-brunowv/0.1/coininfo_brunowv-0.1.tar.gz

---
+++ packages/coininfo-brunowv/coininfo_brunowv-0.1.tar.gz/coininfo_brunowv-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="coininfo_brunowv",  # replace with coininfo_Brunowv
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["requests==2.31.0"],  # matches your package name
+)
