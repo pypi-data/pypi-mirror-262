c476a82970885e4a1bd36f827bdf6b1a5644487f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 03:04:29 +0000

    Add package unika-content-gen/0.1/unika_content_gen-0.1.tar.gz

---
+++ packages/unika-content-gen/unika_content_gen-0.1.tar.gz/unika_content_gen-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unika_content_gen',
+    version='0.1',
+    packages=find_packages(),
+    description='A package for generating content',
+    author='Unika Infocom',
+    author_email='kamal.unikainfocom@gmail.com',
+    url='https://github.com/Kamalhossain24',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
