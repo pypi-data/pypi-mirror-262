51da272bff7c0338eebbc9324f854e702a401997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 05:06:11 +0000

    Add package RPI-control-center/0.2.3.0/RPI-control-center-0.2.3.0.tar.gz

---
+++ packages/RPI-control-center/RPI-control-center-0.2.3.0.tar.gz/RPI-control-center-0.2.3.0/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RPI-control-center",
+    keywords = 'Raspberry Pi, Raspi, Python, GPIO, USB, Mass storage, API, non-blocking',
+    version="0.2.3.0",
+    author="Mohamed Debbagh",
+    author_email="moha7108@protonmail.com",
+    description="""This package provides additional suite of python based rpi abstraction for handling rpi hardware control.
+                    The package currently includes an abstraction layer and API engine for the RPi.GPIO package for python, which allows for multi-process and non-blocking control of GPIO pins.
+                    The package also includes a module for handling usb mass storage device mounting, data dumping, and unmounting,and other data handling. Finally the Package also includes a module for common sensors.
+                    """,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moha7108/RPi_control_center",
+    project_urls={
+        "Bug Tracker": "https://github.com/moha7108/RPi_control_center/issues",
+        "Github": "https://github.com/moha7108/RPi_control_center"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 3 - Alpha",
+    ],
+    license='GNU GPLv3',
+    packages=['rpi_control_center','rpi_sensor_monitors','rpi_sensor_monitors.gravity'],
+    python_requires=">=3.6",
+    install_requires=[
+          'logzero',
+          'RPi.GPIO',
+          'pigpio',
+          'smbus',
+          'spidev'
+      ]
+)
