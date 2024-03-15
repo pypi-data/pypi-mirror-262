9994dc96a93a1f30d82491df939181db290c47f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:05:04 +0000

    Add package openxps/0.0.0/openxps-0.0.0-py3-none-any.whl

---
+++ packages/openxps/openxps-0.0.0-py3-none-any.whl/openxps/__init__.py
@@ -0,0 +1,4 @@
+"""
+Extended Phase-Space Methods with OpenMM
+"""
+from ._version import __version__  # noqa: F401
---
+++ packages/openxps/openxps-0.0.0-py3-none-any.whl/openxps/openxps.py
@@ -0,0 +1,8 @@
+"""
+.. module:: openxps
+   :platform: Linux, Windows, macOS
+   :synopsis: Extended Phase-Space Simulations with OpenMM
+
+.. moduleauthor:: Charlles Abreu <craabreu@gmail.com>
+
+"""
