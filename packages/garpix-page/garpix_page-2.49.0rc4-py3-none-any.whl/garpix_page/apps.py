58af75b4f7f1812824bcce554b158a293c8170bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 03:04:05 +0000

    Add package garpix-page/2.49.0rc4/garpix_page-2.49.0rc4-py3-none-any.whl

---
+++ packages/garpix-page/garpix_page-2.49.0rc4-py3-none-any.whl/garpix_page/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class GarpixPageConfig(AppConfig):
+    name = 'garpix_page'
+    verbose_name = _('Pages')
+
+    def ready(self):
+        import garpix_page.signals  # noqa
