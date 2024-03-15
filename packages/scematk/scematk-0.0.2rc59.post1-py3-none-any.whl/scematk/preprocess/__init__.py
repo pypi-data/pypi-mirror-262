4cfb7bc02d285578f74a0d31be6ac32c7adcc891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:37 +0000

    Add package scematk/0.0.2rc59.post1/scematk-0.0.2rc59.post1-py3-none-any.whl

---
+++ packages/scematk/scematk-0.0.2rc59.post1-py3-none-any.whl/scematk/preprocess/__init__.py
@@ -0,0 +1,19 @@
+from ._colour import (
+    RGBToGrey,
+    RGBToGray,
+    GreyToOD,
+    GrayToOD,
+    RGBToOD
+)
+from ._preprocessor import Preprocessor
+from ._process import Process
+
+__all__ = [
+    "Preprocessor",
+    "Process",
+    "RGBToGrey",
+    "RGBToGray",
+    "GreyToOD",
+    "GrayToOD",
+    "RGBToOD"
+]
