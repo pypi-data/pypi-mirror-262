d524fcd27f2567dc54015c28a6861571dbe405bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 14:03:57 +0000

    Add package stripe/8.8.0b1/stripe-8.8.0b1-py2.py3-none-any.whl

---
+++ packages/stripe/stripe-8.8.0b1-py2.py3-none-any.whl/stripe/_api_version.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# File generated from our OpenAPI spec
+class _ApiVersion:
+    CURRENT = "2023-10-16"
+    PREVIEW = "2024-02-15.preview-v2"
---
+++ packages/stripe/stripe-8.8.0b1-py2.py3-none-any.whl/stripe/entitlements/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# File generated from our OpenAPI spec
+from stripe.entitlements._feature import Feature as Feature
+from stripe.entitlements._feature_service import (
+    FeatureService as FeatureService,
+)
