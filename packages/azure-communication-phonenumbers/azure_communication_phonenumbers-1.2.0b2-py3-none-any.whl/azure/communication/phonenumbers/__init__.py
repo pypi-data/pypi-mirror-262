509b8264cb99f7612256fa001069736e8fdb678f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 15:04:58 +0000

    Add package azure-communication-phonenumbers/1.2.0b2/azure_communication_phonenumbers-1.2.0b2-py3-none-any.whl

---
+++ packages/azure-communication-phonenumbers/azure_communication_phonenumbers-1.2.0b2-py3-none-any.whl/azure/communication/phonenumbers/__init__.py
@@ -0,0 +1,42 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+from ._phone_numbers_client import PhoneNumbersClient
+
+from ._generated.models import (
+    PurchasedPhoneNumber,
+    PhoneNumberCapabilities,
+    PhoneNumberCost,
+    PhoneNumberSearchResult,
+    BillingFrequency,
+    PhoneNumberAssignmentType,
+    PhoneNumberCapabilityType,
+    PhoneNumberType,
+    PhoneNumberAreaCode,
+    PhoneNumberAdministrativeDivision,
+    PhoneNumberCountry,
+    PhoneNumberLocality,
+    PhoneNumberOffering,
+    OperatorInformationResult,
+)
+
+__all__ = [
+    'PurchasedPhoneNumber',
+    'PhoneNumberCapabilities',
+    'PhoneNumberCost',
+    'PhoneNumberSearchResult',
+    'BillingFrequency',
+    'PhoneNumberAssignmentType',
+    'PhoneNumberCapabilityType',
+    'PhoneNumberType',
+    'PhoneNumberAreaCode',
+    'PhoneNumberAdministrativeDivision',
+    'PhoneNumberCountry',
+    'PhoneNumberLocality',
+    'PhoneNumberOffering',
+    'OperatorInformationResult',
+    'PhoneNumbersClient'
+]
