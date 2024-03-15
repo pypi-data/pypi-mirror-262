387d24ccae469193146ae783a773ebb997455f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 15:04:25 +0000

    Add package garpix-admin-lock/1.3.1/garpix_admin_lock-1.3.1.tar.gz

---
+++ packages/garpix-admin-lock/garpix_admin_lock-1.3.1.tar.gz/garpix_admin_lock-1.3.1/garpix_admin_lock/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.join(path.abspath(path.dirname(__file__)), 'garpix_admin_lock')
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='garpix_admin_lock',
+    version='1.3.1',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/garpixcms/garpix_admin_lock',
+    author='Garpix LTD',
+    author_email='info@garpix.com',
+    license='MIT',
+    packages=find_packages(exclude=['testproject', 'testproject.*']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3.8',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Django >= 1.11, < 5',
+    ]
+)
