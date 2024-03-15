27bdb109bb83dcc8d1d5a32783fbd6eff9ffc982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 15:04:57 +0000

    Add package python-genshin-artifact/1.0.7/python_genshin_artifact-1.0.7.tar.gz

---
+++ packages/python-genshin-artifact/python_genshin_artifact-1.0.7.tar.gz/python_genshin_artifact-1.0.7/genshin_artifact/mona_core/src/character/characters/dendro/mod.rs
@@ -0,0 +1,17 @@
+pub mod collei;
+pub mod tighnari;
+pub mod nahida;
+pub mod yaoyao;
+pub mod alhaitham;
+pub mod kaveh;
+pub mod baizhu;
+pub mod kirara;
+
+pub use collei::Collei;
+pub use tighnari::Tighnari;
+pub use nahida::Nahida;
+pub use yaoyao::Yaoyao;
+pub use alhaitham::Alhaitham;
+pub use kaveh::Kaveh;
+pub use baizhu::Baizhu;
+pub use kirara::Kirara;
---
+++ packages/python-genshin-artifact/python_genshin_artifact-1.0.7.tar.gz/python_genshin_artifact-1.0.7/genshin_artifact/mona_core/src/common/skill_type.rs
@@ -0,0 +1,21 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+pub enum SkillType {
+    NormalAttack,
+    ChargedAttack,
+    PlungingAttackInAction,
+    PlungingAttackOnGround,
+    ElementalSkill,
+    ElementalBurst,
+}
+
+impl SkillType {
+    pub fn is_plunging(&self) -> bool {
+        match *self {
+            SkillType::PlungingAttackInAction | SkillType::PlungingAttackOnGround => true,
+            _ => false
+        }
+    }
+}
