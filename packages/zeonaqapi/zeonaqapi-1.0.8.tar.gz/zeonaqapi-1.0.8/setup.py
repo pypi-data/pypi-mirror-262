fb5fcf994aaf34648e97cc4257ae58f275a56328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:27 +0000

    Add package zeonaqapi/1.0.8/zeonaqapi-1.0.8.tar.gz

---
+++ packages/zeonaqapi/zeonaqapi-1.0.8.tar.gz/zeonaqapi-1.0.8/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zeonaqapi',
+    version='1.0.8',
+    packages=find_packages(),
+    install_requires=[
+        # Add your project's dependencies here
+        'requests', 'pandas', 'monsterapi', 'openai'
+    ],
+    author='Craig Carmichael',
+    author_email='operations@darkhorsestech.com',
+    description='An API client library for the ZeonAQ API.',
+    license='MIT',
+    url='https://github.com/darkhorsestech/zeonaqapi',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
