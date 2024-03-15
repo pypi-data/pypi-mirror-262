93cbd547a64aa54a3d658760f333669d0d965550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 05:06:02 +0000

    Add package jinja-render/0.1.7/jinja_render-0.1.7.tar.gz

---
+++ packages/jinja-render/jinja_render-0.1.7.tar.gz/jinja_render-0.1.7/jinja_render/get_info_top.py
@@ -0,0 +1,27 @@
+import os
+
+
+def get_repo_and_hash(branch="HEAD"):
+    hash = get_hash_from_branch(branch)
+    name = get_repo_name()
+    return {"repo": name, "hash": hash}
+
+
+def get_hash_from_branch(branch):
+    command = "git config --global --add safe.directory /workdir"
+    run_command(command)
+    stream = os.popen(f"git rev-parse {branch}")
+    output = stream.read()
+    return output.split("\n")[0]
+
+
+def get_repo_name():
+    command = "git config --global --add safe.directory /workdir"
+    run_command(command)
+    stream = os.popen("basename $(git remote get-url origin) | cut -d '.' -f 1")
+    return stream.read().strip()
+
+
+def run_command(command):
+    exit_code = os.system(command)
+    assert exit_code == 0
