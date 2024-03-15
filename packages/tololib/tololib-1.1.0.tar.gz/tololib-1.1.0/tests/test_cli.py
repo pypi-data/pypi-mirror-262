ce17ca59c8c670ea7bd7377bec47c84615203197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 03:04:28 +0000

    Add package tololib/1.1.0/tololib-1.1.0.tar.gz

---
+++ packages/tololib/tololib-1.1.0.tar.gz/tololib-1.1.0/tests/test_cli.py
@@ -0,0 +1,102 @@
+from argparse import ArgumentParser, Namespace
+from unittest import TestCase, mock
+
+from tests import TEST_PORT
+from tests.templates import ServerCommunicationTestCase
+from tololib.cli.common import Command
+from tololib.cli.main import main
+
+
+class CliDataStructureTest(TestCase):
+    def test_command(self) -> None:
+        command = Command(ArgumentParser())
+        self.assertRaises(NotImplementedError, command, Namespace())
+
+
+class CliInteractionTest(ServerCommunicationTestCase):
+    def test_discover(self) -> None:
+        with mock.patch(
+            "argparse.ArgumentParser.parse_args",
+            return_value=Namespace(
+                log_level="INFO",
+                command="discover",
+                broadcast_address="127.0.0.1",
+                port=TEST_PORT,
+                retry_count=1,
+                retry_timeout=1,
+            ),
+        ):
+            result = main()
+            self.assertEqual(0, result)
+
+        with mock.patch(
+            "argparse.ArgumentParser.parse_args",
+            return_value=Namespace(
+                log_level="INFO",
+                command="discover",
+                broadcast_address="127.0.0.1",
+                port=TEST_PORT + 1,
+                retry_count=1,
+                retry_timeout=1,
+            ),
+        ):
+            result = main()
+            self.assertEqual(1, result)
+
+    def test_get_status(self) -> None:
+        with mock.patch(
+            "argparse.ArgumentParser.parse_args",
+            return_value=Namespace(
+                log_level="INFO",
+                command="get-status",
+                address="127.0.0.1",
+                port=TEST_PORT,
+                retry_count=1,
+                retry_timeout=1,
+            ),
+        ):
+            result = main()
+            self.assertEqual(0, result)
+
+        with mock.patch(
+            "argparse.ArgumentParser.parse_args",
+            return_value=Namespace(
+                log_level="INFO",
+                command="get-status",
+                address="127.0.0.1",
+                port=TEST_PORT + 1,
+                retry_count=1,
+                retry_timeout=1,
+            ),
+        ):
+            result = main()
+            self.assertEqual(1, result)
+
+    def test_get_settings(self) -> None:
+        with mock.patch(
+            "argparse.ArgumentParser.parse_args",
+            return_value=Namespace(
+                log_level="INFO",
+                command="get-settings",
+                address="127.0.0.1",
+                port=TEST_PORT,
+                retry_count=1,
+                retry_timeout=1,
+            ),
+        ):
+            result = main()
+            self.assertEqual(0, result)
+
+        with mock.patch(
+            "argparse.ArgumentParser.parse_args",
+            return_value=Namespace(
+                log_level="INFO",
+                command="get-settings",
+                address="127.0.0.1",
+                port=TEST_PORT + 1,
+                retry_count=1,
+                retry_timeout=1,
+            ),
+        ):
+            result = main()
+            self.assertEqual(1, result)
