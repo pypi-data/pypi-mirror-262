b829d0ed3a6507c3725dcc4363f0659efc93d8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:04:00 +0000

    Add package dbnd-examples/1.0.14.3/dbnd-examples-1.0.14.3.tar.gz

---
+++ packages/dbnd-examples/dbnd-examples-1.0.14.3.tar.gz/dbnd-examples-1.0.14.3/src/dbnd_examples/tests/orchestration/examples/test_salad.py
@@ -0,0 +1,14 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+import logging
+
+from dbnd.testing.helpers_pytest import assert_run_task
+from dbnd_examples.orchestration.examples.salad import prepare_salad
+
+
+logger = logging.getLogger(__name__)
+
+
+class TestTutorialSyntax(object):
+    def test_prepare_salad(self):
+        assert_run_task(prepare_salad.task())
