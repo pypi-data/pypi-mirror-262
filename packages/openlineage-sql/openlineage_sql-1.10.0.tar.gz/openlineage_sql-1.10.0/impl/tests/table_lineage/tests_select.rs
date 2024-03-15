35f0e576f90220abbb72600892ae0ffa8b9f25a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:04:39 +0000

    Add package openlineage-sql/1.10.0/openlineage_sql-1.10.0.tar.gz

---
+++ packages/openlineage-sql/openlineage_sql-1.10.0.tar.gz/openlineage_sql-1.10.0/impl/tests/table_lineage/tests_select.rs
@@ -0,0 +1,150 @@
+// Copyright 2018-2023 contributors to the OpenLineage project
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::test_utils::*;
+use openlineage_sql::{parse_sql, BigQueryDialect, TableLineage};
+
+#[test]
+fn select_simple() {
+    assert_eq!(
+        test_sql("SELECT * FROM table0;",).unwrap().table_lineage,
+        TableLineage {
+            in_tables: tables(vec!["table0"]),
+            out_tables: vec![]
+        }
+    )
+}
+#[test]
+fn select_from_schema_table() {
+    assert_eq!(
+        test_sql("SELECT * FROM schema0.table0;",)
+            .unwrap()
+            .table_lineage,
+        TableLineage {
+            in_tables: tables(vec!["schema0.table0"]),
+            out_tables: vec![]
+        }
+    )
+}
+#[test]
+fn select_join() {
+    assert_eq!(
+        test_sql(
+            "
+                SELECT col0, col1, col2
+                FROM table0
+                JOIN table1
+                ON t1.col0 = t2.col0",
+        )
+        .unwrap()
+        .table_lineage,
+        TableLineage {
+            in_tables: tables(vec!["table0", "table1"]),
+            out_tables: vec![]
+        }
+    )
+}
+
+#[test]
+fn select_inner_join() {
+    assert_eq!(
+        test_sql(
+            "
+                SELECT col0, col1, col2
+                FROM table0
+                INNER JOIN table1
+                ON t1.col0 = t2.col0",
+        )
+        .unwrap()
+        .table_lineage,
+        TableLineage {
+            in_tables: tables(vec!["table0", "table1"]),
+            out_tables: vec![]
+        }
+    )
+}
+
+#[test]
+fn select_left_join() {
+    assert_eq!(
+        test_sql(
+            "
+            SELECT col0, col1, col2
+            FROM table0
+            LEFT JOIN table1
+            ON t1.col0 = t2.col0",
+        )
+        .unwrap()
+        .table_lineage,
+        TableLineage {
+            in_tables: tables(vec!["table0", "table1"]),
+            out_tables: vec![]
+        }
+    )
+}
+
+#[test]
+fn select_bigquery_excaping() {
+    assert_eq!(
+        parse_sql(
+            "
+            SELECT *
+            FROM `random-project`.`dbt_test1`.`source_table`
+            WHERE id = 1
+            ",
+            &BigQueryDialect,
+            None
+        )
+        .unwrap()
+        .table_lineage,
+        TableLineage {
+            in_tables: tables(vec!["`random-project`.`dbt_test1`.`source_table`"]),
+            out_tables: vec![]
+        }
+    )
+}
+
+#[test]
+fn select_into() {
+    assert_eq!(
+        test_sql("SELECT * INTO table0 FROM table1;",)
+            .unwrap()
+            .table_lineage,
+        TableLineage {
+            in_tables: tables(vec!["table1"]),
+            out_tables: tables(vec!["table0"])
+        }
+    )
+}
+
+#[test]
+fn select_redshift() {
+    assert_eq!(
+        test_sql_dialect("SELECT [col1] FROM [test_schema].[test_table]", "redshift")
+            .unwrap()
+            .table_lineage,
+        TableLineage {
+            in_tables: tables(vec!["[test_schema].[test_table]"]),
+            out_tables: vec![]
+        }
+    )
+}
+
+#[test]
+fn select_with_table_generator() {
+    assert_eq!(
+        test_sql(
+            "
+            SELECT row_number() OVER (ORDER BY col) row_num, d
+            FROM TABLE(GENERATOR(ROWCOUNT => (12 * 6)))
+            JOIN test_schema.test_table ON test_table.d = row_number()
+            "
+        )
+        .unwrap()
+        .table_lineage,
+        TableLineage {
+            in_tables: tables(vec!["test_schema.test_table"]),
+            out_tables: vec![]
+        }
+    )
+}
