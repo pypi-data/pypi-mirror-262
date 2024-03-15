84cca0758c676382cb5df610abc546bbf8ae0488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:30 +0000

    Add package pyrovelocity/0.2.0b11/pyrovelocity-0.2.0b11-py3-none-any.whl

---
+++ packages/pyrovelocity/pyrovelocity-0.2.0b11-py3-none-any.whl/pyrovelocity/__init__.py
@@ -0,0 +1,10 @@
+"""pyrovelocity."""
+
+from importlib import metadata
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+del metadata
