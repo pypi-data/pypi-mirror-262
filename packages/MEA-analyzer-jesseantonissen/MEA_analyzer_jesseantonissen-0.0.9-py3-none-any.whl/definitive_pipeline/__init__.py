22f6425fa78b47af79b95f8fc65c364e527e8ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:24 +0000

    Add package MEA-analyzer-jesseantonissen/0.0.9/MEA_analyzer_jesseantonissen-0.0.9-py3-none-any.whl

---
+++ packages/MEA-analyzer-jesseantonissen/MEA_analyzer_jesseantonissen-0.0.9-py3-none-any.whl/definitive_pipeline/__init__.py
@@ -0,0 +1,5 @@
+import MEA_analyzer_jesseantonissen
+from MEA_analyzer_jesseantonissen.get_nice_message import get_nice_message
+
+# The init file can be left empty or you could provide a nice message as well as some variables describing your Library
+Message = "Have fun with all our functions by analyzing your MEA files!"
