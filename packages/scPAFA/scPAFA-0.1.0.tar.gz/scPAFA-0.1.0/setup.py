1ba2ac3e07fa0980c480aed137e85111e8fe4f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:22 +0000

    Add package scPAFA/0.1.0/scPAFA-0.1.0.tar.gz

---
+++ packages/scPAFA/scPAFA-0.1.0.tar.gz/scPAFA-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scPAFA',
+    version='0.1.0',
+    author='Zhuoli Huang',
+    description='Single Cell Pathway Activity Factor Analysis',
+    packages=find_packages(),
+    install_requires=[
+        'scanpy>=1.9.0',
+        'anndata>=0.9.0',
+        'mofax == 0.3.6',
+        'pandas>=2.0.3',
+        'scipy == 1.11.3',
+        'plotnine',
+        'statsmodels',
+        'pathos',
+        'tqdm',
+        'nheatmap',
+        'seaborn'
+    ]
+)
