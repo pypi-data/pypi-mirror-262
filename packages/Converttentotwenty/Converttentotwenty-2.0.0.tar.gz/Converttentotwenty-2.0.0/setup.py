6729851590d62fa2c64600538457fb759a4598b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:20 +0000

    Add package Converttentotwenty/2.0.0/Converttentotwenty-2.0.0.tar.gz

---
+++ packages/Converttentotwenty/Converttentotwenty-2.0.0.tar.gz/Converttentotwenty-2.0.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 13:34:20 2024
+
+@author: Joris
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Converttentotwenty",
+    version="2.0.0",
+    description="Bibliotèque python conversion base 20",
+    long_description=open("README.md").read(),
+    long_description_content_type="text",
+    url="https://github.com/Joris-ROBIN/Converttentotwenty.git",
+    author="Skai",
+    author_email="jorisrobin34@gmail.com",
+    license="MEEF",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
