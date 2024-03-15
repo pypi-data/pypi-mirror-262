afea68163bf8a9263884c98850d3362461fcbf10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 03:03:57 +0000

    Add package django-tabbed-admin-for-garpixcms/1.1.1/django_tabbed_admin_for_garpixcms-1.1.1-py3-none-any.whl

---
+++ packages/django-tabbed-admin-for-garpixcms/django_tabbed_admin_for_garpixcms-1.1.1-py3-none-any.whl/tabbed_admin/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'tabbed_admin.apps.TabbedAdminConfig'
---
+++ packages/django-tabbed-admin-for-garpixcms/django_tabbed_admin_for_garpixcms-1.1.1-py3-none-any.whl/tabbed_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TabbedAdminConfig(AppConfig):
+    name = 'tabbed_admin'
+    verbose_name = 'TabbedAdmin'
