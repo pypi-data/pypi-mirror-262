1c5ec27ef5b82cf2e91acaf74f5bf83309923aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:04:30 +0000

    Add package Eeron/1.0.0/Eeron-1.0.0.tar.gz

---
+++ packages/Eeron/Eeron-1.0.0.tar.gz/Eeron-1.0.0/README.md
@@ -0,0 +1,1 @@
+New org style for opps
---
+++ packages/Eeron/Eeron-1.0.0.tar.gz/Eeron-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Eeron',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/Cyrp48/Eeron/tree/maste',
+    license='Copyright [2024] [cypr]',
+    author='cypr',
+    author_email='cyprcyrp@gmail.com',
+    description='New organaizer module'
+)
