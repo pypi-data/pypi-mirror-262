f21f3e433e33c2ff77b0220f4e2184ff094b3f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:04:22 +0000

    Add package hammadpy/2.6.1/hammadpy-2.6.1-py3-none-any.whl

---
+++ packages/hammadpy/hammadpy-2.6.1-py3-none-any.whl/hammadpy/ml/__init__.py
@@ -0,0 +1,6 @@
+from .cross_encoder import CrossEncode
+from .loss import Loss
+from .semantic_search import SemanticSearch
+from .sentence_embedder import SentenceBERT
+from .sentence_similarity import SentenceSimilarity
+from .instruct import Instruct
