32a672de1122447b3da792f6fd38188330186178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:04:21 +0000

    Add package scrapegraphai/0.0.9/scrapegraphai-0.0.9.tar.gz

---
+++ packages/scrapegraphai/scrapegraphai-0.0.9.tar.gz/scrapegraphai-0.0.9/scrapegraphai/graphs/speech_summary_graph.py
@@ -0,0 +1,122 @@
+""" 
+Module for extracting the summary from the speach
+"""
+from scrapegraphai.utils.save_audio_from_bytes import save_audio_from_bytes
+from ..models import OpenAI, OpenAITextToSpeech
+from .base_graph import BaseGraph
+from ..nodes import (
+    FetchHTMLNode,
+    ParseNode,
+    RAGNode,
+    GenerateAnswerNodeFromRag,
+    TextToSpeechNode,
+)
+
+
+class SpeechSummaryGraph:
+    """
+    SpeechSummaryGraph is a tool that automates the process of extracting and summarizing
+    information from web pages, then converting that summary into spoken word via an MP3 file.
+
+    Attributes:
+        url (str): The URL of the web page to scrape and summarize.
+        llm_config (dict): Configuration parameters for the language model, 
+        with 'api_key' mandatory.
+        summary_prompt (str): The prompt used to guide the summarization process.
+        output_path (Path): The path where the generated MP3 file will be saved.
+
+    Methods:
+        run(): Executes the web scraping, summarization, and text-to-speech process.
+
+    Args:
+        url (str): The URL of the web page to scrape and summarize.
+        llm_config (dict): A dictionary containing configuration options for the language model.
+        summary_prompt (str): The prompt used to guide the summarization process.
+        output_path (str): The file path where the generated MP3 should be saved.
+    """
+
+    def __init__(self, prompt: str, url: str, llm_config: dict,
+                 output_path: str = "website_summary.mp3"):
+        """
+        Initializes the SmartScraper with a prompt, URL, and language model configuration.
+        """
+        self.prompt = f"{prompt} - Save the summary in a key called 'summary'."
+        self.url = url
+        self.llm_config = llm_config
+        self.llm = self._create_llm()
+        self.output_path = output_path
+        self.text_to_speech_model = OpenAITextToSpeech(
+            llm_config, model="tts-1", voice="alloy")
+        self.graph = self._create_graph()
+
+    def _create_llm(self):
+        """
+        Creates an instance of the ChatOpenAI class with the provided language model configuration.
+
+        Returns:
+            ChatOpenAI: An instance of the ChatOpenAI class.
+
+        Raises:
+            ValueError: If 'api_key' is not provided in llm_config.
+        """
+        llm_defaults = {
+            "model_name": "gpt-3.5-turbo",
+            "temperature": 0,
+            "streaming": True
+        }
+        # Update defaults with any LLM parameters that were provided
+        llm_params = {**llm_defaults, **self.llm_config}
+        # Ensure the api_key is set, raise an error if it's not
+        if "api_key" not in llm_params:
+            raise ValueError("LLM configuration must include an 'api_key'.")
+        # Create the ChatOpenAI instance with the provided and default parameters
+        return OpenAI(llm_params)
+
+    def _create_graph(self):
+        """
+        Creates the graph of nodes representing the workflow for web scraping.
+
+        Returns:
+            BaseGraph: An instance of the BaseGraph class.
+        """
+        fetch_html_node = FetchHTMLNode("fetch_html")
+        parse_document_node = ParseNode(doc_type="html", chunks_size=4000, node_name="parse_document")
+        rag_node = RAGNode(self.llm, "rag")
+        generate_answer_node = GenerateAnswerNodeFromRag(self.llm, "generate_answer")
+        text_to_speech_node = TextToSpeechNode(
+            self.text_to_speech_model, "text_to_speech")
+
+        return BaseGraph(
+            nodes={
+                fetch_html_node,
+                parse_document_node,
+                rag_node,
+                generate_answer_node,
+                text_to_speech_node
+            },
+            edges={
+                (fetch_html_node, parse_document_node),
+                (parse_document_node, rag_node),
+                (rag_node, generate_answer_node),
+                (generate_answer_node, text_to_speech_node)
+            },
+            entry_point=fetch_html_node
+        )
+
+    def run(self) -> str:
+        """
+        Executes the scraping process by running the graph and returns the extracted information.
+
+        Returns:
+            str: The answer extracted from the web page, corresponding to the given prompt.
+        """
+        inputs = {"user_input": self.prompt, "url": self.url}
+        final_state = self.graph.execute(inputs)
+
+        audio = final_state.get("audio", None)
+        if not audio:
+            raise ValueError("No audio generated from the text.")
+        save_audio_from_bytes(audio, self.output_path)
+        print(f"Audio saved to {self.output_path}")
+
+        return final_state
---
+++ packages/scrapegraphai/scrapegraphai-0.0.9.tar.gz/scrapegraphai-0.0.9/scrapegraphai/nodes/generate_answer_node_from_rag.py
@@ -0,0 +1,137 @@
+"""
+Module for generating the answer node
+"""
+# Imports from standard library
+from tqdm import tqdm
+
+# Imports from Langchain
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.runnables import RunnableParallel
+
+# Imports from the library
+from .base_node import BaseNode
+
+
+class GenerateAnswerNodeFromRag(BaseNode):
+    """
+    A node that generates an answer using a language model (LLM) based on the user's input
+    and the content extracted from a webpage. It constructs a prompt from the user's input
+    and the scraped content, feeds it to the LLM, and parses the LLM's response to produce
+    an answer.
+
+    Attributes:
+        llm (ChatOpenAI): An instance of a language model client, configured for generating answers.
+        node_name (str): The unique identifier name for the node, defaulting 
+        to "GenerateAnswerNode".
+        node_type (str): The type of the node, set to "node" indicating a 
+        standard operational node.
+
+    Args:
+        llm: An instance of the language model client (e.g., ChatOpenAI) used 
+        for generating answers.
+        node_name (str, optional): The unique identifier name for the node. 
+        Defaults to "GenerateAnswerNode".
+
+    Methods:
+        execute(state): Processes the input and document from the state to generate an answer,
+                        updating the state with the generated answer under the 'answer' key.
+    """
+
+    def __init__(self, llm, node_name: str):
+        """
+        Initializes the GenerateAnswerNode with a language model client and a node name.
+        Args:
+            llm (OpenAIImageToText): An instance of the OpenAIImageToText class.
+            node_name (str): name of the node
+        """
+        super().__init__(node_name, "node")
+        self.llm = llm
+
+    def execute(self, state: dict) -> dict:
+        """
+        Generates an answer by constructing a prompt from the user's input and the scraped
+        content, querying the language model, and parsing its response.
+
+        The method updates the state with the generated answer under the 'answer' key.
+
+        Args:
+            state (dict): The current state of the graph, expected to contain 'user_input',
+                          and optionally 'parsed_document' or 'relevant_chunks' within 'keys'.
+
+        Returns:
+            dict: The updated state with the 'answer' key containing the generated answer.
+
+        Raises:
+            KeyError: If 'user_input' or 'document' is not found in the state, indicating
+                      that the necessary information for generating an answer is missing.
+        """
+
+        print("---GENERATING ANSWER---")
+        try:
+            user_input = state["user_input"]
+            document = state["document"]
+        except KeyError as e:
+            print(f"Error: {e} not found in state.")
+            raise
+
+        parsed_document = state.get("parsed_document", None)
+        relevant_chunks = state.get("relevant_chunks", None)
+
+        if relevant_chunks:
+            context = relevant_chunks
+        elif parsed_document:
+            context = parsed_document
+        else:
+            context = document
+
+        output_parser = JsonOutputParser()
+        format_instructions = output_parser.get_format_instructions()
+
+        template_chunks = """You are a website scraper and you have just scraped the
+        following content from a website.
+        You are now asked to answer a question about the content you have scraped.\n {format_instructions} \n
+        The website is big so I am giving you one chunk at the time to be merged later with the other chunks.\n
+        Content of {chunk_id}: {context}
+        Question: {question}
+                """
+        template_merge = """You are a website scraper and you have just scraped the
+        following content from a website.
+        You are now asked to answer a question about the content you have scraped.\n {format_instructions} \n
+        You have scraped many chunks since the website is big and now you are asked to merge them into a single answer without repetitions (if there are any).\n
+        Content to merge: {context}
+        Question: {question}
+                """
+
+        chains_dict = {}
+
+        # Use tqdm to add progress bar
+        for i, chunk in enumerate(tqdm(context, desc="Processing chunks")):
+            prompt = PromptTemplate(
+                template=template_chunks,
+                input_variables=["question"],
+                partial_variables={"context": chunk.page_content,
+                                   "chunk_id": i + 1, "format_instructions": format_instructions},
+            )
+            # Dynamically name the chains based on their index
+            chain_name = f"chunk{i+1}"
+            chains_dict[chain_name] = prompt | self.llm | output_parser
+
+        # Use dictionary unpacking to pass the dynamically named chains to RunnableParallel
+        map_chain = RunnableParallel(**chains_dict)
+        # Chain
+        answer_map = map_chain.invoke({"question": user_input})
+
+        # Merge the answers from the chunks
+        merge_prompt = PromptTemplate(
+            template=template_merge,
+            input_variables=["context", "question"],
+            partial_variables={"format_instructions": format_instructions},
+        )
+        merge_chain = merge_prompt | self.llm | output_parser
+        answer = merge_chain.invoke(
+            {"context": answer_map, "question": user_input})
+
+        # Update the state with the generated answer
+        state.update({"answer": answer})
+        return state
