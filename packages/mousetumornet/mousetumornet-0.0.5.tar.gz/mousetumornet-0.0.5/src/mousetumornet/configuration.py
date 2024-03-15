ad6783ed55622a8c425f655d3c404267091aa3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 05:06:01 +0000

    Add package mousetumornet/0.0.5/mousetumornet-0.0.5.tar.gz

---
+++ packages/mousetumornet/mousetumornet-0.0.5.tar.gz/mousetumornet-0.0.5/src/mousetumornet/configuration.py
@@ -0,0 +1,10 @@
+MIN_SIZE_PX = 150
+
+MODELS = {
+#     'v40': ("https://zenodo.org/record/7961832/files/nnunetv2_weights_fullres.zip",
+#             "d26e446d7b95934c958f6a4cb23ced5399c636c599463ebe414d76474588d442"),
+#     'v41': ("https://zenodo.org/record/7961832/files/nnunetv2_export_fullres256.zip",
+#            "4ed6edc11b4330d4d14351076c2a5039b29cc4ad61336d0c83666d9b2ce2b661"),
+    'v42': ("https://zenodo.org/records/10074441/files/nnunetv2_export_fullres_nov23.zip",
+           "e343b4808a3741b0b5752961b1225e88ab96519c22510bfcc0726c597213ed1e")
+}
