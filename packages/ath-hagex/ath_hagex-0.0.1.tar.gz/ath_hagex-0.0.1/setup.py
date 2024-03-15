3ebff30079c74ec43071de7b4f055822157970ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 03:03:52 +0000

    Add package ath-hagex/0.0.1/ath_hagex-0.0.1.tar.gz

---
+++ packages/ath-hagex/ath_hagex-0.0.1.tar.gz/ath_hagex-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='ath_hagex', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Arti579',
+    author_email='your@domain.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
