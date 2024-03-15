caf35a656c3a2f50356d1119283ea78a9b5af709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 14:03:59 +0000

    Add package cndi/2.4.0/cndi-2.4.0-py3-none-any.whl

---
+++ packages/cndi/cndi-2.4.0-py3-none-any.whl/cndi/flask/flask_app.py
@@ -0,0 +1,82 @@
+import threading
+
+from cndi.annotations import Component, ConditionalRendering, getBeanObject
+from cndi.annotations.threads import ContextThreads
+from cndi.env import getContextEnvironment, getContextEnvironments
+
+def __check_flask_enabled__(x):
+    """
+    Checks whether Flask is enabled.
+
+    This function checks the environment for a variable named 'app.flask.enabled'. If the variable is set to 'True', the function returns True. If the variable is not set or is set to any other value, the function returns False.
+
+    Returns:
+        True if Flask is enabled, False otherwise.
+    """
+    try:
+        from flask import Flask
+        return getContextEnvironment("app.flask.enabled", castFunc=bool, defaultValue=False);
+    except ImportError:
+        return False
+
+@Component
+@ConditionalRendering(callback=__check_flask_enabled__)
+class FlaskApplication:
+    """
+    This class represents a Flask application.
+
+    It is marked as a component and is conditionally rendered based on whether Flask is enabled.
+
+    Attributes:
+        appName: The name of the Flask application.
+        port: The port on which the Flask application runs.
+        host: The host on which the Flask application runs.
+        configs: A dictionary of configuration settings for the Flask application.
+        app: The Flask application instance.
+
+    Methods:
+        postConstruct: A method that is called after the Flask application is constructed.
+        run: Starts the Flask application.
+    """
+    def __init__(self):
+        """
+        Initializes a new instance of the Flask application.
+
+        The application's name, port, host, debug mode, and configuration settings are retrieved from the environment.
+        """
+        from flask import Flask
+        self.appName = getContextEnvironment("app.flask.name", defaultValue="app")
+        self.port = getContextEnvironment("app.flask.port", defaultValue=8080, castFunc=int)
+        self.host = getContextEnvironment("app.flask.listen", defaultValue="0.0.0.0")
+        contextEnvs = getContextEnvironments()
+        self.configs = dict(map(lambda items: (items[0]['app.flask.configs.'.__len__():], items[1]),
+                                filter(lambda items: items[0].startswith('app.flask.configs'), contextEnvs.items())))
+
+        self.app = Flask(self.appName)
+
+    def postConstruct(self):
+        """
+        A method that is called after the Flask application is constructed.
+
+        This method currently does nothing, but can be overridden in subclasses to perform setup tasks.
+        """
+        pass
+
+    def run(self):
+        """
+        Starts the Flask application.
+
+        This method starts the Flask application on the specified host and port.
+        """
+        from werkzeug import run_simple
+        serverThread = threading.Thread(name="thread-" + self.appName, target=run_simple, kwargs={
+            "hostname": self.host,
+            "port": self.port,
+            "application": self.app,
+            **self.configs
+        })
+
+        contextThread: ContextThreads = getBeanObject('.'.join([ContextThreads.__module__ , ContextThreads.__name__]))
+        contextThread.add_thread(serverThread)
+
+        serverThread.start()
