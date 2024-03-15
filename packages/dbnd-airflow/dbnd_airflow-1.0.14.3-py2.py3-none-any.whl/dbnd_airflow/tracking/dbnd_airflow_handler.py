3640c1664e38e22f48571ad44d7200c726b526bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:03:54 +0000

    Add package dbnd-airflow/1.0.14.3/dbnd_airflow-1.0.14.3-py2.py3-none-any.whl

---
+++ packages/dbnd-airflow/dbnd_airflow-1.0.14.3-py2.py3-none-any.whl/dbnd_airflow/tracking/dbnd_airflow_handler.py
@@ -0,0 +1,194 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+import logging
+import os
+
+from typing import ContextManager, Optional
+
+from airflow.operators.subdag_operator import SubDagOperator
+from airflow.utils.log.file_task_handler import FileTaskHandler
+from more_itertools import first_true
+
+import dbnd
+
+from dbnd import config, dbnd_tracking_stop, get_dbnd_project_config
+from dbnd._core.constants import AD_HOC_DAG_PREFIX
+from dbnd._core.context.databand_context import DatabandContext, new_dbnd_context
+from dbnd._core.log.buffered_log_manager import BufferedLogManager
+from dbnd._core.tracking.airflow_dag_inplace_tracking import calc_task_key_from_af_ti
+from dbnd._core.tracking.script_tracking_manager import is_dbnd_tracking_active
+from dbnd._core.utils.basics import environ_utils
+from dbnd_airflow.tracking.execute_tracking import is_dag_eligable_for_tracking
+from dbnd_airflow.tracking.fakes import FakeTaskRun
+from dbnd_airflow.utils import get_task_run_attempt_uid_from_af_ti
+
+
+AIRFLOW_FILE_TASK_HANDLER = FileTaskHandler.__name__
+
+AIRFLOW_TASK_LOGGER = "airflow.task"
+
+
+class DbndAirflowHandler(logging.Handler):
+    """
+    This is a logger handler that is used as an Entry Point to airflow run.
+    It's injected to the Logger(name="airflow.task"), and used by entering the context on the beginning of the task
+    instance run, and getting close when the task instance is done.
+    """
+
+    def __init__(self, logger):
+        logging.Handler.__init__(self)
+
+        self.dbnd_context: Optional[DatabandContext] = None
+        self.dbnd_context_manage: Optional[ContextManager[DatabandContext]] = None
+
+        self.task_run_attempt_uid = None
+        self.task_env_key = None
+
+        self.airflow_logger = logger
+
+        self.in_memory_log_manager: Optional[BufferedLogManager] = None
+
+    def set_context(self, ti):
+        """
+        Airflow's log handler use this method to setup the context when running a TaskInstance(=ti).
+        We use this method to setup the dbnd context and communicate information to
+        the `<airflow_operator>_execute` task, that we create in `execute_tracking.py`.
+        """
+        # we setting up only when we are not in our own orchestration dag
+        if ti.dag_id.startswith(AD_HOC_DAG_PREFIX):
+            return
+
+        if not is_dag_eligable_for_tracking(ti.dag_id):
+            return
+
+        if config.getboolean("mlflow_tracking", "databand_tracking"):
+            self.airflow_logger.warning(
+                "dbnd can't track mlflow and airflow together please disable dbnd config "
+                "`databand_tracking` in section `mlflow_tracking`"
+            )
+            return
+
+        # we are not tracking SubDagOperator
+        if ti.operator is None or ti.operator == SubDagOperator.__name__:
+            return
+
+        # Airflow is running with two process `run` and `--raw run`.
+        # But we want the handler to run only once (Idempotency)
+        # So we are using an environment variable to sync those two process
+        task_key = calc_task_key_from_af_ti(ti)
+
+        if ti.raw:
+            self.in_memory_log_manager = BufferedLogManager()
+            self.airflow_logger.debug(
+                f"Initiated In Memory Log Manager with task {task_key}"
+            )
+
+        if os.environ.get(task_key, False):
+            # This key is already set which means we are in `--raw run`
+            return
+        else:
+            # We are in the outer `run`
+            self.task_env_key = task_key
+            # marking the environment with the current key for the
+            environ_utils.set_on(task_key)
+            from dbnd_airflow.tracking.dbnd_airflow_conf import (
+                set_dbnd_config_from_airflow_connections,
+            )
+
+            # When we are in `--raw run`, in tracking, it runs the main airflow process
+            # for every task, which made some of the features to run twice,
+            # once when the `worker` process ran, and once when the `main` one ran,
+            # which made some of the features to run with different configurations.
+            # it still runs twice, but know with the same configurations.
+            set_dbnd_config_from_airflow_connections()
+
+        self.task_run_attempt_uid = get_task_run_attempt_uid_from_af_ti(ti)
+
+        # make sure we are not polluting the airflow logs
+        get_dbnd_project_config().quiet_mode = True
+
+        # context with disabled logs
+        self.dbnd_context_manage: ContextManager[DatabandContext] = new_dbnd_context(
+            conf={"log": {"disabled": True}}
+        )
+        self.dbnd_context = self.dbnd_context_manage.__enter__()
+
+        # tracking msg
+        self.airflow_logger.info(
+            "Databand Tracking Started {version}".format(version=dbnd.__version__)
+        )
+
+    def close(self):
+        if self.dbnd_context and self.in_memory_log_manager:
+            try:
+                fake_task_run = FakeTaskRun(
+                    task_run_attempt_uid=self.task_run_attempt_uid
+                )
+                in_memory_log_body = self.in_memory_log_manager.get_log_body()
+                self.dbnd_context.tracking_store.save_task_run_log(
+                    task_run=fake_task_run, log_body=in_memory_log_body
+                )
+            except Exception:
+                self.airflow_logger.exception("Exception occurred when saving task log")
+
+        self.dbnd_context = None
+        self.in_memory_log_manager = None
+
+        try:
+            if self.dbnd_context_manage:
+                if is_dbnd_tracking_active():
+                    # Stops and clears the script tracking if exists and the user forgot to stop it.
+                    self.airflow_logger.warning(
+                        "A Databand Context from tracking is still active, did you forget to"
+                        " use 'dbnd_tracking_stop'?\nWe encourage you to use 'dbnd_tracking' "
+                        "context instead of 'dbnd_tracking_start' and 'dbnd_tracking_stop'"
+                    )
+                    dbnd_tracking_stop()
+
+                self.dbnd_context_manage.__exit__(None, None, None)
+        except Exception:
+            self.airflow_logger.exception(
+                "Exception occurred when trying to exit dbnd_context_manager"
+            )
+        finally:
+            self.dbnd_context_manage = None
+
+        try:
+            if self.task_env_key and self.task_env_key in os.environ:
+                del os.environ[self.task_env_key]
+        except Exception:
+            self.airflow_logger.exception(
+                "Exception occurred when trying to remove task_env_key from env"
+            )
+        finally:
+            self.task_env_key = None
+
+    def emit(self, record):
+        if self.in_memory_log_manager:
+            try:
+                msg = self.format(record)
+                self.in_memory_log_manager.add_log_msg(msg)
+            except RecursionError:  # See issue 36272
+                raise
+            except Exception:
+                self.handleError(record)
+
+
+def set_dbnd_handler():
+    """
+    Build and inject the dbnd handler to airflow's logger.
+    """
+    airflow_logger = logging.getLogger(AIRFLOW_TASK_LOGGER)
+    dbnd_handler = DbndAirflowHandler(logger=airflow_logger)
+
+    inheriting_handler = first_true(
+        airflow_logger.handlers,
+        pred=lambda handler: handler.__class__.__name__ == AIRFLOW_FILE_TASK_HANDLER,
+        default=airflow_logger.handlers[0] if airflow_logger.handlers else None,
+    )
+    if inheriting_handler:
+        handler_formatter = inheriting_handler.formatter
+        dbnd_handler.setFormatter(handler_formatter)
+        for handler_filter in inheriting_handler.filters:
+            dbnd_handler.addFilter(handler_filter)
+    airflow_logger.addHandler(dbnd_handler)
---
+++ packages/dbnd-airflow/dbnd_airflow-1.0.14.3-py2.py3-none-any.whl/dbnd_airflow/tracking/execute_tracking.py
@@ -0,0 +1,329 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+import logging
+
+from contextlib import contextmanager
+from itertools import islice
+from typing import Optional
+
+from dbnd import config, get_dbnd_project_config, log_metric, log_metrics
+from dbnd._core.constants import TaskRunState
+from dbnd._core.errors.errors_utils import log_exception
+from dbnd._core.log.dbnd_log import dbnd_log_exception, dbnd_log_tracking
+from dbnd._core.task_run.task_run import TaskRun
+from dbnd._core.task_run.task_run_error import TaskRunError
+from dbnd._core.tracking.airflow_task_context import AirflowTaskContext
+from dbnd._core.tracking.managers.callable_tracking import _log_result
+from dbnd._core.tracking.script_tracking_manager import (
+    dbnd_airflow_tracking_start,
+    dbnd_tracking_stop,
+)
+from dbnd._core.utils.basics.environ_utils import env
+from dbnd._core.utils.basics.nested_context import nested
+from dbnd_airflow.raw_constants import MONITOR_DAG_NAME
+from dbnd_airflow.tracking.config import AirflowTrackingConfig
+from dbnd_airflow.tracking.dbnd_airflow_conf import get_tracking_information, get_xcoms
+from dbnd_airflow.tracking.wrap_operators import (
+    get_airflow_operator_handlers_config,
+    wrap_operator_with_tracking_info,
+)
+from dbnd_airflow.utils import get_airflow_instance_uid
+
+
+logger = logging.getLogger(__name__)
+
+AIRFLOW_MONITOR_CONFIG_NAME = "airflow_monitor"
+DAG_IDS_FOR_TRACKING_CONFIG_NAME = "dag_ids"
+IS_SYNC_ENABLED_TRACKING_CONFIG_NAME = "is_sync_enabled"
+
+
+def get_tracking_dag_ids_from_airflow_json():
+    try:
+        from dbnd_airflow.tracking.dbnd_airflow_conf import (
+            get_dbnd_json_config_from_airflow_connections,
+        )
+
+        json = get_dbnd_json_config_from_airflow_connections()
+        if json:
+            monitor_config = json.get(AIRFLOW_MONITOR_CONFIG_NAME, None)
+            if monitor_config:
+                is_sync_enabled = monitor_config.get(
+                    IS_SYNC_ENABLED_TRACKING_CONFIG_NAME, True
+                )
+                if not is_sync_enabled:
+                    return []
+                dag_ids_config = monitor_config.get(
+                    DAG_IDS_FOR_TRACKING_CONFIG_NAME, None
+                )
+
+                if dag_ids_config and isinstance(dag_ids_config, str):
+                    return dag_ids_config.split(",")
+
+                if dag_ids_config and isinstance(dag_ids_config, list):
+                    return dag_ids_config
+        return None
+    except Exception as e:
+        log_exception(
+            "exception caught while running on dbnd new execute {}".format(e), e
+        )
+        return None
+
+
+def is_monitor_dag(dag_id):
+    return dag_id == MONITOR_DAG_NAME
+
+
+def is_subdag_eligible_for_tracking(dag_id, tracking_list):
+    for tracked_dag_id in tracking_list:
+        if dag_id.startswith(f"{tracked_dag_id}."):
+            return True
+
+    return False
+
+
+def is_dag_eligable_for_tracking(dag_id):
+    # If return value is None, we track all dags. If it's empty list - we don't track anything.
+    tracking_list = get_tracking_dag_ids_from_airflow_json()
+    return (
+        tracking_list is None
+        or dag_id in tracking_list
+        or is_subdag_eligible_for_tracking(dag_id, tracking_list)
+        or is_monitor_dag(dag_id)
+    )
+
+
+def new_execute(context):
+    """
+    This function replaces the operator's original `execute` function
+    """
+
+    # IMPORTANT!!: copied_operator:
+    # ---------------------------------------
+    # The task (=operator) is copied when airflow enters to TaskInstance._run_raw_task.
+    # Then, only the copy_task (=copy_operator) is changed or called (render jinja, signal_handler,
+    # pre_execute, execute, etc..).
+    copied_operator = context["task_instance"].task
+
+    dbnd_log_tracking("Running tracked .execute for %s", copied_operator.task_id)
+
+    if not is_dag_eligable_for_tracking(context["task_instance"].dag_id):
+        execute = get_execute_function(copied_operator)
+        result = execute(copied_operator, context)
+        return result
+
+    try:
+        # Set that we are in Airflow tracking mode
+        get_dbnd_project_config().set_is_airflow_runtime()
+
+        task_context = extract_airflow_context(context)
+        # start operator execute run with current airflow context
+        task_run = dbnd_airflow_tracking_start(
+            airflow_context=task_context
+        )  # type: Optional[TaskRun]
+
+    except Exception as e:
+        task_run = None
+        dbnd_log_exception(
+            "exception caught while running on dbnd new execute {}".format(e)
+        )
+
+    from airflow.exceptions import AirflowRescheduleException
+
+    # running the operator's original execute function
+    try:
+        operator_config = getattr(copied_operator, "dbnd_config", {})
+        with config(operator_config, "airflow_operator_definition"):
+            with af_tracking_context(task_run, context, copied_operator):
+                execute = get_execute_function(copied_operator)
+                result = execute(copied_operator, context)
+
+    # Check if this is sensor task that is retrying - normal behavior and not really an exception
+    except AirflowRescheduleException:
+        dbnd_tracking_stop(finalize_run=False)
+        raise
+    # catch if the original execute failed
+    except Exception as ex:
+        if task_run:
+            error = TaskRunError.build_from_ex(ex, task_run)
+            task_run.set_task_run_state(state=TaskRunState.FAILED, error=error)
+
+        dbnd_tracking_stop()
+        raise
+
+    # if we have a task run here we want to log results and xcoms
+    if task_run:
+        try:
+            track_config = AirflowTrackingConfig.from_databand_context()
+            if track_config.track_xcom_values:
+                # reporting xcoms as metrix of the task
+                log_xcom(context, track_config)
+
+            if track_config.track_airflow_execute_result:
+                # reporting the result
+                log_operator_result(
+                    task_run, result, copied_operator, track_config.track_xcom_values
+                )
+
+        except Exception as e:
+            dbnd_log_exception(
+                "exception caught while logging airflow operator results {}".format(e)
+            )
+
+    # make sure we close and return the original results
+    dbnd_tracking_stop()
+    return result
+
+
+def log_xcom(context, track_config):
+    task_instance = context["task_instance"]
+    xcoms = get_xcoms(task_instance)
+    if xcoms:
+        # get only the first xcoms
+        xcoms_head = islice(xcoms, track_config.max_xcom_length)
+        log_metrics(dict(xcoms_head))
+
+
+def log_operator_result(task_run, result, operator, track_xcom):
+    _log_result(task_run, result)
+
+    # after airflow runs the operator it xcom_push the result, so we log it
+    if track_xcom and operator.do_xcom_push and result is not None:
+        from airflow.models import XCOM_RETURN_KEY
+
+        log_metric(key=XCOM_RETURN_KEY, value=result)
+
+
+@contextmanager
+def af_tracking_context(task_run, airflow_context, operator):
+    """
+    Wrap the execution with handling the environment management
+    """
+    if not task_run:
+        # aborting -  can't enter the context without task_run
+        yield
+        return
+
+    try:
+        tracking_info = get_tracking_information(airflow_context, task_run)
+        airflow_operator_handlers = get_airflow_operator_handlers_config(
+            task_run.run.context.settings.tracking.airflow_operator_handlers
+        )
+        operator_wrapper = wrap_operator_with_tracking_info(
+            tracking_info, operator, airflow_operator_handlers
+        )
+
+    except Exception as e:
+        logger.error(
+            "exception caught adding tracking context to operator execution {}"
+            "continue without tracking context".format(e),
+            exc_info=True,
+        )
+        yield
+        return
+
+    # wrap the execution with tracking info in the environment
+    with nested(env(**tracking_info), operator_wrapper):
+        yield
+
+
+def new_execute_for_class(self, context):
+    """
+    Different wrapper for operator's class
+    """
+    return new_execute(context)
+
+
+def track_operator(operator):
+    """
+    Replace the operator's execute method with our `tracked execute` method
+    """
+    import inspect
+
+    if inspect.isclass(operator):
+        from airflow.models import BaseOperator
+
+        # we only track operators which base on Airflow BaseOperator
+        if not issubclass(operator, BaseOperator):
+            return operator
+
+        # the operator class is already tracked
+        if (
+            hasattr(operator, "_tracked_class")
+            and operator.__name__ == operator._tracked_class
+        ):
+            return operator
+
+        # this is the first time we encounter this class so we mark it
+        operator._tracked_class = operator.__name__
+        operator.__execute__ = operator.execute
+        operator.execute = new_execute_for_class
+        return operator
+
+    else:
+        # the operator instance is already tracked
+        if hasattr(operator, "_tracked_instance"):
+            return operator
+
+        # this is the first time we encounter this instance so we mark it
+        operator._tracked_instance = True
+
+        # if the operator's class is tracked, we can't used `operator.__class__.execute`
+        # need to use the original execute (__execute__)
+        if (
+            hasattr(operator, "_tracked_class")
+            and operator.__class__.__name__ == operator._tracked_class
+        ):
+            operator.__execute__ = operator.__class__.__execute__
+        else:
+            # this can be a problem when the operator class doesn't implement it's own execute
+            operator.__execute__ = operator.__class__.execute
+
+        operator.execute = new_execute
+
+    return operator
+
+
+def get_execute_function(copied_operator):
+    """
+    After tracking operator we need to access the original execution function
+    """
+    if hasattr(copied_operator, "_tracked_instance"):
+        return copied_operator.__execute__
+
+    if hasattr(copied_operator, "_tracked_class"):
+        return copied_operator.__class__.__execute__
+
+    raise AttributeError("tracked failed to run original operator.execute")
+
+
+def extract_airflow_context(airflow_context):
+    # type: (dict[str, Any]) -> Optional[AirflowTaskContext]
+    """Create AirflowTaskContext for airflow_context dict"""
+
+    task_instance = airflow_context.get("task_instance")
+    if task_instance is None:
+        return None
+
+    dag_id = task_instance.dag_id
+    task_id = task_instance.task_id
+    execution_date = str(task_instance.execution_date)
+    try_number = task_instance.try_number
+
+    if dag_id and task_id and execution_date:
+        return AirflowTaskContext(
+            dag_id=dag_id,
+            execution_date=execution_date,
+            task_id=task_id,
+            try_number=try_number,
+            context=airflow_context,
+            airflow_instance_uid=get_airflow_instance_uid(),
+            is_subdag=airflow_context.get("dag").is_subdag,
+        )
+
+    logger.debug(
+        "airflow context from inspect, at least one of those params is missing"
+        "dag_id: {}, execution_date: {}, task_id: {}".format(
+            dag_id, execution_date, task_id
+        )
+    )
+    return None
