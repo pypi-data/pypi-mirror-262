d2c2254f3c2ae83b24e272d2988776224b6afa7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:04:45 +0000

    Add package cppython-git/0.1.1.dev3/cppython_git-0.1.1.dev3.tar.gz

---
+++ packages/cppython-git/cppython_git-0.1.1.dev3.tar.gz/cppython_git-0.1.1.dev3/tests/integration/test_version_control.py
@@ -0,0 +1,19 @@
+"""Tests the cppython built-in SCM plugin"""
+
+import pytest
+from pytest_cppython.tests import SCMIntegrationTests
+
+from cppython_git.plugin import GitSCM
+
+
+class TestGitInterface(SCMIntegrationTests[GitSCM]):
+    """Integration tests for the Git SCM plugin"""
+
+    @pytest.fixture(name="plugin_type", scope="session")
+    def fixture_plugin_type(self) -> type[GitSCM]:
+        """A required testing hook that allows type generation
+
+        Returns:
+            The SCM type
+        """
+        return GitSCM
