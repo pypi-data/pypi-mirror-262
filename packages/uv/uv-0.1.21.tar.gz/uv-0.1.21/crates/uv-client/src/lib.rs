62b2ef49a7e8aad07183d6285abafcb3607327e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:35 +0000

    Add package uv/0.1.21/uv-0.1.21.tar.gz

---
+++ packages/uv/uv-0.1.21.tar.gz/uv-0.1.21/crates/uv-client/src/lib.rs
@@ -0,0 +1,19 @@
+pub use cached_client::{CacheControl, CachedClient, CachedClientError, DataWithCachePolicy};
+pub use error::{BetterReqwestError, Error, ErrorKind};
+pub use flat_index::{FlatDistributions, FlatIndex, FlatIndexClient, FlatIndexError};
+pub use registry_client::{
+    Connectivity, RegistryClient, RegistryClientBuilder, SimpleMetadata, SimpleMetadatum,
+    VersionFiles,
+};
+pub use rkyvutil::OwnedArchive;
+
+mod cached_client;
+mod error;
+mod flat_index;
+mod html;
+mod httpcache;
+mod middleware;
+mod registry_client;
+mod remote_metadata;
+mod rkyvutil;
+mod tls;
