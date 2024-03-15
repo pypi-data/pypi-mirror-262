2d7bc13139e3e4464f9ed8468bd044561fd11e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 07:04:16 +0000

    Add package st-theme/1.2.0/st-theme-1.2.0.tar.gz

---
+++ packages/st-theme/st-theme-1.2.0.tar.gz/st-theme-1.2.0/setup.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="st-theme",
+    version="1.2.0",
+    author="Gabriel Tem Pass",
+    author_email="redo_hint_0x@icloud.com",
+    description="A component that returns the active theme of the Streamlit app.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gabrieltempass/streamlit-theme",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8",
+    install_requires=["streamlit >= 1.20"],
+)
