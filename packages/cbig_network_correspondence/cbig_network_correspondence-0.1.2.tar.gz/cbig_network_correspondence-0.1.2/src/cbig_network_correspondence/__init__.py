85a4c4ad6efc5f7b55e5361fd9c51172eb53d0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:04:04 +0000

    Add package cbig_network_correspondence/0.1.2/cbig_network_correspondence-0.1.2.tar.gz

---
+++ packages/cbig_network_correspondence/cbig_network_correspondence-0.1.2.tar.gz/cbig_network_correspondence-0.1.2/src/cbig_network_correspondence/__init__.py
@@ -0,0 +1,15 @@
+from . import compute_overlap_with_atlases
+from . import visualize_overlap_lib
+from . import load_example
+from . import visualize_report_lib
+import os
+os.environ["GIT_PYTHON_REFRESH"] = "quiet"
+from git import Repo
+
+data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+if not os.path.exists(data_dir):
+    print("Download data")
+    Repo.clone_from('https://github.com/rubykong/cbig_network_correspondence_data', data_dir)
+else:
+    repo_dir = Repo(data_dir)
+    repo_dir.remotes.origin.pull('master')
