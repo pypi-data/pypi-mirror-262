69eb53e4b0314c29f828489581d2f056937b967e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:29 +0000

    Add package py-eth-utils/0.0.5/py_eth_utils-0.0.5.tar.gz

---
+++ packages/py-eth-utils/py_eth_utils-0.0.5.tar.gz/py_eth_utils-0.0.5/setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setup(
+    name="py_eth_utils",
+    version="0.0.5",
+    packages=find_packages(),
+    # 依赖库
+    install_requires=[
+        'web3==6.15.1'
+    ],
+    author="Stars Bing",
+    author_email="bing@starsxu.com",
+    description="eth for tools, contracts、transfer、balance",
+    long_description="",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
