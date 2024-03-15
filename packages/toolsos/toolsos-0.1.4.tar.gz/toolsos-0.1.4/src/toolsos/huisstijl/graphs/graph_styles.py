b61855a3c07a81bad9e1f9bd8e296a1676cd4aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:24 +0000

    Add package toolsos/0.1.4/toolsos-0.1.4.tar.gz

---
+++ packages/toolsos/toolsos-0.1.4.tar.gz/toolsos-0.1.4/src/toolsos/huisstijl/graphs/graph_styles.py
@@ -0,0 +1,24 @@
+STYLE_OLD = {
+    "font": {"family": "Corbel", "font_size": 15},
+    "plot_bgcolor": "#FFFFFF",
+    "gridline_color": "#E8E8E8",
+    "gridlinewidth": 0.75,
+    "showline": True,
+    "showgrid": False,
+    "zerolinecolor": "#E8E8E8",
+    "gridcolor": "#E8E8E8",
+    "separators": ",",
+}
+
+
+STYLE_NEW = {
+    "font": "Amsterdam Sans ExtraBold, Corbel",
+    "font_size": 15,
+    "plot_bgcolor": "#FFFFFF",
+    "gridline_color": "#E8E8E8",
+    "gridlinewidth": 0.75,
+    "showline": True,
+    "showgrid": False,
+    "zerolinecolor": "#E8E8E8",
+    "gridcolor": "#E8E8E8",
+}
