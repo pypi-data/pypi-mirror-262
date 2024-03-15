a36de6481d279fa5495fd8f67245a2e0c09ee274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 14:03:33 +0000

    Add package eurec4a/0.0.6/eurec4a-0.0.6.tar.gz

---
+++ packages/eurec4a/eurec4a-0.0.6.tar.gz/eurec4a-0.0.6/test/test_metadata_availability.py
@@ -0,0 +1,21 @@
+import eurec4a
+
+
+def test_flight_segment_availability():
+    segments = eurec4a.get_flight_segments()
+    assert "HALO" in segments
+
+
+def test_flight_segment_availability_by_version():
+    segments = eurec4a.get_flight_segments("v1.0.1-rc1")
+    assert "HALO" in segments
+
+
+def test_inake_catalog_availability():
+    cat = eurec4a.get_intake_catalog()
+    assert "barbados" in cat
+
+
+def test_ipfs_inake_catalog_availability():
+    cat = eurec4a.get_intake_catalog(use_ipfs=True)
+    assert "radiosondes" in cat
