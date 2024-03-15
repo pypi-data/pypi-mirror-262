23504ebcf2d8710565ee75e86bb6459037c3f0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 16:03:50 +0000

    Add package pycopyEcho/0.1.0/pycopyEcho-0.1.0.tar.gz

---
+++ packages/pycopyEcho/pycopyEcho-0.1.0.tar.gz/pycopyEcho-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages  
+
+setup(  
+    name="pycopyEcho",  
+    version="0.1.0",  
+    description="Copy one directory to another",  
+    author="Echo",  
+    author_email="176188484@qq.com",  
+    packages=find_packages(),  
+    install_requires=[""],  
+    classifiers=[  
+        "Programming Language :: Python :: 3",  
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: POSIX :: Linux",  
+    ],  
+)
