be53059987110987c56e47564f9a4cb961e956c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 14:03:56 +0000

    Add package toutsurmoneau/0.0.24/toutsurmoneau-0.0.24-py3-none-any.whl

---
+++ packages/toutsurmoneau/toutsurmoneau-0.0.24-py3-none-any.whl/toutsurmoneau/const.py
@@ -0,0 +1,30 @@
+# Known providers, give the base url, other can be added
+# https://d13qcyivyon4xf.cloudfront.net/assistant/v1/Suez_LDEF/LivingActorProject/resources/local_v24.js
+# First one shall be the generic www.toutsurmoneau.fr
+KNOWN_PROVIDER_URLS = [
+    "https://www.toutsurmoneau.fr",
+    "https://assainissement.eaudetoulousemetropole.fr",
+    "https://colmarienne.toutsurmoneau.fr",
+    "https://eau-agglodebrive.toutsurmoneau.fr",
+    "https://eaudubaslanguedoc.toutsurmoneau.fr",
+    "https://eaux-d-albatre.toutsurmoneau.fr",
+    "https://leaudorleansmetropole.toutsurmoneau.fr",
+    "https://leauduvalenciennois.toutsurmoneau.fr",
+    "https://sidesol.toutsurmoneau.fr",
+    "https://so-eau.toutsurmoneau.fr",
+    "https://www.dolea.fr",
+    "https://www.eau-olivet.fr",
+    "https://www.eaugrandavignon.fr",
+    "https://www.eaux-de-calais.com",
+    "https://www.eaux-dunkerque.fr",
+    "https://www.eauxdelest.com",
+    "https://www.gaz-et-eaux.info",
+    "https://www.odivea.fr",
+    "https://www.orleanaise-des-eaux.fr",
+    "https://www.seop.fr",
+    "https://www.sevesc.fr",
+    "https://www.seynoisedeseaux.fr",
+    "https://www.usagers.eaux-de-normandie.fr",
+    "https://www.usagers.leau.bordeaux-metropole.fr",
+    "https://www.stephanoise-eaux.fr",
+]
