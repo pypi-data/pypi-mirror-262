f00eb392d50cc6b64914bb541b569a62286d4c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 14:03:33 +0000

    Add package mylibisyourlib/0.1/mylibisyourlib-0.1.tar.gz

---
+++ packages/mylibisyourlib/mylibisyourlib-0.1.tar.gz/mylibisyourlib-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mylibisyourlib",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Ajoutez ici les dépendances nécessaires
+    ],
+    # Plusieurs autres options peuvent être ajoutées ici
+)
