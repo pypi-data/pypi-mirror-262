b19afaed99839128a26b774f09ae00a8591f5510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 04:03:13 +0000

    Add package CoreLogger/1.0.2/CoreLogger-1.0.2.tar.gz

---
+++ packages/CoreLogger/CoreLogger-1.0.2.tar.gz/CoreLogger-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='CoreLogger',
+
+    version='1.0.2',
+
+    author='Ahmet Kıpkıp',
+
+    author_email='ahmtkipkip@gmail.com',
+
+    description='A simple logger for sending logs to a Loki server for Internal use at IZBB.',
+
+    packages=find_packages(),
+
+)
