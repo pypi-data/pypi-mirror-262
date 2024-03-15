8c9ee6551c46a0c8f9ccdd717af4daf1f35aed08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 06:05:12 +0000

    Add package magnum-cluster-api/0.14.1/magnum_cluster_api-0.14.1.tar.gz

---
+++ packages/magnum-cluster-api/magnum_cluster_api-0.14.1.tar.gz/magnum_cluster_api-0.14.1/magnum_cluster_api/tests/unit/test_resources.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2023 VEXXHOST, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from magnum.objects import fields
+
+from magnum_cluster_api import resources
+
+
+def test_generate_machine_deployments_for_cluster_with_deleting_node_group(
+    context, mocker
+):
+    cluster_template = mocker.Mock()
+    cluster_template.labels = {"kube_tag": "v1.26.2"}
+
+    cluster = mocker.Mock()
+    cluster.cluster_template = cluster_template
+    cluster.labels = {}
+    cluster.nodegroups = [
+        mocker.Mock(
+            name="creating-worker",
+            status=fields.ClusterStatus.CREATE_IN_PROGRESS,
+            labels={},
+        ),
+        mocker.Mock(
+            name="created-worker",
+            status=fields.ClusterStatus.CREATE_COMPLETE,
+            labels={},
+        ),
+        mocker.Mock(
+            name="deleting-worker",
+            status=fields.ClusterStatus.DELETE_IN_PROGRESS,
+            labels={},
+        ),
+        mocker.Mock(
+            name="deleted-worker",
+            status=fields.ClusterStatus.DELETE_COMPLETE,
+            labels={},
+        ),
+    ]
+
+    cluster_get_by_uuid = mocker.patch("magnum.objects.Cluster.get_by_uuid")
+    cluster_get_by_uuid.return_value = cluster
+
+    mock_get_default_boot_volume_type = mocker.patch(
+        "magnum_cluster_api.integrations.cinder.get_default_boot_volume_type"
+    )
+    mock_get_default_boot_volume_type.return_value = "foo"
+
+    mock_get_image_uuid = mocker.patch("magnum_cluster_api.utils.get_image_uuid")
+    mock_get_image_uuid.return_value = "foo"
+
+    mds = resources.generate_machine_deployments_for_cluster(
+        context,
+        cluster,
+    )
+
+    assert len(mds) == 2
