8528aa1ac338768cb7a980195542a60a49dc6b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 05:05:34 +0000

    Add package bigframes/0.25.0/bigframes-0.25.0.tar.gz

---
+++ packages/bigframes/bigframes-0.25.0.tar.gz/bigframes-0.25.0/tests/system/small/operations/test_datetimes.py
@@ -0,0 +1,221 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pandas as pd
+import pytest
+
+import bigframes.series
+from tests.system.utils import assert_series_equal, skip_legacy_pandas
+
+DATETIME_COL_NAMES = [("datetime_col",), ("timestamp_col",)]
+DATE_COLUMNS = [
+    ("datetime_col",),
+    ("timestamp_col",),
+    ("date_col",),
+]
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATE_COLUMNS,
+)
+@skip_legacy_pandas
+def test_dt_day(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.day.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.day
+
+    assert_series_equal(
+        pd_result.astype(pd.Int64Dtype()),
+        bf_result,
+    )
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATETIME_COL_NAMES,
+)
+@skip_legacy_pandas
+def test_dt_date(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.date.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.date
+
+    assert_series_equal(
+        pd_result,
+        bf_result,
+    )
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATE_COLUMNS,
+)
+@skip_legacy_pandas
+def test_dt_dayofweek(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.dayofweek.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.dayofweek
+
+    assert_series_equal(pd_result, bf_result, check_dtype=False)
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATETIME_COL_NAMES,
+)
+@skip_legacy_pandas
+def test_dt_hour(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.hour.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.hour
+
+    assert_series_equal(
+        pd_result.astype(pd.Int64Dtype()),
+        bf_result,
+    )
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATETIME_COL_NAMES,
+)
+@skip_legacy_pandas
+def test_dt_minute(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.minute.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.minute
+
+    assert_series_equal(
+        pd_result.astype(pd.Int64Dtype()),
+        bf_result,
+    )
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATE_COLUMNS,
+)
+@skip_legacy_pandas
+def test_dt_month(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.month.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.month
+
+    assert_series_equal(
+        pd_result.astype(pd.Int64Dtype()),
+        bf_result,
+    )
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATE_COLUMNS,
+)
+@skip_legacy_pandas
+def test_dt_quarter(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.quarter.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.quarter
+
+    assert_series_equal(
+        pd_result.astype(pd.Int64Dtype()),
+        bf_result,
+    )
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATETIME_COL_NAMES,
+)
+@skip_legacy_pandas
+def test_dt_second(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.second.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.second
+
+    assert_series_equal(
+        pd_result.astype(pd.Int64Dtype()),
+        bf_result,
+    )
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATETIME_COL_NAMES,
+)
+@skip_legacy_pandas
+def test_dt_time(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.time.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.time
+
+    assert_series_equal(
+        pd_result,
+        bf_result,
+    )
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATE_COLUMNS,
+)
+@skip_legacy_pandas
+def test_dt_year(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.year.to_pandas()
+    pd_result = scalars_pandas_df[col_name].dt.year
+
+    assert_series_equal(
+        pd_result.astype(pd.Int64Dtype()),
+        bf_result,
+    )
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATETIME_COL_NAMES,
+)
+@skip_legacy_pandas
+def test_dt_tz(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.tz
+    pd_result = scalars_pandas_df[col_name].dt.tz
+
+    assert bf_result == pd_result
+
+
+@pytest.mark.parametrize(
+    ("col_name",),
+    DATETIME_COL_NAMES,
+)
+@skip_legacy_pandas
+def test_dt_unit(scalars_dfs, col_name):
+    scalars_df, scalars_pandas_df = scalars_dfs
+    bf_series: bigframes.series.Series = scalars_df[col_name]
+    bf_result = bf_series.dt.unit
+    pd_result = scalars_pandas_df[col_name].dt.unit
+
+    assert bf_result == pd_result
