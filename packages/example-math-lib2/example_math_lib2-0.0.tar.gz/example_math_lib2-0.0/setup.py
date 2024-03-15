37edbffaa64e2373f2f8bc8c4f275e4b7619536b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-15 03:03:50 +0000

    Add package example-math-lib2/0.0/example_math_lib2-0.0.tar.gz

---
+++ packages/example-math-lib2/example_math_lib2-0.0.tar.gz/example_math_lib2-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='example_math_lib2',
+      version='0.0',
+      description='University lesson',
+      packages=find_packages(),
+      author_email='example@gmail.com',
+      zip_safe=False)
