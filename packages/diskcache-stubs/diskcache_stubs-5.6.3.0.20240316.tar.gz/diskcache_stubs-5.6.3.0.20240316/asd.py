630a823ed6fbdaa75e97e0cb6f30237bfb5cfa6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 15:03:50 +0000

    Add package diskcache-stubs/5.6.3.0.20240316/diskcache_stubs-5.6.3.0.20240316.tar.gz

---
+++ packages/diskcache-stubs/diskcache_stubs-5.6.3.0.20240316.tar.gz/diskcache_stubs-5.6.3.0.20240316/asd.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import diskcache
+import sqlite3
+from diskcache.core import MODE_RAW
+from diskcache.persistent import Deque, Index
+
+a = diskcache.Cache()
+
+disk = a.disk
+b=disk.fetch(3, '', 123, True)
+a.pull()
+d = Deque([1,2,3, "Qwe"])
+diskcache.Averager(a, 'asd')
+
+b = a.get(1)
+aaa = Index()
+from diskcache import core
---
+++ packages/diskcache-stubs/diskcache_stubs-5.6.3.0.20240316.tar.gz/diskcache_stubs-5.6.3.0.20240316/src/diskcache/fanout.pyi
@@ -0,0 +1,37 @@
+from typing import Any
+
+from _typeshed import StrOrBytesPath
+from diskcache.core import DEFAULT_SETTINGS as DEFAULT_SETTINGS
+from diskcache.core import ENOVAL as ENOVAL
+from diskcache.core import Cache as Cache
+from diskcache.core import Disk as Disk
+from diskcache.core import Timeout as Timeout
+from diskcache.persistent import Deque as Deque
+from diskcache.persistent import Index as Index
+from typing_extensions import Unpack
+
+from ._typeshed import BaseCache, KeyType, Settings, ValueType
+
+__all__ = ["FanoutCache"]
+
+class FanoutCache(BaseCache):
+    def __init__(
+        self,
+        directory: StrOrBytesPath | None = ...,
+        shards: int = ...,
+        timeout: float = ...,
+        disk: type[Disk] = ...,
+        **settings: Unpack[Settings],
+    ) -> None: ...
+    def __getattr__(self, name: str) -> Any: ...
+    def read(self, key: KeyType) -> ValueType: ...
+    def expire(self, retry: bool = ...) -> int: ...
+    def cache(
+        self,
+        name: str,
+        timeout: int = ...,
+        disk: type[Disk] | None = ...,
+        **settings: Unpack[Settings],
+    ) -> Cache: ...
+    def deque(self, name: str, maxlen: int | None = ...) -> Deque[Any]: ...
+    def index(self, name: str) -> Index[Any, Any]: ...
