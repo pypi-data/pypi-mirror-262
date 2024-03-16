a18ce473af5f2d92b526735c36a5f79a5a21d090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:30 +0000

    Add package scipy-maxentropy/1.0/scipy_maxentropy-1.0-py3-none-any.whl

---
+++ packages/scipy-maxentropy/scipy_maxentropy-1.0-py3-none-any.whl/scipy_maxentropy/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+from .info import __doc__
+from .maxentropy import *
+# For backward compatibility with scipy.maxentropy:
+from scipy.special import logsumexp
