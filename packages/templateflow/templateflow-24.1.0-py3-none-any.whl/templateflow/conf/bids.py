916bf1591a0328d3bbc0c6cc7425d640a779318b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:04:31 +0000

    Add package templateflow/24.1.0/templateflow-24.1.0-py3-none-any.whl

---
+++ packages/templateflow/templateflow-24.1.0-py3-none-any.whl/templateflow/conf/bids.py
@@ -0,0 +1,18 @@
+"""Extending pyBIDS for querying TemplateFlow."""
+from bids.layout import BIDSLayout, add_config_paths
+
+from . import load_data
+
+add_config_paths(templateflow=load_data("config.json"))
+
+
+class Layout(BIDSLayout):
+    def __repr__(self):
+        # A tidy summary of key properties
+        s = """\
+TemplateFlow Layout
+ - Home: {}
+ - Templates: {}.""".format(
+            self.root, ", ".join(sorted(self.get_templates()))
+        )
+        return s
