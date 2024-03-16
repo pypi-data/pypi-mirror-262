013837599d6b7044772ee86201289f951fbf7320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 04:05:26 +0000

    Add package odoo12-addon-base-edi/12.0.1.0.1/odoo12_addon_base_edi-12.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo12-addon-base-edi/odoo12_addon_base_edi-12.0.1.0.1-py3-none-any.whl/odoo/addons/base_edi/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 ACSONE
+# @author: Simone Orsi <simahawk@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Base EDI",
+    "summary": """Base module to aggregate EDI features.""",
+    "version": "12.0.1.0.1",
+    "development_status": "Alpha",
+    "license": "LGPL-3",
+    'website': 'https://github.com/OCA/edi',
+    "author": "ACSONE,Odoo Community Association (OCA)",
+    "maintainers": ["simahawk"],
+    "depends": ["base"],
+    "data": [
+        "data/module_category.xml",
+        "security/edi_groups.xml",
+        "views/edi_menu.xml",
+    ],
+}
