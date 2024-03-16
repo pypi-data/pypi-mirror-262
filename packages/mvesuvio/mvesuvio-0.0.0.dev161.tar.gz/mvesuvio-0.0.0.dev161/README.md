0e6eded817f26922abac170052095527b841ccc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 05:05:15 +0000

    Add package mvesuvio/0.0.0.dev161/mvesuvio-0.0.0.dev161.tar.gz

---
+++ packages/mvesuvio/mvesuvio-0.0.0.dev161.tar.gz/mvesuvio-0.0.0.dev161/README.md
@@ -0,0 +1,76 @@
+# VESUIVO REPOSITORY
+
+This repository contains:
+- `mvesuvio` package containing the Optimized NCP analysis procedures, published nightly.
+- Vesuvio calibration script
+
+## mvesuvio package
+
+### Install mamba
+
+To use the `mvesuvio` package you will need to use the `conda` package manager (or preferably  `mamba`, a much faster implementation of `conda`).
+
+This is also the recommended best practice way of using the mantid packages.
+
+To download and install mamba:
+- https://mamba.readthedocs.io/en/latest/installation/mamba-installation.html
+
+### Create and activate an environment for mvesuvio
+
+Firstly create a conda environment where `mvesuvio` is going to be installed:
+- `mamba create -n <environment_name>`
+
+Next activate the environment you created:
+- `mamba activate <environment_name>`
+
+Finally, install `mvesuvio` through the mantid channel:
+- `mamba install -c mantid/label/nightly mvesuvio`
+
+### Using mvesuvio via the command line
+
+You can use `mvesuvio` via the command line. There are two commands available: `config` and `run`.
+
+#### config
+
+The `config` command has three optional arguments:
+- `--set-cache` - Sets the location `mvesuvio` will set up the experiment directory.
+- `--set-experiment` - Sets the current experiment. This creates a new directory in the experiment directory (if not existing) which contains an input file. `mvesuvio` caches and outputs workspaces here.
+- `--set-ipfolder` - Sets the directory in which `mvesuvio` will look for instrument parameter files.
+
+If any of these arguments are not provided a default location/experiment will be selected. These will be output on the running of `mvesuvo config`
+
+Usage examples:
+- `mvesuvio config --set-cache C:\Vesuvio --set-experiment example1 --set-ipfolder C:\IPFolder` - Set cache and IP folder, create a new experiment directory called `example1`.
+- `mvesuvio config --set-experiment example2` - Creates a new experiment directory in `C:\Vesuvio` called `example2`.
+
+#### run
+
+The `run` command has one optional argument:
+- `--yes` - If provided, this argument automatically inputs `Y` when prompted for user input.
+
+Usage example:
+- `mvesuvio run --yes` - Run the vesuvio analysis, automatically providing `Y` when prompted.
+- `mvesuvio run`- Run the vesuvio analysis, will wait for user input when prompted.
+
+### Using mvesuvio via workbench
+
+You can also use `mvesuvio` via `mantidworkbench` if you desire a higher degree of interaction with the output workspaces.
+
+To do this, install workbench into your existing vesuvio conda environment:
+- `mamba install mantidworkbench`
+
+Start workbench using the command line:
+- `workbench`
+
+In the workbench script editor you must first import mvesuvio:
+
+- `import mvesuvio as mv`
+
+After this you can set the config if desired, as above in the command line example. All arguments are optional.
+
+- `mv.set_config(cache_directory='C:\Vesuvio', experiment_id='example3', ip_folder='C:\IPFolder')`
+
+Following the setting of the config, you can use workbench to open and edit the analysis input file created in the relevant experiment directory.
+Once the inputs have been ammended and the file saved, run the analysis:
+
+- `mv.run(yes_to_all=True)`
---
+++ packages/mvesuvio/mvesuvio-0.0.0.dev161.tar.gz/mvesuvio-0.0.0.dev161/mvesuvio/scripts/__init__.py
@@ -0,0 +1,107 @@
+"""Package defining entry points.
+"""
+import argparse
+from os import environ, path
+from mvesuvio.scripts import handle_config
+
+
+def main(manual_args=None):
+    args = __setup_and_parse_args() if not manual_args else manual_args
+    if args.command == "config":
+        __setup_config(args)
+
+    if args.command == "run":
+        if not handle_config.config_set():
+            __setup_config(None)
+        __run_analysis(args.yes)
+
+def __setup_and_parse_args():
+    parser = __set_up_parser()
+    args = parser.parse_args()
+    return args
+
+def __set_up_parser():
+    parser = argparse.ArgumentParser(
+        description="Package to analyse Vesuvio instrument data"
+    )
+    subparsers = parser.add_subparsers(dest="command", required=True)
+    config_parser = subparsers.add_parser("config", help="set mvesuvio configuration")
+    config_parser.add_argument(
+        "--set-cache", "-c", help="set the cache directory", default="", type=str
+    )
+    config_parser.add_argument(
+        "--set-ipfolder",
+        "-p",
+        help="set the intrument parameters directory",
+        default="",
+        type=str,
+    )
+    config_parser.add_argument(
+        "--set-experiment",
+        "-e",
+        help="set the current experiment",
+        default="",
+        type=str,
+    )
+
+    run_parser = subparsers.add_parser("run", help="run mvesuvio analysis")
+    run_parser.add_argument(
+        "--yes", "-y", help="Say yes to all input prompts", action="store_true"
+    )
+    return parser
+
+
+def __setup_config(args):
+    config_dir = handle_config.VESUVIO_CONFIG_PATH
+    handle_config.setup_config_dir(config_dir)
+    ipfolder_dir = handle_config.VESUVIO_IPFOLDER_PATH
+
+    if handle_config.config_set():
+        cache_dir = (
+            handle_config.read_config_var("caching.location")
+            if not args or not args.set_cache
+            else args.set_cache
+        )
+        experiment = (
+            handle_config.read_config_var("caching.experiment")
+            if not args or not args.set_experiment
+            else args.set_experiment
+        )
+        ipfolder_dir = (
+            handle_config.read_config_var("caching.ipfolder")
+            if not args or not args.set_ipfolder
+            else args.set_ipfolder
+        )
+    else:
+        cache_dir = config_dir if not args or not args.set_cache else args.set_cache
+        experiment = (
+            "default" if not args or not args.set_experiment else args.set_experiment
+        )
+        ipfolder_dir = (
+            ipfolder_dir if not args or not args.set_ipfolder else args.set_ipfolder
+        )
+
+        handle_config.setup_default_ipfile_dir()
+
+    handle_config.set_config_vars(
+        {
+            "caching.location": cache_dir,
+            "caching.experiment": experiment,
+            "caching.ipfolder": ipfolder_dir,
+        }
+    )
+    handle_config.setup_expr_dir(cache_dir, experiment)
+    handle_config.check_dir_exists("IP folder", ipfolder_dir)
+
+
+def __run_analysis(yes_to_all):
+    environ["MANTIDPROPERTIES"] = path.join(
+        handle_config.VESUVIO_CONFIG_PATH, "Mantid.user.properties"
+    )
+    from mvesuvio import analysis_runner
+
+    analysis_runner.run(yes_to_all)
+
+
+if __name__ == "__main__":
+    main()
