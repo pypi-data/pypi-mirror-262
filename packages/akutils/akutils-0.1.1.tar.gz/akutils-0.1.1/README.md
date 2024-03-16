1d9ca7f9d1204e98468248ae07a7795c7cd20a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 05:05:12 +0000

    Add package akutils/0.1.1/akutils-0.1.1.tar.gz

---
+++ packages/akutils/akutils-0.1.1.tar.gz/akutils-0.1.1/README.md
@@ -0,0 +1,4 @@
+# akutils
+
+A utility library providing generic methods for performing recurrent data manipulation \
+in Pandas and Python.
---
+++ packages/akutils/akutils-0.1.1.tar.gz/akutils-0.1.1/src/akutils/__init__.py
@@ -0,0 +1,9 @@
+# module level doc-string
+__docformat__ = "restructuredtext"
+__doc__ = """
+utils for py general manipulations
+==================================
+"""
+
+# Import API
+from akutils.api import *
