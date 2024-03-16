ac1a41116ddf467f4ea13687ecc1eb180ba80754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:28 +0000

    Add package fkpackage/0.0.4/fkpackage-0.0.4-py3-none-any.whl

---
+++ packages/fkpackage/fkpackage-0.0.4-py3-none-any.whl/fkpackage/addition.py
@@ -0,0 +1,3 @@
+def add(x,y):
+    print("this is newer version of add function")
+    return x + y
---
+++ packages/fkpackage/fkpackage-0.0.4-py3-none-any.whl/fkpackage/extras/division.py
@@ -0,0 +1,3 @@
+def divide(x,y):
+    print("this is newer version of divide function")
+    return x / y
