a09fc7ca32916e7e9ef6a0fa42eefae31cb6df69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 05:05:13 +0000

    Add package antakia/0.2.24/antakia-0.2.24-py3-none-any.whl

---
+++ packages/antakia/antakia-0.2.24-py3-none-any.whl/antakia/__init__.py
@@ -0,0 +1,10 @@
+"""
+This is antakia package documentation.
+"""
+
+import importlib.metadata
+from antakia.antakia import AntakIA
+
+__version__ = importlib.metadata.version("antakia")
+__author__ = "AI-vidence"
+
