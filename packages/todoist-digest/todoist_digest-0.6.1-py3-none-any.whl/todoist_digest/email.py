5f6bc0d06245883bc148654d3be553fd996991a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 04:04:07 +0000

    Add package todoist-digest/0.6.1/todoist_digest-0.6.1-py3-none-any.whl

---
+++ packages/todoist-digest/todoist_digest-0.6.1-py3-none-any.whl/todoist_digest/email.py
@@ -0,0 +1,42 @@
+import logging
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from urllib.parse import urlparse
+
+import markdown2
+
+logger = logging.getLogger(__name__)
+
+
+def send_markdown_email(auth_url, markdown_content, subject, to_address):
+    parsed_url = urlparse(auth_url)
+    html_content = markdown2.markdown(markdown_content)
+
+    msg = MIMEMultipart()
+    msg["From"] = os.environ.get("EMAIL_FROM", parsed_url.username)
+    msg["To"] = to_address
+    msg["Subject"] = subject
+
+    logger.info(
+        "creating email for %s, from %s, content length %i",
+        to_address,
+        parsed_url.username,
+        len(markdown_content),
+    )
+
+    # this caused the html version to be ignored...
+    # msg.attach(MIMEText(markdown_content, "plain"))
+
+    # TODO any sane HTML styling we can setup?
+    msg.attach(MIMEText(html_content, "html"))
+
+    with smtplib.SMTP_SSL(parsed_url.hostname, parsed_url.port) as server:
+        logger.info("Sending email to %s", to_address)
+
+        login_result = server.login(parsed_url.username, parsed_url.password)
+        logger.info("Login result: %s", login_result)
+        result = server.send_message(msg)
+        logger.info("Send result: %s", result)
+        server.quit()
