e9c173cd6307bf5b522b34bfd90fc5d4c6718894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 06:04:42 +0000

    Add package StatWrap/0.2.16/StatWrap-0.2.16-py3-none-any.whl

---
+++ packages/StatWrap/StatWrap-0.2.16-py3-none-any.whl/tests/test_fpp.py
@@ -0,0 +1,20 @@
+import unittest
+import pandas as pd
+import matplotlib.pyplot as plt
+from statwrap.fpp import apply_pd_changes, sd
+
+class TestStd(unittest.TestCase):
+
+	def setUp(self):
+		self.numbers = [-1,0,1]
+		self.df = pd.DataFrame({'col1': self.numbers})
+
+	def test_sd(self):
+		sd_ = sd(self.numbers)
+		self.assertFalse(sd_ == 1)
+		apply_pd_changes()
+		self.assertTrue(self.df.col1.sd() == sd_)
+		self.assertTrue(sd_ == sd(*self.numbers))
+
+	def tearDown(self):
+		pass
