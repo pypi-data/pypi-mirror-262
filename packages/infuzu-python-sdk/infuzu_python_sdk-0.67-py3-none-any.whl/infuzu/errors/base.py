efbd0917550fe37ab371388fc0a5045b7c013047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 05:05:24 +0000

    Add package infuzu-python-sdk/0.67/infuzu_python_sdk-0.67-py3-none-any.whl

---
+++ packages/infuzu-python-sdk/infuzu_python_sdk-0.67-py3-none-any.whl/infuzu/errors/base.py
@@ -0,0 +1,30 @@
+class InfuzuError(Exception):
+    pass
+
+
+class NotFoundError(InfuzuError):
+    pass
+
+
+class EligibilityError(InfuzuError):
+    pass
+
+
+class InputError(InfuzuError):
+    pass
+
+
+class MissingRequiredFieldError(InputError):
+    pass
+
+
+class InvalidFieldError(InputError):
+    pass
+
+
+class InputFormatError(InputError):
+    pass
+
+
+class InputTypeError(InputError, TypeError):
+    pass
