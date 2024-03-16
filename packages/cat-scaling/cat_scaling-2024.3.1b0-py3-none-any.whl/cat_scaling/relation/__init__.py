b48ec8d24c3568db9ca4bb44e3d5a389705bd360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:36 +0000

    Add package cat-scaling/2024.3.1b0/cat_scaling-2024.3.1b0-py3-none-any.whl

---
+++ packages/cat-scaling/cat_scaling-2024.3.1b0-py3-none-any.whl/cat_scaling/relation/__init__.py
@@ -0,0 +1,4 @@
+from .analysis import AdsorbToDeltaE  # noqa: F401
+from .builder import Builder  # noqa: F401
+from .descriptors import Descriptors  # noqa: F401
+from .relation import DeltaERelation, EadsRelation  # noqa: F401
