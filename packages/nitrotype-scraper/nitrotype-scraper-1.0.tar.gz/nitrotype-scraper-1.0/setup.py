67800bd32f4855c78828012daba37983b58ffe95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 15:04:03 +0000

    Add package nitrotype-scraper/1.0/nitrotype-scraper-1.0.tar.gz

---
+++ packages/nitrotype-scraper/nitrotype-scraper-1.0.tar.gz/nitrotype-scraper-1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nitrotype-scraper',
+    version='1.0',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'cloudscraper',
+        'beautifulsoup4',
+    ],
+    author='Malakai',
+    author_email='Almightyslider2@gmail.com',
+    description="A package to use the nitrotype api and get player or team stats along with accessing the Nitrotype bootstrap",
+    url='https://github.com/yourusername/nitrotype-scraper',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
