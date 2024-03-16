8e8c0f80e5738d519abed4ad000b8a6ef0a61afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:40 +0000

    Add package CTFdHelper/0.0.2/CTFdHelper-0.0.2.tar.gz

---
+++ packages/CTFdHelper/CTFdHelper-0.0.2.tar.gz/CTFdHelper-0.0.2/README.md
@@ -0,0 +1,9 @@
+# CTFdHelper
+
+This is a small project to assist in interacting with CTFd over the command line.
+
+It supports initial configuration and a limited set of features to interact over API
+
+
+## Testing
+
---
+++ packages/CTFdHelper/CTFdHelper-0.0.2.tar.gz/CTFdHelper-0.0.2/src/CTFdHelper/__main__.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+"""
+def show_usage():
+        print("Usage: python3 -m ctfdhelper <ctfd_url> <api_key>")
+
+def main():
+        if len(sys.argv) != 2:
+                show_usage()
+                sys.exit(1)
+
+
+if __name__ == "__main__":
+        main()
+        """
+
+
