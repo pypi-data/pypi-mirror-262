5a03145cf4c7e44c74502b8cf7754fee235a35dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 15:03:57 +0000

    Add package llm-chat-openapi/1.0.7/llm-chat-openapi-1.0.7.tar.gz

---
+++ packages/llm-chat-openapi/llm-chat-openapi-1.0.7.tar.gz/llm-chat-openapi-1.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llm-chat-openapi',
+    version='1.0.7',
+    packages=find_packages(),
+    author='helloxdan',
+    author_email='hello.xdan@gmail.com',
+    description='chat api  collection for popular LLM',
+    long_description='chat api  collection for popular LLM',
+    long_description_content_type='text/markdown',
+    url='https://github.com/helloxdan/llm-chat-openapi',
+    install_requires=[
+        # List of dependencies
+		'flask>=2.0.2',
+		'flask-cors>=4.0.0',
+		'openai>0.19.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
