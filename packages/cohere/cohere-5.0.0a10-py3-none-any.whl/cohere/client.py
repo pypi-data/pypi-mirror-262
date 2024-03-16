4b6092f8edc54e91b5ba3f6c7ba03af8954cd289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:04:39 +0000

    Add package cohere/5.0.0a10/cohere-5.0.0a10-py3-none-any.whl

---
+++ packages/cohere/cohere-5.0.0a10-py3-none-any.whl/cohere/client.py
@@ -0,0 +1,50 @@
+import typing
+
+import httpx
+
+from .base_client import BaseCohere, AsyncBaseCohere
+from .environment import CohereEnvironment
+
+
+class Client(BaseCohere):
+    def __init__(
+            self,
+            api_key: typing.Union[str, typing.Callable[[], str]],
+            *,
+            base_url: typing.Optional[str] = None,
+            environment: CohereEnvironment = CohereEnvironment.PRODUCTION,
+            client_name: typing.Optional[str] = None,
+            timeout: typing.Optional[float] = 60,
+            httpx_client: typing.Optional[httpx.Client] = None,
+    ):
+        BaseCohere.__init__(
+            self,
+            base_url=base_url,
+            environment=environment,
+            client_name=client_name,
+            token=api_key,
+            timeout=timeout,
+            httpx_client=httpx_client,
+        )
+
+
+class AsyncClient(AsyncBaseCohere):
+    def __init__(
+            self,
+            api_key: typing.Union[str, typing.Callable[[], str]],
+            *,
+            base_url: typing.Optional[str] = None,
+            environment: CohereEnvironment = CohereEnvironment.PRODUCTION,
+            client_name: typing.Optional[str] = None,
+            timeout: typing.Optional[float] = 60,
+            httpx_client: typing.Optional[httpx.AsyncClient] = None,
+    ):
+        AsyncBaseCohere.__init__(
+            self,
+            base_url=base_url,
+            environment=environment,
+            client_name=client_name,
+            token=api_key,
+            timeout=timeout,
+            httpx_client=httpx_client,
+        )
