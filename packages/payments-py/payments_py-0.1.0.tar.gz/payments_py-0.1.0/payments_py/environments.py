14b04f227e3e2b179493b41e714a2ed8a0d17d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:04:33 +0000

    Add package payments-py/0.1.0/payments_py-0.1.0.tar.gz

---
+++ packages/payments-py/payments_py-0.1.0.tar.gz/payments_py-0.1.0/payments_py/environments.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Environment(Enum):
+    local = {"frontend": "http://localhost:3000", "backend": "http://localhost:3200"}
+    appStaging = {"frontend": "https://staging.nevermined.app", "backend": "https://one-backend.staging.nevermined.app"}
+    appTesting = {"frontend": "https://testing.nevermined.app", "backend": "https://one-backend.testing.nevermined.app"}
+    appArbitrum = {"frontend": "https://nevermined.app", "backend": "https://one-backend.arbitrum.nevermined.app"}
+    appGnosis = {"frontend": "https://gnosis.nevermined.app", "backend": "https://one-backend.gnosis.nevermined.app"}
+    appMatic = {"frontend": "https://matic.nevermined.app", "backend": "https://one-backend.matic.nevermined.app"}
+    # Define more environments as needed...
