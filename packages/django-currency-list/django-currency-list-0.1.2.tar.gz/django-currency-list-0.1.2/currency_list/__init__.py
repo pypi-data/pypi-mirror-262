6d7212f5ef7fbdb4f02763dfdefa9a48cda154bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:29 +0000

    Add package django-currency-list/0.1.2/django-currency-list-0.1.2.tar.gz

---
+++ packages/django-currency-list/django-currency-list-0.1.2.tar.gz/django-currency-list-0.1.2/currency_list/__init__.py
@@ -0,0 +1,10 @@
+"""A library simplifying the handling of currency fields in Django projects."""
+
+from .core import Currency
+
+CurrencyChoices = Currency.CurrencyChoices
+currency_code_list = Currency.currency_code_list
+get_label = Currency.get_label
+currency_dict = Currency.currency_dict
+
+__all__ = ['Currency', 'CurrencyChoices', 'currency_code_list', 'get_label', 'currency_dict']
