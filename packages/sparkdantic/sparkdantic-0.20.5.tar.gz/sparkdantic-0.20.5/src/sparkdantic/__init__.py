d83af0a07742a037bb8bfbc2d7eb3b1ff52e3126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:38 +0000

    Add package sparkdantic/0.20.5/sparkdantic-0.20.5.tar.gz

---
+++ packages/sparkdantic/sparkdantic-0.20.5.tar.gz/sparkdantic-0.20.5/src/sparkdantic/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.20.5'
+__author__ = 'Mitchell Lisle'
+__email__ = 'm.lisle90@gmail.com'
+
+from sparkdantic.generation import ColumnGenerationSpec
+from sparkdantic.model import ColumnSpecs, SparkModel
