d42656af0254bd3824327180bcd6e7dbfa090248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:04:34 +0000

    Add package pyJay/1.0.4/pyJay-1.0.4.tar.gz

---
+++ packages/pyJay/pyJay-1.0.4.tar.gz/pyJay-1.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.4'
+DESCRIPTION = 'Develop games easier'
+LONG_DESCRIPTION = 'PyJay is a module that acts like a game engine for Pygame. Define buttons, animations, and more. View the documentation at connorlayson.github.io/pyJay'
+
+# Setting up
+setup(
+    name="pyJay",
+    version=VERSION,
+    author="Connor Layson",
+    author_email="<clayson2573@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pygame'],
+    keywords=['python', 'pygame', 'game'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
