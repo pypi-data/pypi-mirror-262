359bf198eb4fdde04f18a6cb5ee578c8767666f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 05:04:34 +0000

    Add package coinprice/1.1.1/coinprice-1.1.1.tar.gz

---
+++ packages/coinprice/coinprice-1.1.1.tar.gz/coinprice-1.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='coinprice',
+    version='1.1.1',
+    author='Bohdan',
+    author_email='Bohd4n@proton.me',
+    description='CryptoTracker is a command-line tool for tracking cryptocurrency prices across multiple exchanges.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['requests>=2.25.1'],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    keywords='cryptocurrency, crypto, bitcoin, ethereum, tracking, prices, finance, binance, okx, bybit, coinbase',
+    project_urls={
+        'GitHub': 'https://github.com/7GitGuru/crypto-tracker/tree/cmd',
+        'Documentation': 'https://github.com/7GitGuru/crypto-tracker/blob/cmd/README.md'
+    },
+    entry_points={
+        'console_scripts': [
+            'price = crypto_tracker.main:main',
+        ],
+    },
+    python_requires='>=3.8'
+
+)
