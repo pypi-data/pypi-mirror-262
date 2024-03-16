474ccea09cbdc23652b722a0118fca796974fdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 15:04:03 +0000

    Add package pegasuspy/1.9.1/pegasuspy-1.9.1-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/pegasuspy/pegasuspy-1.9.1-cp311-cp311-macosx_10_9_x86_64.whl/pegasus/plotting/__init__.py
@@ -0,0 +1,18 @@
+from .plot_library import (
+    scatter,
+    compo_plot,
+    scatter_groups,
+    spatial,
+    violin,
+    heatmap,
+    dotplot,
+    dendrogram,
+    hvfplot,
+    qcviolin,
+    volcano,
+    rank_plot,
+    ridgeplot,
+    wordcloud,
+    plot_gsea,
+    elbowplot,
+)
