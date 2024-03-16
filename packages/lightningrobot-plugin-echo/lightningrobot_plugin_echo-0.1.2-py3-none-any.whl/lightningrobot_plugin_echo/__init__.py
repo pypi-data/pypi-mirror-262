a394dd4e4570767c5099431ee8b62bb72d754a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:05:17 +0000

    Add package lightningrobot-plugin-echo/0.1.2/lightningrobot_plugin_echo-0.1.2-py3-none-any.whl

---
+++ packages/lightningrobot-plugin-echo/lightningrobot_plugin_echo-0.1.2-py3-none-any.whl/lightningrobot_plugin_echo/__init__.py
@@ -0,0 +1,15 @@
+import re
+from lightningrobot.plugin import Plugin
+def __init__(self, adapter):
+    self.adapter = adapter  # 存储适配器引用
+
+class Main(Plugin):
+    async def command(self, message,event_type,id):
+        if re.search(r"^(#|/)?(echo|复读)$", message):
+            echo(message,event_type,id)
+        else:
+            pass
+
+async def echo(self,message,event_type,id):
+    text = re.sub(r"^(#|/)?(echo|复读)$","",message)
+    await self.adapter.send_message(event_type,id,text)
