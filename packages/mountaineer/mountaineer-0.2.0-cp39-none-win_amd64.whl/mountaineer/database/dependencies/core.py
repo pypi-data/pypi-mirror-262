b1a630c3821a0aecc46589a2fc501e9d279b1879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 04:05:24 +0000

    Add package mountaineer/0.2.0/mountaineer-0.2.0-cp39-none-win_amd64.whl

---
+++ packages/mountaineer/mountaineer-0.2.0-cp39-none-win_amd64.whl/mountaineer/database/dependencies/core.py
@@ -0,0 +1,24 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
+
+from mountaineer.database.config import DatabaseConfig
+from mountaineer.dependencies import CoreDependencies
+
+
+def get_db(
+    config: DatabaseConfig = Depends(
+        CoreDependencies.get_config_with_type(DatabaseConfig)
+    ),
+):
+    if not config.SQLALCHEMY_DATABASE_URI:
+        raise RuntimeError("No SQLALCHEMY_DATABASE_URI set")
+
+    return create_async_engine(str(config.SQLALCHEMY_DATABASE_URI))
+
+
+async def get_db_session(
+    engine: AsyncEngine = Depends(get_db),
+):
+    session_maker = async_sessionmaker(engine, expire_on_commit=False)
+    async with session_maker() as session:
+        yield session
