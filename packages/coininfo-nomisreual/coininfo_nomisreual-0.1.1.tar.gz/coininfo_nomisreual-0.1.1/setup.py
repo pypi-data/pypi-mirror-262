78935afdc56ce2867b76bed691d402b1155cc387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:04:18 +0000

    Add package coininfo-nomisreual/0.1.1/coininfo_nomisreual-0.1.1.tar.gz

---
+++ packages/coininfo-nomisreual/coininfo_nomisreual-0.1.1.tar.gz/coininfo_nomisreual-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as file:
+    description = file.read()
+
+setup(
+    name="coininfo_nomisreual",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=["requests==2.31.0"],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
