e78b288e548f6be6059500133607ffddbb6f0e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:04:55 +0000

    Add package paravastu/1.2.1/paravastu-1.2.1.tar.gz

---
+++ packages/paravastu/paravastu-1.2.1.tar.gz/paravastu-1.2.1/paravastu/__init__.py
@@ -0,0 +1,2 @@
+from .navigation import double_click, double_click_button, fix_dropbox_location, translate_path, change_directory, do_nothing
+from .proteins import all 
---
+++ packages/paravastu/paravastu-1.2.1.tar.gz/paravastu-1.2.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="paravastu", # Replace with your own username
+    version="1.2.1",
+    author="Paravastu Lab",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        'numpy',
+        'pandas',
+        'biopandas',
+        'dtale',
+	    'plotnine',
+        "glob",
+        "matplotlib",
+        "matplotlib.pyplot",
+        "itertools"
+    ]
+)
