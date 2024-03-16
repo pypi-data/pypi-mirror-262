bcdec63274197bebff2f1cc26606abbffd7e6623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 16:04:56 +0000

    Add package niutrader/0.0.14/niutrader-0.0.14.tar.gz

---
+++ packages/niutrader/niutrader-0.0.14.tar.gz/niutrader-0.0.14/niutrader/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import urllib3
+
+from niutrader import exceptions
+from niutrader.api import use  # , follower
+from niutrader.log import logger
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+__version__ = "0.0.14"
+__author__ = "NiuGe"
