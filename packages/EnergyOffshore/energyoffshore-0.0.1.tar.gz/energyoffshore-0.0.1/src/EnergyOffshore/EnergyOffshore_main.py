28f02cb79399f760ecd78f80d616313d0fea2382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 04:04:54 +0000

    Add package EnergyOffshore/0.0.1/energyoffshore-0.0.1.tar.gz

---
+++ packages/EnergyOffshore/energyoffshore-0.0.1.tar.gz/energyoffshore-0.0.1/src/EnergyOffshore/EnergyOffshore_main.py
@@ -0,0 +1,15 @@
+"""
+THIS IS THE VERY FIRST ITERATION OF THE ENERGY OFFSHORE
+"""
+import xarray as xr
+import numpy as np
+
+
+def main():
+    '''
+    call all other functions
+    '''
+
+if __name__ == '__main__':
+    '''EXECUTE ENERGY OFFSHORE -- STATISTICS IN SUPPORT OF SITING'''
+    main()
