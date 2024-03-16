43966ffc74270ab3d1a1ff128b2d99465cc655b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 15:03:58 +0000

    Add package cs.app.myke/20240316.1/cs.app.myke-20240316.1-py3-none-any.whl

---
+++ packages/cs.app.myke/cs.app.myke-20240316.1-py3-none-any.whl/cs/app/myke/__init__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+''' My make programme, a parallel make tool with superior expression syntax.
+'''
+
+__version__ = '20240316.1'
+
+DISTINFO = {
+    'description':
+    "my make program; parallel make tool with superior expression syntax",
+    'keywords': ["python3"],
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    'install_requires': [
+        'cs.cmdutils',
+        'cs.debug',
+        'cs.deco',
+        'cs.excutils',
+        'cs.inttypes',
+        'cs.later',
+        'cs.lex',
+        'cs.logutils',
+        'cs.pfx',
+        'cs.py.func',
+        'cs.queues',
+        'cs.result',
+        'cs.threads',
+    ],
+    'entry_points': {
+        'console_scripts': {
+            'myke': 'cs.app.myke.__main__:main',
+        },
+    },
+}
+
+DEFAULT_MAKE_COMMAND = 'myke'
