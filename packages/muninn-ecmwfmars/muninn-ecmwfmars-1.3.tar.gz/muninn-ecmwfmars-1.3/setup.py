ef276eb3052e8d8a94fbfbd2f9ab069bdcaebeca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:05:04 +0000

    Add package muninn-ecmwfmars/1.3/muninn-ecmwfmars-1.3.tar.gz

---
+++ packages/muninn-ecmwfmars/muninn-ecmwfmars-1.3.tar.gz/muninn-ecmwfmars-1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="muninn-ecmwfmars",
+    version="1.3",
+    description="Muninn extension for ECMWF GRIB products from the mars archive",
+    url="https://github.com/stcorp/muninn-ecmwfmars",
+    author="S[&]T",
+    license="BSD",
+    py_modules=["muninn_ecmwfmars"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Environment :: Plugins",
+    ],
+    install_requires=["muninn"]
+)
