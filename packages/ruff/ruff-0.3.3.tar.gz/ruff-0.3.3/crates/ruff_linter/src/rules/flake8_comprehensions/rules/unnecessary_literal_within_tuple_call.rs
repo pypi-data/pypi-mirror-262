01a8de2f240a8f2818192cb7e25a21684c6db994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 05:04:35 +0000

    Add package ruff/0.3.3/ruff-0.3.3.tar.gz

---
+++ packages/ruff/ruff-0.3.3.tar.gz/ruff-0.3.3/crates/ruff_linter/src/rules/flake8_comprehensions/rules/unnecessary_literal_within_tuple_call.rs
@@ -0,0 +1,115 @@
+use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
+use ruff_macros::{derive_message_formats, violation};
+use ruff_python_ast::{self as ast, Expr};
+use ruff_text_size::{Ranged, TextSize};
+
+use crate::checkers::ast::Checker;
+
+use super::helpers;
+
+/// ## What it does
+/// Checks for `tuple` calls that take unnecessary list or tuple literals as
+/// arguments.
+///
+/// ## Why is this bad?
+/// It's unnecessary to use a list or tuple literal within a `tuple()` call,
+/// since there is a literal syntax for these types.
+///
+/// If a list literal was passed, then it should be rewritten as a `tuple`
+/// literal. Otherwise, if a tuple literal was passed, then the outer call
+/// to `tuple()` should be removed.
+///
+/// ## Examples
+/// ```python
+/// tuple([1, 2])
+/// tuple((1, 2))
+/// ```
+///
+/// Use instead:
+/// ```python
+/// (1, 2)
+/// (1, 2)
+/// ```
+///
+/// ## Fix safety
+/// This rule's fix is marked as unsafe, as it may occasionally drop comments
+/// when rewriting the call. In most cases, though, comments will be preserved.
+#[violation]
+pub struct UnnecessaryLiteralWithinTupleCall {
+    literal: String,
+}
+
+impl AlwaysFixableViolation for UnnecessaryLiteralWithinTupleCall {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let UnnecessaryLiteralWithinTupleCall { literal } = self;
+        if literal == "list" {
+            format!(
+                "Unnecessary `{literal}` literal passed to `tuple()` (rewrite as a `tuple` literal)"
+            )
+        } else {
+            format!(
+                "Unnecessary `{literal}` literal passed to `tuple()` (remove the outer call to `tuple()`)"
+            )
+        }
+    }
+
+    fn fix_title(&self) -> String {
+        let UnnecessaryLiteralWithinTupleCall { literal } = self;
+        {
+            if literal == "list" {
+                "Rewrite as a `tuple` literal".to_string()
+            } else {
+                "Remove outer `tuple` call".to_string()
+            }
+        }
+    }
+}
+
+/// C409
+pub(crate) fn unnecessary_literal_within_tuple_call(checker: &mut Checker, call: &ast::ExprCall) {
+    if !call.arguments.keywords.is_empty() {
+        return;
+    }
+    let Some(argument) =
+        helpers::first_argument_with_matching_function("tuple", &call.func, &call.arguments.args)
+    else {
+        return;
+    };
+    if !checker.semantic().is_builtin("tuple") {
+        return;
+    }
+    let argument_kind = match argument {
+        Expr::Tuple(_) => "tuple",
+        Expr::List(_) => "list",
+        _ => return,
+    };
+
+    let mut diagnostic = Diagnostic::new(
+        UnnecessaryLiteralWithinTupleCall {
+            literal: argument_kind.to_string(),
+        },
+        call.range(),
+    );
+
+    // Convert `tuple([1, 2])` to `tuple(1, 2)`
+    diagnostic.set_fix({
+        // Replace from the start of the call to the start of the inner list or tuple with `(`.
+        let call_start = Edit::replacement(
+            "(".to_string(),
+            call.start(),
+            argument.start() + TextSize::from(1),
+        );
+
+        // Replace from the end of the inner list or tuple to the end of the call with `)`.
+        let call_end = Edit::replacement(
+            ")".to_string(),
+            argument.end() - TextSize::from(1),
+            call.end(),
+        );
+
+        Fix::unsafe_edits(call_start, [call_end])
+    });
+
+    checker.diagnostics.push(diagnostic);
+}
---
+++ packages/ruff/ruff-0.3.3.tar.gz/ruff-0.3.3/crates/ruff_linter/src/rules/flake8_simplify/rules/needless_bool.rs
@@ -0,0 +1,223 @@
+use ruff_diagnostics::{Diagnostic, Edit, Fix, FixAvailability, Violation};
+use ruff_macros::{derive_message_formats, violation};
+use ruff_python_ast::{self as ast, Arguments, ElifElseClause, Expr, ExprContext, Stmt};
+use ruff_python_semantic::analyze::typing::{is_sys_version_block, is_type_checking_block};
+use ruff_text_size::{Ranged, TextRange};
+
+use crate::checkers::ast::Checker;
+use crate::fix::snippet::SourceCodeSnippet;
+
+/// ## What it does
+/// Checks for `if` statements that can be replaced with `bool`.
+///
+/// ## Why is this bad?
+/// `if` statements that return `True` for a truthy condition and `False` for
+/// a falsey condition can be replaced with boolean casts.
+///
+/// ## Example
+/// ```python
+/// if foo:
+///     return True
+/// else:
+///     return False
+/// ```
+///
+/// Use instead:
+/// ```python
+/// return bool(foo)
+/// ```
+///
+/// ## References
+/// - [Python documentation: Truth Value Testing](https://docs.python.org/3/library/stdtypes.html#truth-value-testing)
+#[violation]
+pub struct NeedlessBool {
+    condition: SourceCodeSnippet,
+    replacement: Option<SourceCodeSnippet>,
+}
+
+impl Violation for NeedlessBool {
+    const FIX_AVAILABILITY: FixAvailability = FixAvailability::Sometimes;
+
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let NeedlessBool { condition, .. } = self;
+        if let Some(condition) = condition.full_display() {
+            format!("Return the condition `{condition}` directly")
+        } else {
+            format!("Return the condition directly")
+        }
+    }
+
+    fn fix_title(&self) -> Option<String> {
+        let NeedlessBool { replacement, .. } = self;
+        if let Some(replacement) = replacement
+            .as_ref()
+            .and_then(SourceCodeSnippet::full_display)
+        {
+            Some(format!("Replace with `{replacement}`"))
+        } else {
+            Some(format!("Inline condition"))
+        }
+    }
+}
+
+/// SIM103
+pub(crate) fn needless_bool(checker: &mut Checker, stmt_if: &ast::StmtIf) {
+    let ast::StmtIf {
+        test: if_test,
+        body: if_body,
+        elif_else_clauses,
+        range: _,
+    } = stmt_if;
+
+    // Extract an `if` or `elif` (that returns) followed by an else (that returns the same value)
+    let (if_test, if_body, else_body, range) = match elif_else_clauses.as_slice() {
+        // if-else case
+        [ElifElseClause {
+            body: else_body,
+            test: None,
+            ..
+        }] => (if_test.as_ref(), if_body, else_body, stmt_if.range()),
+        // elif-else case
+        [.., ElifElseClause {
+            body: elif_body,
+            test: Some(elif_test),
+            range: elif_range,
+        }, ElifElseClause {
+            body: else_body,
+            test: None,
+            range: else_range,
+        }] => (
+            elif_test,
+            elif_body,
+            else_body,
+            TextRange::new(elif_range.start(), else_range.end()),
+        ),
+        _ => return,
+    };
+
+    let (Some(if_return), Some(else_return)) = (
+        is_one_line_return_bool(if_body),
+        is_one_line_return_bool(else_body),
+    ) else {
+        return;
+    };
+
+    // Determine whether the return values are inverted, as in:
+    // ```python
+    // if x > 0:
+    //     return False
+    // else:
+    //     return True
+    // ```
+    let inverted = match (if_return, else_return) {
+        (Bool::True, Bool::False) => false,
+        (Bool::False, Bool::True) => true,
+        // If the branches have the same condition, abort (although the code could be
+        // simplified).
+        _ => return,
+    };
+
+    // Avoid suggesting ternary for `if sys.version_info >= ...`-style checks.
+    if is_sys_version_block(stmt_if, checker.semantic()) {
+        return;
+    }
+
+    // Avoid suggesting ternary for `if TYPE_CHECKING:`-style checks.
+    if is_type_checking_block(stmt_if, checker.semantic()) {
+        return;
+    }
+
+    let condition = checker.locator().slice(if_test);
+    let replacement = if checker.indexer().has_comments(&range, checker.locator()) {
+        None
+    } else {
+        // If the return values are inverted, wrap the condition in a `not`.
+        if inverted {
+            let node = ast::StmtReturn {
+                value: Some(Box::new(Expr::UnaryOp(ast::ExprUnaryOp {
+                    op: ast::UnaryOp::Not,
+                    operand: Box::new(if_test.clone()),
+                    range: TextRange::default(),
+                }))),
+                range: TextRange::default(),
+            };
+            Some(checker.generator().stmt(&node.into()))
+        } else if if_test.is_compare_expr() {
+            // If the condition is a comparison, we can replace it with the condition, since we
+            // know it's a boolean.
+            let node = ast::StmtReturn {
+                value: Some(Box::new(if_test.clone())),
+                range: TextRange::default(),
+            };
+            Some(checker.generator().stmt(&node.into()))
+        } else if checker.semantic().is_builtin("bool") {
+            // Otherwise, we need to wrap the condition in a call to `bool`.
+            let func_node = ast::ExprName {
+                id: "bool".into(),
+                ctx: ExprContext::Load,
+                range: TextRange::default(),
+            };
+            let value_node = ast::ExprCall {
+                func: Box::new(func_node.into()),
+                arguments: Arguments {
+                    args: Box::from([if_test.clone()]),
+                    keywords: Box::from([]),
+                    range: TextRange::default(),
+                },
+                range: TextRange::default(),
+            };
+            let return_node = ast::StmtReturn {
+                value: Some(Box::new(value_node.into())),
+                range: TextRange::default(),
+            };
+            Some(checker.generator().stmt(&return_node.into()))
+        } else {
+            None
+        }
+    };
+
+    let mut diagnostic = Diagnostic::new(
+        NeedlessBool {
+            condition: SourceCodeSnippet::from_str(condition),
+            replacement: replacement.clone().map(SourceCodeSnippet::new),
+        },
+        range,
+    );
+    if let Some(replacement) = replacement {
+        diagnostic.set_fix(Fix::unsafe_edit(Edit::range_replacement(
+            replacement,
+            range,
+        )));
+    }
+    checker.diagnostics.push(diagnostic);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Bool {
+    True,
+    False,
+}
+
+impl From<bool> for Bool {
+    fn from(value: bool) -> Self {
+        if value {
+            Bool::True
+        } else {
+            Bool::False
+        }
+    }
+}
+
+fn is_one_line_return_bool(stmts: &[Stmt]) -> Option<Bool> {
+    let [stmt] = stmts else {
+        return None;
+    };
+    let Stmt::Return(ast::StmtReturn { value, range: _ }) = stmt else {
+        return None;
+    };
+    let Some(Expr::BooleanLiteral(ast::ExprBooleanLiteral { value, .. })) = value.as_deref() else {
+        return None;
+    };
+    Some((*value).into())
+}
