1c388036ccbf5b10c24301a4be17045d00befba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:17 +0000

    Add package stat-prob/1.0/stat_prob-1.0.tar.gz

---
+++ packages/stat-prob/stat_prob-1.0.tar.gz/stat_prob-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stat_prob',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['stat_prob'],
+      author= 'Antonio Nebres', 
+      author_email= 'anebres.ubiqus@gmail.com',
+      zip_safe=False)
