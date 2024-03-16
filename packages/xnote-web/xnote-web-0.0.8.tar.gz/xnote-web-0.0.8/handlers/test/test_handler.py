1adbc59ece7a37e4f4d6243c320f840b9f5ad802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 16:04:57 +0000

    Add package xnote-web/0.0.8/xnote-web-0.0.8.tar.gz

---
+++ packages/xnote-web/xnote-web-0.0.8.tar.gz/xnote-web-0.0.8/handlers/test/test_handler.py
@@ -0,0 +1,23 @@
+
+import xutils
+from xnote.core import xtemplate
+
+class handler:    
+    def GET(self):
+        return "success"
+
+
+class ExampleHandler:
+
+    def GET(self):
+        name = xutils.get_argument("name", "")
+        if name == "":
+            return xtemplate.render("test/page/example_index.html")
+        else:
+            return xtemplate.render("test/page/example_%s.html" % name)
+
+
+xurls = (
+    r"/test", handler,
+    r"/test/example", ExampleHandler,
+)
