04619b4c83043cf1c3b7d45dda8046f6385a251c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 16:05:05 +0000

    Add package narwhals/0.4.1/narwhals-0.4.1.tar.gz

---
+++ packages/narwhals/narwhals-0.4.1.tar.gz/narwhals-0.4.1/narwhals/translate.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from narwhals.dependencies import get_pandas
+from narwhals.dependencies import get_polars
+
+if TYPE_CHECKING:
+    from narwhals.dataframe import DataFrame
+    from narwhals.series import Series
+    from narwhals.typing import T
+
+
+def to_native(obj: DataFrame[T] | Series[T]) -> T:
+    from narwhals.dataframe import DataFrame
+    from narwhals.series import Series
+
+    if isinstance(obj, DataFrame):
+        return (  # type: ignore[no-any-return]
+            obj._dataframe
+            if obj._implementation == "polars"
+            else obj._dataframe._dataframe
+        )
+    if isinstance(obj, Series):
+        return obj._series if obj._implementation == "polars" else obj._series._series  # type: ignore[no-any-return]
+
+    msg = f"Expected Narwhals object, got {type(obj)}."
+    raise TypeError(msg)
+
+
+__all__ = [
+    "get_pandas",
+    "get_polars",
+    "to_native",
+]
