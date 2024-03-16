401f28ce4f428ae7d437998336caf9e1407fab1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 05:04:48 +0000

    Add package paramount/0.2.5/paramount-0.2.5-py3-none-any.whl

---
+++ packages/paramount/paramount-0.2.5-py3-none-any.whl/paramount/server/db_connector/csv.py
@@ -0,0 +1,23 @@
+# CSV implementation
+from .db import Database
+import os
+import pandas as pd
+
+
+class CSVDatabase(Database):
+    def create_or_append(self, df, table_name, primary_key=None):
+        # Check if the file exists, and if not, create it with header, else append without header
+        filename = table_name+'.csv'
+        if not os.path.isfile(filename):
+            df.to_csv(filename, mode='a', index=False)
+        else:
+            df.to_csv(filename, mode='a', index=False, header=False)
+
+    def table_exists(self, table_name):
+        return os.path.isfile(table_name+'.csv')
+
+    def update_ground_truth(self, df, table_name):
+        df.to_csv(table_name+'.csv', index=False)
+
+    def get_table(self, table_name, all_rows=None, identifier_column_name=None, identifier_value=None):
+        return pd.read_csv(table_name+'.csv')
