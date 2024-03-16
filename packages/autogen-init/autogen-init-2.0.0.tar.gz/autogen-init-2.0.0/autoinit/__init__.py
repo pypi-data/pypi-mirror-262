6db5c37bda093e269a16727238b3fbb16eff3177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 15:03:47 +0000

    Add package autogen-init/2.0.0/autogen-init-2.0.0.tar.gz

---
+++ packages/autogen-init/autogen-init-2.0.0.tar.gz/autogen-init-2.0.0/autoinit/__init__.py
@@ -0,0 +1,7 @@
+from .core import run, find_defs
+
+__all__ = ["run", "find_defs"]
+__version__ = "2.0.0"
+__author__ = (
+    "daniel park <parkminwoo1991@gmail.com>, antonio cheang <teapotv8@proton.me>"
+)
