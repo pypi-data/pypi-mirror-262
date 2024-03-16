568043db60ccdf62d5e45138f71bca26f8246f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 16:05:13 +0000

    Add package peter-explains/0.0.2/peter-explains-0.0.2.tar.gz

---
+++ packages/peter-explains/peter-explains-0.0.2.tar.gz/peter-explains-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="peter-explains",
+    version="0.0.2",
+    description="Linux command explanations from Peter Griffin",
+    author="Atick Faisal",
+    author_email="atickfaisal@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
