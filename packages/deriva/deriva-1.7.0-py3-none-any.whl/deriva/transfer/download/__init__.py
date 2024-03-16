8eb89374f0631036f55f72404edad4aee84e6ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:08 +0000

    Add package deriva/1.7.0/deriva-1.7.0-py3-none-any.whl

---
+++ packages/deriva/deriva-1.7.0-py3-none-any.whl/deriva/transfer/download/__init__.py
@@ -0,0 +1,18 @@
+class DerivaDownloadError(RuntimeError):
+    pass
+
+
+class DerivaDownloadConfigurationError(ValueError):
+    pass
+
+
+class DerivaDownloadAuthenticationError(Exception):
+    pass
+
+
+class DerivaDownloadAuthorizationError(Exception):
+    pass
+
+
+class DerivaDownloadTimeoutError(Exception):
+    pass
