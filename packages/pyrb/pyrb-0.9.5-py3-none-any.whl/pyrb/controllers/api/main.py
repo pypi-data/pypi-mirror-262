3604497f0ab555b8687cc35808d0e28dcfc82ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 15:03:58 +0000

    Add package pyrb/0.9.5/pyrb-0.9.5-py3-none-any.whl

---
+++ packages/pyrb/pyrb-0.9.5-py3-none-any.whl/pyrb/controllers/api/main.py
@@ -0,0 +1,117 @@
+import datetime
+from uuid import UUID
+from zoneinfo import ZoneInfo
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import AwareDatetime, BaseModel
+from starlette.status import HTTP_201_CREATED
+
+from pyrb.controllers.api.deps import AccountServiceDep, RebalanceContextDep
+from pyrb.enums import AssetAllocationStrategyEnum, BrokerageType
+from pyrb.exceptions import InitializationError
+from pyrb.models.account import Account, AccountFactory
+from pyrb.models.order import OrderPlacementResult
+from pyrb.models.position import Position
+from pyrb.services.rebalance import Rebalancer
+from pyrb.services.strategy.asset_allocate import AssetAllocationStrategyFactory
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/ping")
+async def ping() -> str:
+    return "pong"
+
+
+class AccountResponse(BaseModel):
+    account: Account
+
+
+class AccountCreateRequest(BaseModel):
+    brokerage: BrokerageType
+    app_key: str
+    secret_key: str
+
+
+class AccountCreateResponse(BaseModel):
+    account_id: UUID
+
+
+class PortfolioResponse(BaseModel):
+    total_value: float
+    cash_balance: float
+    positions: list[Position]
+
+
+class RebalanceRequest(BaseModel):
+    investment_amount: float | None
+
+
+class RebalanceResponse(BaseModel):
+    rebalanced_at: AwareDatetime
+    placed_orders: list[OrderPlacementResult]
+
+
+@app.get("/accounts/default", response_model=AccountResponse)
+async def get_default_account(account_service: AccountServiceDep) -> AccountResponse:
+    try:
+        account = account_service.get()
+    except InitializationError as e:
+        raise HTTPException(status_code=404, detail="No accounts registered") from e
+
+    return AccountResponse(account=account)
+
+
+@app.post("/accounts", response_model=AccountCreateResponse, status_code=HTTP_201_CREATED)
+async def create_account(
+    account_service: AccountServiceDep, body: AccountCreateRequest
+) -> AccountCreateResponse:
+    account = AccountFactory.create(
+        brokerage=body.brokerage, app_key=body.app_key, app_secret=body.secret_key
+    )
+    account_service.set(account)
+
+    return AccountCreateResponse(account_id=account.id)
+
+
+@app.get("/portfolio", response_model=PortfolioResponse)
+async def get_portfolio(context: RebalanceContextDep) -> PortfolioResponse:
+    portfolio = context.portfolio
+
+    return PortfolioResponse(
+        total_value=portfolio.total_value,
+        cash_balance=portfolio.cash_balance,
+        positions=portfolio.positions,
+    )
+
+
+# TODO: Swagger에서 StrEnum이 제대로 표시되지 않는 문제 원인 파악 후 수정
+@app.post("/strategies/{strategy_type}/rebalance", response_model=RebalanceResponse)
+async def rebalance(
+    context: RebalanceContextDep, strategy_type: AssetAllocationStrategyEnum, body: RebalanceRequest
+) -> RebalanceResponse:
+    strategy = AssetAllocationStrategyFactory.create(strategy_type)
+    rebalancer = Rebalancer(context, strategy)
+
+    orders = rebalancer.prepare_orders(investment_amount=body.investment_amount)
+    placed_orders = rebalancer.place_orders(orders)
+
+    return RebalanceResponse(
+        rebalanced_at=datetime.datetime.now(ZoneInfo("Asia/Seoul")),
+        placed_orders=placed_orders,
+    )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
