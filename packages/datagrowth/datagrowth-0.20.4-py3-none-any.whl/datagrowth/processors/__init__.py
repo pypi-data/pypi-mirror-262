c4f2d94d2e07c83e6630d62757a6ffd64620a92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 04:04:47 +0000

    Add package datagrowth/0.20.4/datagrowth-0.20.4-py3-none-any.whl

---
+++ packages/datagrowth/datagrowth-0.20.4-py3-none-any.whl/datagrowth/processors/__init__.py
@@ -0,0 +1,4 @@
+from .base import Processor, QuerySetProcessor, ProcessorFactory
+from .growth import GrowthProcessor
+from .input import ExtractProcessor, HttpSeedingProcessor, SeedingProcessorFactory
+from .resources.growth import HttpGrowthProcessor
