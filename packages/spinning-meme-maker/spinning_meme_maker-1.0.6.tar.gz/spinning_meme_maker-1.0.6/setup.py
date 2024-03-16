e00a430c2a6ef02f5631d950a02f7a23775f5b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 15:04:04 +0000

    Add package spinning-meme-maker/1.0.6/spinning_meme_maker-1.0.6.tar.gz

---
+++ packages/spinning-meme-maker/spinning_meme_maker-1.0.6.tar.gz/spinning_meme_maker-1.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='spinning_meme_maker',
+    version='1.0.6',
+    description='A package for creating spinning meme GIFs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Overtimepog',
+    author_email='truen2005@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'panda3d',
+        'Pillow',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'spin=spinning_meme_maker.spin:spinning_model',
+        ],
+    },
+)
