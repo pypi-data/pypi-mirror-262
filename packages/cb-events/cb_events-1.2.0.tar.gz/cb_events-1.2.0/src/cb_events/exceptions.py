fdcc5ffccbc9fd06beba9df9bd8713aec5d4519c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 04:05:02 +0000

    Add package cb-events/1.2.0/cb_events-1.2.0.tar.gz

---
+++ packages/cb-events/cb_events-1.2.0.tar.gz/cb_events-1.2.0/src/cb_events/exceptions.py
@@ -0,0 +1,9 @@
+"""Exceptions for Chaturbate poller."""
+
+
+class ChaturbateAPIError(Exception):
+    """Exception raised for errors in the Chaturbate API."""
+
+
+class BaseURLError(Exception):
+    """Raise exception when BASE_URL is missing or incorrect."""
