11f47e501f48d65bc73163295b4120aee1b9ab84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 04:03:56 +0000

    Add package 00-viet-nam-on-top-00/9999/00_viet_nam_on_top_00-9999.tar.gz

---
+++ packages/00-viet-nam-on-top-00/00_viet_nam_on_top_00-9999.tar.gz/00_viet_nam_on_top_00-9999/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='00_viet_nam_on_top_00',
+    version='9999',
+    author='Duy Van To',
+    author_email='tovanduydeptraitop1vn@gmail.com',
+    description='Tô Văn Duy top 1 VN',
+)
