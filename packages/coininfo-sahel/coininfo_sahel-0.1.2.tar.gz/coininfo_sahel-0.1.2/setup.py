79071ab4f2a84fcf50ddccbc117ceeaa61bd3104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:04:18 +0000

    Add package coininfo-sahel/0.1.2/coininfo_sahel-0.1.2.tar.gz

---
+++ packages/coininfo-sahel/coininfo_sahel-0.1.2.tar.gz/coininfo_sahel-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md") as file:
+    description = file.read()
+
+setup(
+    name="coininfo_sahel",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=["requests==2.31.0"],
+    long_description=description, 
+    long_description_content_type="text/markdown"
+)
