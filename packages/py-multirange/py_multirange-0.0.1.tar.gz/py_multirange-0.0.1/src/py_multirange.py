44d23e7a4eaed5a8d3607dd3beda1b3d7e0b19a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 15:04:16 +0000

    Add package py-multirange/0.0.1/py_multirange-0.0.1.tar.gz

---
+++ packages/py-multirange/py_multirange-0.0.1.tar.gz/py_multirange-0.0.1/src/py_multirange.py
@@ -0,0 +1,12 @@
+"""
+py_multirange.py
+
+Created: 16th March 2024
+
+Configurable iterator library for a generalised product of iterators,
+
+In some respects, similar to itertools.product.
+
+@author: Purple Elephant
+"""
+
