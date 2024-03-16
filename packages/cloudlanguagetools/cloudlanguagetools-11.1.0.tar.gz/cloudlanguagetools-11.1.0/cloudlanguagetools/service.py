29291ac9149fcfd97f594509aaea5247b6304d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:17 +0000

    Add package cloudlanguagetools/11.1.0/cloudlanguagetools-11.1.0.tar.gz

---
+++ packages/cloudlanguagetools/cloudlanguagetools-11.1.0.tar.gz/cloudlanguagetools-11.1.0/cloudlanguagetools/service.py
@@ -0,0 +1,23 @@
+
+class Service():
+    def __init__(self):
+        pass
+
+    # used for pre-loading models
+    def load_data(self):
+        pass
+
+    def get_tts_voice_list(self):
+        return []
+
+    def get_translation_language_list(self):
+        return []
+
+    def get_transliteration_language_list(self):
+        return []
+
+    def get_tokenization_options(self):
+        return []
+
+    def get_dictionary_lookup_list(self):
+        return []
