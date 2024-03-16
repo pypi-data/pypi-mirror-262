4722718d860fb79d86d13143429a46541e93cb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 05:05:07 +0000

    Add package pytorch-sidu/0.1.0/pytorch-sidu-0.1.0.tar.gz

---
+++ packages/pytorch-sidu/pytorch-sidu-0.1.0.tar.gz/pytorch-sidu-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch-sidu',
+    version='0.1.0',
+    author='MarcoParola',
+    author_email='marcoparola96@gmail.com',
+    description='',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
