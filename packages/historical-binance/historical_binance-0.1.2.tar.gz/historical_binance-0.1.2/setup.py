e1b694d16007157497fa063504058990712dcb30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 06:04:25 +0000

    Add package historical-binance/0.1.2/historical_binance-0.1.2.tar.gz

---
+++ packages/historical-binance/historical_binance-0.1.2.tar.gz/historical_binance-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="historical_binance",                     # This is the name of the package
+    version="0.1.2",                        # The initial release version
+    author="Denkik",                     # Full name of the author
+    description="A very fast downloader for Binance historical data",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["historical_binance"],             # Name of the python package
+    package_dir={'':'src'},     # Directory of the source code of the package
+    install_requires=[]                     # Install other dependencies if any
+)
