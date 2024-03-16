6bb781d348dbad03babe71b5d6d2ed195628bcc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:05:18 +0000

    Add package opentorsion/0.2.5/opentorsion-0.2.5.tar.gz

---
+++ packages/opentorsion/opentorsion-0.2.5.tar.gz/opentorsion-0.2.5/opentorsion/examples/forced_response.py
@@ -0,0 +1,111 @@
+import numpy as np
+import opentorsion as ot
+
+"""
+Forced response analysis based on https://doi.org/10.1109/TIE.2010.2087301.
+"""
+
+
+def generator_torque(rpm):
+    """
+    Generator torque as a function of rotor rotating speed.
+    """
+    rated_T = 2.9e6
+
+    if rpm < 4:
+        torque = 0
+
+    elif rpm < 15:
+        m = (0.5 - 0.125) / (15 - 4) * rated_T
+        b = 0.5 * rated_T - m * 15
+
+        torque = m * rpm + b
+
+    elif rpm < 22:
+        P = rated_T * 15
+
+        torque = P / rpm
+
+    else:
+        torque = 0
+
+    return torque
+
+
+def get_windmill_excitation(rpm):
+    """
+    Cogging torque and torque ripple as harmonic excitation.
+    (Table III from https://doi.org/10.1109/TIE.2010.2087301)
+    """
+    f_s = rpm
+    vs = np.array([4, 6, 8, 10, 12, 14, 16])
+    omegas = 2 * np.pi * vs * f_s
+
+    rated_T = 2.9e6
+    amplitudes = np.array(
+        [0.0018, 0.0179, 0.0024, 0.0034, 0.0117, 0.0018, 0.0011]
+    ) * generator_torque(rpm)
+    amplitudes[4] += rated_T * 0.0176
+
+    return omegas, amplitudes
+
+
+def forced_response():
+    """
+    First a model of a windmill is created as a system of three lumped masses
+    connected by two shafts. The assembly is given harmonic excitation as
+    input. Finally, the system response is calculated and plotted.
+    """
+    # Parameters of the mechanical model
+    k1 = 3.67e8  # Nm/rad
+    k2 = 5.496e9  # Nm/rad
+    J1 = 1e7  # kgm^2
+    J2 = 5770  # kgm^2
+    J3 = 97030  # kgm^2
+
+    # Creating assembly
+    shafts, disks = [], []
+    disks.append(ot.Disk(0, J1))
+    shafts.append(ot.Shaft(0, 1, None, None, k=k1, I=0))
+    disks.append(ot.Disk(1, J2))
+    shafts.append(ot.Shaft(1, 2, None, None, k=k2, I=0))
+    disks.append(ot.Disk(2, J3))
+
+    assembly = ot.Assembly(shafts, disk_elements=disks)
+    ot.Plots(assembly).plot_assembly()
+
+    M, K = assembly.M, assembly.K  # Mass and stiffness matrices
+    C = assembly.C_modal(M, K, xi=0.02)  # Damping matrix
+
+    # Modal analysis
+    A, B = assembly.state_matrix(C)
+    lam, vec = assembly.undamped_modal_analysis()
+
+    VT_element1 = []
+    VT_element2 = []
+
+    # The excitation depends on the rotational speed of the system.
+    # Here the response is calculated at each rotational speed.
+    # The responses at each rotational speed are summed to get the total response.
+    for rpm in np.linspace(0.1, 25, 5000):
+        omegas, amplitudes = get_windmill_excitation(rpm)
+        excitations = np.zeros((M.shape[0], omegas.shape[0]), dtype="complex128")
+        excitations[2] = amplitudes # Excitation acts on the generator side
+
+        T_vib = assembly.vibratory_torque(excitations, omegas, k_shafts=np.array([k1, k2]), C=C)
+
+        VT_element1.append(np.sum(np.abs(T_vib[0])))
+        VT_element2.append(np.sum(np.abs(T_vib[1])))
+
+    T_e = np.array(
+        [np.array(VT_element1), np.array(VT_element2)]
+    )  # Total response (shaft torque)
+
+    plot_tools = ot.Plots(assembly)
+    plot_tools.torque_response_plot(np.linspace(0.1, 25, 5000), T_e, show_plot=True)
+
+    return
+
+
+if __name__ == "__main__":
+    forced_response()
---
+++ packages/opentorsion/opentorsion-0.2.5.tar.gz/opentorsion-0.2.5/opentorsion/excitation.py
@@ -0,0 +1,169 @@
+import numpy as np
+
+
+class SystemExcitation:
+    """
+    This class is for building system excitation matrices.
+
+    Attributes
+    ----------
+    dofs : int
+        Number of degrees of freedom of the system
+    omegas : ndarray
+        Array of excitation frequencies
+    U : ndarray
+        Array of excitation amplitudes
+    """
+
+    def __init__(self,
+                 dofs,
+                 omegas,
+                 shape=None,
+                 harmonic=True,
+                 transient=False):
+        """
+        Parameters
+        ----------
+        dofs : int
+            Number of degrees of freedom of the system
+        omegas : ndarray
+            Array of excitation frequencies
+        shape : tuple, optional
+            Time domain excitation matrix shape
+        harmonic : bool, optional
+            If True, excitation is harmonic
+        transient : bool
+            If True, excitation is transient
+        """
+
+        if dofs is None:
+            print("Zero DOF system")
+            return None
+
+        self.dofs = dofs
+
+        self.omegas = omegas
+
+        if harmonic:
+            self.U = np.zeros([self.dofs, len(omegas)])
+
+        elif transient:
+            self.U = np.zeros(shape)
+            # TODO
+
+        else:
+            self.U = None
+
+        return
+
+    def time_domain_excitation(self, nodes, data):
+        """
+        Excitation matrix for transient analysis.
+        The function takes as input the node numbers where excitation data is
+        inputted and the time domain excitation data. The excitation data
+        must be listed in the same order as the listed nodes. The excitation
+        data arrays must be of equal length.
+
+        Parameters
+        ----------
+        nodes: list, int
+            List of nodes where excitation data is inputted
+        data: list, ndarray
+            Excitation amplitudes as a list of (1 x n) shaped numpy arrays
+
+        Returns
+        -------
+        ndarray
+            Excitation matrix in time domain, containing excitation amplitudes
+            for each time step
+        """
+
+        if len(nodes) < 1:
+            raise ValueError("No nodes were defined for excitation input.")
+        elif len(data) < 1:
+            raise ValueError("No excitation data was given.")
+        else:
+            for data_array in data:
+                if data_array.shape != data[0].shape:
+                    raise ValueError(
+                        "Excitation data contains arrays of different size."
+                    )
+
+            # TODO: self.dofs may be too large due to gears
+            excitation_array = np.zeros((self.dofs, data[0].shape[0]))
+
+            for i, node in enumerate(nodes):
+                print(node)
+                excitation_array[node] += data[i]
+            self.U = excitation_array
+
+        return excitation_array.T
+
+    def excitation_frequencies(self, interval):
+        """
+        Excitation frequencies for steady-state and vibratory torque analysis
+
+        Parameters
+        ----------
+        interval : list
+            Lowest and highest excitation frequency values
+
+        Returns
+        -------
+        ndarray
+            Excitation frequencies evenly spaced over the specified interval
+        """
+
+        return np.linspace(interval[0], interval[-1])
+
+    def add_sweep(self, node, amplitude):
+        """
+        Adds a sweep excitation with the given uniform amplitude to the given node
+
+        Parameters
+        ----------
+        node : int
+            Node number where excitation is inputted
+        amplitude : ndarray
+            Harmonic excitation amplitudes
+        """
+        amplitudes = np.ones(self.omegas.shape) * amplitude
+        self.add_harmonic(node, amplitudes)
+
+        return
+
+    def add_harmonic(self, node, amplitudes):
+        """
+        Adds a harmonic excitaiton based on the omegas and amplitudes of the
+        excitation this method should extensively check if all of the
+        excitations have same size of omegas
+
+        Parameters
+        ----------
+        node : int
+            Node number where excitation is inputted
+        amplitudes : ndarray
+            Harmonic excitation amplitudes
+        """
+
+        if self.U is None:
+            return "Error"  # TODO
+
+        if len(amplitudes) != len(self.omegas):
+            return "Error"  # TODO
+
+        self.U[node] += amplitudes
+
+        return
+
+    def excitation_amplitudes(self):
+        """
+        Excitation amplitudes for steady-state and vibratory torque analysis
+
+        Returns
+        -------
+        ndarray
+            The excitation amplitude matrix
+        """
+
+        return self.U
