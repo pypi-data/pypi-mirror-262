4513fcfaefc759a7a9cb3bd63cd72bc6f8475b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 04:05:20 +0000

    Add package robotpy-ctre/2024.1.3/robotpy-ctre-2024.1.3.tar.gz

---
+++ packages/robotpy-ctre/robotpy-ctre-2024.1.3.tar.gz/robotpy-ctre-2024.1.3/tests/test_ctre.py
@@ -0,0 +1,33 @@
+import phoenix5
+
+
+def test_wpi_talonsrx():
+    m = phoenix5.WPI_TalonSRX(0)
+    m.setNeutralMode(phoenix5.NeutralMode.Brake)
+    m.set(0.5)
+    assert m.get() == 0.5
+    del m
+
+
+def test_wpi_talonfx():
+    m = phoenix5.WPI_TalonFX(1)
+    m.setNeutralMode(phoenix5.NeutralMode.Brake)
+    m.set(0.5)
+    assert m.get() == 0.5
+    del m
+
+
+def test_wpi_victorspx():
+    m = phoenix5.WPI_VictorSPX(2)
+    m.setNeutralMode(phoenix5.NeutralMode.Brake)
+    m.set(0.5)
+    assert m.get() == 0.5
+    del m
+
+
+def test_follow():
+    m1 = phoenix5.WPI_TalonFX(3)
+    m2 = phoenix5.WPI_TalonFX(4)
+    m2.follow(m1)
+    m1.set(0.5)
+    assert m1.get() == 0.5
