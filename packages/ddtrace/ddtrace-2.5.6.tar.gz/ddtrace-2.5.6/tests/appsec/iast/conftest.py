522761e95544e2268a29e5b465dc0985982cc50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 05:05:33 +0000

    Add package ddtrace/2.5.6/ddtrace-2.5.6.tar.gz

---
+++ packages/ddtrace/ddtrace-2.5.6.tar.gz/ddtrace-2.5.6/tests/appsec/iast/conftest.py
@@ -0,0 +1,109 @@
+import pytest
+
+from ddtrace.appsec._iast import oce
+from ddtrace.appsec._iast._patches.json_tainting import patch as json_patch
+from ddtrace.appsec._iast._patches.json_tainting import unpatch_iast as json_unpatch
+from ddtrace.appsec._iast._taint_tracking import create_context
+from ddtrace.appsec._iast._taint_tracking import reset_context
+from ddtrace.appsec._iast.processor import AppSecIastSpanProcessor
+from ddtrace.appsec._iast.taint_sinks._base import VulnerabilityBase
+from ddtrace.appsec._iast.taint_sinks.path_traversal import patch as path_traversal_patch
+from ddtrace.appsec._iast.taint_sinks.weak_cipher import patch as weak_cipher_patch
+from ddtrace.appsec._iast.taint_sinks.weak_cipher import unpatch_iast as weak_cipher_unpatch
+from ddtrace.appsec._iast.taint_sinks.weak_hash import patch as weak_hash_patch
+from ddtrace.appsec._iast.taint_sinks.weak_hash import unpatch_iast as weak_hash_unpatch
+from ddtrace.contrib.psycopg.patch import patch as psycopg_patch
+from ddtrace.contrib.psycopg.patch import unpatch as psycopg_unpatch
+from ddtrace.contrib.sqlalchemy.patch import patch as sqlalchemy_patch
+from ddtrace.contrib.sqlalchemy.patch import unpatch as sqlalchemy_unpatch
+from ddtrace.contrib.sqlite3.patch import patch as sqli_sqlite_patch
+from ddtrace.contrib.sqlite3.patch import unpatch as sqli_sqlite_unpatch
+from tests.utils import override_env
+from tests.utils import override_global_config
+
+
+def iast_span(tracer, env, request_sampling="100", deduplication="false"):
+    env.update({"DD_IAST_REQUEST_SAMPLING": request_sampling, "_DD_APPSEC_DEDUPLICATION_ENABLED": deduplication})
+    iast_span_processor = AppSecIastSpanProcessor()
+    VulnerabilityBase._reset_cache()
+    with override_global_config(dict(_iast_enabled=True)), override_env(env):
+        oce.reconfigure()
+        with tracer.trace("test") as span:
+            span.span_type = "web"
+            weak_hash_patch()
+            weak_cipher_patch()
+            path_traversal_patch()
+            sqli_sqlite_patch()
+            json_patch()
+            psycopg_patch()
+            sqlalchemy_patch()
+            iast_span_processor.on_span_start(span)
+            yield span
+            iast_span_processor.on_span_finish(span)
+            weak_hash_unpatch()
+            weak_cipher_unpatch()
+            sqli_sqlite_unpatch()
+            json_unpatch()
+            psycopg_unpatch()
+            sqlalchemy_unpatch()
+
+
+@pytest.fixture
+def iast_span_defaults(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_deduplication_enabled(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true"), deduplication="true"):
+        yield t
+
+
+@pytest.fixture
+def iast_span_des_rc2_configured(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_CIPHER_ALGORITHMS="DES, RC2")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_rc4_configured(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_CIPHER_ALGORITHMS="RC4")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_blowfish_configured(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_CIPHER_ALGORITHMS="BLOWFISH, RC2")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_md5_and_sha1_configured(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_HASH_ALGORITHMS="MD5, SHA1")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_only_md4(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_HASH_ALGORITHMS="MD4")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_only_md5(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_HASH_ALGORITHMS="MD5")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_only_sha1(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_HASH_ALGORITHMS="SHA1")):
+        yield t
+
+
+@pytest.fixture(autouse=True)
+def iast_context():
+    _ = create_context()
+    yield
+    reset_context()
