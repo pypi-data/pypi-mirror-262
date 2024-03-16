790cd9c3e208ff575c6673705924185e025c0ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:05:08 +0000

    Add package bug-tracker/1.3.4/bug_tracker-1.3.4.tar.gz

---
+++ packages/bug-tracker/bug_tracker-1.3.4.tar.gz/bug_tracker-1.3.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bug_tracker',
+    version='1.3.4',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            # Add any command-line scripts here
+        ],
+    },
+)
