27e2f7864c178d3b93465740d178d9b13d90e730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:46 +0000

    Add package dmtoolbox/0.1.27/dmtoolbox-0.1.27-py3-none-any.whl

---
+++ packages/dmtoolbox/dmtoolbox-0.1.27-py3-none-any.whl/dmtoolbox/__init__.py
@@ -0,0 +1,18 @@
+from .func import *
+from .genJsons import *
+from .nginxDefaults import *
+from .nginxUtils import *
+from .numericFuncs import *
+from .osFuncs import *
+from .portTools import *
+
+# Definição de __all__ para exportar corretamente os símbolos de cada módulo
+__all__ = (func.__all__ +
+           genJsons.__all__ +
+           nginxDefaults.__all__ +
+           nginxUtils.__all__ +
+           numericFuncs.__all__ +
+           osFuncs.__all__ +
+           portTools.__all__)
+
+
