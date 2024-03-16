c87baaa1e9a77cb639e4344c7e2b3fa92a539474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 14:04:35 +0000

    Add package tndb/0.0.1/tndb-0.0.1.tar.gz

---
+++ packages/tndb/tndb-0.0.1.tar.gz/tndb-0.0.1/setup.py
@@ -0,0 +1,18 @@
+'''
+Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+Date: 2024-03-16 09:14:35
+LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+LastEditTime: 2024-03-16 10:19:06
+FilePath: \新建文件夹\setup.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='tndb',
+    version='0.0.1',
+    description="mysql与pgsql集合",
+    author="tianna",
+    packages=setuptools.find_packages()
+)
