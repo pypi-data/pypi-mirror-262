7d608d064c0704497a81a1453e6ae9830006aaf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-16 03:04:14 +0000

    Add package coininfo-atemon/0.1/coininfo_atemon-0.1.tar.gz

---
+++ packages/coininfo-atemon/coininfo_atemon-0.1.tar.gz/coininfo_atemon-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coininfo_atemon",  # replace with coininfo_<your name>
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["requests==2.31.0"],
+)  # matches your package name)  # matches your package name
