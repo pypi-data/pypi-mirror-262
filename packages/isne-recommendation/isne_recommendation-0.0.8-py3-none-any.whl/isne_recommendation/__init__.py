5bd5a63682c5bfbee81f15c8c0411a19a18eee59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:04:53 +0000

    Add package isne-recommendation/0.0.8/isne_recommendation-0.0.8-py3-none-any.whl

---
+++ packages/isne-recommendation/isne_recommendation-0.0.8-py3-none-any.whl/isne_recommendation/__init__.py
@@ -0,0 +1,3 @@
+from .TfidfLinearKernel import fit, predict, train_test_split, evaluate_model
+from .FeatureRatingsKNN import fit, predict, train_test_split, evaluate_model
+from .Hybrid import fit, predict, train_test_split, evaluate_model
