099fb2b62de3e6e4e14c0124b9487f2b9b053fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:59 +0000

    Add package cornsnake/0.0.43/cornsnake-0.0.43.tar.gz

---
+++ packages/cornsnake/cornsnake-0.0.43.tar.gz/cornsnake-0.0.43/cornsnake/config.py
@@ -0,0 +1,22 @@
+"""Globally accessible config object. Can be configured by for example reading from command line arguments or TOML file (via tomllib), and then writing to the properties in-memory.
+
+- see read_config_ini_file.py for an example of reading from an ini file (TOML format).
+"""
+import logging
+
+IS_VERBOSE = False
+PATH_TO_GIT = r"C:\Program Files\Git\cmd\git.exe"
+
+path_to_repo_dir = r'C:\src\github\my-repo'
+date_param = '2023-06-01'
+branches = ['main']
+file_extensions = ""  # ".log,.zip,.js.map"
+
+blob_size = "512K"
+
+# SYSTEM CONFIG
+IS_COLOR_ENABLED = True
+IS_INTERACTIVE = False  # Normally True. Set to False for 'hands free' mode for automation
+LOGGING_LEVEL = logging.INFO
+LOG_FILENAME = 'my-log.log'
+MINIMIZE_PROGRESS_BAR_OUTPUT = False  # Normally False. Set to True for CI/CD build or other log-first environment.
