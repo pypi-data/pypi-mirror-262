8d14419a2a464aa059bbf712f915d8b21c0a1eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 14:05:05 +0000

    Add package SPLADERunner/0.1.3/SPLADERunner-0.1.3-py3-none-any.whl

---
+++ packages/SPLADERunner/SPLADERunner-0.1.3-py3-none-any.whl/spladerunner/Config.py
@@ -0,0 +1,6 @@
+MODEL_URL = 'https://storage.googleapis.com/flash-rank/{}.zip'
+DEFAULT_CACHE_DIR = "/tmp"
+DEFAULT_MODEL = "Splade_PP_en_v1"
+MODEL_FILE_MAP = {
+    "Splade_PP_en_v1": "model.onnx"
+}
