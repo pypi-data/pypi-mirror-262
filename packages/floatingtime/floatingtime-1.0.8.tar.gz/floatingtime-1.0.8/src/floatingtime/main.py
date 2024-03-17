820b2d5730b9d713cb28bf52c966ac1a6b70be7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:05:21 +0000

    Add package floatingtime/1.0.8/floatingtime-1.0.8.tar.gz

---
+++ packages/floatingtime/floatingtime-1.0.8.tar.gz/floatingtime-1.0.8/src/floatingtime/main.py
@@ -0,0 +1,27 @@
+import time
+import schedule
+import sys
+
+from floatingtime.time_window import TimeWindow
+
+
+def run_job():
+    tw = TimeWindow()
+    tw.show()
+
+
+def run():
+    if len(sys.argv) > 1 and sys.argv[1] == "debug":
+        run_job()
+        quit()
+
+    schedule.every().minute.at(":55").do(run_job)
+    # schedule.every().hour.at("59:55").do(run_job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    run()
