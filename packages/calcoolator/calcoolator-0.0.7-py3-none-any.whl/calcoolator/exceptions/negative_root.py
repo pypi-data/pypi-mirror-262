d0da2b9b95192be48ea27aa7e147320814bd4d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 14:05:04 +0000

    Add package calcoolator/0.0.7/calcoolator-0.0.7-py3-none-any.whl

---
+++ packages/calcoolator/calcoolator-0.0.7-py3-none-any.whl/calcoolator/exceptions/negative_root.py
@@ -0,0 +1,7 @@
+class NegativeRootError(Exception):
+
+    def __init__(self, n, value):
+        self.n = n
+        self.value = value
+        self.message = f"{n}√{value} I don't undertand complex numbers, sorry 💅🏻"
+        super().__init__(self.message)
