43ce4fc1038274fb1580da55600bbd7e6b183189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:04:48 +0000

    Add package pytestarch/2.0.3/pytestarch-2.0.3.tar.gz

---
+++ packages/pytestarch/pytestarch-2.0.3.tar.gz/pytestarch-2.0.3/src/pytestarch/eval_structure/evaluable_structures.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import List
+
+AbstractNode = str
+
+
+class AbstractGraph(ABC):
+    @abstractmethod
+    def direct_successor_nodes(self, node: AbstractNode) -> List[AbstractNode]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def parent_child_relationship(
+        self, parent: AbstractNode, child: AbstractNode
+    ) -> bool:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def direct_predecessor_nodes(self, node: AbstractNode) -> List[AbstractNode]:
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def nodes(self) -> List[AbstractNode]:
+        raise NotImplementedError()
