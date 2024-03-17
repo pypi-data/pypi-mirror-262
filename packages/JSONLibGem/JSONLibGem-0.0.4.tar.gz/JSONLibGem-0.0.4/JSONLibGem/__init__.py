e83676eeb22516e5d68a84c7696b66fe6d3cf403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:33 +0000

    Add package JSONLibGem/0.0.4/JSONLibGem-0.0.4.tar.gz

---
+++ packages/JSONLibGem/JSONLibGem-0.0.4.tar.gz/JSONLibGem-0.0.4/JSONLibGem/__init__.py
@@ -0,0 +1,8 @@
+"""
+    JSON library
+Version: 0.0.4
+Python Version: 3
+
+"""
+
+from .main import *
---
+++ packages/JSONLibGem/JSONLibGem-0.0.4.tar.gz/JSONLibGem-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='JSONLibGem',
+  version='0.0.4',
+  author='Gemion',
+  author_email='ms.m4x@yandex.ru',
+  description='This is the simplest module for serializing and deserializing json.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='',
+  packages=find_packages(),
+  install_requires=[],
+  classifiers=[
+    'Programming Language :: Python :: 3.12',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='json ',
+  project_urls={
+  },
+  python_requires='>=3.6'
+)
