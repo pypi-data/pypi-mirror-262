42aa92bc4023824f9e7dab2741b4b14e683d14c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:05:16 +0000

    Add package optimask/1.0/optimask-1.0.tar.gz

---
+++ packages/optimask/optimask-1.0.tar.gz/optimask-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='optimask',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas'],
+    description="OptiMask: extracting the largest (non-contiguous) submatrix without NaN",
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    author='Cyril Joly',
+    license='MIT',
+    url='https://github.com/CyrilJl/optimask',
+    classifiers=['License :: OSI Approved :: MIT License'],
+)
