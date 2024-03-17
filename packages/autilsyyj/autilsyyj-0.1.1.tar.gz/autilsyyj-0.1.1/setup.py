e312974ed56ff875658d1c58755a38fb3890ca35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 14:05:05 +0000

    Add package autilsyyj/0.1.1/autilsyyj-0.1.1.tar.gz

---
+++ packages/autilsyyj/autilsyyj-0.1.1.tar.gz/autilsyyj-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="autilsyyj",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=["requests",],
+    author="admon",
+    author_email="admon@gmail.com",
+    description="A brief utils of scripts"
+)
