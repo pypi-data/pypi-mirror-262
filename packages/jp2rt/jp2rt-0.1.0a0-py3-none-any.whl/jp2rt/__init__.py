03537dbf109083fa496e1d07f2be806901ed4e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:58 +0000

    Add package jp2rt/0.1.0a0/jp2rt-0.1.0a0-py3-none-any.whl

---
+++ packages/jp2rt/jp2rt-0.1.0a0-py3-none-any.whl/jp2rt/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.0a0'
+
+from jp2rt.java import add_descriptors_via_tsv, descriptors
+from jp2rt.ml import save_model, load_model, load_descriptors, load_retention_times, simple_model_estimate
