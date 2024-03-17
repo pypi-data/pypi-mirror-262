f2f20b5a64a23cbf209911b0049fcafa3ecb8bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:05:09 +0000

    Add package pymobiledevice3/2.46.2/pymobiledevice3-2.46.2.tar.gz

---
+++ packages/pymobiledevice3/pymobiledevice3-2.46.2.tar.gz/pymobiledevice3-2.46.2/pymobiledevice3/remote/utils.py
@@ -0,0 +1,89 @@
+import contextlib
+import platform
+from typing import Generator, List
+
+import psutil
+import requests
+
+from pymobiledevice3.exceptions import AccessDeniedError, TunneldConnectionError
+from pymobiledevice3.remote.remote_service_discovery import RemoteServiceDiscoveryService
+
+REMOTED_PATH = '/usr/libexec/remoted'
+
+TUNNELD_DEFAULT_ADDRESS = ('127.0.0.1', 49151)
+
+
+def get_tunneld_devices(tunneld_address=TUNNELD_DEFAULT_ADDRESS) -> List[RemoteServiceDiscoveryService]:
+    try:
+        # Get the list of tunnels from the specified address
+        resp = requests.get(f'http://{tunneld_address[0]}:{tunneld_address[1]}')
+        tunnels = resp.json()
+    except requests.exceptions.ConnectionError:
+        raise TunneldConnectionError()
+
+    rsds = []
+    for tunnel_udid, tunnel_address in tunnels.items():
+        rsd = RemoteServiceDiscoveryService(tunnel_address)
+        try:
+            rsd.connect()
+            rsds.append(rsd)
+        except (TimeoutError, ConnectionError):
+            continue
+    return rsds
+
+
+def get_remoted_process() -> psutil.Process:
+    for process in psutil.process_iter():
+        if process.pid == 0:
+            # skip kernel task
+            continue
+        try:
+            if process.exe() == REMOTED_PATH:
+                return process
+        except (psutil.ZombieProcess, psutil.NoSuchProcess):
+            continue
+
+
+def stop_remoted_if_required() -> None:
+    if platform.system() != 'Darwin':
+        # only Darwin systems require it
+        return
+
+    remoted = get_remoted_process()
+    if remoted is None:
+        return
+    if remoted.status() == 'stopped':
+        # process already stopped, we don't need to do anything
+        return
+
+    try:
+        remoted.suspend()
+    except psutil.AccessDenied:
+        raise AccessDeniedError()
+
+
+def resume_remoted_if_required() -> None:
+    if platform.system() != 'Darwin':
+        # only Darwin systems require it
+        return
+
+    remoted = get_remoted_process()
+    if remoted is None:
+        return
+    if remoted.status() == 'running':
+        # process already running, we don't need to do anything
+        return
+
+    try:
+        remoted.resume()
+    except psutil.AccessDenied:
+        raise AccessDeniedError()
+
+
+@contextlib.contextmanager
+def stop_remoted() -> Generator[None, None, None]:
+    stop_remoted_if_required()
+    try:
+        yield
+    finally:
+        resume_remoted_if_required()
---
+++ packages/pymobiledevice3/pymobiledevice3-2.46.2.tar.gz/pymobiledevice3-2.46.2/pymobiledevice3/tunneld.py
@@ -0,0 +1,209 @@
+import asyncio
+import dataclasses
+import logging
+import os
+import signal
+import sys
+import traceback
+from contextlib import asynccontextmanager, suppress
+from typing import Dict, List, Optional, Tuple
+
+import fastapi
+import uvicorn
+from fastapi import FastAPI
+from ifaddr import get_adapters
+from packaging.version import Version
+
+from pymobiledevice3.remote.bonjour import query_bonjour
+from pymobiledevice3.remote.common import TunnelProtocol
+from pymobiledevice3.remote.core_device_tunnel_service import TunnelResult
+from pymobiledevice3.remote.module_imports import start_tunnel
+from pymobiledevice3.remote.remote_service_discovery import RSD_PORT, RemoteServiceDiscoveryService
+from pymobiledevice3.remote.utils import stop_remoted
+from pymobiledevice3.utils import asyncio_print_traceback
+
+logger = logging.getLogger(__name__)
+
+# bugfix: after the device reboots, it might take some time for remoted to start answering the bonjour queries
+REATTEMPT_INTERVAL = 5
+REATTEMPT_COUNT = 5
+
+
+@dataclasses.dataclass
+class TunnelTask:
+    task: asyncio.Task
+    udid: Optional[str] = None
+    tunnel: Optional[TunnelResult] = None
+
+
+class TunneldCore:
+    def __init__(self, protocol: TunnelProtocol = TunnelProtocol.QUIC):
+        self.protocol = protocol
+        self.tasks: List[asyncio.Task] = []
+        self.tunnel_tasks: Dict[str, TunnelTask] = {}
+
+    def start(self) -> None:
+        """ Register all tasks """
+        self.tasks = [
+            asyncio.create_task(self.monitor_adapters(), name='monitor_adapters'),
+        ]
+
+    async def monitor_adapters(self):
+        previous_ips = []
+        while True:
+            if sys.platform == 'win32':
+                current_ips = [f'{adapter.ips[0].ip[0]}%{adapter.ips[0].ip[2]}' for adapter in get_adapters() if
+                               adapter.ips[0].is_IPv6]
+            else:
+                current_ips = [f'{adapter.ips[0].ip[0]}%{adapter.nice_name}' for adapter in get_adapters() if
+                               adapter.ips[0].is_IPv6]
+
+            added = [ip for ip in current_ips if ip not in previous_ips]
+            removed = [ip for ip in previous_ips if ip not in current_ips]
+
+            previous_ips = current_ips
+
+            logger.debug(f'added interfaces: {added}')
+            logger.debug(f'removed interfaces: {removed}')
+
+            for ip in removed:
+                if ip in self.tunnel_tasks:
+                    self.tunnel_tasks[ip].task.cancel()
+                    await self.tunnel_tasks[ip].task
+
+            for ip in added:
+                self.tunnel_tasks[ip] = TunnelTask(
+                    task=asyncio.create_task(self.handle_new_ip(ip), name='handle_new_address'))
+
+            # wait before re-iterating
+            await asyncio.sleep(1)
+
+    @asyncio_print_traceback
+    async def handle_new_ip(self, ip: str):
+        rsd = None
+        tun = None
+        try:
+            addresses = None
+            for i in range(REATTEMPT_COUNT):
+                # browse the adapter for CoreDevices
+                query = query_bonjour(ip)
+
+                # validate a CoreDevice was indeed found
+                await asyncio.sleep(1)
+
+                # close zerconf
+                query.service_browser.cancel()
+                query.zc.close()
+
+                addresses = query.listener.addresses
+                if addresses:
+                    break
+                logger.debug(f'No addresses found for: {ip}')
+                await asyncio.sleep(REATTEMPT_INTERVAL)
+
+            if not addresses:
+                raise asyncio.CancelledError()
+
+            peer_address = addresses[0]
+
+            # establish an untrusted RSD handshake
+            rsd = RemoteServiceDiscoveryService((peer_address, RSD_PORT))
+            with stop_remoted():
+                try:
+                    rsd.connect()
+                except ConnectionRefusedError:
+                    raise asyncio.CancelledError()
+
+            if (self.protocol == TunnelProtocol.QUIC) and (Version(rsd.product_version) < Version('17.0.0')):
+                rsd.close()
+                raise asyncio.CancelledError()
+
+            # populate the udid from the untrusted RSD information
+            self.tunnel_tasks[ip].udid = rsd.udid
+
+            # establish a trusted tunnel
+            async with start_tunnel(rsd, protocol=self.protocol) as tun:
+                rsd.close()
+                self.tunnel_tasks[ip].tunnel = tun
+                logger.info(f'Created tunnel --rsd {tun.address} {tun.port}')
+                await tun.client.wait_closed()
+
+        except asyncio.CancelledError:
+            pass
+        except ConnectionResetError:
+            logger.debug(f'got ConnectionResetError from tunnel --rsd {tun.address} {tun.port}')
+        except Exception:
+            logger.error(traceback.format_exc())
+        finally:
+            if tun is not None:
+                logger.info(f'disconnected from tunnel --rsd {tun.address} {tun.port}')
+
+            if rsd is not None:
+                try:
+                    rsd.close()
+                except OSError:
+                    pass
+
+            if ip in self.tunnel_tasks:
+                # in case the tunnel was removed just now
+                self.tunnel_tasks.pop(ip)
+
+    async def close(self):
+        """ close all tasks """
+        for task in self.tasks + [tunnel_task.task for tunnel_task in self.tunnel_tasks.values()]:
+            task.cancel()
+            with suppress(asyncio.CancelledError):
+                await task
+
+    def clear(self) -> None:
+        """ Clear active tunnels """
+        for udid, tunnel in self.tunnel_tasks.items():
+            logger.info(f'Removing tunnel {tunnel}')
+            tunnel.task.cancel()
+        self.tunnel_tasks = {}
+
+
+class TunneldRunner:
+    """ TunneldRunner orchestrate between the webserver and TunneldCore """
+
+    @classmethod
+    def create(cls, host: str, port: int, protocol: TunnelProtocol = TunnelProtocol.QUIC) -> None:
+        cls(host, port, protocol=protocol)._run_app()
+
+    def __init__(self, host: str, port: int, protocol: TunnelProtocol = TunnelProtocol.QUIC):
+        @asynccontextmanager
+        async def lifespan(app: FastAPI):
+            logging.getLogger('zeroconf').disabled = True
+            self._tunneld_core.start()
+            yield
+            logger.info('Closing tunneld tasks...')
+            await self._tunneld_core.close()
+
+        self.host = host
+        self.port = port
+        self.protocol = protocol
+        self._app = FastAPI(lifespan=lifespan)
+        self._tunneld_core = TunneldCore(protocol)
+
+        @self._app.get('/')
+        async def list_tunnels() -> Dict[str, Tuple]:
+            """ Retrieve the available tunnels and format them as {UUID: TUNNEL_ADDRESS} """
+            tunnels = {}
+            for ip, active_tunnel in self._tunneld_core.tunnel_tasks.items():
+                if (active_tunnel.udid is not None) and (active_tunnel.tunnel is not None):
+                    tunnels[active_tunnel.udid] = (active_tunnel.tunnel.address, active_tunnel.tunnel.port)
+            return tunnels
+
+        @self._app.get('/shutdown')
+        async def shutdown() -> fastapi.Response:
+            """ Shutdown Tunneld """
+            os.kill(os.getpid(), signal.SIGINT)
+            return fastapi.Response(status_code=200, content='Server shutting down...')
+
+        @self._app.get('/clear_tunnels')
+        async def clear_tunnels() -> fastapi.Response:
+            self._tunneld_core.clear()
+            return fastapi.Response(status_code=200, content='Cleared tunnels...')
+
+    def _run_app(self) -> None:
+        uvicorn.run(self._app, host=self.host, port=self.port, loop='asyncio')
