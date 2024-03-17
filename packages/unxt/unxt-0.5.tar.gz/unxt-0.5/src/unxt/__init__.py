0fb38d34146c6a7976eefdf4f300b4d3d37a7116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:05:09 +0000

    Add package unxt/0.5/unxt-0.5.tar.gz

---
+++ packages/unxt/unxt-0.5.tar.gz/unxt-0.5/src/unxt/__init__.py
@@ -0,0 +1,32 @@
+# pylint: disable=import-error
+
+"""unxt: Quantities in JAX.
+
+Copyright (c) 2023 Galactic Dynamics. All rights reserved.
+"""
+
+from . import (
+    _base,
+    _compat,
+    _core,
+    _fast,
+    _utils,
+    experimental,  # noqa: F401
+)
+from ._base import *
+from ._compat import *
+from ._core import *
+from ._fast import *
+from ._utils import *
+from ._version import version as __version__
+
+# isort: split
+# Register dispatches
+from . import _register_dispatches, _register_primitives  # noqa: F401
+
+__all__ = ["__version__"]
+__all__ += _core.__all__
+__all__ += _base.__all__
+__all__ += _fast.__all__
+__all__ += _utils.__all__
+__all__ += _compat.__all__
