ad4b3fa5d88b2f1f56456363fb730bdf69ce6e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 14:04:56 +0000

    Add package calcoolator/0.0.5/calcoolator-0.0.5.tar.gz

---
+++ packages/calcoolator/calcoolator-0.0.5.tar.gz/calcoolator-0.0.5/src/calcoolator/testing.py
@@ -0,0 +1,20 @@
+from base_class import Calculator
+from handler_class import HandlerCalculator
+
+base = Calculator(10)
+handler = HandlerCalculator()
+
+base.screen()
+base.divide
+base.screen()
+
+
+# handler.screen()
+# handler.reset(5)
+# print(handler.memory)
+# handler.screen()
+# handler.add(4, 5, only=True)
+# print(handler.memory)
+# handler.add(5, 5, 5)
+# handler.screen()
+# handler.nth_root(2, -5)
