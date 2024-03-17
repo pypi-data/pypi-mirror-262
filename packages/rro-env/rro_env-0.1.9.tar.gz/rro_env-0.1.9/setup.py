7c6516523e4dee98cfd4012b1287ea0502bd4af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:04:59 +0000

    Add package rro-env/0.1.9/rro_env-0.1.9.tar.gz

---
+++ packages/rro-env/rro_env-0.1.9.tar.gz/rro_env-0.1.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rro_env',
+    version='0.1.9',
+    packages=find_packages(),
+    package_data={
+        'rro_env': ['datasets/*']
+    },
+    install_requires=['gymnasium', 'numpy', 'joblib', 'pandas', 'tqdm', 'dill', 'cachetools'],
+    author="Near Yip",
+    author_email="yiptsangkin@gmail.com",
+    description="A Gym environment for 3D container relocation",
+    keywords="CRP, 3D, container relocation, RL, gym, environment",
+    url="https://github.com/JNU-Tangyin/RRO",
+)
