a8292ef89319e2f3322d37f6899780dfb6724c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:05:54 +0000

    Add package finalsa-common-models/0.0.1/finalsa-common-models-0.0.1.tar.gz

---
+++ packages/finalsa-common-models/finalsa-common-models-0.0.1.tar.gz/finalsa-common-models-0.0.1/finalsa/common/models/__init__.py
@@ -0,0 +1,13 @@
+from finalsa.common.models.models import (
+    SqsMessage,
+    SqsReponse,
+    parse_message_attributes
+)
+
+__version__ = "0.0.1"
+
+__all__ = [
+    "SqsMessage",
+    "SqsReponse",
+    "parse_message_attributes"
+]
---
+++ packages/finalsa-common-models/finalsa-common-models-0.0.1.tar.gz/finalsa-common-models-0.0.1/finalsa/common/models/models/functions.py
@@ -0,0 +1,27 @@
+from typing import Dict
+
+
+def parse_message_attributes(attributes: Dict) -> Dict:
+    message_attributes = {}
+    if not attributes:
+        return message_attributes
+    for key, value in attributes.items():
+        if value['Type'] == 'String':
+            message_attributes[key] = value['Value']
+        elif value['Type'] == 'Number':
+            message_attributes[key] = int(value['Value'])
+        elif value['Type'] == 'Binary':
+            message_attributes[key] = bytes(value['Value'], 'utf-8')
+    return message_attributes
+
+
+def to_message_attributes(attributes: Dict) -> Dict:
+    att_dict = {}
+    for key, value in attributes.items():
+        if isinstance(value, str):
+            att_dict[key] = {
+                'DataType': 'String', 'StringValue': value}
+        elif isinstance(value, bytes):
+            att_dict[key] = {
+                'DataType': 'Binary', 'BinaryValue': value}
+    return att_dict
