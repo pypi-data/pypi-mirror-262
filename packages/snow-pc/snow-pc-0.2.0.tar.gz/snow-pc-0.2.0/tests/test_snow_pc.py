da2bb184c96ddf382aa40c64554aac3751de5104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:05:06 +0000

    Add package snow-pc/0.2.0/snow-pc-0.2.0.tar.gz

---
+++ packages/snow-pc/snow-pc-0.2.0.tar.gz/snow-pc-0.2.0/tests/test_snow_pc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `snow_pc` package."""
+
+
+import unittest
+
+from snow_pc import snow_pc
+
+
+class TestSnow_pc(unittest.TestCase):
+    """Tests for `snow_pc` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
