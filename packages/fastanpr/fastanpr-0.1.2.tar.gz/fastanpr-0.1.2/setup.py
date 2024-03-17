0bbe72c9af6b327e19c0710f11d181c1aa0b754f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:05:56 +0000

    Add package fastanpr/0.1.2/fastanpr-0.1.2.tar.gz

---
+++ packages/fastanpr/fastanpr-0.1.2.tar.gz/fastanpr-0.1.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.2'
+DESCRIPTION = 'A fast automatic number-plate recognition (ANPR) library'
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name="fastanpr",
+    version=VERSION,
+    author="arvindrajan92 (Arvind Rajan)",
+    author_email="<arvindrajan92@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    package_data={'': ['*.pt'], 'fastanpr': ['*.pt']},
+    include_package_data=True,
+    install_requires=['ultralytics>=8.1.26', 'paddlepaddle>=2.6.0', 'paddleocr>=2.7.0.3'],
+    python_requires='>=3.8, <3.12',
+    extras_require={'dev': ['pytest-asyncio>=0.23.5', 'twine>=5.0.0', 'python-Levenshtein>=0.25.0']},
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    url='https://github.com/arvindrajan92/fastanpr',
+    keywords=[
+        'python', 'anpr', 'fast', 'licence plate', 'number plate', 'detection', 'recognition', 'YOLOv8', 'paddleocr',
+        'paddlepaddle'
+    ]
+)
