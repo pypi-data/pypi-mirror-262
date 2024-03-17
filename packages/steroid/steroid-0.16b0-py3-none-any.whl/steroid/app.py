21243b7fbf60f47d0f2e6e4099ba54d71164a40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:58 +0000

    Add package steroid/0.16b0/steroid-0.16b0-py3-none-any.whl

---
+++ packages/steroid/steroid-0.16b0-py3-none-any.whl/steroid/app.py
@@ -0,0 +1,45 @@
+from typing import Union
+
+import uvicorn
+from fastapi import FastAPI
+
+from steroid.constants import APP_COMPONENT, APP_COMPONENT_TYPE, APP_CONTROLLER
+from steroid.controller import Controller
+from steroid.logging import getLogConfig, getLogger
+from steroid.utils import clearTerminal
+
+
+class App:
+    _APP: FastAPI = None
+    logger = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._APP is None:
+            cls.logger = getLogger()
+            cls._APP = FastAPI(*args, **kwargs)
+
+        return cls
+
+    @classmethod
+    @property
+    def app(cls):
+        return cls._APP
+
+    @classmethod
+    def start(cls, host: str = "0.0.0.0", port: int = 8000):
+        clearTerminal()
+
+        uvicorn.run(cls.app, host=host, port=port, log_config=getLogConfig())
+
+    @classmethod
+    def addController(cls, controller):
+        componentType = getattr(controller, APP_COMPONENT_TYPE, None)
+        component: Union[Controller, None] = getattr(controller, APP_COMPONENT, None)
+
+        if not componentType == APP_CONTROLLER:
+            raise Exception(f"Controller component must be of type {APP_CONTROLLER}")
+
+        if not component:
+            raise Exception(f"Controller component not detected")
+
+        component.setupController(cls.app)
