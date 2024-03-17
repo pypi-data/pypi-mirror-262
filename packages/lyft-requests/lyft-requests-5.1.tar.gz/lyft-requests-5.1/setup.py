8cc0d7cbf42f3b166e03bb595c7ab98505d6f974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:05:11 +0000

    Add package lyft-requests/5.1/lyft-requests-5.1.tar.gz

---
+++ packages/lyft-requests/lyft-requests-5.1.tar.gz/lyft-requests-5.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'lyft-requests',      
+  packages = ['lyft-requests'],  
+  version = '5.1',      
+  license='MIT',        
+  description = 'Lyft service',  
+  author = 'SherlocksHat',              
+  author_email = 'sherlockshat007@gmail.com',    
+  url = 'https://github.com/user/sherlockshat',  
+  download_url = 'http://notapplicable.notapplicable',   
+  keywords = ['Lyft', 'FRONTEND'], 
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
