719844ff289b2fcd073c82bd5306b42914e72933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:42 +0000

    Add package report-generation-utils/0.1.0/report_generation_utils-0.1.0-py2.py3-none-any.whl

---
+++ packages/report-generation-utils/report_generation_utils-0.1.0-py2.py3-none-any.whl/report_generation_utils/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Report Generation Utils."""
+
+__author__ = """Jaideep Sundaram"""
+__email__ = 'jai.python3@gmail.com'
+__version__ = '0.1.0'
+
+from .manager import Manager as ReportGeneratorManager
