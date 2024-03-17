6f96940615649a859faa2b8cd18f3bb643287abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:26 +0000

    Add package Taoufiq/0.0.1/Taoufiq-0.0.1.tar.gz

---
+++ packages/Taoufiq/Taoufiq-0.0.1.tar.gz/Taoufiq-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='Taoufiq',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT License',
+    author='SAMAD88',
+    author_email='samadtaoufik@gmail.com',
+    description='A package to test uploading a package to PyPi ',
+    install_requires=['streamlit',
+                  'pandas',
+                  'numpy'],
+    classifiers = [
+       'License :: OSI Approved :: MIT License',
+
+       'Natural Language :: English',
+
+       'Operating System :: OS Independent',]
+
+        )
