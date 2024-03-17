501678f74b38ae341ed68f424ec852d2834f6bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:05:19 +0000

    Add package hammadml/0.1.5/hammadml-0.1.5.tar.gz

---
+++ packages/hammadml/hammadml-0.1.5.tar.gz/hammadml-0.1.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+setuptools.setup(
+    name="hammadml",
+    version="0.1.5",
+    author="Hammad Saeed",
+    author_email="hammad@supportvectors.com",
+    description="ML",
+    long_description="""
+ML
+    """,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3.9',
+    install_requires=[
+"hammadpy>=3.0.1",
+'annoy',
+'anthropic',
+'ebooklib',
+'instructor',
+'pandas',
+'pathlib',
+'pdfminer.six',
+'PyPDF2',
+'sentence-transformers',
+'spacy',
+"Whoosh",
+    ]
+)
