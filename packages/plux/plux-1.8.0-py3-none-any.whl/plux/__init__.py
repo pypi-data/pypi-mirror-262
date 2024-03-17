f8d12af81f88bf26f8cf5d34827c98c3dfa66146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 14:05:15 +0000

    Add package plux/1.8.0/plux-1.8.0-py3-none-any.whl

---
+++ packages/plux/plux-1.8.0-py3-none-any.whl/plux/__init__.py
@@ -0,0 +1,36 @@
+# TODO: should start to migrate things from the plugin package to plux
+from plux.core.plugin import (
+    CompositePluginLifecycleListener,
+    FunctionPlugin,
+    Plugin,
+    PluginDisabled,
+    PluginException,
+    PluginFactory,
+    PluginFinder,
+    PluginLifecycleListener,
+    PluginSpec,
+    PluginSpecResolver,
+    PluginType,
+    plugin,
+)
+from plux.runtime.manager import PluginContainer, PluginManager
+
+name = "plux"
+
+__version__ = "1.8.0"
+
+__all__ = [
+    "FunctionPlugin",
+    "Plugin",
+    "PluginDisabled",
+    "PluginException",
+    "PluginFactory",
+    "PluginFinder",
+    "PluginLifecycleListener",
+    "PluginManager",
+    "PluginContainer",
+    "PluginSpec",
+    "PluginSpecResolver",
+    "PluginType",
+    "plugin",
+]
