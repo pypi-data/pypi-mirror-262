42ba6c579d1bf3c70061d24beb2b835b1e9e8210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:23 +0000

    Add package notemgmt/0.0.2/notemgmt-0.0.2.tar.gz

---
+++ packages/notemgmt/notemgmt-0.0.2.tar.gz/notemgmt-0.0.2/notemgmt/__init__.py
@@ -0,0 +1,5 @@
+def joke():
+    print("Heloooo testing packaging")
+
+def command1():
+    print("testing command 1")
---
+++ packages/notemgmt/notemgmt-0.0.2.tar.gz/notemgmt-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="notemgmt",
+    version="0.0.2",
+    description="a note management tool",
+    url="https://github.com/VinitraMk/notemgmt",
+    author="Vinitra Muralikrishnan",
+    author_email="vinitramk@gmail.com",
+    license="GNU",
+    packages=["notemgmt"],
+    zip_safe=False
+)
