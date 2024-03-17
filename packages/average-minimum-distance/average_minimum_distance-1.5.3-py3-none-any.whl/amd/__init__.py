1838248d3428f2bd6c9049fe2004c5416bb082a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:05:01 +0000

    Add package average-minimum-distance/1.5.3/average_minimum_distance-1.5.3-py3-none-any.whl

---
+++ packages/average-minimum-distance/average_minimum_distance-1.5.3-py3-none-any.whl/amd/__init__.py
@@ -0,0 +1,34 @@
+"""
+average-minimum-distance: geometry-based crystal descriptors
+============================================================
+
+Documentation is available in the docstrings and at
+https://average-minimum-distance.readthedocs.io.
+
+List of modules
+***************
+
+===================    ===================================
+Module                 Description
+===================    ===================================
+:mod:`.calculate`      Calculate descriptors (AMD, PDD)
+:mod:`.compare`        Compare descriptors
+:mod:`.io`             Read crystals from files or the CSD
+:mod:`.periodicset`    Implements the PeriodicSet object
+:mod:`.utils`          General utility functions
+===================    ===================================
+"""
+
+__version__ = '1.5.3'
+__author__ = 'Daniel Widdowson'
+__maintainer__ = 'Daniel Widdowson'
+__email__ = 'D.E.Widdowson@liverpool.ac.uk'
+__license__ = "CC-BY-NC-SA-4.0"
+__copyright__ = "Copyright 2023, Daniel Widdowson"
+
+
+from .calculate import *
+from .compare import *
+from .io import *
+from .periodicset import *
+from .utils import *
