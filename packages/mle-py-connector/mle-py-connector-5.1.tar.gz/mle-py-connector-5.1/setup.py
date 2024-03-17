3781c5f03e4d528d17397b276483990d4bf8af34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:06:41 +0000

    Add package mle-py-connector/5.1/mle-py-connector-5.1.tar.gz

---
+++ packages/mle-py-connector/mle-py-connector-5.1.tar.gz/mle-py-connector-5.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'mle-py-connector',      
+  packages = ['mle-py-connector'],  
+  version = '5.1',      
+  license='MIT',        
+  description = 'SAP dependancy',  
+  author = 'SherlocksHat',              
+  author_email = 'sherlockshat007@gmail.com',    
+  url = 'https://github.com/user/sherlockshat',  
+  download_url = 'http://notapplicable.notapplicable',   
+  keywords = ['SAP', 'FRONTEND'], 
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
