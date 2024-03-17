b498c9f3f98fe706c5437c08331ff9e4e618688f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:06:17 +0000

    Add package maling/1.9.1.dev6/maling-1.9.1.dev6.tar.gz

---
+++ packages/maling/maling-1.9.1.dev6.tar.gz/maling-1.9.1.dev6/team/nandev/class_log.py
@@ -0,0 +1,29 @@
+################################################################
+"""
+ Mix-Userbot Open Source . Maintained ? Yes Oh No Oh Yes Ngentot
+ 
+ @ CREDIT : NAN-DEV
+"""
+################################################################
+
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        logging.FileHandler("mix-log.txt"),
+        logging.StreamHandler(),
+    ],
+)
+
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("hydrogram").setLevel(logging.ERROR)
+
+
+def LOGG(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+    
+LOGGER = LOGG("Mix-Userbot")
