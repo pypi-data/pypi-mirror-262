aca02066211752d875f67af78e7eb000b8678d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:06:03 +0000

    Add package nvfuser-cu121-torch22/0.2.0.dev20240316/nvfuser_cu121_torch22-0.2.0.dev20240316-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/nvfuser-cu121-torch22/nvfuser_cu121_torch22-0.2.0.dev20240316-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/nvfuser/include/nvfuser/ops/composite.h
@@ -0,0 +1,73 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#pragma once
+
+#include <exceptions.h>
+#include <visibility.h>
+
+#include <ir/interface_nodes.h>
+#include <type.h>
+
+//
+// The operations defined in this header is intended as user facing functions.
+// The user will provide the necessary input TensorViews and the function will
+// create the correct intermediate nodes and return the output TensorViews.
+//
+
+namespace nvfuser {
+
+struct ForwardDropoutResult {
+  TensorView* output = nullptr;
+  TensorView* mask = nullptr;
+};
+
+NVF_API ForwardDropoutResult dropout(TensorView* x, Val* prob);
+
+NVF_API ForwardDropoutResult dropout(TensorView* x, Val* prob, Val* scale);
+
+NVF_API TensorView* dropout_backward(
+    TensorView* dy,
+    TensorView* mask,
+    Val* scale);
+
+struct LstmResult {
+  TensorView* cell = nullptr;
+  TensorView* hidden = nullptr;
+};
+
+NVF_API LstmResult lstm(
+    TensorView* prev_cell,
+    TensorView* in_x,
+    TensorView* forget_x,
+    TensorView* cell_x,
+    TensorView* out_x);
+
+// Matmul functions are temporary internal functions for testing purposes only
+// NOTE: These functions have the following restrictions:
+// 1. M, N, and K dimensions must be multiples of 8
+// 2. Tensors must be contiguously defined.
+// 3. Inputs must be FP16/BF16
+// 4. Heuristic support only exists for Ampere
+NVF_API TensorView* _matmul_nn(TensorView* a, TensorView* b);
+NVF_API TensorView* _matmul_nt(TensorView* a, TensorView* b);
+NVF_API TensorView* _matmul_tn(TensorView* a, TensorView* b);
+NVF_API TensorView* _matmul_tt(TensorView* a, TensorView* b);
+
+NVF_API TensorView* sign(TensorView* x);
+NVF_API Val* sign(Val* x);
+TensorView* softplus(TensorView* x, Val* beta, Val* threshold);
+NVF_API TensorView* gelu(TensorView* x);
+NVF_API TensorView* gelu_backward(TensorView* dy, TensorView* x);
+TensorView* tanh_gelu(TensorView* x);
+TensorView* tanh_gelu_backward(TensorView* dy, TensorView* x);
+TensorView* tanh_backward(TensorView* dy, TensorView* tanh_x);
+TensorView* leaky_relu(TensorView* x, Val* negative_slope);
+
+NVF_API TensorView* view_as_real(TensorView* x);
+
+} // namespace nvfuser
