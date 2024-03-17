04dd268ba20fb2ed988ab024bb06201d9033051f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:55 +0000

    Add package arroba/0.5/arroba-0.5.tar.gz

---
+++ packages/arroba/arroba-0.5.tar.gz/arroba-0.5/arroba/server.py
@@ -0,0 +1,36 @@
+"""Temporary!"""
+import os
+
+from flask import request
+from lexrpc.server import Server
+
+from .util import parse_at_uri
+
+
+# XRPC server
+server = Server(validate=False)
+
+# initialized in app.py, testutil.XrpcTestCase.setUp
+storage = None
+
+
+def auth():
+    token = os.environ.get('REPO_TOKEN')
+    if not token:
+      raise NotImplementedError(
+          'Authenticated XRPC methods are not currently supported')
+
+    if request.headers.get('Authorization') != f'Bearer {token}':
+        raise ValueError('Invalid bearer token in Authorization header')
+
+
+def load_repo(did_or_at_uri):
+    if did_or_at_uri.startswith('at://'):
+        did_or_handle, _, _ = parse_at_uri(did_or_at_uri)
+    else:
+        did_or_handle = did_or_at_uri
+
+    repo = storage.load_repo(did_or_handle)
+    if not repo:
+        raise ValueError(f'Repo {repo} not found')
+    return repo
