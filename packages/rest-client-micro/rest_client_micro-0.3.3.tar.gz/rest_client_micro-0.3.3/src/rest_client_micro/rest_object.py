a75393e9828e05147bf272e69a135111bcf3ad11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:06:09 +0000

    Add package rest-client-micro/0.3.3/rest_client_micro-0.3.3.tar.gz

---
+++ packages/rest-client-micro/rest_client_micro-0.3.3.tar.gz/rest_client_micro-0.3.3/src/rest_client_micro/rest_object.py
@@ -0,0 +1,25 @@
+"""Module providing RESTObject class"""
+
+from .basic_auth import BasicAuth
+
+
+class RESTObject(object):
+    """
+    Generic REST config class, provides standardised properties
+    for generating a requests call 
+    """
+
+    operation: str
+    endpoint: str
+    params: dict
+    headers: dict
+    payload: str
+    basic_auth: BasicAuth
+
+    def __init__(self, **kwargs) -> None:
+        self.operation = 'get'
+        self.params = {}
+        self.payload = ''
+        self.headers = {}
+        self.basic_auth = None
+        self.__dict__.update(kwargs)
