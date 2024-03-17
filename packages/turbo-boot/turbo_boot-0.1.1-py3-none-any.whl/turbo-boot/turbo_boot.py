64b4a0abdd08c10ff575b68ff9c80189dc49818a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:05:07 +0000

    Add package turbo-boot/0.1.1/turbo_boot-0.1.1-py3-none-any.whl

---
+++ packages/turbo-boot/turbo_boot-0.1.1-py3-none-any.whl/turbo-boot/turbo_boot.py
@@ -0,0 +1,50 @@
+import os
+import inspect
+import os
+from importlib import import_module
+
+from fastapi import FastAPI
+
+def convert_filename_to_classname(filename):
+    if '_' in filename:
+        words = filename.split('_')
+        capitalized_words = [word.capitalize() for word in words]
+        class_name = ''.join(capitalized_words)
+        return class_name[:-3]
+    return filename[:-3]
+
+def build_application(application_path: str):
+    if application_path is None:
+        raise ValueError("application_path can not be None")
+    
+    app = FastAPI()
+    
+    for root, dirs, files in os.walk(application_path):
+        for file in files:
+            if file.endswith('.py') and not file.startswith('__'):
+                module_name = os.path.relpath(os.path.join(root, file), application_path)
+                module_name = module_name.replace(os.path.sep, '.')[:-3]
+                
+                try:
+                    module = import_module(module_name)
+                    class_info = {key: value for key, value in inspect.getmembers(module, inspect.isclass)}
+                    class_name = convert_filename_to_classname(module_name.split(".")[-1])
+                    
+                    if class_name in class_info.keys():
+                        if "__api_router__" in class_info[class_name].__dict__.keys():
+                            app.include_router(class_info[class_name].__dict__["__api_router__"])
+                except ImportError as e:
+                    print(f"Error loading module '{module_name}': {e}")
+
+    return app
+
+class TurboBoot:
+    
+    @staticmethod
+    def get_app(application_path: str = None):
+        if application_path is None:
+            application_path = os.getcwd()
+        
+        app = build_application(application_path)
+        
+        return app
