4434257cdccb907b68bdba66c62f96e0268b7383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:04:48 +0000

    Add package todoism/1.1/todoism-1.1.tar.gz

---
+++ packages/todoism/todoism-1.1.tar.gz/todoism-1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='todoism',
+    version='1.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'todoism=todoism.main:main',
+        ],
+    },
+    install_requires=[],
+    author='Qichen Liu',
+    author_email='liuqichne@email.com',
+    description='A simple but interactive and intuitive todo CLI',
+    url='https://github.com/Q1CHENL/todoism',
+)
