6f5d5410c07194b1ab27e8f0c091172d95a5ab09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:05:06 +0000

    Add package cdo-python-sdk/1.0.0/cdo_python_sdk-1.0.0-py3-none-any.whl

---
+++ packages/cdo-python-sdk/cdo_python_sdk-1.0.0-py3-none-any.whl/cdo-python-sdk/api/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+
+# import apis into api package
+from cdo-python-sdk.api.change_requests_api import ChangeRequestsApi
+from cdo-python-sdk.api.changelogs_api import ChangelogsApi
+from cdo-python-sdk.api.connectors_api import ConnectorsApi
+from cdo-python-sdk.api.inventory_api import InventoryApi
+from cdo-python-sdk.api.meta_api import MetaApi
+from cdo-python-sdk.api.object_management_api import ObjectManagementApi
+from cdo-python-sdk.api.remote_access_monitoring_api import RemoteAccessMonitoringApi
+from cdo-python-sdk.api.search_api import SearchApi
+from cdo-python-sdk.api.tenant_management_api import TenantManagementApi
+from cdo-python-sdk.api.transactions_api import TransactionsApi
+from cdo-python-sdk.api.users_api import UsersApi
+
