f07c98f3aff008e37d9ef9fae0338264f75f4479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:06:48 +0000

    Add package task-app-micro/0.1.0/task_app_micro-0.1.0-py3-none-any.whl

---
+++ packages/task-app-micro/task_app_micro-0.1.0-py3-none-any.whl/task_app_micro/__init__.py
@@ -0,0 +1,12 @@
+"""
+Description/License information
+Useful to put usage info here
+"""
+
+from .todo_app import TaskApp
+
+VERSION = (0, 1, 0)
+
+VERSION_STRING = ".".join(map(str, VERSION))
+
+TaskApp
