d82ff619e3eb610618a74360d8dcd4ea34f4f11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:55 +0000

    Add package mnx-solver/0.1.0/mnx-solver-0.1.0.tar.gz

---
+++ packages/mnx-solver/mnx-solver-0.1.0.tar.gz/mnx-solver-0.1.0/mnx/__init__.py
@@ -0,0 +1,14 @@
+from mnx.sel.Solver import LinearSolver
+
+from .sel import (
+    Cholesky,
+    Gauss,
+    LU,
+    Solver,
+    IMethod,
+    Gauss_Jordan
+)
+
+from .examples import (
+    example_2,
+)
