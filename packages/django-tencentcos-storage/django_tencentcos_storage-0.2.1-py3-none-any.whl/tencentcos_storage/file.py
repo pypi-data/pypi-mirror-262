5c4ab31aed6fb2a68321341ca04b0d7e6f2d02d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:05:27 +0000

    Add package django-tencentcos-storage/0.2.1/django_tencentcos_storage-0.2.1-py3-none-any.whl

---
+++ packages/django-tencentcos-storage/django_tencentcos_storage-0.2.1-py3-none-any.whl/tencentcos_storage/file.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+from shutil import copyfileobj
+from tempfile import SpooledTemporaryFile
+
+from django.core.files.base import File
+
+
+class TencentCOSFile(File):
+    def __init__(self, name, storage):
+        self.name = name
+        self._storage = storage
+        self._file = None
+
+    @property
+    def file(self):
+        if self._file is None:
+            self._file = SpooledTemporaryFile()
+            response = self._storage.client.get_object(
+                Bucket=self._storage.bucket,
+                Key=self.name,
+            )
+            # >>> type(raw_stream)
+            # <class 'urllib3.response.HTTPResponse'>
+            raw_stream = response["Body"].get_raw_stream()
+            with BytesIO(raw_stream.data) as file_content:
+                copyfileobj(file_content, self._file)
+            self._file.seek(0)
+        return self._file
+
+    @file.setter
+    def file(self, value):
+        self._file = value
