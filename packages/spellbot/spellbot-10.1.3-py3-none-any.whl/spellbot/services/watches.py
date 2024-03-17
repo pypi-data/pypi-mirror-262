a0fc0f2717b606d17e3772ead72f27a53a4ed847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:31 +0000

    Add package spellbot/10.1.3/spellbot-10.1.3-py3-none-any.whl

---
+++ packages/spellbot/spellbot-10.1.3-py3-none-any.whl/spellbot/services/watches.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing import Any
+
+from asgiref.sync import sync_to_async
+
+from spellbot.database import DatabaseSession
+from spellbot.models import Watch
+
+
+class WatchesService:
+    @sync_to_async()
+    def fetch(self, guild_xid: int) -> list[dict[str, Any]]:
+        watches = (
+            DatabaseSession.query(Watch)
+            .filter(Watch.guild_xid == guild_xid)
+            .order_by(Watch.user_xid)
+            .all()
+        )
+        return [watch.to_dict() for watch in watches]
