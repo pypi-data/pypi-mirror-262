060b0d9f0b37306d314c3fa42afba9a4825d23fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:06:16 +0000

    Add package rkkr/2.0/rkkr-2.0.tar.gz

---
+++ packages/rkkr/rkkr-2.0.tar.gz/rkkr-2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rkkr',
+    version='2.0',
+    packages=find_packages(),
+    install_requires=[
+        'keras',
+        'keras-tuner',
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'plotly'
+    ],
+    entry_point={
+        "console_scripts":[
+            "rkkr = rkkr:welcome",
+        ],
+    },
+
+)
