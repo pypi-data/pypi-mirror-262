cbe77eeeafd72189fdcdc31b0c957ad04ff3a1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:06:16 +0000

    Add package hi-basics/0.0.0/hi_basics-0.0.0-py3-none-any.whl

---
+++ packages/hi-basics/hi_basics-0.0.0-py3-none-any.whl/hi_basics/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'hi-basics'
+__description__ = 'hi-basics - A Python package for basic informatic processes'
+__version__ = '0.0.0'
+__author__ = 'Nate Harms'
+__author_email__ = 'harmsinformatics@gmail.com'
+__license__ = 'MIT'
+__url__ = 'https://github.com/HarmsInformatics/hi-basics'
