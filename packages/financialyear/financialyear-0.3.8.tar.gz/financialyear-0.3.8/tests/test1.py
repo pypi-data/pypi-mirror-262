9697a65a2605b36bb6cfc01acf02ea8f3e3ae41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:06:09 +0000

    Add package financialyear/0.3.8/financialyear-0.3.8.tar.gz

---
+++ packages/financialyear/financialyear-0.3.8.tar.gz/financialyear-0.3.8/tests/test1.py
@@ -0,0 +1,13 @@
+# from fi
+# from fi
+ 
+#  # Example usage:
+# def test_fiancial_data():
+#     financial_year = Calculation("2023-24")
+#     print(financial_year.start_month(1))  # Output: 01-04-2023
+#     print(financial_year.end_month(1))    # Output: 30-04-2023
+#     print(financial_year.previous_month_dates(month=1))
+#     print(financial_year.month_list())    # Output: ['04-2023', '05-2023', '06-2023', ..., '03-2024']
+
+# if __name__ == '__main__':
+#     test_fiancial_data()
