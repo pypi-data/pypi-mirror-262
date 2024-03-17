7cf27ea2afd50fd94d226a0f4bbc9644d2e3d327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 03:06:03 +0000

    Add package digplexq/0.0.7/digplexq-0.0.7.tar.gz

---
+++ packages/digplexq/digplexq-0.0.7.tar.gz/digplexq-0.0.7/digplexq/__init__.py
@@ -0,0 +1,11 @@
+"""
+DigplexQ
+=========
+
+DigplexQ is a Python package to perform computations with digraph-based complexes.
+
+See https://github.com/heitorbaldo/DigplexQ for complete documentation.
+"""
+
+#Current version:
+__version__ = "0.0.7"
