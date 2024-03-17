c3c7c685fbf707987d608c97a9a2e16d4924ce5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 04:04:35 +0000

    Add package shrinkit/0.0.8/shrinkit-0.0.8-py2.py3-none-any.whl

---
+++ packages/shrinkit/shrinkit-0.0.8-py2.py3-none-any.whl/shrinkit/runner.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import streamlit.web.cli as stcli
+
+class ShrinkitRunner:
+    def __init__(self):
+        pass
+
+    def run(self):
+        # sys.argv = ["streamlit", "run", "./shrinkit/main.py"]
+        # sys.exit(stcli.main())
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        print(current_dir)
+        main_file_path = os.path.join(current_dir, 'main.py', '--browser.gatherUsageStats false')
+        sys.argv = ["streamlit", "run", main_file_path]
+        sys.exit(stcli.main())
