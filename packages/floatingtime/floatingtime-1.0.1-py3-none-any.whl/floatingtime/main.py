b93ca2027969da159519b1ab0ed5a33cf5c600c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 14:05:23 +0000

    Add package floatingtime/1.0.1/floatingtime-1.0.1-py3-none-any.whl

---
+++ packages/floatingtime/floatingtime-1.0.1-py3-none-any.whl/floatingtime/main.py
@@ -0,0 +1,28 @@
+import time
+import schedule
+import sys
+
+
+from time_window import TimeWindow
+
+
+def run_job():
+    tw = TimeWindow()
+    tw.show()
+
+
+def run():
+    if len(sys.argv) > 1 and sys.argv[1] == "debug":
+        run_job()
+        quit()
+
+    # schedule.every().hour.at("29:55").do(run_job)
+    schedule.every().hour.at("59:55").do(run_job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    run()
