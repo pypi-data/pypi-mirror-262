20be8ea5fdafc384ab42ad6708d714c56cb92e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-17 15:04:58 +0000

    Add package fivenines_agent/0.0.1/fivenines_agent-0.0.1-py3-none-any.whl

---
+++ packages/fivenines_agent/fivenines_agent-0.0.1-py3-none-any.whl/fivenines_agent/ip.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import traceback
+
+from fivenines_agent.env import debug_mode
+
+def ipv4():
+  try:
+    with os.popen('curl -4 -s https://ip.fivenines.io', 'r') as f:
+      result = f.read().rstrip('\n')
+    if debug_mode():
+      print(f'ipv4: {repr(result)}')
+
+    if result != '':
+      return result
+  except Exception as e:
+    print(e, file=sys.stderr)
+    print(traceback.print_exc(), file=sys.stderr)
+
+def ipv6():
+  try:
+    with os.popen('curl -6 -s https://ip.fivenines.io', 'r') as f:
+      result = f.read().rstrip('\n')
+    if debug_mode():
+      print(f'ipv6: {repr(result)}')
+
+    if result != '':
+      return result
+  except Exception as e:
+    print(e, file=sys.stderr)
+    print(traceback.print_exc(), file=sys.stderr)
---
+++ packages/fivenines_agent/fivenines_agent-0.0.1-py3-none-any.whl/fivenines_agent/processes.py
@@ -0,0 +1,25 @@
+import platform
+import psutil
+
+def processes():
+    processes = []
+    attrs = [
+        'pid',
+        'ppid',
+        'name',
+        'username',
+        'memory_percent',
+        'cpu_percent',
+        'cpu_times',
+        'num_threads',
+        'status',
+        'connections',
+    ]
+
+    for proc in psutil.process_iter():
+        try:
+            process = proc.as_dict(attrs=attrs)
+            processes.append(process)
+        except psutil.NoSuchProcess:
+            pass
+    return processes
