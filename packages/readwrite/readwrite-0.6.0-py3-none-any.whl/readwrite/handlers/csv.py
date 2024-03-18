663eb79d0be91421655f5e97d2206c85ea36dc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:05 +0000

    Add package readwrite/0.6.0/readwrite-0.6.0-py3-none-any.whl

---
+++ packages/readwrite/readwrite-0.6.0-py3-none-any.whl/readwrite/handlers/csv.py
@@ -0,0 +1,39 @@
+from .base import Handler, Param
+
+
+class CsvHandler(Handler):
+
+    def __init__(self):
+        super().__init__(
+            "csv",
+            ["csv"]
+        )
+
+    def read(self, path: str, **kwargs):
+        import pandas
+
+        return pandas.read_csv(path, **kwargs)
+
+    def read_params(self):
+        return {
+            "index_col": Param(
+                int,
+                help="Column to use as row label, denoted by column index.",
+            )
+        }
+
+    def write(self, x, path, **kwargs):
+        import pandas
+
+        if not isinstance(x, pandas.DataFrame):
+            raise ValueError("value must be a dataframe")
+
+        x.to_csv(path, **kwargs)
+
+    def write_params(self):
+        return {
+            "index": Param(
+                bool,
+                help="Write row names (index)."
+            ),
+        }
