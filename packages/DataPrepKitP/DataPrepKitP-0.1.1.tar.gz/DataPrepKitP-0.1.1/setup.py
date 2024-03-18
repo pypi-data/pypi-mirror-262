1f6b348afaa590715d4248383bf8f000fc4ae18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:08 +0000

    Add package DataPrepKitP/0.1.1/DataPrepKitP-0.1.1.tar.gz

---
+++ packages/DataPrepKitP/DataPrepKitP-0.1.1.tar.gz/DataPrepKitP-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools 
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+setuptools.setup( 
+    name='DataPrepKitP',   
+    version='0.1.1', 
+    author="Roqaiah Bin Jamil", 
+    author_email="roqaiahjamil@gmail.com", 
+    description="A Python package for data preparation tasks.", 
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    readme = "README.md",
+    requires_python = ">=3.8",
+    packages=setuptools.find_packages(), 
+    install_requires=[
+            'pandas',
+            'numpy',
+        ],
+    classifiers=[ 
+    "Programming Language :: Python :: 3", 
+    "License :: OSI Approved :: MIT License", 
+    "Operating System :: OS Independent", 
+    ], 
+) 
