99d86c80bc2e634cb935c861de10f6f7775e04d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 14:04:27 +0000

    Add package secretflow/1.4.0b0/secretflow-1.4.0b0-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/secretflow/secretflow-1.4.0b0-cp38-cp38-manylinux2014_x86_64.whl/secretflow/ml/boost/sgb_v/factory/components/tree_trainer/__init__.py
@@ -0,0 +1,5 @@
+from .leaf_wise_tree_trainer import LeafWiseTreeTrainer
+from .level_wise_tree_trainer import LevelWiseTreeTrainer
+from .tree_trainer import TreeTrainer
+
+__all__ = ['TreeTrainer', 'LevelWiseTreeTrainer', 'LeafWiseTreeTrainer']
