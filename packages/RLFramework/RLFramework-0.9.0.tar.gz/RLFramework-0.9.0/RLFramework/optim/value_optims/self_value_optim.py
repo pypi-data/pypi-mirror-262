147f8a139f84047e571b6867de03e84f498f13c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 15:05:11 +0000

    Add package RLFramework/0.9.0/RLFramework-0.9.0.tar.gz

---
+++ packages/RLFramework/RLFramework-0.9.0.tar.gz/RLFramework-0.9.0/RLFramework/optim/value_optims/self_value_optim.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+from torch.optim.adam import Adam
+from .. import ValueOptimizer
+from ...traj import Sample
+
+
+class SelfValueOptim(ValueOptimizer):
+    def __init__(self, lr=1e-4, epoch=1, gamma=1, clip_grad=None, level=1):
+        super().__init__(
+            required_list=[
+                "v"
+            ]
+        )
+
+        self.lr = lr
+        self.epoch = epoch
+
+        self.gamma = gamma
+
+        self.clip_grad = clip_grad
+        self.level = level
+
+        self.v_optim = None
+
+    def init_optim(self):
+        self.v_optim = Adam(self.v.parameters(), lr=self.lr)
+
+    def step(self, x: Sample):
+        states, actions, _, rewards, next_states, constants = x.get_batch(gamma=self.gamma, level=self.level)
+
+        next_v = self.v(next_states, eval=True)
+        target_v = rewards.reshape(-1, 1) + constants.reshape(-1, 1) * next_v
+
+        for _ in range(self.epoch):
+            pred_v = self.v(states)
+
+            loss = nn.MSELoss()(pred_v, target_v.item())
+
+            if self.clip_grad is not None:
+                nn.utils.clip_grad_norm_(self.v.parameters(), self.clip_grad)
+
+            self.v_optim.zero_grad()
+            loss.backward()
+            self.v_optim.step()
+
+        return loss.item()
