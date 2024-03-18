8f864585e177b22e7052fae670d20f49168634b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:25 +0000

    Add package infini/2.1.7/infini-2.1.7-py3-none-any.whl

---
+++ packages/infini/infini-2.1.7-py3-none-any.whl/infini/doc.py
@@ -0,0 +1,44 @@
+from typing import Dict, Optional, TypedDict
+
+import json
+
+
+class Annotation(TypedDict, total=False):
+    usage: Optional[str]
+    description: Optional[str]
+    epilog: Optional[str]
+    var_doc: Dict[str, str]
+    sub_cmd: Dict[str, str]
+
+
+class Doc:
+    pre_interceptors: Dict[str, Annotation]
+    handlers: Dict[str, Annotation]
+    events: Dict[str, Annotation]
+    global_variables: Dict[str, Annotation]
+    interceptors: Dict[str, Annotation]
+
+    def __init__(self) -> None:
+        self.pre_interceptors = {}
+        self.handlers = {}
+        self.events = {}
+        self.global_variables = {}
+        self.interceptors = {}
+
+    def update(self, __object: "Doc") -> None:
+        self.pre_interceptors.update(__object.pre_interceptors)
+        self.handlers.update(__object.handlers)
+        self.events.update(__object.events)
+        self.global_variables.update(__object.global_variables)
+        self.interceptors.update(__object.interceptors)
+
+    def dumps(self) -> str:
+        return json.dumps(
+            {
+                "pre_interceptors": self.pre_interceptors,
+                "handlers": self.handlers,
+                "events": self.events,
+                "global_variables": self.global_variables,
+                "interceptors": self.interceptors,
+            }
+        )
