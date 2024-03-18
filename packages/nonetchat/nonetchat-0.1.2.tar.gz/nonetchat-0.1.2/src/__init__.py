0ee5508da7893b106b50fe605b3b8c07c7f57839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:14 +0000

    Add package nonetchat/0.1.2/nonetchat-0.1.2.tar.gz

---
+++ packages/nonetchat/nonetchat-0.1.2.tar.gz/nonetchat-0.1.2/src/__init__.py
@@ -0,0 +1,17 @@
+from argparse import Namespace, ArgumentParser
+import ollama
+from rich.console import Console
+
+def run():
+    console = Console()
+    parser = ArgumentParser(description='Communicate with AI locally')
+    parser.add_argument('-m', '--message', type=str, required=True, help="type -m or --message to tell the cli that you have a question to ask")
+    parser.add_argument('message', type=str, required=True, help="Question you want to ask")
+
+    args = parser.parse_args()
+    # print(args.message)
+    response = ollama.ask(args.message)
+    console.print(response)
+
+if __name__ == "__main__":
+    run()
