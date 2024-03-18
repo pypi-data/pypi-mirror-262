0695a28fb3c7cc55301c6d6af61d82bf06f7b8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 05:04:30 +0000

    Add package seevooplay/1.0.9/seevooplay-1.0.9.tar.gz

---
+++ packages/seevooplay/seevooplay-1.0.9.tar.gz/seevooplay-1.0.9/seevooplay/urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+from .views import email_guests, event_page, resend_page
+
+urlpatterns = [
+    path('admin/email_guests/<int:event_id>/', email_guests, name='email_guests'),
+    path('admin/', admin.site.urls),
+    path('rsvp/<int:event_id>/', event_page, name='invitation'),
+    path('rsvp/<int:event_id>/<guest_uuid>/', event_page),
+    path('djrichtextfield/', include('djrichtextfield.urls')),
+    path('', resend_page),
+]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
