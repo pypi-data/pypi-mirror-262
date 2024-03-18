918b922c8b8ac963d637834a2402a52f16316333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 05:04:24 +0000

    Add package komodo-sdk/0.0.31/komodo_sdk-0.0.31.tar.gz

---
+++ packages/komodo-sdk/komodo_sdk-0.0.31.tar.gz/komodo_sdk-0.0.31/sample/appliance.py
@@ -0,0 +1,54 @@
+from komodo import KomodoApp
+from komodo.core.agents.default import translator_agent, summarizer_agent
+
+from komodo.core.agents.librarian_agent import LibrarianAgent
+from komodo.core.tools.web.serpapi_search import SerpapiSearch
+from komodo.framework.komodo_datasource import default_data_directory
+from komodo.loaders.appliance_loader import ApplianceLoader
+from komodo.models.framework.appliance_runner import ApplianceRunner
+
+
+class SampleAppliance(KomodoApp):
+    def __init__(self):
+        super().__init__(shortcode='sample', name='Sample', purpose='To test the Komodo Appliances SDK')
+        self.add_agent(LibrarianAgent.create(path=default_data_directory(), collection_name="default"))
+        self.add_agent(summarizer_agent())
+        self.add_agent(translator_agent())
+        self.add_tool(SerpapiSearch())
+
+
+def build_and_run():
+    appliance = SampleAppliance()
+    prompt = '''
+        Summarize the following text in 5 words and then translate into Spanish, Hindi and German:
+        This is a sample application using the new Komodo SDK.
+    '''
+    runner = ApplianceRunner(appliance)
+    response = runner.run(prompt)
+    print(response.text)
+
+
+def build_and_search():
+    appliance = SampleAppliance()
+    appliance.index()
+    prompt = '''
+        what is NCC?
+    '''
+    runner = ApplianceRunner(appliance)
+    response = runner.run(prompt)
+    print(response.text)
+
+
+def load_and_run():
+    appliance = ApplianceLoader.load('sample')
+    prompt = '''
+        Summarize the following text in 5 words and translate into Spanish, Hindi and German:
+        This is a sample application using the new Komodo 9 SDK.
+    '''
+    runner = ApplianceRunner(appliance)
+    response = runner.run(prompt)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    build_and_search()
