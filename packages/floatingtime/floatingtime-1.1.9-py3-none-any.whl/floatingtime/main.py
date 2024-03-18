d9756bfb8c3e557620480b46e43a15dfeacc2a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:24 +0000

    Add package floatingtime/1.1.9/floatingtime-1.1.9-py3-none-any.whl

---
+++ packages/floatingtime/floatingtime-1.1.9-py3-none-any.whl/floatingtime/main.py
@@ -0,0 +1,27 @@
+import time
+import schedule
+import sys
+
+from floatingtime.time_window import TimeWindow
+
+
+def _show():
+    tw = TimeWindow()
+    tw.show()
+
+
+def run():
+    # schedule.every().minute.at(":55").do(_show)
+    schedule.every().hour.at("29:55").do(_show)
+    schedule.every().hour.at("59:55").do(_show)
+
+    while True:
+        try:
+            schedule.run_pending()
+            time.sleep(1)
+        except Exception as ex:
+            print(ex)
+
+
+if __name__ == "__main__":
+    _show()
