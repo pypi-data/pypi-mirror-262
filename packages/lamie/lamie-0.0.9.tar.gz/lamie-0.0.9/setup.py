dc81a547e3ac4bae99ee9aed8ea7ae252e0d2137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:22 +0000

    Add package lamie/0.0.9/lamie-0.0.9.tar.gz

---
+++ packages/lamie/lamie-0.0.9.tar.gz/lamie-0.0.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.9'
+DESCRIPTION = 'genzing'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="lamie",
+    version=VERSION,
+    author="syed",
+    author_email="syedadil093@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
