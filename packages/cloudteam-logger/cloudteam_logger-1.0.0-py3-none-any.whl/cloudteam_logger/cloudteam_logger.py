9cc01d3b8e3a1ca9c4ed6d746e5739450652eae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:52 +0000

    Add package cloudteam-logger/1.0.0/cloudteam_logger-1.0.0-py3-none-any.whl

---
+++ packages/cloudteam-logger/cloudteam_logger-1.0.0-py3-none-any.whl/cloudteam_logger/cloudteam_logger.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+import json,inspect
+
+
+class ct_logging:
+    def __init__(self,logLocations,mode=""):
+        self.timegenerated = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.Loglocation = logLocations
+        self.currentfile = (inspect.getframeinfo(inspect.currentframe().f_back).filename).split("/")[-1]
+        self.mode = mode
+        with open((self.Loglocation)+"/logFile.json","w+") as LogFIle:
+            start = json.dumps([])
+            LogFIle.write(start)
+
+    def createJsonFile(self,data):
+        data = [data]
+        currentData = ""
+        with open((self.Loglocation)+"/logFile.json","r") as LogFIle:
+            try:
+                currentData = json.loads(LogFIle.read())
+            except:
+                currentData = LogFIle.read()
+        with open((self.Loglocation)+"/logFile.json","w") as LogFIle:
+            newData = currentData+data
+            newData = json.dumps(newData)
+            LogFIle.write(newData)
+
+    def __getCurrentTimeIso(self):
+        currentTime =  datetime.now()
+        currentTimeIso = currentTime.isoformat()
+        return currentTimeIso
+    
+    def Error(self,message):
+        log = {
+            "timegenerated" : self.timegenerated,
+            "date_time":self.__getCurrentTimeIso(),
+            "source_log":self.currentfile,
+            "log_level":"ERROR",
+            "message":message
+        }
+        self.createJsonFile(log)
+        print(log)
+    
+    def Info(self,message):
+        log = {
+            "timegenerated" : self.timegenerated,
+            "date_time":self.__getCurrentTimeIso(),
+            "source_log":self.currentfile,
+            "log_level":"INFO",
+            "message":message
+        }
+        self.createJsonFile(log)
+        print(log)
+    
+    def Warning(self,message):
+        log = {
+            "timegenerated" : self.timegenerated,
+            "date_time":self.__getCurrentTimeIso(),
+            "source_log":self.currentfile,
+            "log_level":"WARNING",
+            "message":message
+        }
+        self.createJsonFile(log)
+        print(log)
+    
+    def Critical(self,message):
+        log = {
+            "timegenerated" : self.timegenerated,
+            "date_time":self.__getCurrentTimeIso(),
+            "source_log":self.currentfile,
+            "log_level":"CRITICAL",
+            "message":message
+        }
+        self.createJsonFile(log)
+        print(log)
+
+    def Debug(self,message):
+        if (self.mode == "debug"):
+            log = {
+                "timegenerated" : self.timegenerated,
+                "date_time":self.__getCurrentTimeIso(),
+                "source_log":self.currentfile,
+                "log_level":"DEBUG",
+                "message":message
+            }
+            self.createJsonFile(log)
+            print(log)
