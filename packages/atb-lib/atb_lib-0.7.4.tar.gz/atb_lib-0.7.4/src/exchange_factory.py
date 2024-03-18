8cf9e681a1cbb79a0588781bdc28165dcb1de0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:13 +0000

    Add package atb-lib/0.7.4/atb_lib-0.7.4.tar.gz

---
+++ packages/atb-lib/atb_lib-0.7.4.tar.gz/atb_lib-0.7.4/src/exchange_factory.py
@@ -0,0 +1,10 @@
+﻿from atb_lib.src.exchange_binance import ExchangeBinanceWS
+
+
+class ExchangeFactory:
+    @staticmethod
+    def create(exchange_id, market, symbol, logger, callback):
+        if exchange_id.lower() == "binance":
+            return ExchangeBinanceWS(market, symbol, logger, callback)
+        else:
+            raise ValueError(f"Unsupported exchange: '{exchange_id}'")
---
+++ packages/atb-lib/atb_lib-0.7.4.tar.gz/atb_lib-0.7.4/src/modules_version.py
@@ -0,0 +1,17 @@
+﻿import sys
+from importlib.metadata import distributions
+
+
+def modules_version(logger):
+    python_version = sys.version.split()[0]
+    logger.info(f"Python Version: {python_version}")
+
+    installed_packages = distributions()
+    installed_packages_list = sorted([f"{dist.metadata['Name']}=={dist.version}" for dist in installed_packages])
+    logger.info("Installed Packages:")
+    for package in installed_packages_list:
+        for module in ['atb-lib', 'aio-pika', 'asyncio', 'colorlog', 'webservices', 'importlib', 'fastapi',
+                       'websockets', 'msgpack', 'arrow', 'uvicorn', 'pip', 'wheel', 'ujson']:
+            if module in package:
+                logger.info(f"  {package}")
+                break
