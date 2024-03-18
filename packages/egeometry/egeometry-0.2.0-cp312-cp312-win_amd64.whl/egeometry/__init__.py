1c016d1627ff10d1863a40a9cc5e8308a1b3c0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:13 +0000

    Add package egeometry/0.2.0/egeometry-0.2.0-cp312-cp312-win_amd64.whl

---
+++ packages/egeometry/egeometry-0.2.0-cp312-cp312-win_amd64.whl/egeometry/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+__all__ = [
+    "DRectangle2d",
+    "FRectangle2d",
+]
+
+
+# egeometry
+from ._drectangle2d import DRectangle2d
+from ._frectangle2d import FRectangle2d
