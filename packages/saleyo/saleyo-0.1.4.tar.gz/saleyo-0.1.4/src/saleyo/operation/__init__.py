478f30bd7e31a544c8a6f9205ea3f4d78dcd7872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 14:04:32 +0000

    Add package saleyo/0.1.4/saleyo-0.1.4.tar.gz

---
+++ packages/saleyo/saleyo-0.1.4.tar.gz/saleyo-0.1.4/src/saleyo/operation/__init__.py
@@ -0,0 +1,6 @@
+from .accessor import Accessor as Accessor
+from .overwrite import OverWrite as OverWrite
+from .processor import Processor as Processor
+from .intercept import Intercept as Intercept
+from .hook import Pre as Pre
+from .hook import Post as Post
