b383d2bc43b5012b0f27769f081db6931afb0011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 16:07:13 +0000

    Add package m3-gar-client/1.1.0/m3-gar-client-1.1.0.tar.gz

---
+++ packages/m3-gar-client/m3-gar-client-1.1.0.tar.gz/m3-gar-client-1.1.0/src/m3_gar_client/backends/m3_rest_gar/proxy_rest/authentication.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+from rest_framework.authentication import (
+    SessionAuthentication
+)
+
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+
+    """Авторизация без проверки CSRF токена.
+
+    Пользователь должен быть авторизован, но проверка CSRF токена не
+    выполняется, т.к. токен не передается с формы.
+    """
+
+    def enforce_csrf(self, request):
+        return
