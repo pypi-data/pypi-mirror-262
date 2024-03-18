bae39d38f547b14f71193aa18cb8d8d88c628a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 14:04:31 +0000

    Add package shuttleai/3.1/shuttleai-3.1.tar.gz

---
+++ packages/shuttleai/shuttleai-3.1.tar.gz/shuttleai-3.1/src/shuttleai/schemas/__init__.py
@@ -0,0 +1,15 @@
+from .schemas import (
+    Model,
+    Models,
+    Item,
+    Image,
+    Audio,
+    Message,
+    Delta,
+    StreamChoice,
+    Choice,
+    Usage,
+    Chat,
+    ChatChunk,
+    Embedding
+)
