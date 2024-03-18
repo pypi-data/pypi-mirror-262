d4810cabd6f01acf94c506470fb7d69a91a25b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 14:04:16 +0000

    Add package mlxcli/0.1.2/mlxcli-0.1.2-py3-none-any.whl

---
+++ packages/mlxcli/mlxcli-0.1.2-py3-none-any.whl/mlxcli/main.py
@@ -0,0 +1,46 @@
+import cmd
+from .generate import chat
+from mlx_lm import load
+
+class MlxCLI(cmd.Cmd):
+    prompt = ">>> "
+
+    def __init__(self):
+        super().__init__()
+        self.model, self.tokenizer = load("mistralai/Mistral-7B-Instruct-v0.2")
+        self.verbose = False
+        self.messages = []
+    
+    def default(self, line):
+        if line == "/verbose": 
+            self.run_verbose()
+        elif line == "/exit": 
+            self.run_exit()
+        else:
+            self.messages.append({
+                'role': 'user', 
+                'content': line
+            })
+
+            response = chat(self.messages, self.model, self.tokenizer, self.verbose)
+
+            self.messages.append({
+                'role': 'assistant', 
+                'content': response
+            })
+            print()
+    
+    def run_verbose(self):
+        if self.verbose:
+            self.verbose = False 
+            print("Verbose output disabled!")
+        else: 
+            self.verbose = True
+            print("Verbose output enabled!")
+        
+
+    def run_exit(self):
+        return True
+
+if __name__ == '__main__':
+    MlxCLI().cmdloop()
