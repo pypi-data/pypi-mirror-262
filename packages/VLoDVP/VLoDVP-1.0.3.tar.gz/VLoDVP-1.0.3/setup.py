4c0191c3de6dc2bcc61363396cc8c52762a74857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 14:04:38 +0000

    Add package VLoDVP/1.0.3/VLoDVP-1.0.3.tar.gz

---
+++ packages/VLoDVP/VLoDVP-1.0.3.tar.gz/VLoDVP-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="VLoDVP",
+    version="1.0.3",
+    description="Validating Licenses on Discord Validating Package",
+    author="tagoWorks",
+    author_email="santiagobuisnessmail@gmail.com",
+    install_requires=[
+        'requests', 'cryptography'
+    ]
+)
