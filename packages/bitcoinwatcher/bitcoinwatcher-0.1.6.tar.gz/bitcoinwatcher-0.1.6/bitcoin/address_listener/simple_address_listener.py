dca5d485672ce17150ae38b7c183a57539fa5aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:12 +0000

    Add package bitcoinwatcher/0.1.6/bitcoinwatcher-0.1.6.tar.gz

---
+++ packages/bitcoinwatcher/bitcoinwatcher-0.1.6.tar.gz/bitcoinwatcher-0.1.6/bitcoin/address_listener/simple_address_listener.py
@@ -0,0 +1,23 @@
+from bitcoin.address_listener.address_listener import AbstractAddressListener, AddressTxData, AddressTxType
+from bitcoin.tx_listener.zmq_listener import ZMQTXListener
+
+
+class SimpleAddressListener(AbstractAddressListener):
+    def consume(self, subscribed_address, address_tx_data: [AddressTxData]):
+        all_output = list(filter(lambda x: x.type == AddressTxType.OUTPUT and x.address == subscribed_address,
+                                 address_tx_data))
+        total_amount_in_output = sum(map(lambda x: x.amount_in_btc(), all_output))
+        # scale ito 4 decimal places
+        total_amount_in_output = round(total_amount_in_output, self.DECIMAL_SCALE)
+        print(f"Address {subscribed_address} received {total_amount_in_output} BTC in tx {address_tx_data[0].tx_id}")
+
+    def __init__(self, addresses_to_listen: {str}):
+        self.addresses = address
+        super().__init__(addresses_to_listen=addresses_to_listen)
+
+
+if __name__ == '__main__':
+    address = ["3P4WqXDbSLRhzo2H6MT6YFbvBKBDPLbVtQ"]
+    address_watcher = SimpleAddressListener(address)
+    zmq_listener = ZMQTXListener(address_watcher)
+    zmq_listener.start()
