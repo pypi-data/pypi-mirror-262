bf74bd07c4ed1576362a8f008c99f380e9eb1360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:25 +0000

    Add package finol/0.0.2/finol-0.0.2.tar.gz

---
+++ packages/finol/finol-0.0.2.tar.gz/finol-0.0.2/finol/data_layer/scaler_selector.py
@@ -0,0 +1,18 @@
+from sklearn import preprocessing
+from finol.config import *
+
+scaler_dict = {
+    'StandardScaler': preprocessing.StandardScaler,
+    'MinMaxScaler': preprocessing.MinMaxScaler,
+    'MaxAbsScaler': preprocessing.MaxAbsScaler,
+    'RobustScaler': preprocessing.RobustScaler,
+}
+
+
+def select_scaler():
+    scaler_cls = scaler_dict.get(SCALER, None)
+    if scaler_cls is None:
+        raise ValueError(f"Invalid scaler name: {SCALER}. Supported scaler are: {scaler_dict.keys()}")
+
+    scaler = scaler_cls()
+    return scaler
