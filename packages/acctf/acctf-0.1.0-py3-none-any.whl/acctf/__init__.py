03be6c43bf340952bafb8a4f49c29fbcfdfe8145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:34 +0000

    Add package acctf/0.1.0/acctf-0.1.0-py3-none-any.whl

---
+++ packages/acctf/acctf-0.1.0-py3-none-any.whl/acctf/__init__.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+class Base:
+    driver: WebDriver
+
+    def __init__(self):
+        options = Options()
+        options.add_argument('--headless')
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.implicitly_wait(10)
+
+    @abstractmethod
+    def login(self, user_id: str, password: str, otp: str | None = None):
+        raise NotImplementedError()
+
+    def close(self):
+        self.driver.quit()
