059d646c57038371d6ec77ac3efb9fbb6a738d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:10 +0000

    Add package sonagent/1.0.8/sonagent-1.0.8-py3-none-any.whl

---
+++ packages/sonagent/sonagent-1.0.8-py3-none-any.whl/sonagent/enums/rpcmessagetype.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class RPCMessageType(str, Enum):
+    STATUS = 'status'
+    WARNING = 'warning'
+    EXCEPTION = 'exception'
+    STARTUP = 'startup'
+
+    def __repr__(self):
+        return self.value
+
+    def __str__(self):
+        return self.value
+    
+
+NO_ECHO_MESSAGES = ()
