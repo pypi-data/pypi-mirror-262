745bc21a03f4523461b8c7a20a273cc5a9aafe25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 14:04:16 +0000

    Add package pymedphys/0.40.0/pymedphys-0.40.0.tar.gz

---
+++ packages/pymedphys/pymedphys-0.40.0.tar.gz/pymedphys-0.40.0/lib/pymedphys/trf.py
@@ -0,0 +1,4 @@
+# pylint: disable = unused-import, missing-docstring
+
+from pymedphys._trf.decode.trf2pandas import trf2pandas as read
+from pymedphys._trf.manage.identify import identify_logfile as identify
