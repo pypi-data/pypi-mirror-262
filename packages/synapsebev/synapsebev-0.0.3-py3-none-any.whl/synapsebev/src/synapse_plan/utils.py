cb1905152432d04e4727b9ad6f8627174631884c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 05:04:32 +0000

    Add package synapsebev/0.0.3/synapsebev-0.0.3-py3-none-any.whl

---
+++ packages/synapsebev/synapsebev-0.0.3-py3-none-any.whl/synapsebev/src/synapse_plan/utils.py
@@ -0,0 +1,98 @@
+
+class Location:
+    def __init__(self, x=-1, y=-1):
+        self.x = x
+        self.y = y
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    def __str__(self):
+        return str((self.x, self.y))
+
+class State:
+    def __init__(self, time, location):
+        self.time = time
+        self.location = location
+    def __eq__(self, other):
+        return self.time == other.time and self.location == other.location
+    def __hash__(self):
+        return hash(str(self.time) + str(self.location.x) + str(self.location.y))
+    def is_equal_except_time(self, state):
+        return self.location == state.location
+    def __str__(self):
+        return str((self.time, self.location.x, self.location.y))
+
+class Conflict:
+    VERTEX = 1
+    EDGE = 2
+    def __init__(self):
+        self.time = -1
+        self.type = -1
+
+        self.agent_1 = ''
+        self.agent_2 = ''
+
+        self.location_1 = Location()
+        self.location_2 = Location() # this is only used in edge conflict. 
+
+    def __str__(self):
+        return '(' + str(self.time) + ', ' + self.agent_1 + ', ' + self.agent_2 + \
+             ', '+ str(self.location_1) + ', ' + str(self.location_2) + ')'
+
+class VertexConstraint:
+    def __init__(self, time, location):
+        self.time = time
+        self.location = location
+
+    def __eq__(self, other):
+        return self.time == other.time and self.location == other.location
+    def __hash__(self):
+        return hash(str(self.time)+str(self.location))
+    def __str__(self):
+        return '(' + str(self.time) + ', '+ str(self.location) + ')'
+
+class EdgeConstraint:
+    def __init__(self, time, location_1, location_2):
+        self.time = time
+        self.location_1 = location_1
+        self.location_2 = location_2
+    def __eq__(self, other):
+        return self.time == other.time and self.location_1 == other.location_1 \
+            and self.location_2 == other.location_2
+    def __hash__(self):
+        return hash(str(self.time) + str(self.location_1) + str(self.location_2))
+    def __str__(self):
+        return '(' + str(self.time) + ', '+ str(self.location_1) +', '+ str(self.location_2) + ')'
+
+class Constraints:
+    def __init__(self):
+        self.vertex_constraints = set()
+        self.edge_constraints = set()
+
+    def add_constraint(self, other):
+        self.vertex_constraints |= other.vertex_constraints
+        self.edge_constraints |= other.edge_constraints
+
+    def __str__(self):
+        return "VC: " + str([str(vc) for vc in self.vertex_constraints])  + \
+            "EC: " + str([str(ec) for ec in self.edge_constraints])
+
+
+class HighLevelNode:
+    def __init__(self):
+        self.solution = {} # {agent_name: List of states}
+        self.constraint_dict = {} # {agent_name: Constraints()}
+        self.cost = 0
+
+    def __eq__(self, other):
+        """
+        Only based on solution and cost.
+        """
+        if not isinstance(other, type(self)): return NotImplemented
+        return self.solution == other.solution and self.cost == other.cost
+
+    def __hash__(self):
+        return hash((self.cost))
+
+    def __lt__(self, other):
+        """ Based on cost. """
+        return self.cost < other.cost
