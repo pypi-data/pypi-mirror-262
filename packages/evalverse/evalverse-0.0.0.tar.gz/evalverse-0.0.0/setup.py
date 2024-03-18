aab5fbfa8e288ac96ad73b5bc27dc435947448c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 15:04:41 +0000

    Add package evalverse/0.0.0/evalverse-0.0.0.tar.gz

---
+++ packages/evalverse/evalverse-0.0.0.tar.gz/evalverse-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="evalverse",
+    version="0.0.0",
+    packages=find_packages(),
+    author="Evalverse Team",
+    author_email="evalverse@upstage.ai",
+    description="placeholder",
+    license="Apache License 2.0",
+)
