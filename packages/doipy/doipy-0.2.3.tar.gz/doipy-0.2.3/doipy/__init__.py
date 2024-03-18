58d065f59b118af0903b5948de11482da57503e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 16:06:57 +0000

    Add package doipy/0.2.3/doipy-0.2.3.tar.gz

---
+++ packages/doipy/doipy-0.2.3.tar.gz/doipy-0.2.3/doipy/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['create', 'list_operations', 'hello', 'search', 'update', 'create_fdo', 'get_design', 'get_init_data']
+
+from doipy.actions import create, list_operations, hello, search, update, create_fdo, get_design, get_init_data
