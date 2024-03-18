f6172e502f130be695e673508629960a4e570a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 15:05:30 +0000

    Add package operagents/0.0.5/operagents-0.0.5.tar.gz

---
+++ packages/operagents/operagents-0.0.5.tar.gz/operagents-0.0.5/operagents/exception.py
@@ -0,0 +1,21 @@
+class OperagentsException(Exception):
+    """Base class for all exceptions raised by operagents"""
+
+
+class OperaFinished(OperagentsException):
+    """Raised when no more scenes are available and the opera has finished"""
+
+
+class SceneFinished(OperagentsException):
+    """Raised when the scene has already finished.
+
+    No more characters can act after the scene has finished.
+    """
+
+
+class BackendError(OperagentsException):
+    """Raised when the backend fails to process the request"""
+
+
+class FlowError(OperagentsException):
+    """Raised when the flow fails to choose the next character"""
