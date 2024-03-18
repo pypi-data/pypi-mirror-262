52feedbc22d8805d9523a8213cecf3776ebebe27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 16:07:21 +0000

    Add package ilearnplus/1.5/ilearnplus-1.5-py3-none-any.whl

---
+++ packages/ilearnplus/ilearnplus-1.5-py3-none-any.whl/ilearnplus/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: zchen(chenzhen-win2009@163.com)
+
+from .iLearnPlusBasic import *
+from .iLearnPlusEstimator import *
+from .iLearnPlusAutoML import *
+from .iLearnPlusLoadModel import *
+from .util import *
+from .iLearnPlus import runiLearnPlus
+
