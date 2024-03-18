d684942f4f7ce5ca6bcb83d11b366814cce2d0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:22 +0000

    Add package libresvip/1.0.6/libresvip-1.0.6-py3-none-any.whl

---
+++ packages/libresvip/libresvip-1.0.6-py3-none-any.whl/libresvip/plugins/nn/options.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+
+from libresvip.core.constants import DEFAULT_BPM
+
+
+class InputOptions(BaseModel):
+    pass
+
+
+class OutputOptions(BaseModel):
+    tempo: float = Field(
+        default=DEFAULT_BPM,
+        title="Constant tempo",
+        description="Use this tempo to reset time axis of projects with dynamic tempos",
+    )
+    track_index: int = Field(
+        default=-1,
+        title="Track index",
+        description="Start from 0, -1 means auto select",
+    )
+    version: int = Field(
+        default=19,
+        title="Version",
+        description="Version of NIAONiao project file",
+    )
