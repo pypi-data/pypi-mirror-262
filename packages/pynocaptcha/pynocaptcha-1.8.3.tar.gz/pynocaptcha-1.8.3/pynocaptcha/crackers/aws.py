f3a0b79c4c1569834cf727e572279bd8b952b369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 15:05:09 +0000

    Add package pynocaptcha/1.8.3/pynocaptcha-1.8.3.tar.gz

---
+++ packages/pynocaptcha/pynocaptcha-1.8.3.tar.gz/pynocaptcha-1.8.3/pynocaptcha/crackers/aws.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+
+
+from .base import BaseCracker
+
+
+class AwsUniversalCracker(BaseCracker):
+    cracker_name = "aws"
+    cracker_version = "universal"
+
+    """
+    aws universal cracker
+    :param href: 触发验证的页面地址
+    :param user_agent: 请求头
+    调用示例:
+    cracker = AkamaiV2Cracker(
+        user_token="xxx",
+        href="xxx",
+        api="xxx",
+
+        # debug=True,
+        # proxy=proxy,
+    )
+    ret = cracker.crack()
+    """
+
+    # 必传参数
+    must_check_params = ["href"]
+    # 默认可选参数
+    option_params = {
+        "user_agent": ""
+    }
