1207832ca0d9ebdd04650fe79eda0c09dc9a023a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:40 +0000

    Add package latex-table-and-image-generator/0.2.0/latex_table_and_image_generator-0.2.0-py3-none-any.whl

---
+++ packages/latex-table-and-image-generator/latex_table_and_image_generator-0.2.0-py3-none-any.whl/hw_2/task_2_2_main.py
@@ -0,0 +1,7 @@
+from latex_table_and_image_generatort import hw_2
+
+# latex_image  = generate_latex_image()
+
+# with open("artifacts/2_2_image.tex", "w") as tex_file:
+#     tex_file.write(latex_image)
+
