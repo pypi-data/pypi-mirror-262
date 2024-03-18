044a2d2f601161bfbfcb4bce6d2d86a4f51c17df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:49 +0000

    Add package HelloWorldPrint/0.0.1/HelloWorldPrint-0.0.1.tar.gz

---
+++ packages/HelloWorldPrint/HelloWorldPrint-0.0.1.tar.gz/HelloWorldPrint-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+# Setting up
+setup(
+    name="HelloWorldPrint",
+    version="0.0.1",
+    author="Tokai Faclo",
+    author_email="otsronca@gmail.com",
+    description="Just Silly Libary",
+    url="https://github.com/FalcoTK/Hello-World-Print",
+    packages=find_packages(),
+    keywords=["python", "Print", "Hello World", "Hello"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
