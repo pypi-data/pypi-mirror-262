d1a91d415172fb997f1d874eec1b4b37f3ecf26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:05 +0000

    Add package hatch-ci/0.1.4b57/hatch_ci-0.1.4b57-py3-none-any.whl

---
+++ packages/hatch-ci/hatch_ci-0.1.4b57-py3-none-any.whl/hatch_ci/_build.py
@@ -0,0 +1,9 @@
+# autogenerate build file
+branch = 'beta/0.1.4'
+build = '57'
+current = '0.1.4b57'
+ref = 'refs/heads/beta/0.1.4'
+runid = '8317901778'
+sha = '8cdb40575b663d678dc080f07a1ac4c26096a795'
+version = '0.1.4b57'
+workflow = 'beta'
