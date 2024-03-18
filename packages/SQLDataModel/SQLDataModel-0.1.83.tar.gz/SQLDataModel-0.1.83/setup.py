975ba6711df027d8f8b10e22a76d47f3ce53c77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 14:04:17 +0000

    Add package SQLDataModel/0.1.83/SQLDataModel-0.1.83.tar.gz

---
+++ packages/SQLDataModel/SQLDataModel-0.1.83.tar.gz/SQLDataModel-0.1.83/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='SQLDataModel',  
+     version='0.1.83',
+     scripts=['src/SQLDataModel/SQLDataModel.py'] ,
+     author="Ante Tonkovic-Capin",
+     author_email="antetc@icloud.com",
+     description="A data model based on in-memory sqlite to fetch, manipulate and push data to and from multiple sources",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/AnteT/SQLDataModel",
+     project_urls = {
+        'Home':"https://github.com/AnteT/SQLDataModel",
+        'Docs':"https://sqldatamodel.readthedocs.io/en/latest/",
+        'Source': "https://github.com/AnteT/SQLDataModel.git"
+    },
+     package_dir = {"": "src"},
+     packages = setuptools.find_packages(where="src"),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     python_requires = ">=3.9"
+ )
