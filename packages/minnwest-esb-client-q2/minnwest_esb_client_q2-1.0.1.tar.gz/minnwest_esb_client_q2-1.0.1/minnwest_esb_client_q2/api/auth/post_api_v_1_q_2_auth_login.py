3d6c6b6672d737307afe718d0df826802f42f706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 14:04:48 +0000

    Add package minnwest-esb-client-q2/1.0.1/minnwest_esb_client_q2-1.0.1.tar.gz

---
+++ packages/minnwest-esb-client-q2/minnwest_esb_client_q2-1.0.1.tar.gz/minnwest_esb_client_q2-1.0.1/minnwest_esb_client_q2/api/auth/post_api_v_1_q_2_auth_login.py
@@ -0,0 +1,163 @@
+from http import HTTPStatus
+from typing import Any, Dict, Optional, Union, cast
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.login_model import LoginModel
+from ...models.token_model_api_result import TokenModelApiResult
+from ...types import Response
+
+
+def _get_kwargs(
+    *,
+    body: LoginModel,
+) -> Dict[str, Any]:
+    headers: Dict[str, Any] = {}
+
+    _kwargs: Dict[str, Any] = {
+        "method": "post",
+        "url": "/api/v1-q2/Auth/login",
+    }
+
+    _body = body.to_dict()
+
+    _kwargs["json"] = _body
+    headers["Content-Type"] = "application/json"
+
+    _kwargs["headers"] = headers
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[Union[Any, TokenModelApiResult]]:
+    if response.status_code == HTTPStatus.OK:
+        response_200 = TokenModelApiResult.from_dict(response.json())
+
+        return response_200
+    if response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR:
+        response_500 = cast(Any, None)
+        return response_500
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[Union[Any, TokenModelApiResult]]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: LoginModel,
+) -> Response[Union[Any, TokenModelApiResult]]:
+    """Login to access API.
+
+    Args:
+        body (LoginModel):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[Any, TokenModelApiResult]]
+    """
+
+    kwargs = _get_kwargs(
+        body=body,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: LoginModel,
+) -> Optional[Union[Any, TokenModelApiResult]]:
+    """Login to access API.
+
+    Args:
+        body (LoginModel):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[Any, TokenModelApiResult]
+    """
+
+    return sync_detailed(
+        client=client,
+        body=body,
+    ).parsed
+
+
+async def asyncio_detailed(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: LoginModel,
+) -> Response[Union[Any, TokenModelApiResult]]:
+    """Login to access API.
+
+    Args:
+        body (LoginModel):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[Any, TokenModelApiResult]]
+    """
+
+    kwargs = _get_kwargs(
+        body=body,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: LoginModel,
+) -> Optional[Union[Any, TokenModelApiResult]]:
+    """Login to access API.
+
+    Args:
+        body (LoginModel):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[Any, TokenModelApiResult]
+    """
+
+    return (
+        await asyncio_detailed(
+            client=client,
+            body=body,
+        )
+    ).parsed
