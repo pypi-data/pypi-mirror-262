095a44368112b83879f5e76f8ca13a3228ff41cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:04:59 +0000

    Add package sonagent/1.0.7/sonagent-1.0.7.tar.gz

---
+++ packages/sonagent/sonagent-1.0.7.tar.gz/sonagent-1.0.7/sonagent/utils/utils.py
@@ -0,0 +1,22 @@
+import hashlib
+
+def read_text_from_file(file_path: str) -> str:
+    f = open(file_path, "r")
+    f.close()
+    return f.read()
+
+
+def hash_str(string: str) -> str:
+    sha = hashlib.sha256()
+    sha.update(string.encode())
+    return sha.hexdigest()
+
+
+def hash_md5_str(string: str) -> str:
+    m = hashlib.md5(string.encode('UTF-8'))
+    return m.hexdigest()
+
+def create_github_issue():
+    pass
+
+
