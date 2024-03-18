8c0a108ac477ede7cf3e0e41eb9f74c2573a5c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 15:05:37 +0000

    Add package omniunibot/0.2.0/omniunibot-0.2.0.tar.gz

---
+++ packages/omniunibot/omniunibot-0.2.0.tar.gz/omniunibot-0.2.0/omniunibot/__init__.py
@@ -0,0 +1,5 @@
+from .server.server import OmniUniBotServer
+from .client.client import OmniUniBotClient
+from .common.data_type import OmniUniBotConfig, Msg, MsgType
+
+from .__version__ import __version__
