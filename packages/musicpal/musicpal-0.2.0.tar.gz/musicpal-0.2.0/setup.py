f1b02a7a2f05998d3368658d1ccabf01ffcf6ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 03:05:16 +0000

    Add package musicpal/0.2.0/musicpal-0.2.0.tar.gz

---
+++ packages/musicpal/musicpal-0.2.0.tar.gz/musicpal-0.2.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from setuptools import setup  # type: ignore
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="musicpal",
+    author="Joerg Mechnich",
+    author_email="joerg.mechnich@gmail.com",
+    license="GNU GPLv3",
+    description="Command line interface for remote controlling a Freecom MusicPal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jmechnich/musicpal",
+    packages=["musicpal"],
+    entry_points={
+        "console_scripts": [
+            "musicpal = musicpal.__main__:main",
+        ]
+    },
+    use_scm_version={"local_scheme": "no-local-version"},
+    setup_requires=["setuptools_scm"],
+    install_requires=["beautifulsoup4", "lxml", "requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.8",
+)
