b886a824fdbca8e2ee432e395c647ccabcea7fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:04:54 +0000

    Add package tiny-solver/0.3.2/tiny_solver-0.3.2-cp38-abi3-manylinux_2_34_x86_64.whl

---
+++ packages/tiny-solver/tiny_solver-0.3.2-cp38-abi3-manylinux_2_34_x86_64.whl/tiny_solver/factors/__init__.pyi
@@ -0,0 +1,7 @@
+import numpy as np
+
+class BetweenFactorSE2:
+    def __init__(self, x: float, y: float, theta: float) -> None: ...
+
+class PriorFactor:
+    def __init__(self, x: np.ndarray, /) -> None: ...
