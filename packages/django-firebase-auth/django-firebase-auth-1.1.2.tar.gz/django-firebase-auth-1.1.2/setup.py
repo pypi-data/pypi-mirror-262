33f4963d74e9a748c557b7b760872968fd7dfa00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 05:04:39 +0000

    Add package django-firebase-auth/1.1.2/django-firebase-auth-1.1.2.tar.gz

---
+++ packages/django-firebase-auth/django-firebase-auth-1.1.2.tar.gz/django-firebase-auth-1.1.2/setup.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="django-firebase-auth",
+    version="1.1.2",
+    packages=find_packages(),
+    install_requires=["firebase-admin", "djangorestframework"],
+    url="https://github.com/maycuatroi/django-firebase-auth",
+    include_package_data=True,
+    license="MIT License",
+    description="Django authentication middle ware using Firebase Authentication Service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nguyen Anh Binh",
+    author_email="sometimesocrazy@gmail.com",
+)
