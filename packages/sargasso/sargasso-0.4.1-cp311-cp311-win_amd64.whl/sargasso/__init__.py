4891e371b9baab5fd44d1babd28c1f5db4a8a013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:02 +0000

    Add package sargasso/0.4.1/sargasso-0.4.1-cp311-cp311-win_amd64.whl

---
+++ packages/sargasso/sargasso-0.4.1-cp311-cp311-win_amd64.whl/sargasso/__init__.py
@@ -0,0 +1,15 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_5_4():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, '.'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_5_4()
+del _delvewheel_patch_1_5_4
+# end delvewheel patch
+
+from _sargasso import add, sdot
+from importlib import metadata
+__version__ = metadata.version("sargasso")
