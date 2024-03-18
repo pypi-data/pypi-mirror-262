d49bde13326b8d724e01777dbeb5520d027304fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 04:05:04 +0000

    Add package texutils-hw2/0.0.0/texutils_hw2-0.0.0.tar.gz

---
+++ packages/texutils-hw2/texutils_hw2-0.0.0.tar.gz/texutils_hw2-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="texutils_hw2",
+    packages=find_packages(),
+    author="Vladimir Kravtsov",
+    author_email="vladimir140201@yandex.ru",
+    description="Latex markdown functions",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
