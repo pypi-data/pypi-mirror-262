fdfec7312eccc1a6d8b25c77075072c7a2157e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 16:07:12 +0000

    Add package scmopy/0.0.1/scmopy-0.0.1.tar.gz

---
+++ packages/scmopy/scmopy-0.0.1.tar.gz/scmopy-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scmopy",
+    description="placeholder for scmopy",
+    packages=find_packages(include=["placeholder", "placeholder.*"]),
+    version="0.0.1",
+)
+
