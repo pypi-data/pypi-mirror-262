b9855a47caf212826e6932f5f9af9fb5f94ec6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 15:04:41 +0000

    Add package cpg-workflows/1.21.2/cpg-workflows-1.21.2.tar.gz

---
+++ packages/cpg-workflows/cpg-workflows-1.21.2.tar.gz/cpg-workflows-1.21.2/cpg_workflows/large_cohort/dataproc_script.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""
+Generic script to run a function on dataproc.
+"""
+
+import click
+import logging
+from cpg_utils.hail_batch import start_query_context
+from cpg_utils import to_path
+from importlib import import_module
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+
+@click.command()
+@click.argument('import_module_name')
+@click.argument('function_name')
+@click.argument('function_str_args', nargs=-1)
+@click.option('-p', '--path', 'function_path_args', multiple=True)
+def main(
+    import_module_name: str,
+    function_name: str,
+    function_str_args: list[str],
+    function_path_args: list[str],
+):
+    module = import_module(import_module_name)
+    func = getattr(module, function_name)
+    start_query_context()
+    func(*[to_path(path) for path in function_path_args], *function_str_args)
+
+
+if __name__ == '__main__':
+    main()
