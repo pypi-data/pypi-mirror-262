9fcbb72b6dd45e2bb00dab00a14c533525f73ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-18 15:04:42 +0000

    Add package wxtools/0.2.6.1/wxtools-0.2.6.1.tar.gz

---
+++ packages/wxtools/wxtools-0.2.6.1.tar.gz/wxtools-0.2.6.1/setup.py
@@ -0,0 +1,35 @@
+"""""""""""""""""""""""""""""
+Project: wxtools
+Author: Terance Jiang
+Date: 1/16/2024
+"""""""""""""""""""""""""""""
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='wxtools',
+    version='0.2.6.1',
+    packages=find_packages(),
+    install_requires=requirements,
+)
+
+entry_points = {
+    'console_scripts': [
+        'my_command=package_name.module:function',
+    ],
+},
+
+author = 'JY',
+author_email = 'yingjiang.jy@gmail.com',
+url = 'https://github.com/terancejiang/wxtools'
+description = 'wxtools'
+long_description = open('README.md').read(),
+long_description_content_type = 'text/markdown',
+url = '',
+license = 'MIT',
+classifiers = [
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
+],
---
+++ packages/wxtools/wxtools-0.2.6.1.tar.gz/wxtools-0.2.6.1/wxtools/io_utils/__init__.py
@@ -0,0 +1,6 @@
+"""""""""""""""""""""""""""""
+Project: wxtools
+Author: Terance Jiang
+Date: 1/16/2024
+"""""""""""""""""""""""""""""
+from .io_utils import *
