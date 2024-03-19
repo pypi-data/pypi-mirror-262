c457dcd542f5cc43beab9728d0166cbe0e95eb55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:04:39 +0000

    Add package WES-Calculation/7.0.0/WES_Calculation-7.0.0-py3-none-any.whl

---
+++ packages/WES-Calculation/WES_Calculation-7.0.0-py3-none-any.whl/WES_Calculation/ref_func/__init__.py
@@ -0,0 +1,12 @@
+'''
+Date         : 2023-01-24 11:30:46
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2024-03-17 23:52:32
+LastEditors  : <BDFD>
+Description  : 
+FilePath     : f:\GitHub Project\bdfd\Section5.2-PyPi-WES_Calculation\WES_Calculation\ref_func\__init__.py
+Copyright (c) 2023 by BDFD, All Rights Reserved. 
+'''
+from .common_func import data_conv, data_conv_int, return_img_stream
+from .windspeed_func import phi1fo, phi1f, psi1f, psi2f, psi3f, A, B, att
---
+++ packages/WES-Calculation/WES_Calculation-7.0.0-py3-none-any.whl/WES_Calculation/windwave.py
@@ -0,0 +1,21 @@
+'''
+Date         : 2022-12-18 12:32:53
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2024-03-11 13:25:23
+LastEditors  : <BDFD>
+Description  : 
+FilePath     : \WES_Calculation\windwave.py
+Copyright (c) 2022 by BDFD, All Rights Reserved. 
+'''
+
+
+# For the Wind Generate Wave Python Calculations
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def windwave(o1, ad, X, U10k, atm, atr, wdu, o2, beta, slc, o5, o4, xs, d0, Ksb, xlook):
+    print("Hello World")
+    print("test test")
