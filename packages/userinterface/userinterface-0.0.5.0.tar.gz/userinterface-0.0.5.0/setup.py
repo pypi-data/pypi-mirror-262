75a0ba7eeec458cd1a499b0583fb7c0766478540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:04:55 +0000

    Add package userinterface/0.0.5.0/userinterface-0.0.5.0.tar.gz

---
+++ packages/userinterface/userinterface-0.0.5.0.tar.gz/userinterface-0.0.5.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+long_description = """userinterface
+-------
+userinterface"""
+setup(
+    name="userinterface",
+    packages=["userinterface"],
+    version="0.0.5.0",
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="userinterface",
+    author="Ruud van der Ham",
+    author_email="info@salabim.org",
+    url="https://github.com/salabim/userinterface",
+    download_url="https://github.com/salabim/userinterface",
+    keywords=["property", "addon"],
+    classifiers=[],
+)
