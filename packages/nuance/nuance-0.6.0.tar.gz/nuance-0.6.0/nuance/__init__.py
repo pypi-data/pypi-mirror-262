0348e9f5d6e3ba9e7a3840811fc35b307d1db95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:05:03 +0000

    Add package nuance/0.6.0/nuance-0.6.0.tar.gz

---
+++ packages/nuance/nuance-0.6.0.tar.gz/nuance-0.6.0/nuance/__init__.py
@@ -0,0 +1,10 @@
+import jax
+from jax.config import config
+
+DEVICES_COUNT = jax.device_count()
+config.update("jax_enable_x64", True)
+
+from nuance.combined import CombinedNuance
+from nuance.nuance import Nuance
+from nuance.search_data import SearchData
+from nuance.star import Star
