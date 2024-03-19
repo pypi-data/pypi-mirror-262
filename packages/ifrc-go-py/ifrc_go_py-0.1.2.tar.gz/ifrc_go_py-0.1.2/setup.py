c8c1b7bde78a6a7fecd8703bc06793554f5dc35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:04:52 +0000

    Add package ifrc-go-py/0.1.2/ifrc_go_py-0.1.2.tar.gz

---
+++ packages/ifrc-go-py/ifrc_go_py-0.1.2.tar.gz/ifrc_go_py-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ifrc_go_py',
+    version='0.1.2',
+    description='A library for working with IFRC GO data.',
+    author='Jonathan Garro',
+    author_email='jonathan.garro@gmail.com',
+    project_urls={
+        'GitHub': 'https://github.com/JonathanGarro/go-py',
+    },
+    license='MIT',
+    packages=['ifrc_go_py'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests>=2.0.0', 
+        'seaborn==0.12.2',
+        'matplotlib==3.7.2',
+        'geopandas==0.13.2',
+        'pandas>=1.5.0'
+    ]
+    )
