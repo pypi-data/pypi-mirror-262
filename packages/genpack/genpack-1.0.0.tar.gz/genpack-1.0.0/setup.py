40e4414130ed49c836cc28b0ebed175703dc1cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:05:09 +0000

    Add package genpack/1.0.0/genpack-1.0.0.tar.gz

---
+++ packages/genpack/genpack-1.0.0.tar.gz/genpack-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='genpack',
+    version='1.0.0',
+    description='default setup for python',
+    author='Konoval Evgen',
+    author_email='zenakonoval6@email.com',
+    packages=['genpack'],
+    install_requires=[],  # залежності вашого пакету
+)
