f0c1479b90d4b02478870748f60d339ee855feef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:05:02 +0000

    Add package tire-code-parser/0.1.0/tire_code_parser-0.1.0.tar.gz

---
+++ packages/tire-code-parser/tire_code_parser-0.1.0.tar.gz/tire_code_parser-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="tire_code_parser",
+    version="0.1.0",
+    description="A package to parse tire codes",
+    author="Stefior",
+    author_email="contact@stefior.com",
+    packages=["tire_code_parser"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
+
