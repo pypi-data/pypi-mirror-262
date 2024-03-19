47e9751c610a3d09b468cb9a42adc61f09d68054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:04:56 +0000

    Add package shapiq/0.0.5/shapiq-0.0.5.tar.gz

---
+++ packages/shapiq/shapiq-0.0.5.tar.gz/shapiq-0.0.5/shapiq/utils/__init__.py
@@ -0,0 +1,21 @@
+"""This module contains utility functions for the shapiq package."""
+
+from .modules import safe_isinstance
+from .sets import (
+    generate_interaction_lookup,
+    get_explicit_subsets,
+    pair_subset_sizes,
+    powerset,
+    split_subsets_budget,
+)
+
+__all__ = [
+    # sets
+    "powerset",
+    "pair_subset_sizes",
+    "split_subsets_budget",
+    "get_explicit_subsets",
+    "generate_interaction_lookup",
+    # modules
+    "safe_isinstance",
+]
