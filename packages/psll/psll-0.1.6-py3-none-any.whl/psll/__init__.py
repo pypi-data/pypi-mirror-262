1ecc9b9c34921151c87f6c886f8dc12c2224af1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:04:14 +0000

    Add package psll/0.1.6/psll-0.1.6-py3-none-any.whl

---
+++ packages/psll/psll-0.1.6-py3-none-any.whl/psll/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.1.6"
+
+import sys
+
+if sys.version_info < (3, 9):
+    raise RuntimeError("Upgrade to python 3.9, or newer.")  # pragma: no cover
+
+
+class PsllSyntaxError(SyntaxError):
+    pass
+
+
+from . import (  # noqa: F401, E402
+    preprocessor,
+    lexer,
+    macros,
+    build,
+    optimisers,
+)
