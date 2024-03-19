d16f1853c845cb32286a314e9758347e0841c6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:04:52 +0000

    Add package DJDJDJ/2.2.1/DJDJDJ-2.2.1.tar.gz

---
+++ packages/DJDJDJ/DJDJDJ-2.2.1.tar.gz/DJDJDJ-2.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DJDJDJ',
+    version='2.2.1',
+    packages=find_packages(),
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A Python package named DJDJDJ',
+    long_description='A Python package named DJDJDJ for demonstration purposes.',
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/DJDJDJ',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
