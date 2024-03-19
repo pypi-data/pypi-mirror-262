05a975270c9434e7d716f746515d235eb0c6db83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:04:33 +0000

    Add package typeapi/2.2.0/typeapi-2.2.0.tar.gz

---
+++ packages/typeapi/typeapi-2.2.0.tar.gz/typeapi-2.2.0/src/typeapi/__init__.py
@@ -0,0 +1,29 @@
+__version__ = "2.2.0"
+
+from .typehint import (
+    AnnotatedTypeHint,
+    ClassTypeHint,
+    ForwardRefTypeHint,
+    LiteralTypeHint,
+    TupleTypeHint,
+    TypeHint,
+    TypeVarTypeHint,
+    UnionTypeHint,
+)
+from .utils import get_annotations, is_typed_dict, type_repr
+
+__all__ = [
+    # .typehint
+    "AnnotatedTypeHint",
+    "ClassTypeHint",
+    "ForwardRefTypeHint",
+    "LiteralTypeHint",
+    "TupleTypeHint",
+    "TypeHint",
+    "TypeVarTypeHint",
+    "UnionTypeHint",
+    # .utils
+    "get_annotations",
+    "is_typed_dict",
+    "type_repr",
+]
