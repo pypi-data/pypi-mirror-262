710911572d6ca7531cd0370cf0a63e2221e3b65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:04:57 +0000

    Add package lusid-workflow-sdk/2.0.33/lusid_workflow_sdk-2.0.33-py3-none-any.whl

---
+++ packages/lusid-workflow-sdk/lusid_workflow_sdk-2.0.33-py3-none-any.whl/lusid_workflow/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from lusid_workflow.api.task_definitions_api import TaskDefinitionsApi
+from lusid_workflow.api.tasks_api import TasksApi
+from lusid_workflow.api.workers_api import WorkersApi
+
