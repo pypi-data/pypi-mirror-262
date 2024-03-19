6b4f337e4f2d10245c34d67f1462d8408ca55281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:04:56 +0000

    Add package latex-gen-1-2/0.0.0/latex_gen_1_2-0.0.0.tar.gz

---
+++ packages/latex-gen-1-2/latex_gen_1_2-0.0.0.tar.gz/latex_gen_1_2-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='latex_gen_1_2',
+    description='Functions for generating LaTeX code',
+    author='esaulvn',
+    packages=find_packages(),
+)
