e99f1173cc8ebd46e9a0feb220ac607c91cc5f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:04:37 +0000

    Add package mediqbox-jobctxmgr/0.0.1/mediqbox-jobctxmgr-0.0.1.tar.gz

---
+++ packages/mediqbox-jobctxmgr/mediqbox-jobctxmgr-0.0.1.tar.gz/mediqbox-jobctxmgr-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as fp:
+  long_description = fp.read()
+
+setup(
+  name='mediqbox-jobctxmgr',
+  version='0.0.1',
+  description="A mediqbox component for job context management",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  package_dir={"": "src"},
+  packages=find_namespace_packages(
+    where="src", include=["mediqbox.*"]
+  ),
+  install_requires=[
+  ]
+)
