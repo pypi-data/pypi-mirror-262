b05a8c3bd1bfd08fde62775125f8e8ace0f32243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:04:54 +0000

    Add package LatexGenerationCustomLibrary/0.1.0/LatexGenerationCustomLibrary-0.1.0.tar.gz

---
+++ packages/LatexGenerationCustomLibrary/LatexGenerationCustomLibrary-0.1.0.tar.gz/LatexGenerationCustomLibrary-0.1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='LatexGenerationCustomLibrary',
+    version='0.1.0',
+    py_modules=['list_of_lists_to_latex_document', 'image_to_latex_document'],
+)
