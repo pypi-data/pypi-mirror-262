31028600876be68d911f55e160119b218e1d8dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:05:12 +0000

    Add package mne-lsl/1.3.0/mne_lsl-1.3.0.tar.gz

---
+++ packages/mne-lsl/mne_lsl-1.3.0.tar.gz/mne_lsl-1.3.0/mne_lsl/lsl/__init__.py
@@ -0,0 +1,9 @@
+from .functions import (  # noqa: F401
+    library_version,
+    local_clock,
+    protocol_version,
+    resolve_streams,
+)
+from .stream_info import StreamInfo  # noqa: F401
+from .stream_inlet import StreamInlet  # noqa: F401
+from .stream_outlet import StreamOutlet  # noqa: F401
